f94d511d5743bb7a4f8d45c37ce58cdba9088319
Tom Brooks <tom.brooks@oddball.io>
2022-03-10 22:03:00 -0500

    Removed broken references

--- src/views/BusinessCase/ViewOnly/index.tsx
+++ src/views/BusinessCase/ViewOnly/index.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
 
 import BusinessCaseReview from 'components/BusinessCaseReview';
 import PageHeading from 'components/PageHeading';
-import GetGRTFeedbackQuery from 'queries/GetGRTFeedbackQuery';
-import {
-  GetGRTFeedback,
-  GetGRTFeedbackVariables
-} from 'queries/types/GetGRTFeedback';
 import { BusinessCaseModel } from 'types/businessCase';
 
 type BusinessCaseViewOnlyProps = {
@@ -15,24 +9,13 @@ type BusinessCaseViewOnlyProps = {
 };
 
 const BusinessCaseView = ({ businessCase }: BusinessCaseViewOnlyProps) => {
-  const { data: grtFeedbackPayload } = useQuery<
-    GetGRTFeedback,
-    GetGRTFeedbackVariables
-  >(GetGRTFeedbackQuery, {
-    variables: {
-      intakeID: businessCase.systemIntakeId
-    }
-  });
-
-  const grtFeedbacks = grtFeedbackPayload?.systemIntake?.grtFeedbacks;
-
   return (
     <>
       <div className="grid-container">
         <PageHeading>Review your Business Case</PageHeading>
       </div>
       <div className="business-case-review">
-        <BusinessCaseReview values={businessCase} grtFeedbacks={grtFeedbacks} />
+        <BusinessCaseReview values={businessCase} />
       </div>
     </>
   );
--- src/views/GovernanceReviewTeam/RequestOverview.tsx
+++ src/views/GovernanceReviewTeam/RequestOverview.tsx
@@ -172,7 +172,6 @@ const RequestOverview = () => {
               render={() => (
                 <BusinessCaseReview
                   businessCase={businessCase}
-                  grtFeedbacks={systemIntake.grtFeedbacks}
                 />
               )}
             />
