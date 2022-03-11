39b7f708f42e654e89f4bc277a45f3b38074bd95
Tom Brooks <tom.brooks@oddball.io>
2022-03-11 09:48:54 -0500

    Removed all remaining queries except GetCurrentUserQuery.ts

--- src/queries/DeleteCedarSystemBookmarkQuery.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from '@apollo/client';
-
-export default gql`
-  mutation DeleteCedarSystemBookmark($input: CreateCedarSystemBookmarkInput!) {
-    deleteCedarSystemBookmark(input: $input) {
-      cedarSystemId
-    }
-  }
-`;
