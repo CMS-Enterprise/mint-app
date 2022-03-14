01acf949b0451255806a6af7deb551d558fbc73c
Patrick Segura <95709965+patrickseguraoddball@users.noreply.github.com>
2022-03-14 06:10:26 -0700

    EASI-1698 - Frontend skeleton (#5)
    * FE CMMI Skeleton init/work
    
    * Reduced nav bar to only Home
    
    * Moved table translation to new file and updated snapshots
    
    * Ran cypress for login, added video

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -1,45 +1,26 @@
-import taskList from 'i18n/en-US/taskList';
-
-import accessibility from './accessibility';
 import accessibilityStatement from './accessibilityStatement';
-import action from './action';
 import auth from './auth';
-import businessCase from './businessCase';
 import cookies from './cookies';
 import error from './error';
 import footer from './footer';
 import general from './general';
-import governanceReviewBoard from './governanceReviewBoard';
-import governanceReviewTeam from './governanceReviewTeam';
 import header from './header';
 import home from './home';
-import intake from './intake';
-import makingARequest from './makingARequest';
 import privacyPolicy from './privacyPolicy';
-import systemProfile from './systemProfile';
 import tableAndPagination from './tableAndPagination';
 import termsAndConditions from './termsAndConditions';
 
 const enUS = {
-  accessibility,
   accessibilityStatement,
-  action,
   auth,
-  businessCase,
   cookies,
   error,
   footer,
   general,
-  governanceReviewBoard,
-  governanceReviewTeam,
   header,
   home,
-  intake,
-  makingARequest,
   privacyPolicy,
-  systemProfile,
   tableAndPagination,
-  taskList,
   termsAndConditions
 };
 
--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,20 +1,9 @@
 import { combineReducers } from 'redux';
 
-import actionReducer from 'reducers/actionReducer';
-import systemIntakesReducer from 'reducers/systemIntakesReducer';
-
 import authReducer from './authReducer';
-import businessCaseReducer from './businessCaseReducer';
-import fileReducer from './fileReducer';
-import systemIntakeReducer from './systemIntakeReducer';
 
 const rootReducer = combineReducers({
-  systemIntake: systemIntakeReducer,
-  systemIntakes: systemIntakesReducer,
-  businessCase: businessCaseReducer,
-  action: actionReducer,
-  auth: authReducer,
-  files: fileReducer
+  auth: authReducer
 });
 
 export default rootReducer;
