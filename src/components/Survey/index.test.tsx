0ca621182d46e8e40439e0fb21523074265611e2
Tom Brooks <tom.brooks@oddball.io>
2022-03-23 19:24:34 -0400

    Updated src/components/Survey/index.test.tsx easi references

--- src/components/Survey/index.test.tsx
+++ src/components/Survey/index.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { AnythingWrongSurvey, ImproveEasiSurvey } from './index';
+import { AnythingWrongSurvey, ImproveMINTSurvey } from './index';
 
 describe('The Survey component', () => {
   it('renders without crashing', () => {
     shallow(<AnythingWrongSurvey />);
-    shallow(<ImproveEasiSurvey />);
+    shallow(<ImproveMINTSurvey />);
   });
 });
