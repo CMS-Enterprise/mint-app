9d83138b266081c3bbcecd0853acc82076454125
Steven Wade <steven.wade@oddball.io>
2022-03-29 15:32:43 -0700

    basic place holder migrations for collaborator and plan basics

--- migrations/V3__Add_Plan_Collaborator.sql
+++ migrations/V3__Add_Plan_Collaborator.sql
@@ -1,13 +1,15 @@
-create table model_plan (
+create table plan_collaborator (
     id uuid PRIMARY KEY not null,
+    model_plan_id uuid  not null, --foreign key to model plan
     eua_user_id text CHECK (eua_user_id ~ '^[A-Z0-9]{4}$'::text),
-    requester text CHECK (requester IS NOT NULL),
-    requester_component text,
-    Main_Point_Of_Contact text,
-    Point_Of_Contact_Component text,
+    full_name text,
+    component text,
+    team_role text,
+
     created_by text, --this could be the eua_user_id?
     created_dts timestamp with time zone,
     modified_by text, --this could be the eua_user_id?
     modified_dts timestamp with time zone
 
 );
+
--- migrations/V4__Add_Plan_Basics.sql
+++ migrations/V4__Add_Plan_Basics.sql
@@ -1,13 +1,17 @@
-create table model_plan (
+create table plan_basics (
     id uuid PRIMARY KEY not null,
+    model_plan_id uuid  not null, --foreign key to model plan
     eua_user_id text CHECK (eua_user_id ~ '^[A-Z0-9]{4}$'::text),
-    requester text CHECK (requester IS NOT NULL),
-    requester_component text,
-    Main_Point_Of_Contact text,
-    Point_Of_Contact_Component text,
+    model_name text,
+    model_category text,
+    cms_center text, --should select from list
+    cmmi_group text,
+    model_type text,
+
     created_by text, --this could be the eua_user_id?
     created_dts timestamp with time zone,
     modified_by text, --this could be the eua_user_id?
     modified_dts timestamp with time zone
 
 );
+
