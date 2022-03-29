30ad2154fdcf49cfec849db12988fae843e30c3f
Gary Zhao <gary.j.zhao@gmail.com>
2022-03-29 14:48:34 -0400

    Add more copy

--- src/i18n/en-US/home.ts
+++ src/i18n/en-US/home.ts
@@ -1,6 +1,12 @@
 const home = {
   title: 'Welcome to MINT',
-  signIn: 'Sign in to start'
+  subheading:
+    'The place to collaborate on new CMMI models and demonstrations and access resources to assist with your model plans.',
+  signIn: 'Sign in to start',
+  newModelSummaryBox: {
+    copy: 'Have a new model or demonstration?',
+    cta: 'Start a draft model plan'
+  }
 };
 
 export default home;
