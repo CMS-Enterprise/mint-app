ec811e9b043d2b7725552a4aabbb7e64c34df8af
Steven Wade <steven.wade@oddball.io>
2022-03-30 16:28:19 -0700

    updated Plan Basics for new fields

--- pkg/models/plan_basics.go
+++ pkg/models/plan_basics.go
@@ -8,15 +8,19 @@ import (
 )
 
 type PlanBasics struct {
-	ID          uuid.UUID   `json:"id" db:"id"`
-	ModelPlanID uuid.UUID   `json:"modelPlanID" db:"model_plan_id"`
-	EUAUserID   null.String `json:"euaUserId" db:"eua_user_id"`
+	ID          uuid.UUID `json:"id" db:"id"`
+	ModelPlanID uuid.UUID `json:"modelPlanID" db:"model_plan_id"`
 
 	ModelName     null.String `json:"modelName" db:"model_name"`
 	ModelCategory null.String `json:"modelCategory" db:"model_category"`
 	CMSCenter     null.String `json:"cmsCenter" db:"cms_center"`
 	CMMIGroup     null.String `json:"cmmiGroup" db:"cmmi_group"`
 	ModelType     null.String `json:"modelType" db:"model_type"`
+
+	Problem        null.String `json:"problem" db:"problem"`
+	Goal           null.String `json:"goal" db:"goal"`
+	TestInventions null.String `json:"testInventions" db:"test_inventions"`
+	Note           null.String `json:"note" db:"note"`
 
 	CreatedBy   null.String `json:"createdBy" db:"created_by"`
 	CreatedDts  *time.Time  `json:"createdDts" db:"created_dts"`
