225589fe5b5b9b46d8611a113e2afb58881f5564
Steven Wade <steven.wade@oddball.io>
2022-03-30 16:39:56 -0700

    remove EUA_user_id field, it is duplicated by created by field

--- migrations/V3__Add_Model_Plan.sql
+++ migrations/V3__Add_Model_Plan.sql
@@ -1,6 +1,5 @@
 create table model_plan (
     id uuid PRIMARY KEY not null,
-    eua_user_id eua_id,
     requester text CHECK (requester IS NOT NULL),
     requester_component text,
     main_point_of_contact text,
