be74caf5564b3f224705b9993c0b0b679782b1bc
Steven Wade <steven.wade@oddball.io>
2022-03-30 13:29:53 -0700

    add basic plan_collaborator type

---
+++ pkg/models/plan_collaborator.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	// "errors"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/guregu/null"
+)
+
+type plan_collaborator struct {
+	ID          uuid.UUID   `json:"id" db:"id"`
+	ModelPlanID uuid.UUID   `json:"modelPlanID" db:"model_plan_id"`
+	EUAUserID   null.String `json:"euaUserId" db:"eua_user_id"`
+	FullName    null.String `json:"fullName" db:"full_name"`
+	Component   null.String `json:"component" db:"component"`
+	TeamRole    null.String `json:"teamRole" db:"team_role"`
+
+	CreatedBy   null.String `json:"createdBy" db:"created_by"`
+	CreatedDts  *time.Time  `json:"createdDts" db:"created_dts"`
+	ModifiedBy  null.String `json:"modifiedBy" db:"modified_by"`
+	ModifiedDts *time.Time  `json:"modifiedDts" db:"modified_dts"`
+}
