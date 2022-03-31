b0bd840f0e92b644ebc3651288aa1cfb3d3f916d
Gary Zhao <gary.j.zhao@gmail.com>
2022-03-31 15:37:28 -0400

    Adding Model Plan Steps Overview copy

--- src/views/ModelPlan/ModelPlanStepsOverview/index.tsx
+++ src/views/ModelPlan/ModelPlanStepsOverview/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Trans, useTranslation } from 'react-i18next';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import {
   Breadcrumb,
   BreadcrumbBar,
@@ -8,128 +8,96 @@ import {
   Link as UswdsLink,
   ProcessList,
   ProcessListHeading,
-  ProcessListItem
+  ProcessListItem,
+  SummaryBox
 } from '@trussworks/react-uswds';
 
 import UswdsReactLink from 'components/LinkWrapper';
+import MainContent from 'components/MainContent';
 import PageHeading from 'components/PageHeading';
-import CollapsableLink from 'components/shared/CollapsableLink';
 
 // import './index.scss';
 
 const ModelPlanStepsOverview = () => {
-  const { t } = useTranslation('accessibility');
+  const { t } = useTranslation('modelPlan');
 
   return (
-    <div className="grid-container">
-      <div className="tablet:grid-col-10">
-        <BreadcrumbBar variant="wrap">
-          <Breadcrumb>
-            <BreadcrumbLink asCustom={Link} to="/">
-              <span>Home</span>
-            </BreadcrumbLink>
-          </Breadcrumb>
-          <Breadcrumb current>Steps involved in 508 testing</Breadcrumb>
-        </BreadcrumbBar>
-        <PageHeading>{t('testingStepsOverview.heading')}</PageHeading>
-        <p className="accessibility-testing-overview__description">
-          {t('testingStepsOverview.description')}
-        </p>
+    <MainContent className="margin-bottom-5">
+      <div className="grid-container">
+        <div className="tablet:grid-col-12">
+          <BreadcrumbBar variant="wrap">
+            <Breadcrumb>
+              <BreadcrumbLink asCustom={Link} to="/">
+                <span>Home</span>
+              </BreadcrumbLink>
+            </Breadcrumb>
+            <Breadcrumb current>{t('stepsOverview.heading')}</Breadcrumb>
+          </BreadcrumbBar>
+          <PageHeading>{t('stepsOverview.heading')}</PageHeading>
+          <SummaryBox
+            heading=""
+            className="bg-base-lightest border-0 radius-0 padding-2"
+          >
+            <p>{t('stepsOverview.summaryBox.copy')}</p>
+            <ul>
+              <li>{t('stepsOverview.summaryBox.listItem.add')}</li>
+              <li>{t('stepsOverview.summaryBox.listItem.upload')}</li>
+            </ul>
+            <p>{t('stepsOverview.summaryBox.email')}</p>
+          </SummaryBox>
+        </div>
+        <div className="tablet:grid-col-6 margin-top-6">
+          <PageHeading>{t('stepsOverview.steps.heading')}</PageHeading>
+          <p>{t('stepsOverview.steps.description')}</p>
+          <ProcessList>
+            <ProcessListItem>
+              <ProcessListHeading type="h3">
+                {t('stepsOverview.steps.first.heading')}
+              </ProcessListHeading>
+              <p>{t('stepsOverview.steps.first.description')}</p>
+            </ProcessListItem>
+            <ProcessListItem>
+              <ProcessListHeading type="h3">
+                {t('stepsOverview.steps.second.heading')}
+              </ProcessListHeading>
+              <p>{t('stepsOverview.steps.second.description')}</p>
+            </ProcessListItem>
+            <ProcessListItem>
+              <ProcessListHeading type="h3">
+                {t('stepsOverview.steps.third.heading')}
+              </ProcessListHeading>
+              <p>{t('stepsOverview.steps.third.description')}</p>
+            </ProcessListItem>
+          </ProcessList>
+          <hr className="margin-y-3" />
+          <ProcessList
+            start={4}
+            className="easi-governance-overview__governance-steps"
+          >
+            <ProcessListItem>
+              <ProcessListHeading type="h3">
+                {t('stepsOverview.steps.fourth.heading')}
+              </ProcessListHeading>
+              <p>{t('stepsOverview.steps.fourth.description')}</p>
+            </ProcessListItem>
+            <ProcessListItem>
+              <ProcessListHeading type="h3">
+                {t('stepsOverview.steps.fourth.heading')}
+              </ProcessListHeading>
+              <p>{t('stepsOverview.steps.fourth.description')}</p>
+            </ProcessListItem>
+          </ProcessList>
+          <UswdsReactLink
+            className="usa-button margin-top-8"
+            variant="unstyled"
+            to="/508/requests/new"
+            data-testid="continue-link"
+          >
+            {t('stepsOverview.testingStepsOverview.start')}
+          </UswdsReactLink>
+        </div>
       </div>
-      <div className="tablet:grid-col-6 margin-top-6">
-        <ProcessList>
-          <ProcessListItem>
-            <ProcessListHeading type="h3">
-              {t('testingStepsOverview.fillForm.heading')}
-            </ProcessListHeading>
-            <p>{t('testingStepsOverview.fillForm.description')}</p>
-          </ProcessListItem>
-          <ProcessListItem>
-            <ProcessListHeading type="h3">
-              {t('testingStepsOverview.prepareVPAT.heading')}
-            </ProcessListHeading>
-            <Trans
-              i18nKey="accessibility:testingStepsOverview.prepareVPAT.fillOutVPAT"
-              className="margin-0"
-            >
-              indexZero
-              <UswdsReactLink
-                to="/508/templates"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                templatesLink
-              </UswdsReactLink>
-              indexTwo
-            </Trans>
-            <p className="margin-bottom-0">
-              {t('testingStepsOverview.prepareVPAT.changesVPAT')}
-            </p>
-          </ProcessListItem>
-          <ProcessListItem>
-            <ProcessListHeading type="h3">
-              {t('testingStepsOverview.testingSession.heading')}
-            </ProcessListHeading>
-            <p>{t('testingStepsOverview.testingSession.description')}</p>
-          </ProcessListItem>
-          <ProcessListItem>
-            <ProcessListHeading type="h3">
-              {t('testingStepsOverview.results.heading')}
-            </ProcessListHeading>
-            <p>{t('testingStepsOverview.results.description')}</p>
-            <dl title="508 test scores">
-              <dt className="text-bold margin-bottom-1">
-                {t('testingStepsOverview.results.score.above99.heading')}
-              </dt>
-              <dd className="margin-left-0 margin-bottom-2">
-                {t('testingStepsOverview.results.score.above99.description')}
-              </dd>
-              <dt className="text-bold margin-bottom-1">
-                {t('testingStepsOverview.results.score.interval75.heading')}
-              </dt>
-              <dd className="margin-left-0 margin-bottom-2">
-                {t('testingStepsOverview.results.score.interval75.description')}
-              </dd>
-              <dt className="text-bold margin-bottom-1">
-                {t('testingStepsOverview.results.score.below75.heading')}
-              </dt>
-              <dd className="margin-left-0">
-                {t('testingStepsOverview.results.score.below75.description')}
-              </dd>
-            </dl>
-          </ProcessListItem>
-        </ProcessList>
-        <CollapsableLink
-          id="easi-508-testing-exception"
-          label={t('testingStepsOverview.exception.label')}
-          styleLeftBar={false}
-        >
-          <div className="line-height-body-5">
-            <p className="margin-top-0">
-              {t('testingStepsOverview.exception.description')}
-            </p>
-            <p>{t('testingStepsOverview.exception.exceptionFineprint')}</p>
-            <p>
-              <Trans i18nKey="accessibility:testingStepsOverview.exception.contact">
-                indexZero
-                <UswdsLink href="mailto:CMS_Section508@cms.hhs.gov">
-                  email
-                </UswdsLink>
-                indexTwo
-              </Trans>
-            </p>
-          </div>
-        </CollapsableLink>
-        <UswdsReactLink
-          className="usa-button margin-top-8"
-          variant="unstyled"
-          to="/508/requests/new"
-          data-testid="continue-link"
-        >
-          {t('testingStepsOverview.start')}
-        </UswdsReactLink>
-      </div>
-    </div>
+    </MainContent>
   );
 };
 
