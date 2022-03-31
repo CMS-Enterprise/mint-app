652a582f8d2955122c3e4a0f99c519ebcdc77972
Gary Zhao <gary.j.zhao@gmail.com>
2022-03-31 15:36:59 -0400

    Model Plan Translation copy

--- src/i18n/en-US/modelPlan.ts
+++ src/i18n/en-US/modelPlan.ts
@@ -1,6 +1,44 @@
 const modelPlan = {
-  title: 'Welcome to MINT',
-  signIn: 'Sign in to start'
+  stepsOverview: {
+    heading: 'Start a new model plan',
+    summaryBox: {
+      copy: 'Use this service to:',
+      listItem: {
+        add:
+          'add a new Innovation Model request and kick off the model clearance process',
+        upload:
+          'upload and access documents related to your model request, like concept documents or policy papers'
+      },
+      email:
+        'If you have any questions, you can reach the [CMMI POC Team] at: [email]@cms.hhs.gov.'
+    },
+    steps: {
+      heading: 'Steps involved in the process',
+      description:
+        'Below is an overview of the process involved in creating your draft model plan.',
+      first: {
+        heading:
+          'Fill out the sections of the draft model plan form and document model requirements',
+        description:
+          'Tell the Model Intake Team about your model and collaborate with the rest of your team to draft model requirements around participants, population, operations, payment options, and more.'
+      },
+      second: {
+        heading: 'Upload any existing model documentation',
+        description:
+          'Add additional details to your model plan by uploading additional documents such as [DOCUMENT EXAMPLES].'
+      },
+      third: {
+        heading: 'Iterate on your draft model plan',
+        description:
+          'Work with the Model Intake Team and other administrative teams to Iterate on model requirements such as cost and budget estimates.'
+      },
+      fourth: {
+        heading: 'Finalize your model',
+        description:
+          'Review all forms and confirm with the Model Intake Team that your model is ready for internal clearance processes.'
+      }
+    }
+  }
 };
 
 export default modelPlan;
