c3779d3a34df0357ab3f65ba6cc60273c34c9cba
Patrick Segura <95709965+patrickseguraoddball@users.noreply.github.com>
2022-03-31 11:30:03 -0700

    [EASI-1859] job codes and roles (#23)
    * Fixed capitalization on codeAdmin variable
    
    * Added check for okta-token-storage on user-diagnostics
    
    * Updated grapghql docs with MINT jbos codes
    
    Co-authored-by: ClayBenson94 <contact@claybenson.me>

--- pkg/services/auth.go
+++ pkg/services/auth.go
@@ -14,40 +14,19 @@ func HasRole(ctx context.Context, role model.Role) (bool, error) {
 	logger := appcontext.ZLogger(ctx)
 	principal := appcontext.Principal(ctx)
 	switch role {
-	case model.RoleEasiUser:
-		if !principal.AllowEASi() {
+	case model.RoleMintBaseUser:
+		if !principal.AllowMINT() {
 			logger.Info("does not have EASi job code")
 			return false, nil
 		}
 		logger.Info("user authorized as EASi user", zap.Bool("Authorized", true))
 		return true, nil
-	case model.RoleEasiGovteam:
-		if !principal.AllowGRT() {
-			logger.Info("does not have Govteam job code")
+	case model.RoleMintAdminUser:
+		if !principal.AllowADMIN() {
+			logger.Info("does not have ADMIN job code")
 			return false, nil
 		}
-		logger.Info("user authorized as Govteam member", zap.Bool("Authorized", true))
-		return true, nil
-	case model.RoleEasi508Tester:
-		if !principal.Allow508Tester() {
-			logger.Info("does not have 508 tester job code")
-			return false, nil
-		}
-		logger.Info("user authorized as 508 Tester", zap.Bool("Authorized", true))
-		return true, nil
-	case model.RoleEasi508User:
-		if !principal.Allow508User() {
-			logger.Info("does not have 508 User job code")
-			return false, nil
-		}
-		logger.Info("user authorized as 508 User", zap.Bool("Authorized", true))
-		return true, nil
-	case model.RoleEasi508TesterOrUser:
-		is508UserOrTester := principal.Allow508Tester() || principal.Allow508User()
-		if !is508UserOrTester {
-			logger.Info("does not have 508 User nor Tester job code")
-			return false, nil
-		}
+		logger.Info("user authorized as ADMIN member", zap.Bool("Authorized", true))
 		return true, nil
 	default:
 		logger.With(zap.String("Role", role.String())).Info("Unrecognized user role")
@@ -57,11 +36,11 @@ func HasRole(ctx context.Context, role model.Role) (bool, error) {
 
 // AuthorizeHasEASiRole authorizes that the user can use EASi
 func AuthorizeHasEASiRole(ctx context.Context) (bool, error) {
-	return HasRole(ctx, model.RoleEasiUser)
+	return HasRole(ctx, model.RoleMintBaseUser)
 }
 
-// AuthorizeRequireGRTJobCode authorizes a user as being a member of the
-// GRT (Governance Review Team)
-func AuthorizeRequireGRTJobCode(ctx context.Context) (bool, error) {
-	return HasRole(ctx, model.RoleEasiGovteam)
+// AuthorizeRequireADMINJobCode authorizes a user as being a member of the
+// ADMIN (MINT Admin Team)
+func AuthorizeRequireADMINJobCode(ctx context.Context) (bool, error) {
+	return HasRole(ctx, model.RoleMintAdminUser)
 }
