f7cd9e6ba0d4b6528a555f5487f0c555cbd22726
Gary Zhao <gary.j.zhao@gmail.com>
2022-04-01 14:48:23 -0400

    Basic user unit test

--- src/views/Home/index.test.tsx
+++ src/views/Home/index.test.tsx
@@ -6,6 +6,7 @@ import { mount, ReactWrapper, shallow } from 'enzyme';
 import { mockFlags, resetLDMocks } from 'jest-launchdarkly-mock';
 import configureMockStore from 'redux-mock-store';
 
+import UswdsReactLink from 'components/LinkWrapper';
 import { MessageProvider } from 'hooks/useMessage';
 import { Flags } from 'types/flags';
 import Table from 'views/ModelPlan/Table';
@@ -46,7 +47,7 @@ describe('The home page', () => {
     });
 
     describe('User is logged in', () => {
-      it('displays process options', async () => {
+      it('renders the correct components', async () => {
         mockFlags({ ...defaultFlags });
         const mockStore = configureMockStore();
         const store = mockStore({ auth: mockAuthReducer });
@@ -62,82 +63,10 @@ describe('The home page', () => {
             </MemoryRouter>
           );
           component.update();
-          expect(component.find('a[children="Start now"]').exists()).toEqual(
-            false
-          );
-          expect(
-            component.find('a[children="IT Governance"]').exists()
-          ).toEqual(true);
-          expect(
-            component.find('a[children="Section 508 compliance"]').exists()
-          ).toEqual(true);
+          expect(component.find(UswdsReactLink).exists()).toEqual(true);
           expect(component.find('hr').exists()).toBeTruthy();
           expect(component.find(Table).exists()).toBeTruthy();
         });
-      });
-    });
-  });
-
-  describe('is an admin user', () => {
-    const mockAuthReducer = {
-      isUserSet: true,
-      groups: ['ADMIN_DEV']
-    };
-
-    const mountComponent = (mockedStore: any): ReactWrapper => {
-      const mockStore = configureMockStore();
-      const store = mockStore(mockedStore);
-      return mount(
-        <MemoryRouter initialEntries={['/']} initialIndex={0}>
-          <Provider store={store}>
-            <MessageProvider>
-              <Home />
-            </MessageProvider>
-          </Provider>
-        </MemoryRouter>
-      );
-    };
-
-    it('renders without crashing', () => {
-      const mockStore = configureMockStore();
-      const store = mockStore({ auth: mockAuthReducer });
-      const shallowComponent = () =>
-        shallow(
-          <MemoryRouter initialEntries={['/']} initialIndex={0}>
-            <Provider store={store}>
-              <MessageProvider>
-                <Home />
-              </MessageProvider>
-            </Provider>
-          </MemoryRouter>
-        );
-      expect(shallowComponent).not.toThrow();
-    });
-
-    it('renders the open requests table', async () => {
-      mockFlags(defaultFlags);
-      const homePage = mountComponent({
-        auth: mockAuthReducer
-      });
-
-      await act(async () => {
-        homePage.update();
-        expect(homePage.text()).toContain('There are 2 open requests');
-      });
-    });
-
-    it('renders the closed requests table', async () => {
-      mockFlags(defaultFlags);
-      const homePage = mountComponent({
-        auth: mockAuthReducer
-      });
-
-      homePage
-        .find('[data-testid="view-closed-intakes-btn"]')
-        .simulate('click');
-      await act(async () => {
-        homePage.update();
-        expect(homePage.text()).toContain('There is 1 closed request');
       });
     });
   });
