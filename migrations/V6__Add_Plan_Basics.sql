ef18ffe4c372caea0acd469df1e3b66f79aa734b
Steven Wade <steven.wade@oddball.io>
2022-04-05 07:47:27 -0700

    remove EUA-USERID from plan basics table

--- migrations/V6__Add_Plan_Basics.sql
+++ migrations/V6__Add_Plan_Basics.sql
@@ -1,7 +1,6 @@
 create table plan_basics (
     id uuid PRIMARY KEY not null,
     model_plan_id uuid not null, --foreign key to model plan
-    eua_user_id eua_id, -- duplicated by created by?
     model_name TEXT,
     model_category TEXT, --select from list
     cms_center TEXT, --should select from list
