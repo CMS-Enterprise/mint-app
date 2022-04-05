fdc7980f7f17a1e224960618f5f4d2c0d063c5c0
Gary Zhao <gary.j.zhao@gmail.com>
2022-04-05 13:25:45 -0400

    Update routes

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -19,6 +19,7 @@ import Cookies from 'views/Cookies';
 import FlagsWrapper from 'views/FlagsWrapper';
 import Home from 'views/Home';
 import Login from 'views/Login';
+import ModelPlan from 'views/ModelPlan';
 import ModelPlanStepsOverview from 'views/ModelPlan/ModelPlanStepsOverview';
 import NewPlan from 'views/ModelPlan/NewPlan';
 import TaskList from 'views/ModelPlan/TaskList';
@@ -64,7 +65,7 @@ const AppRoutes = () => {
       <SecureRoute path="/user-diagnostics" component={UserInfo} />
 
       {/* Model Routes */}
-      <Redirect exact from="/models" to="/models/new-plan" />
+      <SecureRoute path="/models" exact component={ModelPlan} />
       <SecureRoute
         path="/models/new-plan"
         exact
