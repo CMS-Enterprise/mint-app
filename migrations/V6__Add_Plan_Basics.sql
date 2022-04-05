0b5ea11a620f012df3940f82d7b9bf0bd76b1fe0
Steven Wade <steven.wade@oddball.io>
2022-04-05 09:35:00 -0700

    correct issue in plan basics migration syntax

--- migrations/V6__Add_Plan_Basics.sql
+++ migrations/V6__Add_Plan_Basics.sql
@@ -17,7 +17,7 @@ create table plan_basics (
     created_by eua_id,
     created_dts timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modified_by eua_id,
-    modified_dts timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP
+    modified_dts timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     status TEXT -- can become and ENUM/TYPE
 );
 
