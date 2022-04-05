5b774ffe04f068d4b7967b53ff305ddcb99d7bf3
Patrick Segura <95709965+patrickseguraoddball@users.noreply.github.com>
2022-04-05 06:22:40 -0700

    Added sign in page for MINT (#26)

--- src/i18n/en-US/home.ts
+++ src/i18n/en-US/home.ts
@@ -1,6 +1,12 @@
 const home = {
   title: 'Welcome to MINT',
-  signIn: 'Sign in to start'
+  signIn: 'Sign in to start',
+  mintPurpose: 'You can use MINT to:',
+  startNow: 'Start a draft model plan',
+  mintTasks: [
+    'collaborate on requirements for new CMMI models and demonstrations',
+    'access resources to help you complete your model plans'
+  ]
 };
 
 export default home;
