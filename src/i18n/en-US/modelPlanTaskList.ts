6626b7e08d508ac2f27d85c1286a04caf46aa7b4
Gary Zhao <gary.j.zhao@gmail.com>
2022-04-12 15:19:40 -0400

    add copy

--- src/i18n/en-US/modelPlanTaskList.ts
+++ src/i18n/en-US/modelPlanTaskList.ts
@@ -1,13 +1,60 @@
 const modelPlanTaskList = {
   navigation: {
     home: 'Home',
-    modelPlanTaskList: 'Model Plan task list'
+    modelPlanTaskList: 'Model Plan task list',
   },
   subheading: 'for <1>{{modelName}}</1>',
   summaryBox: {
     copy: 'There are no documents uploaded for <1>{{modelName}}</1>.',
-    cta: 'Upload a document'
-  }
+    cta: 'Upload a document',
+  },
+  numberedList: {
+    modelBasic: {
+      heading: 'Model basics',
+      copy:
+        'Start filling out as much of the basic model information as you know and reach out to the Model Assessment Team if you need help.',
+    },
+    characteristics: {
+      heading: 'General characteristics',
+      copy:
+        'Start filling out as much of the general model characteristics as you know and reach out to the Model Assessment Team if you need help.',
+    },
+    participants: {
+      heading: 'Participants and providers',
+      copy:
+        'Start filling out as much of the model participant information as you know and reach out to the Model Assessment Team if you need help.',
+    },
+    beneficiaries: {
+      heading: 'Beneficiaries',
+      copy:
+        'Start filling out as much of the beneficiary information as you know and reach out to the Model Assessment Team if you need help.',
+    },
+    operations: {
+      heading: 'Operations, evaluation, and learning',
+      copy:
+        'Start filling out as much of the model operation information as you know and reach out to the Model Assessment Team if you need help.',
+    },
+    payment: {
+      heading: 'Payment',
+      copy:
+        'Start filling out as much of the payment information as you know and reach out to the Model Assessment Team if you need help.',
+    },
+    review: {
+      heading: 'Review draft Model Plan',
+      copy:
+        'Finalize your draft model plan to the best of your ability so that the Budget Team can begin drafting cost estimates.',
+    },
+    costEstimates: {
+      heading: 'Draft cost estimates',
+      copy:
+        'The Budget Team will review your Model Plan and draft cost estimates for your model. If they need your assistance, they will reach out to you.',
+    },
+    finalizeModelPlan: {
+      heading: 'Finalize Model Plan',
+      copy:
+        'Review all sections of the Model Plan and confirm with the Model Assessment Team that your model is ready for internal clearance processes.',
+    },
+  },
 };
 
 export default modelPlanTaskList;
