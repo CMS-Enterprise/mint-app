8f5dec86462f935c3414f54318e7905f0d358986
Gary Zhao <gary.j.zhao@gmail.com>
2022-04-13 14:33:27 -0400

    work on sidenav

--- src/views/ModelPlan/TaskList/components/TaskListSideNav/index.tsx
+++ src/views/ModelPlan/TaskList/components/TaskListSideNav/index.tsx
@@ -19,43 +19,37 @@ const TaskListSideNav = () => {
 
   return (
     <div
-      className="sidenav-actions grid-row flex-column margin-left-4 border-top-05 border-primary-lighter"
+      className="sidenav-actions margin-left-4 border-top-05 border-primary-lighter padding-top-2"
       data-testid="sidenav-actions"
     >
-      <div className="grid-col margin-top-105">
-        <UswdsReactLink to="/">Save & Exit</UswdsReactLink>
-      </div>
-      {true && (
-        <div className="grid-col margin-top-2">
-          <Button
-            className="line-height-body-5 test-withdraw-request"
-            type="button"
-            unstyled
-            onClick={() => setModalOpen(true)}
-          >
-            Remove your request to add a new system
-          </Button>
-          <Modal isOpen={isModalOpen} closeModal={() => setModalOpen(false)}>
-            <PageHeading headingLevel="h2" className="margin-top-0">
-              request name
-            </PageHeading>
-            <p>{t('taskList:withdraw_modal:warning')}</p>
-            <Button
-              type="button"
-              className="margin-right-4"
-              onClick={() => console.log('arhcived')}
-            >
-              {/* {t('taskList:withdraw_modal:confirm')} */}
-              confirm
-            </Button>
-            <Button type="button" unstyled onClick={() => setModalOpen(false)}>
-              {/* {t('taskList:withdraw_modal:cancel')} */}
-              cancel
-            </Button>
-          </Modal>
-        </div>
-      )}
-      <div className="grid-col margin-top-5">
+      <UswdsReactLink to="/">{t('sideNav.saveAndExit')}</UswdsReactLink>
+      <Button
+        className="line-height-body-5 test-withdraw-request"
+        type="button"
+        unstyled
+        onClick={() => setModalOpen(true)}
+      >
+        {t('sideNav.remove')}
+      </Button>
+      <Modal isOpen={isModalOpen} closeModal={() => setModalOpen(false)}>
+        <PageHeading headingLevel="h2" className="margin-top-0">
+          request name
+        </PageHeading>
+        <p>{t('taskList:withdraw_modal:warning')}</p>
+        <Button
+          type="button"
+          className="margin-right-4"
+          onClick={() => console.log('arhcived')}
+        >
+          {/* {t('taskList:withdraw_modal:confirm')} */}
+          confirm
+        </Button>
+        <Button type="button" unstyled onClick={() => setModalOpen(false)}>
+          {/* {t('taskList:withdraw_modal:cancel')} */}
+          cancel
+        </Button>
+      </Modal>
+      <div className="margin-top-5">
         <h4>Related Content</h4>
         <UswdsReactLink
           aria-label="Open overview for adding a system in a new tab"
