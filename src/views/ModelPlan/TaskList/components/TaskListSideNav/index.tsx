c8fa1515d34820611b77c0f4bf073579afead2cd
Gary Zhao <gary.j.zhao@gmail.com>
2022-04-13 15:59:45 -0400

    Added team member list component

--- src/views/ModelPlan/TaskList/components/TaskListSideNav/index.tsx
+++ src/views/ModelPlan/TaskList/components/TaskListSideNav/index.tsx
@@ -6,6 +6,8 @@ import UswdsReactLink from 'components/LinkWrapper';
 import Modal from 'components/Modal';
 import PageHeading from 'components/PageHeading';
 
+import TeamMembersList from './TeamMembersList';
+
 // import { isIntakeOpen } from 'utils/systemIntake';
 // type SideNavActionsProps = {
 //   intake: SystemIntake;
@@ -40,39 +42,59 @@ const TaskListSideNav = () => {
     );
   };
 
+  const sampleTeamMembers = [
+    'Jane McModelteam',
+    'Laura Rodriguez',
+    'Allison Li',
+    'Bryce Greenfield-Jones'
+  ];
+
   return (
-    <div
-      className="sidenav-actions margin-left-4 border-top-05 border-primary-lighter padding-top-2"
-      data-testid="sidenav-actions"
-    >
-      <div className="margin-bottom-1">
-        <UswdsReactLink to="/">{t('sideNav.saveAndExit')}</UswdsReactLink>
-      </div>
-      <Button
-        className="line-height-body-5 test-withdraw-request"
-        type="button"
-        unstyled
-        onClick={() => setModalOpen(true)}
-      >
-        {t('sideNav.remove')}
-      </Button>
-      <div className="margin-top-4">
-        <h4 className="margin-bottom-1">{t('sideNav.relatedContent')}</h4>
-        <UswdsReactLink
-          aria-label={t('sideNav.ariaLabelForOverview')}
-          className="line-height-body-5"
-          to="/governance-overview"
-          variant="external"
-          target="_blank"
-        >
-          <Trans i18nKey="modelPlanTaskList:sideNav.overview">
-            indexZero
-            <span aria-hidden /> indexTwo
-          </Trans>
-        </UswdsReactLink>
-      </div>
+    <>
       {renderModal()}
-    </div>
+      <div
+        className="sidenav-actions margin-left-4 border-top-05 border-primary-lighter padding-top-2"
+        data-testid="sidenav-actions"
+      >
+        <div className="margin-bottom-1">
+          <UswdsReactLink to="/">{t('sideNav.saveAndExit')}</UswdsReactLink>
+        </div>
+        <Button
+          className="line-height-body-5 test-withdraw-request"
+          type="button"
+          unstyled
+          onClick={() => setModalOpen(true)}
+        >
+          {t('sideNav.remove')}
+        </Button>
+        <div className="margin-top-4 margin-bottom-7">
+          <h4 className="margin-bottom-1">{t('sideNav.relatedContent')}</h4>
+          <UswdsReactLink
+            aria-label={t('sideNav.ariaLabelForOverview')}
+            className="line-height-body-5"
+            to="/governance-overview"
+            variant="external"
+            target="_blank"
+          >
+            <Trans i18nKey="modelPlanTaskList:sideNav.overview">
+              indexZero
+              <span aria-hidden /> indexTwo
+            </Trans>
+          </UswdsReactLink>
+        </div>
+        <div>
+          <h3 className="margin-bottom-05">{t('sideNav.modelTeam')}</h3>
+          <div className="margin-bottom-2">
+            <UswdsReactLink to="/">{t('sideNav.editTeam')}</UswdsReactLink>
+          </div>
+          <div className="sidenav-actions__teamList">
+            <ul className="usa-list usa-list--unstyled">
+              <TeamMembersList team={sampleTeamMembers} />
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
