3b203aef2389ccad9a7b46b2020623619add488f
Gary Zhao <gary.j.zhao@gmail.com>
2022-04-13 15:59:23 -0400

    Added team member list copm

---
+++ src/views/ModelPlan/TaskList/components/TaskListSideNav/TeamMembersList.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const TeamMembersList = ({ team }: { team: string[] }) => {
+  const arrayOfColors = [
+    'bg-accent-cool-lighter',
+    'bg-secondary-lighter',
+    'bg-primary-lighter',
+    'bg-accent-warm-lighter'
+  ];
+
+  return (
+    <>
+      {team.map((name, index) => {
+        return (
+          <li
+            className="display-flex flex-align-center margin-bottom-1"
+            key={name}
+          >
+            <div
+              className={`display-flex flex-align-center flex-justify-center circle-4 margin-right-1 ${arrayOfColors[index]}`}
+            >
+              {
+                // @ts-ignore
+                name
+                  .match(/(\b\S)?/g)
+                  .join('')
+                  .match(/(^\S|\S$)?/g)
+                  .join('')
+                  .toUpperCase()
+              }
+            </div>
+            <p className="margin-y-0">{name}</p>
+          </li>
+        );
+      })}
+    </>
+  );
+};
+
+export default TeamMembersList;
