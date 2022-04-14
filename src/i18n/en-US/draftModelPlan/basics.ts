946b0a8169dabe4645c00d3ecc5e51eb55db98c0
Patrick Segura <patrick.segura@oddball.io>
2022-04-14 09:42:50 -0400

    Translation text entry work

---
+++ src/i18n/en-US/draftModelPlan/basics.ts
@@ -0,0 +1,49 @@
+const basics = {
+  heading: 'Model basics',
+  breadcrumb: 'Model basics',
+  modelName: 'Model Name',
+  modelCategory: 'Model Category',
+  cmsComponent: 'CMS Component',
+  cmsComponents: [
+    'CMMI',
+    'Center for Medicare (CM)',
+    'Federal Coordinated Health Care',
+    'Center for Clinical Standards and Quality (CCSQ)',
+    'Center for Program Integrity (CPI)',
+    'Other'
+  ],
+  cmmiGroup: 'CMMI Group',
+  cmmiGroups: [
+    'Patient Care Models Group (PCMG)',
+    'Policy and Programs Group (PPG)',
+    'Preventative and Population Health Care Models Group (PPHCMG)',
+    'Seamless Care Models Group (SCMG)',
+    'State Innovations Group (SIG)',
+    'To be determined'
+  ],
+  modelType: 'Model Type',
+  voluntary: 'Voluntary',
+  mandatory: 'Mandatory',
+  problem: 'Problem',
+  goal: 'Goal',
+  goalHelp:
+    'Please include the high level goal of the program and a description of the project.',
+  testInterventions: 'Test Interventions',
+  highLevelTimeline: 'High level timeline',
+  completeICIP: 'Complete ICIP',
+  clearance: 'Clearance',
+  clearanceStartDate: 'Clearance start date',
+  clearanceEndDate: 'Clearance end date',
+  annouceModel: 'Announce model',
+  applicationPeriod: 'Application period',
+  applicationStartDate: 'Application start date',
+  applicationEndDate: 'Application end date',
+  demonstrationPerformance: 'Demonstration or model period of performance',
+  modelWrapUp: 'Model wrap-up end date',
+  tightTimeline:
+    'If timelines are tight, might there be pieces of the model that can be phased in over time?',
+  tightTimelineInfo:
+    'That is, the basic model would start at the earliest possible date but additional facets could be phased in at a later quarter.'
+};
+
+export default basics;
---
+++ src/i18n/en-US/draftModelPlan/participantsAndProviders.ts
@@ -0,0 +1,56 @@
+const participantsAndProviders = {
+  heading: 'Participants and Providers',
+  participantInfo:
+    'Participants are organizations/individuals that have contracts with CMMI to participate in a model or demonstration. Participants may or may nor ve Medicare-enrolled providers.',
+  providerInfo:
+    'Providers are organizations/indviduals that work with the Participants and deliver care or services to the beneficiaries impacted by our model.',
+  whoAreParticipants: 'Who are the particpants? Select all that apply.',
+  participantsCMMI: 'Are the participants in CMMI models now?',
+  participantsCMMIInfo:
+    'If you have selected more than one participant in the questions above, specify which participants already participate in CMMI models.',
+  modelLevel: 'At what level will the model apply?',
+  modelLevelInfo:
+    '(e.g., all services identified at the CPT or ICD-10 code level, epsiode of care for a specific span of time, all hospitals unpaid under the Inpatient Prospective Payment System in a certain state)',
+  selectedParticipants: 'Selected participants',
+  participantQuestions: 'Participant specific questions',
+  typeMedicateProvider: 'Which type of medicare providers?',
+  describeStates: 'Please describe states',
+  describeOther: 'Please describe other',
+  howManyParticipants: 'How many participants do you expect?',
+  howManyInfo:
+    'Note: Sometimes RRCEG provides a minimum for the power calculation. Other times stakeholder research may inform the team about who are good candidates or might be interested in participating.',
+  estimateConfidence: 'What is your level of confidence on this esitmate?',
+  estimateOptions: {
+    notAtAll: 'Not at all confident',
+    slightly: 'Slightly confident',
+    fairly: 'Fairly confident',
+    completely: 'Compeletely confident'
+  },
+  recruitPartifipants: 'How will you recruit the participants?',
+  recruitOptions: {
+    loi: 'LOI (Letter of interest)',
+    rfa: 'RFA (Request for applications)',
+    recruitInfo:
+      'CMMI writes, Office of General Council (OGC) reviews and approves',
+    nofo:
+      'NOFO (Notice of funding opportunity for grants/cooperative agreements',
+    notApplicable: 'Not applicable, it is a mandatory model'
+  },
+  howWillYouSelect: 'How will you select participants? Select all that appy.',
+  selectOtions: {
+    reviewApplications: 'Model team wil review applications',
+    solicitSupport: 'Solicit support for CMII staff',
+    anotherComponent: 'Another CMS component or process will prived support',
+    applicationReview: 'Use an Application Review and Scoring tool',
+    applicationSupport: 'Get an application support contractor',
+    criteria:
+      'We have a basic set of criteria and plan to accept any participant that meets them',
+    anotherProcess:
+      'Another CMS oricess or component supports participant selection',
+    notApplicable: 'Not applicable, it is a mandatory model'
+  },
+  participantAgreementNeeded:
+    'Will more than one standard participant agreement be needed depending on awardee selections or characteristics, such as risk/type/size?'
+};
+
+export default participantsAndProviders;
