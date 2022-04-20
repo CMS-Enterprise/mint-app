614f6307927f1a91e1397403851c895d59eb3a7b
Gary Zhao <garyjzhao@users.noreply.github.com>
2022-04-20 13:31:42 -0400

    [Easi-1931] Draft Model Plan Task List (#33)
    * Model plan task list init
    
    * Model plan task list i18n.ts
    
    * Moved around components for better file nav
    
    * Imported new comps
    
    * Adding the copy
    
    * Add stsyles
    
    * add copy
    
    * add sttyles
    
    * remove trailing coma
    
    * Object key all the goods
    
    * checking to see if status box works
    
    * adding task list cta
    
    * add more copy to translation
    
    * add more copy to translation
    
    * Add more tag colors
    
    * DRYing up code
    
    * DRYing up code
    
    * adjust some margins
    
    * Add Divider copm
    
    * Task List CTA comp init
    
    * importing task list cta
    
    * remove divider on last item
    
    * modify variable name
    
    * adding space for readabiltiy
    
    * adding more space
    
    * adding sidenav coy
    
    * work on sidenav
    
    * adding copy
    
    * Extracted the modal from the rest of the component
    
    * Added team member list copm
    
    * added spacing
    
    * Added team member list component
    
    * copy
    
    * Step 7 and 8 no more
    
    * fix modal
    
    * trying to use variable colors
    
    * module operator to make life easier
    
    * Initial importing gql info
    
    * Adding gql things
    
    * Adding gql things
    
    * gql wip
    
    * Adjusting some names
    
    * adding loading
    
    * temp solution
    
    * add TODO comment
    
    * Removing nalme
    
    * clean up code
    
    * First attempt for test
    
    * redirect to home
    
    * Fix key warning
    
    * add last udpated status copu
    
    * add last udpated status copu
    
    * add last udpated status copu
    
    * wip
    
    * Cleaned up the test
    
    * pass down key to adjust test id name for easier testing
    
    * Add TODO comment
    
    * using USWDS color token for gold
    
    * using USDWS token colors
    
    * remove completed since it has been removed from figma
    
    * using USWDS color token
    
    * Removing commented code
    
    * Reducing the Sass for favor of Utilties
    
    * 🧹
    
    * 🧹
    
    * 🧹
    
    * using switch case instead
    
    * using switch case instead
    
    * more cleaning
    
    * clean up sttyles
    
    * clean up sttyles
    
    * clean up styles
    
    * clean up styles
    
    * using Tag component
    
    * QA Feedback
    
    * 🧹
    
    * convert to switch
    
    * remove unused prop
    
    * remove unused prop
    
    * add paths
    
    * renamed folder
    
    * renamed folder
    
    * renamed folder
    
    * add Beneficiaries route
    
    * nevermind dont need path
    
    * renamed TaskListCta to TaskListButton
    
    * Pass down path as prop
    
    * sort props
    
    * sort props
    
    * remove not needed status since.. its ... not needed
    
    * Add TaskListButton test
    
    * remove empty test file
    
    * wip
    
    * 🧹
    
    * fix tests
    
    * comments
    
    * comments

---
+++ src/i18n/en-US/modelPlanTaskList.ts
@@ -0,0 +1,79 @@
+const modelPlanTaskList = {
+  navigation: {
+    home: 'Home',
+    modelPlanTaskList: 'Model Plan task list'
+  },
+  subheading: 'for <1>{{modelName}}</1>',
+  summaryBox: {
+    copy: 'There are no documents uploaded for <1>{{modelName}}</1>.',
+    cta: 'Upload a document'
+  },
+  sideNav: {
+    saveAndExit: 'Save & Exit',
+    remove: 'Remove your Model Plan',
+    relatedContent: 'Related Content',
+    ariaLabelForOverview: 'Open overview for adding a system in a new tab',
+    overview: 'Overview for adding a model <1>(opens in a new tab)</1>',
+    modelTeam: 'Model team',
+    editTeam: 'Edit team'
+  },
+  withdraw_modal: {
+    header: 'Confirm you want to remove {{-requestName}}.',
+    warning: 'You will lose any information you have filled in.',
+    confirm: 'Remove request',
+    cancel: 'Cancel',
+    confirmationText_name: 'The request for {{-requestName}} has been removed',
+    confirmationText_noName: 'The request has been removed'
+  },
+  numberedList: {
+    basics: {
+      heading: 'Model basics',
+      copy:
+        'Start filling out as much of the basic model information as you know and reach out to the Model Assessment Team if you need help.'
+    },
+    characteristics: {
+      heading: 'General characteristics',
+      copy:
+        'Start filling out as much of the general model characteristics as you know and reach out to the Model Assessment Team if you need help.'
+    },
+    participants: {
+      heading: 'Participants and providers',
+      copy:
+        'Start filling out as much of the model participant information as you know and reach out to the Model Assessment Team if you need help.'
+    },
+    beneficiaries: {
+      heading: 'Beneficiaries',
+      copy:
+        'Start filling out as much of the beneficiary information as you know and reach out to the Model Assessment Team if you need help.'
+    },
+    operations: {
+      heading: 'Operations, evaluation, and learning',
+      copy:
+        'Start filling out as much of the model operation information as you know and reach out to the Model Assessment Team if you need help.'
+    },
+    payment: {
+      heading: 'Payment',
+      copy:
+        'Start filling out as much of the payment information as you know and reach out to the Model Assessment Team if you need help.'
+    },
+    finalizeModelPlan: {
+      heading: 'Finalize Model Plan',
+      copy:
+        'Review all sections of the Model Plan and confirm with the Model Assessment Team that your model is ready for internal clearance processes.'
+    }
+  },
+  taskListButton: {
+    start: 'Start',
+    continue: 'Continue'
+  },
+  taskListItem: {
+    ready: 'Ready to start',
+    inProgress: 'In progress',
+    completed: 'Completed',
+    cannotStart: 'Cannot start yet',
+    notNeeded: 'Not needed',
+    lastUpdated: 'Last updated:'
+  }
+};
+
+export default modelPlanTaskList;
---
+++ src/queries/types/GetModelPlan.ts
@@ -0,0 +1,28 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: GetModelPlan
+// ====================================================
+
+export interface GetModelPlan_modelPlan_basics {
+  __typename: "PlanBasics";
+  id: UUID | null;
+}
+
+export interface GetModelPlan_modelPlan {
+  __typename: "ModelPlan";
+  id: UUID | null;
+  modelName: string | null;
+  basics: GetModelPlan_modelPlan_basics | null;
+}
+
+export interface GetModelPlan {
+  modelPlan: GetModelPlan_modelPlan | null;
+}
+
+export interface GetModelPlanVariables {
+  id: UUID;
+}
