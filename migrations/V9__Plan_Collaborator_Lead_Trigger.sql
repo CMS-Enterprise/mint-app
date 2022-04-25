30f860b0f0d2e3c16c3ad7c76f9d54670f07d712
Steven Wade <steven.wade@oddball.io>
2022-04-25 11:00:08 -0700

    basic trigger on delete and update of model_collaborator that requires 1 model lead

---
+++ migrations/V9__Plan_Collaborator_Lead_Trigger.sql
@@ -0,0 +1,26 @@
+CREATE FUNCTION collaborator_role_check_trigger() RETURNS TRIGGER AS $role_check$
+BEGIN
+ IF (SELECT count(*)
+ FROM plan_collaborator
+ WHERE  team_role = 'MODEL_LEAD' and model_plan_id = OLD.model_plan_id 
+ )<2
+ THEN
+    RAISE EXCEPTION 'There must be at least one MODEL_LEAD assigned to each model plan';
+     END IF;
+    RETURN OLD;
+END
+$role_check$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER collaborator_lead_req_update
+    BEFORE UPDATE ON plan_collaborator
+	FOR EACH ROW
+    WHEN ((OLD.team_role = 'MODEL_LEAD') AND (NEW.team_role != 'MODEL_LEAD'))
+    EXECUTE FUNCTION collaborator_role_check_trigger();
+	
+	
+CREATE TRIGGER collaborator_lead_req_delete
+    BEFORE DELETE ON plan_collaborator
+	FOR EACH ROW
+    WHEN (OLD.team_role = 'MODEL_LEAD') 
+    EXECUTE FUNCTION collaborator_role_check_trigger();	
