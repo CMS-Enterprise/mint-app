33e6314471a2b55e926f8d7829db6e22bc22f930
Steven Wade <steven.wade@oddball.io>
2022-04-26 15:08:20 -0700

    update plan collaborator test to remove CMMIgroup

--- pkg/graph/resolvers/plan_collaborator_test.go
+++ pkg/graph/resolvers/plan_collaborator_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func MakeTestCollaborator() models.PlanCollaborator {
+func makeTestCollaborator() models.PlanCollaborator {
 	return models.PlanCollaborator{
 		FullName:    "Fake name",
 		EUAUserID:   "FAKE",
@@ -23,15 +23,7 @@ func MakeTestCollaborator() models.PlanCollaborator {
 }
 func TestCreatePlanCollaborator(t *testing.T) {
 	tc := GetDefaultTestConfigs()
-	colab := models.PlanCollaborator{
-		FullName:    "Fake name",
-		EUAUserID:   "FAKE",
-		CreatedBy:   models.StringPointer("FAKE"),
-		ModifiedBy:  models.StringPointer("FAKE"),
-		ID:          uuid.MustParse("f85c15cd-f920-4080-bdeb-e79b3d8f056a"),
-		ModelPlanID: uuid.MustParse("85b3ff03-1be2-4870-b02f-55c764500e48"),
-		TeamRole:    models.TeamRoleLearning,
-	}
+	colab := makeTestCollaborator()
 
 	result, err := CreatePlanCollaborator(tc.Logger, &colab, colab.CreatedBy, tc.Store)
 	assert.NoError(t, err)
@@ -40,23 +32,14 @@ func TestCreatePlanCollaborator(t *testing.T) {
 
 func TestUpdatePlanCollaborator(t *testing.T) {
 	tc := GetDefaultTestConfigs()
-
-	colab := models.PlanCollaborator{
-		FullName:    "Fake name",
-		EUAUserID:   "FAKE",
-		CreatedBy:   models.StringPointer("FAKE"),
-		ModifiedBy:  models.StringPointer("FAKE"),
-		ID:          uuid.MustParse("f85c15cd-f920-4080-bdeb-e79b3d8f056a"),
-		ModelPlanID: uuid.MustParse("85b3ff03-1be2-4870-b02f-55c764500e48"),
-		TeamRole:    models.TeamRoleLearning,
-	}
+	colab := makeTestCollaborator()
+	colab.TeamRole = models.TeamRoleLeadership
 
 	result, err := UpdatePlanCollaborator(tc.Logger, &colab, colab.CreatedBy, tc.Store)
 
 	assert.NoError(t, err)
 	assert.NotNil(t, result.ID)
-	//TODO MAKE A CHANGE and assert, use the common method
-	// assert.EqualValues(t, result.CMSCenter, colab.CMSCenter)
+	assert.EqualValues(t, result.TeamRole, colab.TeamRole)
 
 }
 
