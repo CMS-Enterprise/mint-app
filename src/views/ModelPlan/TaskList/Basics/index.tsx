e069f2efd0c1ff67235373d284c945d90ccc9fa9
Gary Zhao <gary.j.zhao@gmail.com>
2022-04-27 15:00:12 -0400

    Building out the basics page

--- src/views/ModelPlan/TaskList/Basics/index.tsx
+++ src/views/ModelPlan/TaskList/Basics/index.tsx
@@ -1,32 +1,59 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { Link, Route, Switch, useParams } from 'react-router-dom';
+import {
+  Breadcrumb,
+  BreadcrumbBar,
+  BreadcrumbLink
+} from '@trussworks/react-uswds';
 
 import MainContent from 'components/MainContent';
+import PageHeading from 'components/PageHeading';
 import NotFound, { NotFoundPartial } from 'views/NotFound';
 
 const BasicsContent = () => {
-  return <h1>hello world</h1>;
+  const { modelId } = useParams<{ modelId: string }>();
+  const { t: h } = useTranslation('draftModelPlan');
+  const { t } = useTranslation('basics');
+
+  return (
+    <MainContent className="margin-bottom-5">
+      <div className="grid-container">
+        <div className="tablet:grid-col-12">
+          <BreadcrumbBar variant="wrap">
+            <Breadcrumb>
+              <BreadcrumbLink asCustom={Link} to="/">
+                <span>{h('home')}</span>
+              </BreadcrumbLink>
+            </Breadcrumb>
+            <Breadcrumb current>{t('breadcrumb')}</Breadcrumb>
+          </BreadcrumbBar>
+          <PageHeading className="margin-top-4 margin-bottom-2">
+            {t('heading')}
+          </PageHeading>
+        </div>
+      </div>
+    </MainContent>
+  );
 };
 
 export const Basics = () => {
   return (
-    <MainContent className="grid-container" data-testid="model-basics">
-      <Switch>
-        <Route
-          path="/models/:modelId/task-list/basics"
-          render={() => <BasicsContent />}
-        />
-        <Route
-          path="/models/:modelId/task-list/basics/overview"
-          render={() => <NotFound />}
-        />
-        <Route
-          path="/models/:modelId/task-list/basics/milestones"
-          render={() => <NotFound />}
-        />
-        <Route path="*" render={() => <NotFoundPartial />} />
-      </Switch>
-    </MainContent>
+    <Switch>
+      <Route
+        path="/models/:modelId/task-list/basics"
+        render={() => <BasicsContent />}
+      />
+      <Route
+        path="/models/:modelId/task-list/basics/overview"
+        render={() => <NotFound />}
+      />
+      <Route
+        path="/models/:modelId/task-list/basics/milestones"
+        render={() => <NotFound />}
+      />
+      <Route path="*" render={() => <NotFoundPartial />} />
+    </Switch>
   );
 };
 
