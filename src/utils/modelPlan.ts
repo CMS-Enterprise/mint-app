95e056496fa8fdd43a44f4d7865fa979bc5e9034
Gary Zhao <gary.j.zhao@gmail.com>
2022-04-27 15:48:42 -0400

    add translateModelCategory switchcase

--- src/utils/modelPlan.ts
+++ src/utils/modelPlan.ts
@@ -2,7 +2,7 @@ import i18next from 'i18next';
 /**
  * Translate the API enum to a human readable string
  */
-const translateTeamRole = (teamRole: string) => {
+export const translateTeamRole = (teamRole: string) => {
   switch (teamRole) {
     case 'EVALUATION':
       return i18next.t('modelPlan:teamRoles.evaluation');
@@ -19,4 +19,27 @@ const translateTeamRole = (teamRole: string) => {
   }
 };
 
-export default translateTeamRole;
+export const translateModelCategory = (category: string) => {
+  switch (category) {
+    case 'ACCOUNTABLE_CARE':
+      return i18next.t('basics:modelCategories.accountableCare');
+    case 'DEMONSTRATION':
+      return i18next.t('basics:modelCategories.demonstration');
+    case 'EPISODE_BASED_PAYMENT_INITIATIVES':
+      return i18next.t('basics:modelCategories.paymentInitiatives');
+    case 'INIT_ACCEL_DEV_AND_TEST':
+      return i18next.t('basics:modelCategories.devAndTest');
+    case 'INIT_MEDICAID_CHIP_POP':
+      return i18next.t('basics:modelCategories.chipPop');
+    case 'INIT_SPEED_ADOPT_BEST_PRACTICE':
+      return i18next.t('basics:modelCategories.speedBestPractice');
+    case 'INIT__MEDICARE_MEDICAID_ENROLLEES':
+      return i18next.t('basics:modelCategories.medicareMedicaidEnrollees');
+    case 'PRIMARY_CARE_TRANSFORMATION':
+      return i18next.t('basics:modelCategories.primaryCare');
+    case 'UNKNOWN':
+      return i18next.t('basics:modelCategories.unknown');
+    default:
+      return '';
+  }
+};
