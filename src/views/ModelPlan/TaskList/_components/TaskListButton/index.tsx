40884884a936b7da7a36cd369f457181de9c5567
Gary Zhao <gary.j.zhao@gmail.com>
2022-04-27 14:20:21 -0400

    Updated tasklistbutton to use generic button

--- src/views/ModelPlan/TaskList/_components/TaskListButton/index.tsx
+++ src/views/ModelPlan/TaskList/_components/TaskListButton/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useHistory, useParams } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { Button } from '@trussworks/react-uswds';
 
-import UswdsReactLink from 'components/LinkWrapper';
+import CreatePlanBasics from 'queries/CreatePlanBasics';
+import { CreatePlanBasics as CreatePlanBasicsType } from 'queries/types/CreatePlanBasics';
 
 type TaskListButtonProps = {
   path: string;
@@ -10,21 +14,44 @@ type TaskListButtonProps = {
 
 const TaskListButton = ({ path, status }: TaskListButtonProps) => {
   const { t } = useTranslation('modelPlanTaskList');
+  const { modelId } = useParams<{ modelId: string }>();
+  const history = useHistory();
+  const [create] = useMutation<CreatePlanBasicsType>(CreatePlanBasics);
+
+  const handleCreatePlanBasics = () => {
+    // TODO: figure out how to handle if plan basics already exists
+    create({
+      variables: {
+        input: {
+          modelPlanID: modelId
+        }
+      }
+    }).then(response => {
+      if (!response.errors) {
+        history.push(`task-list/${path}`);
+      }
+    });
+  };
 
   if (status === 'CANNOT_START') {
     return <></>;
   }
 
   return (
-    <UswdsReactLink
-      className="usa-button"
-      variant="unstyled"
-      to={`task-list/${path}`}
-    >
+    // <UswdsReactLink
+    //   className="usa-button"
+    //   variant="unstyled"
+    //   to={`task-list/${path}`}
+    // >
+    // {status === 'READY'
+    //   ? t('taskListButton.start')
+    //   : t('taskListButton.continue')}
+    // </UswdsReactLink>
+    <Button type="button" onClick={handleCreatePlanBasics}>
       {status === 'READY'
         ? t('taskListButton.start')
         : t('taskListButton.continue')}
-    </UswdsReactLink>
+    </Button>
   );
 };
 
