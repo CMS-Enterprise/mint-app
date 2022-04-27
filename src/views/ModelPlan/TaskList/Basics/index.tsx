b21eee09c9eadf6c175c6fb2cee1e5edf0789cb7
Gary Zhao <gary.j.zhao@gmail.com>
2022-04-27 14:19:59 -0400

    WIP

--- src/views/ModelPlan/TaskList/Basics/index.tsx
+++ src/views/ModelPlan/TaskList/Basics/index.tsx
@@ -4,13 +4,17 @@ import { Route, Switch } from 'react-router-dom';
 import MainContent from 'components/MainContent';
 import NotFound, { NotFoundPartial } from 'views/NotFound';
 
+const BasicsContent = () => {
+  return <h1>hello world</h1>;
+};
+
 export const Basics = () => {
   return (
     <MainContent className="grid-container" data-testid="model-basics">
       <Switch>
         <Route
-          path="/models/:modelId/task-list/basics/info"
-          render={() => <NotFound />}
+          path="/models/:modelId/task-list/basics"
+          render={() => <BasicsContent />}
         />
         <Route
           path="/models/:modelId/task-list/basics/overview"
