1be22036efd21dbbf9bae8c5538b7c55362fe518
Patrick Segura <patrick.segura@oddball.io>
2022-04-28 11:38:54 -0400

    Removed adding of user when model plan created, updated unit test

--- src/views/ModelPlan/TaskList/_components/TaskListSideNav/index.test.tsx
+++ src/views/ModelPlan/TaskList/_components/TaskListSideNav/index.test.tsx
@@ -1,83 +1,27 @@
 import React from 'react';
-import { Button } from '@trussworks/react-uswds';
-import { shallow } from 'enzyme';
-
-// TODO:  Replace all Enzyme test suites with RTL
-import Modal from 'components/Modal';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { MockedProvider } from '@apollo/client/testing';
+import { render, waitFor } from '@testing-library/react';
 
 import TaskListSideNav from './index';
 
-const renderComponent = () => {
-  return shallow(<TaskListSideNav />);
-};
-
 describe('The TaskListSideNavActions', () => {
-  it('renders without crashing', () => {
-    expect(renderComponent).not.toThrow();
-  });
-
-  describe('save and exit', () => {
-    it('displays text', () => {
-      const component = renderComponent();
-      expect(
-        component.find('UswdsReactLink').at(0).dive().prop('children')
-      ).toEqual('Save & Exit');
-    });
-
-    it('goes to home', () => {
-      const component = renderComponent();
-      expect(component.find('UswdsReactLink').at(0).prop('to')).toEqual('/');
-    });
-  });
-
-  describe('remove your Model Plan', () => {
-    it('displays text', () => {
-      const component = renderComponent();
-      expect(component.find('.test-withdraw-request').dive().text()).toEqual(
-        'Remove your Model Plan'
-      );
-    });
-    it('has a closed modal by default', () => {
-      const component = renderComponent();
-      expect(component.find(Modal).prop('isOpen')).toEqual(false);
-    });
-    it('opens a modal', () => {
-      const component = renderComponent();
-      component.find('.test-withdraw-request').simulate('click');
-      expect(component.find(Modal).prop('isOpen')).toEqual(true);
-    });
-    it('has 2 buttons in the modal', () => {
-      const component = renderComponent();
-      component.find('.test-withdraw-request').simulate('click');
-      expect(component.find(Modal).find(Button).length).toEqual(2);
-    });
-  });
-
-  describe('Related Content', () => {
-    it('displays h4', () => {
-      const component = renderComponent();
-      expect(component.find('h4').text()).toEqual('Related Content');
-    });
-
-    describe('overview for adding a system', () => {
-      it('displays translation prop key', () => {
-        const component = renderComponent();
-        expect(component.find('Trans').prop('i18nKey')).toContain(
-          'modelPlanTaskList:sideNav.overview'
-        );
-      });
-
-      it('goes to home for now', () => {
-        const component = renderComponent();
-        expect(component.find('UswdsReactLink').at(1).prop('to')).toEqual('/');
-      });
-
-      it('opens in a new tab', () => {
-        const component = renderComponent();
-        expect(component.find('UswdsReactLink').at(1).prop('target')).toEqual(
-          '_blank'
-        );
-      });
+  it('matches snapshot', async () => {
+    const { asFragment } = render(
+      <MemoryRouter
+        initialEntries={[
+          'models/new-plan/f11eb129-2c80-4080-9440-439cbe1a286f/task-list'
+        ]}
+      >
+        <MockedProvider>
+          <Route path="models/new-plan/:modelId/task-list">
+            <TaskListSideNav />
+          </Route>
+        </MockedProvider>
+      </MemoryRouter>
+    );
+    await waitFor(() => {
+      expect(asFragment()).toMatchSnapshot();
     });
   });
 });
