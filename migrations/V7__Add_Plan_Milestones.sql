aa2226df4587743625cde3704ca91b9c4efe0a35
Steven Wade <steven.wade@oddball.io>
2022-05-02 16:01:58 -0700

    update plan milestones table to add missing fields according to FIGMA

--- migrations/V7__Add_Plan_Milestones.sql
+++ migrations/V7__Add_Plan_Milestones.sql
@@ -2,19 +2,34 @@ CREATE TABLE plan_milestones (
     id UUID PRIMARY KEY,
     model_plan_id UUID NOT NULL UNIQUE, --foreign key to model plan
 
-    enter_cms_clearance TIMESTAMP WITH TIME ZONE NULL,
+    complete_icip TIMESTAMP WITH TIME ZONE NULL,
+    clearance_starts TIMESTAMP WITH TIME ZONE NULL,
+    clearance_ends TIMESTAMP WITH TIME ZONE NULL,
     enter_hhs_omb_clearance TIMESTAMP WITH TIME ZONE NULL,
     cleared TIMESTAMP WITH TIME ZONE NULL,
     announced TIMESTAMP WITH TIME ZONE NULL,
-    applications_due TIMESTAMP WITH TIME ZONE NULL,
-    participants_announced TIMESTAMP WITH TIME ZONE NULL,
+
+
+    applications_starts TIMESTAMP WITH TIME ZONE NULL,
+    applications_ends TIMESTAMP WITH TIME ZONE NULL,
+
+    -- participants_announced TIMESTAMP WITH TIME ZONE NULL, --No spot in the FIGMA for this?
+
     performance_period_starts TIMESTAMP WITH TIME ZONE NULL,
     performance_period_ends TIMESTAMP WITH TIME ZONE NULL,
+
+    wrap_up_ends TIMESTAMP WITH TIME ZONE NULL,
+    high_level_note TEXT,
+
+    phased_in BOOLEAN,
+    phased_in_note BOOLEAN,
+
 
     created_by EUA_ID,
     created_dts TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modified_by EUA_ID,
-    modified_dts TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+    modified_dts TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    status TASK_STATUS NOT NULL DEFAULT 'READY'
 
 );
 
