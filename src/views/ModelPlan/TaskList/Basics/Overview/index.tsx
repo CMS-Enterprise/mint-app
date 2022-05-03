02234a81ae524e260ecca05bfdf61ffa1f2c9c9c
Gary Zhao <gary.j.zhao@gmail.com>
2022-05-03 11:11:44 -0400

    overview steps init

--- src/views/ModelPlan/TaskList/Basics/Overview/index.tsx
+++ src/views/ModelPlan/TaskList/Basics/Overview/index.tsx
@@ -1,7 +1,65 @@
 import React from 'react';
+import { Trans, useTranslation } from 'react-i18next';
+import { Link, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import {
+  Breadcrumb,
+  BreadcrumbBar,
+  BreadcrumbLink
+} from '@trussworks/react-uswds';
+
+import MainContent from 'components/MainContent';
+import PageHeading from 'components/PageHeading';
+import GetModelPlanQuery from 'queries/GetModelPlanQuery';
+import {
+  GetModelPlan,
+  GetModelPlanVariables
+} from 'queries/types/GetModelPlan';
 
 const Overview = () => {
-  return <h1>Tasklist Overview temp page</h1>;
+  const { t } = useTranslation('basics');
+  const { t: h } = useTranslation('draftModelPlan');
+  const { modelId } = useParams<{ modelId: string }>();
+
+  const { data } = useQuery<GetModelPlan, GetModelPlanVariables>(
+    GetModelPlanQuery,
+    {
+      variables: {
+        id: modelId
+      }
+    }
+  );
+
+  const { modelName } = data?.modelPlan || {};
+
+  return (
+    <MainContent className="margin-bottom-5">
+      <div className="grid-container">
+        <div className="tablet:grid-col-12">
+          <BreadcrumbBar variant="wrap">
+            <Breadcrumb>
+              <BreadcrumbLink asCustom={Link} to="/">
+                <span>{h('home')}</span>
+              </BreadcrumbLink>
+            </Breadcrumb>
+            <Breadcrumb current>{t('breadcrumb')}</Breadcrumb>
+          </BreadcrumbBar>
+          <PageHeading className="margin-bottom-1">{t('heading')}</PageHeading>
+          <p
+            className="margin-top-0 margin-bottom-1 font-body-lg"
+            data-testid="model-plan-name"
+          >
+            <Trans i18nKey="modelPlanTaskList:subheading">
+              indexZero {modelName} indexTwo
+            </Trans>
+          </p>
+          <p className="margin-bottom-2 font-body-md line-height-sans-4">
+            {t('helpText')}
+          </p>
+        </div>
+      </div>
+    </MainContent>
+  );
 };
 
 export default Overview;
