5f4d0e348778ce7ad2d04b4fb5d8dd5a896d3d5b
Gary Zhao <gary.j.zhao@gmail.com>
2022-05-03 11:18:48 -0400

    basic form init

--- src/views/ModelPlan/TaskList/Basics/Overview/index.tsx
+++ src/views/ModelPlan/TaskList/Basics/Overview/index.tsx
@@ -5,16 +5,26 @@ import { useQuery } from '@apollo/client';
 import {
   Breadcrumb,
   BreadcrumbBar,
-  BreadcrumbLink
+  BreadcrumbLink,
+  Button,
+  Label,
+  TextInput
 } from '@trussworks/react-uswds';
+import { Field, Form, Formik, FormikProps } from 'formik';
 
 import MainContent from 'components/MainContent';
 import PageHeading from 'components/PageHeading';
+import Alert from 'components/shared/Alert';
+import { ErrorAlert, ErrorAlertMessage } from 'components/shared/ErrorAlert';
+import FieldErrorMsg from 'components/shared/FieldErrorMsg';
+import FieldGroup from 'components/shared/FieldGroup';
 import GetModelPlanQuery from 'queries/GetModelPlanQuery';
 import {
   GetModelPlan,
   GetModelPlanVariables
 } from 'queries/types/GetModelPlan';
+import flattenErrors from 'utils/flattenErrors';
+import planBasicsSchema from 'validations/planBasics';
 
 const Overview = () => {
   const { t } = useTranslation('basics');
@@ -31,6 +41,10 @@ const Overview = () => {
   );
 
   const { modelName } = data?.modelPlan || {};
+
+  const initialValues = {
+    modelName: modelName as string
+  };
 
   return (
     <MainContent className="margin-bottom-5">
@@ -56,6 +70,103 @@ const Overview = () => {
           <p className="margin-bottom-2 font-body-md line-height-sans-4">
             {t('helpText')}
           </p>
+
+          <Alert
+            type="info"
+            slim
+            data-testid="mandatory-fields-alert"
+            className="margin-bottom-4"
+          >
+            <span className="mandatory-fields-alert__text">
+              {h('mandatoryFields')}
+            </span>
+          </Alert>
+
+          <Formik
+            // TODO: change intial value of model name of plan via gql
+            initialValues={initialValues}
+            onSubmit={values => {
+              console.log(values);
+            }}
+            enableReinitialize
+            validationSchema={planBasicsSchema.garyTest}
+            validateOnBlur={false}
+            validateOnChange={false}
+            validateOnMount={false}
+          >
+            {(
+              formikProps: FormikProps<{
+                modelName: string;
+              }>
+            ) => {
+              const {
+                dirty,
+                errors,
+                handleSubmit,
+                setErrors,
+                setFieldValue,
+                values
+              } = formikProps;
+              const flatErrors = flattenErrors(errors);
+              return (
+                <>
+                  {Object.keys(errors).length > 0 && (
+                    <ErrorAlert
+                      testId="formik-validation-errors"
+                      classNames="margin-top-3"
+                      heading="Please check and fix the following"
+                    >
+                      {Object.keys(flatErrors).map(key => {
+                        return (
+                          <ErrorAlertMessage
+                            key={`Error.${key}`}
+                            errorKey={key}
+                            message={flatErrors[key]}
+                          />
+                        );
+                      })}
+                    </ErrorAlert>
+                  )}
+                  <Form
+                    onSubmit={e => {
+                      handleSubmit(e);
+                      window.scrollTo(0, 0);
+                    }}
+                  >
+                    <FieldGroup
+                      scrollElement="modelName"
+                      error={!!flatErrors.modelName}
+                      className="margin-top-4"
+                    >
+                      <Label htmlFor="plan-basics-model-name">
+                        {t('modelName')}
+                      </Label>
+                      <FieldErrorMsg>{flatErrors.modelName}</FieldErrorMsg>
+                      <Field
+                        as={TextInput}
+                        error={!!flatErrors.modelName}
+                        id="plan-basics-model-name"
+                        maxLength={50}
+                        name="modelName"
+                        defaultValue={modelName}
+                      />
+                    </FieldGroup>
+
+                    <div className="margin-top-6 margin-bottom-3">
+                      <Button
+                        type="submit"
+                        disabled={!dirty}
+                        className=""
+                        onClick={() => setErrors({})}
+                      >
+                        {h('next')}
+                      </Button>
+                    </div>
+                  </Form>
+                </>
+              );
+            }}
+          </Formik>
         </div>
       </div>
     </MainContent>
