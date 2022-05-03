2e65c622d4b58e6dad4dcb1f5bda4f5d357384e3
Gary Zhao <gary.j.zhao@gmail.com>
2022-05-03 15:19:43 -0400

    Milestone work

--- src/views/ModelPlan/TaskList/Basics/Milestones/index.tsx
+++ src/views/ModelPlan/TaskList/Basics/Milestones/index.tsx
@@ -1,7 +1,188 @@
 import React from 'react';
+import { Trans, useTranslation } from 'react-i18next';
+import { Link, useHistory, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import {
+  Breadcrumb,
+  BreadcrumbBar,
+  BreadcrumbLink,
+  Button,
+  IconArrowBack,
+  Label,
+  TextInput
+} from '@trussworks/react-uswds';
+import { Field, Form, Formik, FormikProps } from 'formik';
+
+import MainContent from 'components/MainContent';
+import PageHeading from 'components/PageHeading';
+import PageNumber from 'components/PageNumber';
+import { ErrorAlert, ErrorAlertMessage } from 'components/shared/ErrorAlert';
+import FieldErrorMsg from 'components/shared/FieldErrorMsg';
+import FieldGroup from 'components/shared/FieldGroup';
+import GetModelPlanQuery from 'queries/GetModelPlanQuery';
+import {
+  GetModelPlan,
+  GetModelPlanVariables
+} from 'queries/types/GetModelPlan';
+import flattenErrors from 'utils/flattenErrors';
+import planBasicsSchema from 'validations/planBasics';
+
+type PlanBasicsOverviewTypes = {
+  modelName: string;
+};
 
 const Milestones = () => {
-  return <div>Milestones</div>;
+  const { t } = useTranslation('basics');
+  const { t: h } = useTranslation('draftModelPlan');
+  const { modelId } = useParams<{ modelId: string }>();
+  const history = useHistory();
+
+  const { data } = useQuery<GetModelPlan, GetModelPlanVariables>(
+    GetModelPlanQuery,
+    {
+      variables: {
+        id: modelId
+      }
+    }
+  );
+
+  const { modelName } = data?.modelPlan || {};
+
+  const initialValues = {
+    modelName: modelName as string
+  };
+
+  const handleFormSubmit = (formikValues: PlanBasicsOverviewTypes) => {
+    console.log(formikValues);
+  };
+
+  return (
+    <MainContent className="margin-bottom-5">
+      <div className="grid-container">
+        <div className="tablet:grid-col-12">
+          <BreadcrumbBar variant="wrap">
+            <Breadcrumb>
+              <BreadcrumbLink asCustom={Link} to="/">
+                <span>{h('home')}</span>
+              </BreadcrumbLink>
+            </Breadcrumb>
+            <Breadcrumb current>{t('breadcrumb')}</Breadcrumb>
+          </BreadcrumbBar>
+          <PageHeading className="margin-bottom-1">{t('heading')}</PageHeading>
+          <p
+            className="margin-top-0 margin-bottom-1 font-body-lg"
+            data-testid="model-plan-name"
+          >
+            <Trans i18nKey="modelPlanTaskList:subheading">
+              indexZero {modelName} indexTwo
+            </Trans>
+          </p>
+          <p className="margin-bottom-2 font-body-md line-height-sans-4">
+            {t('helpText')}
+          </p>
+
+          <Formik
+            initialValues={initialValues}
+            onSubmit={handleFormSubmit}
+            enableReinitialize
+            validationSchema={planBasicsSchema.pageTwoSchema}
+            validateOnBlur={false}
+            validateOnChange={false}
+            validateOnMount={false}
+          >
+            {(formikProps: FormikProps<PlanBasicsOverviewTypes>) => {
+              const {
+                dirty,
+                errors,
+                handleSubmit,
+                setErrors,
+                values
+              } = formikProps;
+              const flatErrors = flattenErrors(errors);
+              return (
+                <>
+                  {Object.keys(errors).length > 0 && (
+                    <ErrorAlert
+                      testId="formik-validation-errors"
+                      classNames="margin-top-3"
+                      heading="Please check and fix the following"
+                    >
+                      {Object.keys(flatErrors).map(key => {
+                        return (
+                          <ErrorAlertMessage
+                            key={`Error.${key}`}
+                            errorKey={key}
+                            message={flatErrors[key]}
+                          />
+                        );
+                      })}
+                    </ErrorAlert>
+                  )}
+                  <Form
+                    className="tablet:grid-col-6"
+                    onSubmit={e => {
+                      handleSubmit(e);
+                      window.scrollTo(0, 0);
+                    }}
+                  >
+                    <FieldGroup
+                      scrollElement="modelName"
+                      error={!!flatErrors.modelName}
+                      className="margin-top-4"
+                    >
+                      <Label htmlFor="plan-basics-model-name">
+                        {t('modelName')}
+                      </Label>
+                      <FieldErrorMsg>{flatErrors.modelName}</FieldErrorMsg>
+                      <Field
+                        as={TextInput}
+                        error={!!flatErrors.modelName}
+                        id="plan-basics-model-name"
+                        maxLength={50}
+                        name="modelName"
+                        defaultValue={modelName}
+                      />
+                    </FieldGroup>
+
+                    <div className="margin-top-6 margin-bottom-3">
+                      <Button
+                        type="button"
+                        className="usa-button usa-button--outline margin-bottom-1"
+                        onClick={() => history.goBack()}
+                      >
+                        {h('back')}
+                      </Button>
+                      <Button
+                        type="submit"
+                        disabled={!dirty}
+                        className=""
+                        onClick={() => setErrors({})}
+                      >
+                        {h('next')}
+                      </Button>
+                    </div>
+                  </Form>
+                </>
+              );
+            }}
+          </Formik>
+        </div>
+        {/* //TODO: To implement a save function */}
+        <Link
+          to={`/models/${modelId}/task-list/`}
+          className="display-flex flex-align-center margin-bottom-6"
+        >
+          <IconArrowBack className="margin-right-1" aria-hidden />
+          {h('saveAndReturn')}
+        </Link>
+        <PageNumber
+          currentPage={3}
+          totalPages={3}
+          className="margin-bottom-10"
+        />
+      </div>
+    </MainContent>
+  );
 };
 
 export default Milestones;
