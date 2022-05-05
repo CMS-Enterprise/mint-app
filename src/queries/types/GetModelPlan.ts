91a9d25dab8d80e1e5b2580ede5c4c730cc9363a
Gary Zhao <gary.j.zhao@gmail.com>
2022-05-05 16:00:24 -0400

    auto gen

--- src/queries/types/GetModelPlan.ts
+++ src/queries/types/GetModelPlan.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { TaskStatus } from "./../../types/graphql-global-types";
+import { ModelCategory, CMSCenter, CMMIGroup, TaskStatus } from "./../../types/graphql-global-types";
 
 // ====================================================
 // GraphQL query operation: GetModelPlan
@@ -18,6 +18,19 @@ export interface GetModelPlan_modelPlan_basics {
 export interface GetModelPlan_modelPlan_milestones {
   __typename: "PlanMilestones";
   id: UUID | null;
+  modelPlanID: UUID | null;
+  completeICIP: Time | null;
+  clearanceStarts: Time | null;
+  clearanceEnds: Time | null;
+  announced: Time | null;
+  applicationsStart: Time | null;
+  applicationsEnd: Time | null;
+  performancePeriodStarts: Time | null;
+  performancePeriodEnds: Time | null;
+  highLevelNote: string | null;
+  wrapUpEnds: Time | null;
+  phasedIn: boolean | null;
+  phasedInNote: string | null;
   status: TaskStatus | null;
 }
 
@@ -25,6 +38,10 @@ export interface GetModelPlan_modelPlan {
   __typename: "ModelPlan";
   id: UUID | null;
   modelName: string | null;
+  modelCategory: ModelCategory | null;
+  cmsCenters: CMSCenter[] | null;
+  cmmiGroups: CMMIGroup[] | null;
+  cmsOther: string | null;
   modifiedDts: Time | null;
   basics: GetModelPlan_modelPlan_basics | null;
   milestones: GetModelPlan_modelPlan_milestones | null;
