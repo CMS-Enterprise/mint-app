75415de8bad7477b016031939b97bee644e385b3
Gary Zhao <gary.j.zhao@gmail.com>
2022-05-17 11:49:40 -0400

    Deleted createPlanBasics and createPlanMilestones

--- src/queries/CreatePlanBasics.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from '@apollo/client';
-
-export default gql`
-  mutation CreatePlanBasics($input: PlanBasicsInput!) {
-    createPlanBasics(input: $input) {
-      id
-    }
-  }
-`;
