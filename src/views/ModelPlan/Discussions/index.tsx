302187247bce86291bf7d926807135ce7f5a66ff
Patrick Segura <patrick.segura@oddball.io>
2022-05-18 12:40:26 -0400

    Began work for asking a question

--- src/views/ModelPlan/Discussions/index.tsx
+++ src/views/ModelPlan/Discussions/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, ReactNodeArray } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import ReactModal from 'react-modal';
 import {
@@ -6,12 +6,18 @@ import {
   Grid,
   GridContainer,
   IconAnnouncement,
-  IconClose
+  IconClose,
+  Textarea
 } from '@trussworks/react-uswds';
+import { Field, Form, Formik, FormikProps } from 'formik';
 import noScroll from 'no-scroll';
 
 import PageHeading from 'components/PageHeading';
+import { ErrorAlert, ErrorAlertMessage } from 'components/shared/ErrorAlert';
+import FieldErrorMsg from 'components/shared/FieldErrorMsg';
+import FieldGroup from 'components/shared/FieldGroup';
 import { GetModelPlan_modelPlan_discussions as DiscussionType } from 'queries/types/GetModelPlan';
+import flattenErrors from 'utils/flattenErrors';
 
 import './index.scss';
 
@@ -40,7 +46,85 @@ const Discussions = ({
   };
 
   const renderQuestion = () => {
-    return <></>;
+    return (
+      <>
+        {' '}
+        <PageHeading headingLevel="h1" className="margin-top-0">
+          {t('askAQuestion')}
+        </PageHeading>
+        <Formik
+          initialValues={{ content: '' }}
+          onSubmit={handleCreateDraftModelPlan}
+          validationSchema={NewModelPlanValidationSchema}
+          validateOnBlur={false}
+          validateOnChange={false}
+          validateOnMount={false}
+        >
+          {(formikProps: FormikProps<{ content: string }>) => {
+            const { errors, setErrors, handleSubmit, dirty } = formikProps;
+            const flatErrors = flattenErrors(errors);
+            return (
+              <>
+                {Object.keys(errors).length > 0 && (
+                  <ErrorAlert
+                    testId="formik-validation-errors"
+                    classNames="margin-top-3"
+                    heading="Please check and fix the following"
+                  >
+                    {Object.keys(flatErrors).map(key => {
+                      return (
+                        <ErrorAlertMessage
+                          key={`Error.${key}`}
+                          errorKey={key}
+                          message={flatErrors[key]}
+                        />
+                      );
+                    })}
+                  </ErrorAlert>
+                )}
+                <Form
+                  onSubmit={e => {
+                    handleSubmit(e);
+                    window.scrollTo(0, 0);
+                  }}
+                >
+                  <FieldGroup
+                    scrollElement="modelName"
+                    error={!!flatErrors.modelName}
+                  >
+                    <Label htmlFor="new-plan-model-name">{t('modeName')}</Label>
+                    <FieldErrorMsg>{flatErrors.modelName}</FieldErrorMsg>
+                    <Field
+                      as={Textarea}
+                      error={!!flatErrors.modelName}
+                      id="new-plan-model-name"
+                      maxLength={50}
+                      name="modelName"
+                    />
+                  </FieldGroup>
+                  <div className="margin-top-5 display-block">
+                    <UswdsReactLink
+                      className="usa-button usa-button--outline margin-bottom-1"
+                      variant="unstyled"
+                      to="/models/steps-overview"
+                    >
+                      {h('cancel')}
+                    </UswdsReactLink>
+                    <Button
+                      type="submit"
+                      disabled={!dirty}
+                      onClick={() => setErrors({})}
+                    >
+                      {h('next')}
+                    </Button>
+                  </div>
+                </Form>
+              </>
+            );
+          }}
+        </Formik>
+      </>
+    );
   };
 
   const renderDiscussions = () => {
