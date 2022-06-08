15e89e4453d76254ef2e2ab0b7d115cad4de8b7e
Steven Wade <steven.wade@oddball.io>
2022-06-08 10:01:20 -0700

    implement  base model

--- pkg/models/plan_ops_eval_and_learning.go
+++ pkg/models/plan_ops_eval_and_learning.go
@@ -178,3 +178,40 @@ const (
 	DataFullTime    DataFullTimeOrIncrementalType = "FULL_TIME"
 	DataIncremental DataFullTimeOrIncrementalType = "INCREMENTAL"
 )
+
+// CalcStatus returns a TaskStatus based on how many fields have been entered in the PlanOpsEvalAndLearning struct
+func (oel *PlanOpsEvalAndLearning) CalcStatus() error {
+
+	status, err := GenericallyCalculateStatus(*oel)
+	if err != nil {
+		return err
+	}
+
+	oel.Status = status
+	return nil
+}
+
+// GetModelTypeName returns the name of the model
+func (oel PlanOpsEvalAndLearning) GetModelTypeName() string {
+	return "Plan_Ops_Eval_And_Learning"
+}
+
+// GetID returns the ID property for a PlanOpsEvalAndLearning struct
+func (oel PlanOpsEvalAndLearning) GetID() uuid.UUID {
+	return oel.ID
+}
+
+// GetPlanID returns the ModelPlanID property for a PlanOpsEvalAndLearning struct
+func (oel PlanOpsEvalAndLearning) GetPlanID() uuid.UUID {
+	return oel.ModelPlanID
+}
+
+// GetModifiedBy returns the ModifiedBy property for a PlanOpsEvalAndLearning struct
+func (oel PlanOpsEvalAndLearning) GetModifiedBy() *string {
+	return oel.ModifiedBy
+}
+
+// GetCreatedBy returns the ModifiedBy property for a PlanOpsEvalAndLearning struct
+func (oel PlanOpsEvalAndLearning) GetCreatedBy() string {
+	return oel.CreatedBy
+}
