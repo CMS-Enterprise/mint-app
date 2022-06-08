7791ae32cb19a18c2272cc4ddfa569e7c88f8c6f
Steven Wade <99294987+StevenWadeOddball@users.noreply.github.com>
2022-06-08 07:48:53 -0700

    Easi 2096/existing models (#107)
    * inital exisitng model table and data. 2 records both had 99 for Pkey, had to add a 1 and 2 to the end
    
    * Exisitng Model store and GQL
    
    * add key characteristics note field for general characteristics
    
    * existing model unit test

---
+++ pkg/storage/SQL/existing_model_collection_get.sql
@@ -0,0 +1,21 @@
+SELECT
+    id,
+    model_name,
+    stage,
+    number_of_participants,
+    category,
+    authority,
+    description,
+    number_of_beneficiaries_impacted,
+    number_of_physicians_impacted,
+    date_began,
+    date_ended,
+    states,
+    keywords,
+    url,
+    display_model_summary,
+    created_by,
+    created_dts,
+    modified_by,
+    modified_dts
+FROM existing_model;
