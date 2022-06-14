68daf0e7c74d1565701a2cfcb26abae0b3412250
Steven Wade <steven.wade@oddball.io>
2022-06-14 14:09:37 -0700

    remove _TOOL_ from all fields

--- migrations/V17__Add_IT_Tools.sql
+++ migrations/V17__Add_IT_Tools.sql
@@ -7,70 +7,70 @@ CREATE TYPE TOOLS_UPDATE_CONTRACT_TYPE AS ENUM (
     'OTHER'
 );
 --PAGE 2
-CREATE TYPE PP_TOOLS_TO_ADVERTISE_TYPE AS ENUM (
+CREATE TYPE PP_TO_ADVERTISE_TYPE AS ENUM (
     'SALESFORCE',
     'GRANT_SOLUTIONS',
     'OTHER'
 );
-CREATE TYPE PP_TOOLS_COLLECT_SCORE_REVIEW_TYPE AS ENUM (
+CREATE TYPE PP_COLLECT_SCORE_REVIEW_TYPE AS ENUM (
     'RFA',
     'ARS',
     'GRANT_SOLUTIONS',
     'OTHER'
 );
-CREATE TYPE PP_TOOLS_APP_SUPPORT_CONTRACTOR_TYPE AS ENUM (
+CREATE TYPE PP_APP_SUPPORT_CONTRACTOR_TYPE AS ENUM (
     'RMDA',
     'OTHER'
 );
 --PAGE 3
-CREATE TYPE PP_TOOLS_COMMUNICATE_WITH_PARTICIPANT_TYPE AS ENUM (
+CREATE TYPE PP_COMMUNICATE_WITH_PARTICIPANT_TYPE AS ENUM (
     'OUTLOOK_MAILBOX',
     'GOV_DELIVERY',
     'SALESFORCE_PORTAL',
     'OTHER'
 );
-CREATE TYPE PP_TOOLS_MANAGE_PROVIDER_OVERLAP_TYPE AS ENUM (
+CREATE TYPE PP_MANAGE_PROVIDER_OVERLAP_TYPE AS ENUM (
     'MDM',
     'OTHER',
     'NA'
 );
-CREATE TYPE B_TOOLS_MANAGE_BENEFICIARY_OVERLAP_TYPE AS ENUM ( -- NOTE THIS IS THE SAME AS THE ABOVE TYPE, should we combine to one type?
+CREATE TYPE B_MANAGE_BENEFICIARY_OVERLAP_TYPE AS ENUM ( -- NOTE THIS IS THE SAME AS THE ABOVE TYPE, should we combine to one type?
     'MDM',
     'OTHER',
     'NA'
 );
 --PAGE 4
-CREATE TYPE OEL_TOOLS_WORKING_AGREEMENT_TYPE AS ENUM (
+CREATE TYPE OEL_WORKING_AGREEMENT_TYPE AS ENUM (
     'IAA',
     'OTHER'
 );
-CREATE TYPE OEL_TOOLS_HELPDESK_SUPPORT_TYPE AS ENUM (
+CREATE TYPE OEL_HELPDESK_SUPPORT_TYPE AS ENUM (
     'CBOSC',
     'CONTRACTOR',
     'OTHER'
 );
-CREATE TYPE OEL_TOOLS_MANAGE_ACO_TYPE AS ENUM ( --This might already exist in the other section.. --> IDDOC_OPERATIONS_TYPE
+CREATE TYPE OEL_MANAGE_ACO_TYPE AS ENUM ( --This might already exist in the other section.. --> IDDOC_OPERATIONS_TYPE
     'ACO_OS',
     'ACO_UI',
     'INNOVATION',
     'OTHER'
 );
 --PAGE 5
-CREATE TYPE OEL_TOOLS_PERFORMANCE_BENCHMARK_TYPE AS ENUM ( --IS this already created somewhere?
+CREATE TYPE OEL_PERFORMANCE_BENCHMARK_TYPE AS ENUM ( --IS this already created somewhere?
     'IDR',
     'CCW',
     'OTHER'
 );
-CREATE TYPE OEL_TOOLS_PROCESS_APPEALS_TYPE AS ENUM (
+CREATE TYPE OEL_PROCESS_APPEALS_TYPE AS ENUM (
     'MEDICARE_APPEAL_SYSTEM',
     'OTHER'
 );
-CREATE TYPE OEL_TOOLS_EVALUATION_CONTRACTOR_TYPE AS ENUM ( --This might be repeated elsewhere
+CREATE TYPE OEL_EVALUATION_CONTRACTOR_TYPE AS ENUM ( --This might be repeated elsewhere
     'RMDA',
     'OTHER'
 );
 --PAGE 6
-CREATE TYPE OEL_TOOLS_COLLECT_DATA_TYPE AS ENUM ( --Check not used elsewhere
+CREATE TYPE OEL_COLLECT_DATA_TYPE AS ENUM ( --Check not used elsewhere
     'IDR',
     'CCW',
     'IDOS',
@@ -78,65 +78,65 @@ CREATE TYPE OEL_TOOLS_COLLECT_DATA_TYPE AS ENUM ( --Check not used elsewhere
     'CONTRACTOR',
     'OTHER'
 );
-CREATE TYPE OEL_TOOLS_OBTAIN_DATA_TYPE AS ENUM ( --Check not used elsewhere
+CREATE TYPE OEL_OBTAIN_DATA_TYPE AS ENUM ( --Check not used elsewhere
     'CCW',
     'IDOS',
     'ISP',
     'OTHER'
 );
-CREATE TYPE OEL_TOOLS_CLAIMS_BASED_MEASURES_TYPE AS ENUM (
+CREATE TYPE OEL_CLAIMS_BASED_MEASURES_TYPE AS ENUM (
     'IDR',
     'CCW',
     'OTHER'
 );
 --PAGE 7
-CREATE TYPE OEL_TOOLS_QUALITY_SCORES_TYPE AS ENUM (
+CREATE TYPE OEL_QUALITY_SCORES_TYPE AS ENUM (
     'EXISTING_DATA_AND_PROCESS',
     'NEW_DATA_AND_CMMI_PROCESS',
     'OTHER_NEW_PROCESS',
     'NONE'
 );
-CREATE TYPE OEL_TOOLS_SEND_REPORTS_TYPE AS ENUM (
+CREATE TYPE OEL_SEND_REPORTS_TYPE AS ENUM (
     'IDOS',
     'RMADA',
     'INTERNAL_STAFF',
     'OTHER'
 );
-CREATE TYPE OEL_TOOLS_LEARNING_CONTRACTOR_TYPE AS ENUM (
+CREATE TYPE OEL_LEARNING_CONTRACTOR_TYPE AS ENUM (
     'RMADA',
     'CROSS_MODEL_CONTRACT',
     'OTHER'
 );
 --PAGE 8
-CREATE TYPE OEL_TOOLS_PARTICIPANT_COLLABORATION_TYPE AS ENUM (
+CREATE TYPE OEL_PARTICIPANT_COLLABORATION_TYPE AS ENUM (
     'CONNECT',
     'OTHER'
 );
-CREATE TYPE OEL_TOOLS_EDUCATE_BENEFICIARIES_TYPE AS ENUM (
+CREATE TYPE OEL_EDUCATE_BENEFICIARIES_TYPE AS ENUM (
     'OC',
     'OTHER'
 );
-CREATE TYPE P_TOOLS_MAKE_CLAIMS_PAYMENTS_TYPE AS ENUM (
+CREATE TYPE P_MAKE_CLAIMS_PAYMENTS_TYPE AS ENUM (
     'SHARED_SYSTEMS',
     'HIGLAS',
     'OTHER'
 );
-CREATE TYPE P_TOOLS_INFORM_FFS_TYPE AS ENUM (
+CREATE TYPE P_INFORM_FFS_TYPE AS ENUM (
     'FFS_COMPETENCY_CENTER',
     'OTHER'
 );
-CREATE TYPE P_TOOLS_NON_CLAIMS_BASED_PAYMENTS_TYPE AS ENUM (
+CREATE TYPE P_NON_CLAIMS_BASED_PAYMENTS_TYPE AS ENUM (
     'APPS',
     'HIGLAS',
     'IPC',
     'MAC',
     'OTHER'
 );
-CREATE TYPE P_TOOLS_SHARED_SAVINGS_PLAN_TYPE AS ENUM (
+CREATE TYPE P_SHARED_SAVINGS_PLAN_TYPE AS ENUM (
     'RMADA',
     'OTHER'
 );
-CREATE TYPE P_TOOLS_RECOVER_PAYMENTS_TYPE AS ENUM (
+CREATE TYPE P_RECOVER_PAYMENTS_TYPE AS ENUM (
     'APPS',
     'IPC',
     'MAC',
@@ -146,101 +146,99 @@ CREATE TABLE it_tools (
     id UUID PRIMARY KEY NOT NULL,
     model_plan_id UUID NOT NULL UNIQUE, --foreign key to model plan
     --page 1
-    gc_tools_part_c_d TOOLS_PART_C_D_TYPE[],
-    gc_tools_part_c_d_other TEXT,
-    gc_tools_part_c_d_note TEXT,
-    gc_tools_collect_bids TOOLS_COLLECT_BIDS_TYPE[],
-    gc_tools_collect_bids_other TEXT,
-    gc_tools_collect_bids_note TEXT,
-    gc_tools_update_contract TOOLS_UPDATE_CONTRACT_TYPE[],
-    gc_tools_update_contract_other TEXT,
-    gc_tools_update_contract_note TEXT,
+    gc_part_c_d TOOLS_PART_C_D_TYPE[],
+    gc_part_c_d_other TEXT,
+    gc_part_c_d_note TEXT,
+    gc_collect_bids TOOLS_COLLECT_BIDS_TYPE[],
+    gc_collect_bids_other TEXT,
+    gc_collect_bids_note TEXT,
+    gc_update_contract TOOLS_UPDATE_CONTRACT_TYPE[],
+    gc_update_contract_other TEXT,
+    gc_update_contract_note TEXT,
     --page 2
-    pp_tools_to_advertise PP_TOOLS_TO_ADVERTISE_TYPE[],
-    pp_tools_to_advertise_other TEXT,
-    pp_tools_to_advertise_note TEXT,
-    pp_tools_collect_score_review PP_TOOLS_COLLECT_SCORE_REVIEW_TYPE[],
-    pp_tools_collect_score_review_other TEXT,
-    pp_tools_collect_score_review_note TEXT,
-    pp_tools_app_support_contractor PP_TOOLS_APP_SUPPORT_CONTRACTOR_TYPE[],
-    pp_tools_app_support_contractor_other TEXT,
-    pp_tools_app_support_contractor_note TEXT,
+    pp_to_advertise PP_TO_ADVERTISE_TYPE[],
+    pp_to_advertise_other TEXT,
+    pp_to_advertise_note TEXT,
+    pp_collect_score_review PP_COLLECT_SCORE_REVIEW_TYPE[],
+    pp_collect_score_review_other TEXT,
+    pp_collect_score_review_note TEXT,
+    pp_app_support_contractor PP_APP_SUPPORT_CONTRACTOR_TYPE[],
+    pp_app_support_contractor_other TEXT,
+    pp_app_support_contractor_note TEXT,
     --page 3
-    pp_tools_communicate_with_participant PP_TOOLS_COMMUNICATE_WITH_PARTICIPANT_TYPE[],
-    pp_tools_communicate_with_participant_other TEXT,
-    pp_tools_communicate_with_participant_note TEXT,
-    pp_tools_manage_provider_overlap PP_TOOLS_MANAGE_PROVIDER_OVERLAP_TYPE[], --ALWAYS REQUIRED
-    pp_tools_manage_provider_overlap_other TEXT
-    pp_tools_manage_provider_overlap_note TEXT
-    b_tools_manage_beneficiary_overlap B_TOOLS_MANAGE_BENEFICIARY_OVERLAP_TYPE[], --ALWAYS REQUIRED
-    b_tools_manage_beneficiary_overlap_other TEXT,
-    b_tools_manage_beneficiary_overlap_note TEXT,
+    pp_communicate_with_participant PP_COMMUNICATE_WITH_PARTICIPANT_TYPE[],
+    pp_communicate_with_participant_other TEXT,
+    pp_communicate_with_participant_note TEXT,
+    pp_manage_provider_overlap PP_MANAGE_PROVIDER_OVERLAP_TYPE[], --ALWAYS REQUIRED
+    pp_manage_provider_overlap_other TEXT
+    pp_manage_provider_overlap_note TEXT
+    b_manage_beneficiary_overlap B_MANAGE_BENEFICIARY_OVERLAP_TYPE[], --ALWAYS REQUIRED
+    b_manage_beneficiary_overlap_other TEXT,
+    b_manage_beneficiary_overlap_note TEXT,
     --page 4
-    oel_tools_working_agreement OEL_TOOLS_WORKING_AGREEMENT_TYPE[],
-    oel_tools_working_agreement_other TEXT,
-    oel_tools_working_agreement_note TEXT,
-    oel_tools_helpdesk_support OEL_TOOLS_HELPDESK_SUPPORT_TYPE[],
-    oel_tools_helpdesk_support_other TEXT,
-    oel_tools_helpdesk_support_note TEXT,
-    oel_tools_manage_aco OEL_TOOLS_MANAGE_ACO_TYPE[], --TODO, should this be IDDOC_OPERATIONS_TYPE that is used for OPS_EVAL && LEARNING?
-    oel_tools_manage_aco_other TEXT,
-    oel_tools_manage_aco_note TEXT,
+    oel_working_agreement OEL_WORKING_AGREEMENT_TYPE[],
+    oel_working_agreement_other TEXT,
+    oel_working_agreement_note TEXT,
+    oel_helpdesk_support OEL_HELPDESK_SUPPORT_TYPE[],
+    oel_helpdesk_support_other TEXT,
+    oel_helpdesk_support_note TEXT,
+    oel_manage_aco OEL_MANAGE_ACO_TYPE[], --TODO, should this be IDDOC_OPERATIONS_TYPE that is used for OPS_EVAL && LEARNING?
+    oel_manage_aco_other TEXT,
+    oel_manage_aco_note TEXT,
     --page 5
-    oel_tools_performance_benchmark OEL_TOOLS_PERFORMANCE_BENCHMARK_TYPE[],
-    oel_tools_performance_benchmark_other TEXT,
-    oel_tools_performance_benchmark_note TEXT,
-    oel_tools_process_appeals OEL_TOOLS_PROCESS_APPEALS_TYPE[],
-    oel_tools_process_appeals_other TEXT,
-    oel_tools_process_appeals_note TEXT,
-    oel_tools_evaluation_contractor OEL_TOOLS_EVALUATION_CONTRACTOR_TYPE[],
-    oel_tools_evaluation_contractor_other TEXT,
-    oel_tools_evaluation_contractor_note TEXT,
+    oel_performance_benchmark OEL_PERFORMANCE_BENCHMARK_TYPE[],
+    oel_performance_benchmark_other TEXT,
+    oel_performance_benchmark_note TEXT,
+    oel_process_appeals OEL_PROCESS_APPEALS_TYPE[],
+    oel_process_appeals_other TEXT,
+    oel_process_appeals_note TEXT,
+    oel_evaluation_contractor OEL_EVALUATION_CONTRACTOR_TYPE[],
+    oel_evaluation_contractor_other TEXT,
+    oel_evaluation_contractor_note TEXT,
     --page 6
-    oel_tools_collect_data OEL_TOOLS_COLLECT_DATA_TYPE[],
-    oel_tools_collect_data_other TEXT,
-    oel_tools_collect_data_note TEXT,
-    oel_tools_obtain_data OEL_TOOLS_OBTAIN_DATA_TYPE[],
-    oel_tools_obtain_data_other TEXT,
-    oel_tools_obtain_data_note TEXT,
-    oel_tools_claims_based_measures OEL_TOOLS_CLAIMS_BASED_MEASURES_TYPE[],
-    oel_tools_claims_based_measures_other TEXT,
-    oel_tools_claims_based_measures_note TEXT,
+    oel_collect_data OEL_COLLECT_DATA_TYPE[],
+    oel_collect_data_other TEXT,
+    oel_collect_data_note TEXT,
+    oel_obtain_data OEL_OBTAIN_DATA_TYPE[],
+    oel_obtain_data_other TEXT,
+    oel_obtain_data_note TEXT,
+    oel_claims_based_measures OEL_CLAIMS_BASED_MEASURES_TYPE[],
+    oel_claims_based_measures_other TEXT,
+    oel_claims_based_measures_note TEXT,
     --page 7
-    oel_tools_quality_scores OEL_TOOLS_QUALITY_SCORES_TYPE[],
-    oel_tools_quality_scores_other TEXT,
-    oel_tools_quality_scores_note TEXT,
-    oel_tools_send_reports OEL_TOOLS_SEND_REPORTS_TYPE[],
-    oel_tools_send_reports_other TEXT,
-    oel_tools_send_reports_note TEXT,
-    oel_tools_learning_contractor OEL_TOOLS_LEARNING_CONTRACTOR_TYPE[],
-    oel_tools_learning_contractor_other TEXT,
-    oel_tools_learning_contractor_note TEXT,
+    oel_quality_scores OEL_QUALITY_SCORES_TYPE[],
+    oel_quality_scores_other TEXT,
+    oel_quality_scores_note TEXT,
+    oel_send_reports OEL_SEND_REPORTS_TYPE[],
+    oel_send_reports_other TEXT,
+    oel_send_reports_note TEXT,
+    oel_learning_contractor OEL_LEARNING_CONTRACTOR_TYPE[],
+    oel_learning_contractor_other TEXT,
+    oel_learning_contractor_note TEXT,
     --page 8 
-    oel_tools_participant_collaboration OEL_TOOLS_PARTICIPANT_COLLABORATION_TYPE[],
-    oel_tools_participant_collaboration_other TEXT,
-    oel_tools_participant_collaboration_note TEXT,
-    oel_tools_educate_beneficiaries OEL_TOOLS_EDUCATE_BENEFICIARIES_TYPE[],
-    oel_tools_educate_beneficiaries_other TEXT,
-    oel_tools_educate_beneficiaries_note TEXT,
-    p_tools_make_claims_payments P_TOOLS_MAKE_CLAIMS_PAYMENTS_TYPE[],
-    p_tools_make_claims_payments_other text,
-    p_tools_make_claims_payments_note text,
+    oel_participant_collaboration OEL_PARTICIPANT_COLLABORATION_TYPE[],
+    oel_participant_collaboration_other TEXT,
+    oel_participant_collaboration_note TEXT,
+    oel_educate_beneficiaries OEL_EDUCATE_BENEFICIARIES_TYPE[],
+    oel_educate_beneficiaries_other TEXT,
+    oel_educate_beneficiaries_note TEXT,
+    p_make_claims_payments P_MAKE_CLAIMS_PAYMENTS_TYPE[],
+    p_make_claims_payments_other text,
+    p_make_claims_payments_note text,
     --page 9
-    p_tools_inform_ffs P_TOOLS_INFORM_FFS_TYPE[],
-    p_tools_inform_ffs_other TEXT,
-    p_tools_inform_ffs_note TEXT,
-    p_tools_non_claims_based_payments P_TOOLS_NON_CLAIMS_BASED_PAYMENTS_TYPE[],
-    p_tools_non_claims_based_payments_other TEXT,
-    p_tools_non_claims_based_payments_note TEXT,
-    p_tools_shared_savings_plan P_TOOLS_SHARED_SAVINGS_PLAN_TYPE[],
-    p_tools_shared_savings_plan_other TEXT,
-    p_tools_shared_savings_plan_note TEXT,
+    p_inform_ffs P_INFORM_FFS_TYPE[],
+    p_inform_ffs_other TEXT,
+    p_inform_ffs_note TEXT,
+    p_non_claims_based_payments P_NON_CLAIMS_BASED_PAYMENTS_TYPE[],
+    p_non_claims_based_payments_other TEXT,
+    p_non_claims_based_payments_note TEXT,
+    p_shared_savings_plan P_SHARED_SAVINGS_PLAN_TYPE[],
+    p_shared_savings_plan_other TEXT,
+    p_shared_savings_plan_note TEXT,
     --page 10
-    p_tools_recover_payments P_TOOLS_RECOVER_PAYMENTS_TYPE[],
-    p_tools_recover_payments_other TEXT,
-    p_tools_recover_payments_note TEXT,
-
-
+    p_recover_payments P_RECOVER_PAYMENTS_TYPE[],
+    p_recover_payments_other TEXT,
+    p_recover_payments_note TEXT,
 
     --META DATA
     created_by EUA_ID NOT NULL,
@@ -250,7 +248,7 @@ CREATE TABLE it_tools (
     status TASK_STATUS NOT NULL DEFAULT 'READY'
 );
 ALTER TABLE it_tools
-ADD CONSTRAINT fk_it_tools_plan FOREIGN KEY (model_plan_id)
+ADD CONSTRAINT fk_it_plan FOREIGN KEY (model_plan_id)
 REFERENCES public.model_plan (id) MATCH SIMPLE
 ON UPDATE NO ACTION
 ON DELETE NO ACTION
