4000aab6cd1c69c7e45e721c66b5d5ec7c73fb2e
Gary Zhao <gary.j.zhao@gmail.com>
2022-06-23 12:56:44 -0400

    Updated beneficiaries index to use truss components

--- src/views/ModelPlan/TaskList/Beneficiaries/index.tsx
+++ src/views/ModelPlan/TaskList/Beneficiaries/index.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { Grid, GridContainer } from '@trussworks/react-uswds';
 
 import MainContent from 'components/MainContent';
 import NotFound, { NotFoundPartial } from 'views/NotFound';
 
 export const Beneficiaries = () => {
   return (
-    <MainContent className="grid-container" data-testid="model-Beneficiaries">
-      <Switch>
-        <Route
-          path="/models/:modelID/task-list/beneficiaries/page-1" // page-* may change pending UX clarifcation
-          render={() => <NotFound />}
-        />
-        <Route path="*" render={() => <NotFoundPartial />} />
-      </Switch>
+    <MainContent data-testid="model-beneficiaries">
+      <GridContainer>
+        <Grid desktop={{ col: 12 }}>
+          <Switch>
+            <Route
+              path="/models/:modelID/task-list/beneficiaries/page-1" // page-* may change pending UX clarifcation
+              render={() => <NotFound />}
+            />
+            <Route path="*" render={() => <NotFoundPartial />} />
+          </Switch>
+        </Grid>
+      </GridContainer>
     </MainContent>
   );
 };
