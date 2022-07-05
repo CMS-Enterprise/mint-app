5a56698bb1744bc7f585f4d103d1df790fe530ac
Gary Zhao <gary.j.zhao@gmail.com>
2022-07-05 12:57:35 -0400

    tests and snapshots

---
+++ src/views/ModelPlan/TaskList/Beneficiaries/PageTwo/index.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { MockedProvider } from '@apollo/client/testing';
+import { render, screen, waitFor } from '@testing-library/react';
+
+import beneficiaryMock from '../mock';
+
+import BeneficiariesPageTwo from './index';
+
+describe('Model Plan Beneficiaries', () => {
+  it('renders without errors', async () => {
+    render(
+      <MemoryRouter
+        initialEntries={[
+          '/models/ce3405a0-3399-4e3a-88d7-3cfc613d2905/task-list/beneficiaries/page-two'
+        ]}
+      >
+        <MockedProvider mocks={beneficiaryMock} addTypename={false}>
+          <Route path="/models/:modelID/task-list/beneficiaries/page-two">
+            <BeneficiariesPageTwo />
+          </Route>
+        </MockedProvider>
+      </MemoryRouter>
+    );
+
+    await waitFor(() => {
+      expect(
+        screen.getByTestId('beneficiaries-page-two-form')
+      ).toBeInTheDocument();
+    });
+  });
+
+  it('matches snapshot', async () => {
+    const { asFragment } = render(
+      <MemoryRouter
+        initialEntries={[
+          '/models/ce3405a0-3399-4e3a-88d7-3cfc613d2905/task-list/beneficiaries/page-two'
+        ]}
+      >
+        <MockedProvider mocks={beneficiaryMock} addTypename={false}>
+          <Route path="/models/:modelID/task-list/beneficiaries/page-two">
+            <BeneficiariesPageTwo />
+          </Route>
+        </MockedProvider>
+      </MemoryRouter>
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
