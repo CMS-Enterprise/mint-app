9452c75630f4e345c63d41b72fce8f72b9b1fe85
Gary Zhao <gary.j.zhao@gmail.com>
2022-07-07 16:26:28 -0400

    initial scaffolding

--- src/views/ModelPlan/TaskList/Payment/FundingSource/index.tsx
+++ src/views/ModelPlan/TaskList/Payment/FundingSource/index.tsx
@@ -1,25 +1,45 @@
 import React, { useRef } from 'react';
-import { useTranslation } from 'react-i18next';
-import { useHistory, useParams } from 'react-router-dom';
+import { Trans, useTranslation } from 'react-i18next';
+import { Link, useHistory, useParams } from 'react-router-dom';
 import { useMutation, useQuery } from '@apollo/client';
-import { FormikProps } from 'formik';
+import {
+  Breadcrumb,
+  BreadcrumbBar,
+  BreadcrumbLink,
+  Button,
+  Grid,
+  GridContainer,
+  IconArrowBack,
+  Label
+} from '@trussworks/react-uswds';
+import { Form, Formik, FormikProps } from 'formik';
+
+import AskAQuestion from 'components/AskAQuestion';
+import PageHeading from 'components/PageHeading';
+import PageNumber from 'components/PageNumber';
+import AutoSave from 'components/shared/AutoSave';
+import { ErrorAlert, ErrorAlertMessage } from 'components/shared/ErrorAlert';
+import FieldErrorMsg from 'components/shared/FieldErrorMsg';
+import FieldGroup from 'components/shared/FieldGroup';
+import flattenErrors from 'utils/flattenErrors';
+import { NotFoundPartial } from 'views/NotFound';
 
 const FundingSource = () => {
   const { t } = useTranslation('payments');
   const { t: h } = useTranslation('draftModelPlan');
   const { modelID } = useParams<{ modelID: string }>();
 
-  // const formikRef = useRef<FormikProps<PaymentsFormType>>(null);
+  const formikRef = useRef<FormikProps<PaymentsFormType>>(null);
   const history = useHistory();
 
-  // const { data, loading, error } = useQuery<GetPerformanceType>(
-  //   GetPerformance,
-  //   {
-  //     variables: {
-  //       id: modelID
-  //     }
-  //   }
-  // );
+  const { data, loading, error } = useQuery<GetPerformanceType>(
+    GetPerformance,
+    {
+      variables: {
+        id: modelID
+      }
+    }
+  );
 
   const {
     id
@@ -32,7 +52,7 @@ const FundingSource = () => {
 
   const handleFormSubmit = (
     formikValues: PaymentsFormType,
-    redirect?: 'next' | 'back' | 'task-list'
+    redirect?: 'next' | 'back'
   ) => {
     const { id: updateId, __typename, ...changeValues } = formikValues;
     update({
@@ -44,17 +64,12 @@ const FundingSource = () => {
       .then(response => {
         if (!response?.errors) {
           if (redirect === 'next') {
-            history.push(`/models/${modelID}/task-list/payments/evaluation`);
+            // *  Claims-based payments triggers 2 extra pages of questions. Non-claims-based payments triggers 1 extra page of questions.
+            // todo: conditionally direct people if those options are selected
+
+            history.push(`/models/${modelID}/task-list/payments/page-TBD`);
           } else if (redirect === 'back') {
-            if (iddocSupport) {
-              history.push(
-                `/models/${modelID}/task-list/payments/iddoc-monitoring`
-              );
-            } else {
-              history.push(`/models/${modelID}/task-list/payments`);
-            }
-          } else if (redirect === 'task-list') {
-            history.push(`/models/${modelID}/task-list`);
+            history.push(`/models/${modelID}/task-list/`);
           }
         }
       })
@@ -63,7 +78,141 @@ const FundingSource = () => {
       });
   };
 
-  return <div>FundingSource</div>;
+  // const intialValues: PaymentsFormType = {
+  //   __typename: 'PlanPayments',
+  //   id: id ?? '',
+  //   tbc
+  // };
+
+  if ((!loading && error) || (!loading && !data?.modelPlan)) {
+    return <NotFoundPartial />;
+  }
+
+  return (
+    <>
+      <BreadcrumbBar variant="wrap">
+        <Breadcrumb>
+          <BreadcrumbLink asCustom={Link} to="/">
+            <span>{h('home')}</span>
+          </BreadcrumbLink>
+        </Breadcrumb>
+        <Breadcrumb>
+          <BreadcrumbLink asCustom={Link} to={`/models/${modelID}/task-list/`}>
+            <span>{h('tasklistBreadcrumb')}</span>
+          </BreadcrumbLink>
+        </Breadcrumb>
+        <Breadcrumb current>{t('breadcrumb')}</Breadcrumb>
+      </BreadcrumbBar>
+      <PageHeading className="margin-top-4 margin-bottom-2">
+        {t('heading')}
+      </PageHeading>
+
+      <p
+        className="margin-top-0 margin-bottom-1 font-body-lg"
+        data-testid="model-plan-name"
+      >
+        <Trans i18nKey="modelPlanTaskList:subheading">
+          indexZero {modelName} indexTwo
+        </Trans>
+      </p>
+      <p className="margin-bottom-2 font-body-md line-height-sans-4">
+        {h('helpText')}
+      </p>
+
+      <AskAQuestion modelID={modelID} />
+
+      <Formik
+        initialValues={initialValues}
+        onSubmit={values => {
+          handleFormSubmit(values, 'next');
+        }}
+        enableReinitialize
+        innerRef={formikRef}
+      >
+        {(formikProps: FormikProps<PaymentsFormType>) => {
+          const {
+            errors,
+            handleSubmit,
+            setErrors,
+            setFieldValue,
+            values
+          } = formikProps;
+          const flatErrors = flattenErrors(errors);
+          return (
+            <>
+              {Object.keys(errors).length > 0 && (
+                <ErrorAlert
+                  testId="formik-validation-errors"
+                  classNames="margin-top-3"
+                  heading={h('checkAndFix')}
+                >
+                  {Object.keys(flatErrors).map(key => {
+                    return (
+                      <ErrorAlertMessage
+                        key={`Error.${key}`}
+                        errorKey={key}
+                        message={flatErrors[key]}
+                      />
+                    );
+                  })}
+                </ErrorAlert>
+              )}
+              <GridContainer className="padding-left-0 padding-right-0">
+                <Grid row gap className="beneficiaries__info">
+                  <Grid desktop={{ col: 6 }}>
+                    <Form
+                      className="margin-top-6"
+                      data-testid="beneficiaries-identification-form"
+                      onSubmit={e => {
+                        handleSubmit(e);
+                      }}
+                    >
+                      <FieldGroup
+                        scrollElement="beneficiaries"
+                        error={!!flatErrors.beneficiaries}
+                        className="margin-top-4"
+                      >
+                        <Label htmlFor="beneficiaries-beneficiaries">
+                          {t('beneficiaries')}
+                        </Label>
+                        <FieldErrorMsg>
+                          {flatErrors.beneficiaries}
+                        </FieldErrorMsg>
+                      </FieldGroup>
+
+                      <div className="margin-top-6 margin-bottom-3">
+                        <Button type="submit" onClick={() => setErrors({})}>
+                          {h('next')}
+                        </Button>
+                      </div>
+                      <Button
+                        type="button"
+                        className="usa-button usa-button--unstyled"
+                        onClick={() => handleFormSubmit(values, 'back')}
+                      >
+                        <IconArrowBack className="margin-right-1" aria-hidden />
+                        {h('saveAndReturn')}
+                      </Button>
+                    </Form>
+                  </Grid>
+                </Grid>
+              </GridContainer>
+              {id && (
+                <AutoSave
+                  values={values}
+                  onSave={() => {
+                    handleFormSubmit(formikRef.current!.values);
+                  }}
+                  debounceDelay={3000}
+                />
+              )}
+            </>
+          );
+        }}
+      </Formik>
+      <PageNumber currentPage={1} totalPages={3} className="margin-y-6" />
+    </>
+  );
 };
 
 export default FundingSource;
