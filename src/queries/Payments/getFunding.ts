a5154f6ff77ab253bc85bff255063a1907357532
Gary Zhao <gary.j.zhao@gmail.com>
2022-07-14 14:56:19 -0400

    asdf

--- src/queries/Payments/getFunding.ts
+++ src/queries/Payments/getFunding.ts
@@ -14,7 +14,6 @@ export default gql`
         fundingSourceR
         fundingSourceRTrustFund
         fundingSourceROther
-        fundingSourceRNote
         payRecipients
         payRecipientsOtherSpecification
         payRecipientsNote
