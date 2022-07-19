80fcd83b5ed9a29dcafc45e2b860309a9189dc49
Gary Zhao <gary.j.zhao@gmail.com>
2022-07-18 22:41:23 -0400

    ClaimsBasedPayment test and snaps

---
+++ src/views/ModelPlan/TaskList/Payment/ClaimsBasedPayment/index.test.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { MockedProvider } from '@apollo/client/testing';
+import { render, screen, waitFor } from '@testing-library/react';
+
+import GetClaimsBasedPayment from 'queries/Payments/GetClaimsBasedPayment';
+import { GetClaimsBasedPayment_modelPlan_payments as GetClaimsBasedPaymentType } from 'queries/Payments/types/GetClaimsBasedPayment';
+import { ClaimsBasedPayType, PayType } from 'types/graphql-global-types';
+
+import ClaimsBasedPayment from './index';
+
+const mockData: GetClaimsBasedPaymentType = {
+  __typename: 'PlanPayments',
+  id: '123',
+  payType: [PayType.CLAIMS_BASED_PAYMENTS],
+  payClaims: [ClaimsBasedPayType.OTHER],
+  payClaimsNote: '',
+  payClaimsOther: 'pay claims other',
+  shouldAnyProvidersExcludedFFSSystems: null,
+  shouldAnyProviderExcludedFFSSystemsNote: '',
+  changesMedicarePhysicianFeeSchedule: null,
+  changesMedicarePhysicianFeeScheduleNote: '',
+  affectsMedicareSecondaryPayerClaims: null,
+  affectsMedicareSecondaryPayerClaimsHow: '',
+  affectsMedicareSecondaryPayerClaimsNote: '',
+  payModelDifferentiation: ''
+};
+
+const paymentsMock = [
+  {
+    request: {
+      query: GetClaimsBasedPayment,
+      variables: { id: 'ce3405a0-3399-4e3a-88d7-3cfc613d2905' }
+    },
+    result: {
+      data: {
+        modelPlan: {
+          id: 'ce3405a0-3399-4e3a-88d7-3cfc613d2905',
+          modelName: 'My excellent plan that I just initiated',
+          payments: mockData
+        }
+      }
+    }
+  }
+];
+
+describe('Model Plan -- Claims Based Payment', () => {
+  it('renders without errors', async () => {
+    render(
+      <MemoryRouter
+        initialEntries={[
+          '/models/ce3405a0-3399-4e3a-88d7-3cfc613d2905/task-list/payment/claims-based-payment'
+        ]}
+      >
+        <MockedProvider mocks={paymentsMock} addTypename={false}>
+          <Route path="/models/:modelID/task-list/payment/claims-based-payment">
+            <ClaimsBasedPayment />
+          </Route>
+        </MockedProvider>
+      </MemoryRouter>
+    );
+
+    await waitFor(() => {
+      expect(
+        screen.getByTestId('payment-claims-based-payment-form')
+      ).toBeInTheDocument();
+    });
+
+    await waitFor(() => {
+      expect(screen.getByTestId('payment-pay-claims-other')).toHaveValue(
+        'Other'
+      );
+    });
+  });
+
+  it('matches snapshot', async () => {
+    const { asFragment } = render(
+      <MemoryRouter
+        initialEntries={[
+          '/models/ce3405a0-3399-4e3a-88d7-3cfc613d2905/task-list/payment/claims-based-payment'
+        ]}
+      >
+        <MockedProvider mocks={paymentsMock} addTypename={false}>
+          <Route path="/models/:modelID/task-list/payment/claims-based-payment">
+            <ClaimsBasedPayment />
+          </Route>
+        </MockedProvider>
+      </MemoryRouter>
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
