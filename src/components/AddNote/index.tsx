ac26aac07051ccf6ab2524545be2bbbfcfbc5c94
Patrick Segura <95709965+patrickseguraoddball@users.noreply.github.com>
2022-07-25 10:55:49 -0700

    Easi 2069/note field updates (#157)
    * Added init exanding note functionality and updated test
    
    * Fixed unit test
    
    * Added comments and adjusted unit test
    
    * Fixed coordinateWorkNote typo
    
    * Updated legacy note implementation

--- src/components/AddNote/index.tsx
+++ src/components/AddNote/index.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button, IconAdd } from '@trussworks/react-uswds';
 import classNames from 'classnames';
-import { Field } from 'formik';
+import { Field, useField } from 'formik';
 
+import { ErrorAlertMessage } from 'components/shared/ErrorAlert';
 import FieldGroup from 'components/shared/FieldGroup';
 import TextAreaField from 'components/shared/TextAreaField';
 
@@ -13,33 +14,63 @@ type AddNoteType = {
   className?: string;
 };
 
-const AddNote = ({ field, id, className }: AddNoteType) => {
+const AddNote = ({ field: fieldName, id, className }: AddNoteType) => {
   const { t } = useTranslation('draftModelPlan');
-  const [note, setNote] = useState(false);
+
+  // Formik hook used to fetch the field value based on the field name
+  // meta isn't technically required for the component to work but ts throws errors if not instantiated alongside 'helpers'
+  const [field, meta, helpers] = useField(fieldName);
+
+  // State used to manage if an existing value is present on load
+  const [note, setNote] = useState<boolean>(!!field.value?.trim());
+
+  // State used to manage manual toggle of note
+  const [open, setOpen] = useState<boolean>(false);
+
+  // State used to manage if touched - ex: Toggle note open, types, then erases all text
+  // Should not collapse the textarea if text is erased
+  const [touched, setTouched] = useState<boolean>(false);
+
+  // Sets the note value from GQL once fetched
+  // Empty spaces characters should act as empty string
+  useEffect(() => {
+    setNote(!!field.value?.trim());
+  }, [field.value, note]);
+
+  const { setValue } = helpers;
 
   return (
     <div className={classNames('margin-top-4 margin-bottom-8', className)}>
-      <Button
-        type="button"
-        data-testid="add-note-toggle"
-        className="usa-button usa-button--unstyled"
-        onClick={() => setNote(true)}
-      >
-        <IconAdd className="margin-right-1" aria-hidden />
-        {t('additionalNote')}
-      </Button>
+      {!note && !open && !touched && (
+        <Button
+          type="button"
+          data-testid="add-note-toggle"
+          className="usa-button usa-button--unstyled"
+          onClick={() => setOpen(true)}
+        >
+          <IconAdd className="margin-right-1" aria-hidden />
+          {t('additionalNote')}
+        </Button>
+      )}
 
-      {note && (
+      {(note || open || touched) && (
         <FieldGroup>
           <Field
             as={TextAreaField}
             className="height-15"
             id={id}
             data-testid={id}
-            name={field}
+            name={fieldName}
             label={t('Notes')}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setValue(e.target.value);
+              setTouched(true);
+            }}
           />
         </FieldGroup>
+      )}
+      {meta.error && (
+        <ErrorAlertMessage errorKey={meta.error} message={meta.error} />
       )}
     </div>
   );
