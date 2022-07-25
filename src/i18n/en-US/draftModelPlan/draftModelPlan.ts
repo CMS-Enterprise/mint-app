3a8a3f2386d2ca5182ca830d158b5978d2dc34b4
Gary Zhao <gary.j.zhao@gmail.com>
2022-07-25 15:40:32 -0400

    Add new copy

--- src/i18n/en-US/draftModelPlan/draftModelPlan.ts
+++ src/i18n/en-US/draftModelPlan/draftModelPlan.ts
@@ -22,7 +22,10 @@ const draftModelPlan = {
   dateWarning:
     'You’ve entered one or more dates that are in the past. Please double-check your dates to make sure they’re accurate.',
   helpText:
-    "If there's a question or field that is not applicable to your model or you don't currently know the answer, you may leave it blank. If you need help, ask a question using the link below."
+    "If there's a question or field that is not applicable to your model or you don't currently know the answer, you may leave it blank. If you need help, ask a question using the link below.",
+  modelPlanStatus: 'Model Plan status',
+  modelPlanCopy:
+    'This section of the Model Plan ({{-sectionName}}) is ready for review.'
 };
 
 export default draftModelPlan;
