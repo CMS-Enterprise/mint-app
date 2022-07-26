b8be7a07bcb9d3d0b64f40321bc0f9d6dfa8106b
Gary Zhao <gary.j.zhao@gmail.com>
2022-07-26 00:09:55 -0400

    it works

--- src/components/ReadyForReview/index.tsx
+++ src/components/ReadyForReview/index.tsx
@@ -8,17 +8,19 @@ import FieldGroup from 'components/shared/FieldGroup';
 import { TaskStatus, TaskStatusInput } from 'types/graphql-global-types';
 
 type ReadyForReviewType = {
-  field: string;
   id: string;
+  field: string;
   sectionName: string;
   status: TaskStatus;
+  setFieldValue: (field: string, value: any) => void;
 };
 
 const ReadyForReview = ({
-  field,
   id,
+  field,
   sectionName,
-  status
+  status,
+  setFieldValue
 }: ReadyForReviewType) => {
   const { t } = useTranslation('draftModelPlan');
   return (
@@ -33,8 +35,14 @@ const ReadyForReview = ({
           label={t('modelPlanCopy', {
             sectionName: `${sectionName}`
           })}
-          value={TaskStatusInput.READY_FOR_REVIEW}
           checked={status === TaskStatus.READY_FOR_REVIEW}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            if (e.target.checked) {
+              setFieldValue('status', TaskStatusInput.READY_FOR_REVIEW);
+            } else {
+              setFieldValue('status', TaskStatusInput.IN_PROGRESS);
+            }
+          }}
         />
       </SummaryBox>
     </FieldGroup>
