541d117dbb74f470dbd24677337ebf5de4d19dbd
ClayBenson94 <clay.benson@oddball.io>
2022-07-28 09:24:25 -0400

    Bolster plan basics tests with data that used to be from milestones and model plan

--- pkg/graph/resolvers/plan_basics_test.go
+++ pkg/graph/resolvers/plan_basics_test.go
@@ -1,6 +1,8 @@
 package resolvers
 
 import (
+	"time"
+
 	"github.com/cmsgov/mint-app/pkg/models"
 )
 
@@ -18,8 +20,24 @@ func (suite *ResolverSuite) TestPlanBasicsGetByModelPlanID() {
 	suite.Nil(basics.ModelType)
 	suite.Nil(basics.Problem)
 	suite.Nil(basics.Goal)
+	suite.Nil(basics.ModelCategory)
+	suite.Nil(basics.CMSCenters)
+	suite.Nil(basics.CMSOther)
+	suite.Nil(basics.CMMIGroups)
 	suite.Nil(basics.TestInterventions)
 	suite.Nil(basics.Note)
+	suite.Nil(basics.CompleteICIP)
+	suite.Nil(basics.ClearanceStarts)
+	suite.Nil(basics.ClearanceEnds)
+	suite.Nil(basics.Announced)
+	suite.Nil(basics.ApplicationsStart)
+	suite.Nil(basics.ApplicationsEnd)
+	suite.Nil(basics.PerformancePeriodStarts)
+	suite.Nil(basics.PerformancePeriodEnds)
+	suite.Nil(basics.WrapUpEnds)
+	suite.Nil(basics.HighLevelNote)
+	suite.Nil(basics.PhasedIn)
+	suite.Nil(basics.PhasedInNote)
 }
 
 func (suite *ResolverSuite) TestUpdatePlanBasics() {
@@ -29,8 +47,14 @@ func (suite *ResolverSuite) TestUpdatePlanBasics() {
 	suite.NoError(err)
 
 	changes := map[string]interface{}{
-		"modelType": models.MTVoluntary,
-		"goal":      "Some goal",
+		"modelType":     models.MTVoluntary,
+		"goal":          "Some goal",
+		"cmsCenters":    []string{"CMMI", "OTHER"},
+		"cmsOther":      "SOME OTHER CMS CENTER",
+		"cmmiGroups":    []string{"PATIENT_CARE_MODELS_GROUP", "SEAMLESS_CARE_MODELS_GROUP"},
+		"completeICIP":  "2020-05-13T20:47:50.12Z",
+		"phasedIn":      true,
+		"highLevelNote": "Some high level note",
 	}
 	updater := "UPDT"
 
@@ -42,6 +66,12 @@ func (suite *ResolverSuite) TestUpdatePlanBasics() {
 	suite.EqualValues(models.MTVoluntary, *updatedBasics.ModelType)
 	suite.Nil(updatedBasics.Problem)
 	suite.EqualValues("Some goal", *updatedBasics.Goal)
+	suite.EqualValues(changes["cmsCenters"], updatedBasics.CMSCenters)
+	suite.EqualValues(changes["cmsOther"], *updatedBasics.CMSOther)
+	suite.EqualValues(changes["cmmiGroups"], updatedBasics.CMMIGroups)
+	suite.WithinDuration(time.Date(2020, 5, 13, 20, 47, 50, 120000000, time.UTC), *updatedBasics.CompleteICIP, 0)
+	suite.EqualValues(changes["highLevelNote"], *updatedBasics.HighLevelNote)
+	suite.EqualValues(changes["phasedIn"], *updatedBasics.PhasedIn)
 	suite.Nil(updatedBasics.TestInterventions)
 	suite.Nil(updatedBasics.Note)
 }
