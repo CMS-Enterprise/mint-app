e801054e1c866fab9c5505ca391596abe9955772
Gary Zhao <gary.j.zhao@gmail.com>
2022-08-01 13:47:04 -0400

    updated UpdatedModelPlan to have two mutations

--- src/queries/UpdateModelPlan.ts
+++ src/queries/UpdateModelPlan.ts
@@ -1,8 +1,16 @@
 import { gql } from '@apollo/client';
 
 export default gql`
-  mutation UpdateModelPlan($id: UUID!, $changes: ModelPlanChanges!) {
+  mutation UpdateModelPlan(
+    $id: UUID!
+    $changes: ModelPlanChanges!
+    $basicsId: UUID!
+    $basicsChanges: PlanBasicsChanges!
+  ) {
     updateModelPlan(id: $id, changes: $changes) {
+      id
+    }
+    updatePlanBasics(id: $basicsId, changes: $basicsChanges) {
       id
     }
   }
