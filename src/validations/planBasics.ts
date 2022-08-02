804690054061908b238c3c961366030956470be7
Gary Zhao <gary.j.zhao@gmail.com>
2022-08-02 13:20:42 -0400

    updated validations

--- src/validations/planBasics.ts
+++ src/validations/planBasics.ts
@@ -20,43 +20,55 @@ const planBasicsSchema = {
 
   pageOneSchemaWithOther: Yup.object().shape({
     modelName: Yup.string().trim().required('Enter the Model Name'),
-    modelCategory: Yup.string().nullable().required('Enter the Model Category'),
-    cmsCenters: Yup.array()
-      .min(1, 'Select a CMS Component')
-      .required('Select a CMS Component'),
-    cmsOther: Yup.string().required('Please specific CMS Component')
+    basics: Yup.object().shape({
+      modelCategory: Yup.string()
+        .nullable()
+        .required('Enter the Model Category'),
+      cmsCenters: Yup.array()
+        .min(1, 'Select a CMS Component')
+        .required('Select a CMS Component'),
+      cmsOther: Yup.string().required('Please specific CMS Component')
+    })
   }),
 
   pageOneSchemaWithOtherAndCmmi: Yup.object().shape({
     modelName: Yup.string().trim().required('Enter the Model Name'),
-    modelCategory: Yup.string().nullable().required('Enter the Model Category'),
-    cmsCenters: Yup.array()
-      .min(1, 'Select a CMS Component')
-      .required('Select a CMS Component'),
-    cmsOther: Yup.string().required('Please specific CMS Component'),
-    cmmiGroups: Yup.array().when('cmsCenters', {
-      is: (val: [string]) => {
-        return val.includes('CMMI');
-      },
-      then: Yup.array()
-        .min(1, 'Select a CMMI Group')
-        .required('Select a CMMI Group')
+    basics: Yup.object().shape({
+      modelCategory: Yup.string()
+        .nullable()
+        .required('Enter the Model Category'),
+      cmsCenters: Yup.array()
+        .min(1, 'Select a CMS Component')
+        .required('Select a CMS Component'),
+      cmsOther: Yup.string().required('Please specific CMS Component'),
+      cmmiGroups: Yup.array().when('cmsCenters', {
+        is: (val: [string]) => {
+          return val.includes('CMMI');
+        },
+        then: Yup.array()
+          .min(1, 'Select a CMMI Group')
+          .required('Select a CMMI Group')
+      })
     })
   }),
 
   pageOneSchemaWithCmmiGroups: Yup.object().shape({
     modelName: Yup.string().trim().required('Enter the Model Name'),
-    modelCategory: Yup.string().nullable().required('Enter the Model Category'),
-    cmsCenters: Yup.array()
-      .min(1, 'Select a CMS Component')
-      .required('Select a CMS Component'),
-    cmmiGroups: Yup.array().when('cmsCenters', {
-      is: (val: [string]) => {
-        return val.includes('CMMI');
-      },
-      then: Yup.array()
-        .min(1, 'Select a CMMI Group')
-        .required('Select a CMMI Group')
+    basics: Yup.object().shape({
+      modelCategory: Yup.string()
+        .nullable()
+        .required('Enter the Model Category'),
+      cmsCenters: Yup.array()
+        .min(1, 'Select a CMS Component')
+        .required('Select a CMS Component'),
+      cmmiGroups: Yup.array().when('cmsCenters', {
+        is: (val: [string]) => {
+          return val.includes('CMMI');
+        },
+        then: Yup.array()
+          .min(1, 'Select a CMMI Group')
+          .required('Select a CMMI Group')
+      })
     })
   }),
 
