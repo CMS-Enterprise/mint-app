470babf0f62348454ca9a6e356a0b5767a44cd2e
Gary Zhao <gary.j.zhao@gmail.com>
2022-08-09 15:46:14 -0400

    Read Only Model init

---
+++ src/views/ModelPlan/ReadOnly/index.tsx
@@ -0,0 +1,183 @@
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Link as RouterLink, NavLink, useParams } from 'react-router-dom';
+import {
+  Breadcrumb,
+  BreadcrumbBar,
+  BreadcrumbLink,
+  Button,
+  Grid,
+  GridContainer,
+  IconArrowBack,
+  IconBookmark,
+  IconExpandMore,
+  Link,
+  SummaryBox
+} from '@trussworks/react-uswds';
+import classnames from 'classnames';
+
+import UswdsReactLink from 'components/LinkWrapper';
+import MainContent from 'components/MainContent';
+import PageHeading from 'components/PageHeading';
+import CollapsableLink from 'components/shared/CollapsableLink';
+import {
+  DescriptionDefinition,
+  DescriptionTerm
+} from 'components/shared/DescriptionGroup';
+
+import './index.scss';
+
+const ReadOnly = () => {
+  const { t } = useTranslation('modelSummary');
+  const { t: h } = useTranslation('generalReadOnly');
+  // const { modelID } = useParams<{ modelID: string }>();
+  const descriptionRef = React.createRef<HTMLElement>();
+  const [
+    isDescriptionExpandable,
+    setIsDescriptionExpandable
+  ] = useState<boolean>(false);
+  const [descriptionExpanded, setDescriptionExpanded] = useState<boolean>(
+    false
+  );
+
+  // Enable the description toggle if it overflows
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    const { current: el } = descriptionRef;
+    if (!el) return;
+    if (el.scrollHeight > el.offsetHeight) {
+      setIsDescriptionExpandable(true);
+    }
+  });
+
+  return (
+    <MainContent
+      className="model-plan-read-only"
+      data-testid="model-plan-read-only"
+    >
+      <SummaryBox
+        heading=""
+        className="padding-y-6 border-0 bg-primary-lighter"
+      >
+        <GridContainer>
+          <Link
+            href="/"
+            className="display-flex flex-align-center margin-bottom-3"
+          >
+            <IconArrowBack className="text-primary margin-right-1" />
+            {h('back')}
+          </Link>
+
+          <PageHeading className="margin-0 line-height-sans-2">
+            Medicare Diabetes Prevention Program - Expanded Model Revamped
+          </PageHeading>
+
+          <CollapsableLink
+            className="margin-top-3"
+            eyeIcon
+            startOpen
+            labelPosition="bottom"
+            closeLabel={h('hideSummary')}
+            styleLeftBar={false}
+            id={t('singleSystem.id')}
+            label={h('showSummary')}
+          >
+            <div
+              className={classnames(
+                'description-truncated',
+                'margin-bottom-2',
+                {
+                  expanded: descriptionExpanded
+                }
+              )}
+            >
+              <DescriptionDefinition
+                definition={t('descriptionFiller')}
+                ref={descriptionRef}
+                className="font-body-lg line-height-body-5 text-light"
+              />
+              {isDescriptionExpandable && (
+                <div>
+                  <Button
+                    unstyled
+                    type="button"
+                    className="margin-top-1"
+                    onClick={() => {
+                      setDescriptionExpanded(!descriptionExpanded);
+                    }}
+                  >
+                    {h(
+                      descriptionExpanded
+                        ? 'description.less'
+                        : 'description.more'
+                    )}
+                    <IconExpandMore className="expand-icon margin-left-05 margin-bottom-2px text-tbottom" />
+                  </Button>
+                </div>
+              )}
+            </div>
+            {/* <Grid row className="margin-top-3">
+                    {cmsComponent && (
+                      <Grid desktop={{ col: 6 }} className="margin-bottom-2">
+                        <DescriptionDefinition
+                          definition={t('singleSystem.summary.subheader1')}
+                        />
+                        <DescriptionTerm
+                          className="font-body-md"
+                          term={cmsComponent}
+                        />
+                      </Grid>
+                    )}
+                    {businessOwners.length && (
+                      <Grid desktop={{ col: 6 }} className="margin-bottom-2">
+                        <DescriptionDefinition
+                          definition={t('singleSystem.summary.subheader2', {
+                            count: businessOwners.length
+                          })}
+                        />
+                        <DescriptionTerm
+                          className="font-body-md"
+                          term={businessOwners
+                            .map(bo => getPersonFullName(bo))
+                            .join(', ')}
+                        />
+                      </Grid>
+                    )}
+                    {flags.systemProfileHiddenFields && (
+                      <>
+                        {/* Go live date
+                        <Grid desktop={{ col: 6 }} className="margin-bottom-2">
+                          <DescriptionDefinition
+                            definition={t('singleSystem.summary.subheader3')}
+                          />
+                          <DescriptionTerm
+                            className="font-body-md"
+                            term="July 27, 2015"
+                          />
+                        </Grid>
+                        {/* Most recent major change
+                        <Grid desktop={{ col: 6 }} className="margin-bottom-2">
+                          <DescriptionDefinition
+                            definition={t('singleSystem.summary.subheader4')}
+                          />
+                          <DescriptionTerm
+                            className="font-body-md"
+                            term="December 4, 2021"
+                          />
+                        </Grid>
+                      </>
+                    )}
+                  </Grid> */}
+          </CollapsableLink>
+        </GridContainer>
+      </SummaryBox>
+      {/* <GridContainer>
+        <Grid desktop={{ col: 12 }}>
+          <h1>asdf</h1>
+        </Grid>
+      </GridContainer> */}
+    </MainContent>
+  );
+};
+
+export default ReadOnly;
