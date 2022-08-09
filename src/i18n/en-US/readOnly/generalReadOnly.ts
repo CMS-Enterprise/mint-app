790a75cd8c56e4ee27867ab26ed5dbb73279395e
Gary Zhao <gary.j.zhao@gmail.com>
2022-08-09 15:18:31 -0400

    add Read Only Model page copy

---
+++ src/i18n/en-US/readOnly/generalReadOnly.ts
@@ -0,0 +1,12 @@
+const generalReadOnly = {
+  back: 'Back to all models',
+  readMore: 'Read more',
+  hideSummary: 'Hide model summary',
+  status: 'Status',
+  updateStatus: 'Update this Model Plan',
+  lastUpdate: 'Last updated on',
+  alert:
+    'Information outlined in this Model Plan can change drastically until it’s been cleared. '
+};
+
+export default generalReadOnly;
---
+++ src/i18n/en-US/readOnly/modelSummary.ts
@@ -0,0 +1,8 @@
+const modelSummary = {
+  keyCharacteristics: 'Key characteristics',
+  modelLeads: 'Model lead(s)',
+  modelStartDate: 'Model start date',
+  crAndTdls: 'CRs and TDLs'
+};
+
+export default modelSummary;
