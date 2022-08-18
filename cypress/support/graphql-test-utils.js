3734ff8f0200101c7bb48fc3cb00ca0ef526d5dc
Tom Brooks <tom.brooks@oddball.io>
2022-08-18 15:23:00 -0400

    fix: unrelated linting error

--- cypress/support/graphql-test-utils.js
+++ cypress/support/graphql-test-utils.js
@@ -2,6 +2,7 @@
 export const hasOperationName = (req, operationName) => {
   const { body } = req;
   return (
+    // eslint-disable-next-line no-prototype-builtins
     body.hasOwnProperty('operationName') && body.operationName === operationName
   );
 };
