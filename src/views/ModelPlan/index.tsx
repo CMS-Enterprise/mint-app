15efd5c9642d1761d7ac782cc3ce0a9f3304bb29
Gary Zhao <gary.j.zhao@gmail.com>
2022-08-22 16:09:32 -0400

    remove temporary landing page

--- src/views/ModelPlan/index.tsx
+++ src/views/ModelPlan/index.tsx
@@ -1,16 +1,7 @@
 import React from 'react';
-import { Grid, GridContainer } from '@trussworks/react-uswds';
-
-import DraftModelPlansTable from './Table';
 
 const ModelPlan = () => {
-  return (
-    <GridContainer>
-      <Grid desktop={{ col: 12 }}>
-        <DraftModelPlansTable readOnly />
-      </Grid>
-    </GridContainer>
-  );
+  return <></>;
 };
 
 export default ModelPlan;
