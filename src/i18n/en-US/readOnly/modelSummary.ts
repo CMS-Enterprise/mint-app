18e38c65fdf13be1662cddba3e38d884ed634054
Gary Zhao <gary.j.zhao@gmail.com>
2022-08-23 14:08:27 -0400

    Add Sub Navigation copy

--- src/i18n/en-US/readOnly/modelSummary.ts
+++ src/i18n/en-US/readOnly/modelSummary.ts
@@ -8,6 +8,19 @@ const modelSummary = {
   noAnswer: {
     noneEntered: 'None entered',
     tBD: 'To be determined'
+  },
+  navigation: {
+    modelBasics: 'Model basics',
+    generalCharacteristics: 'General characteristics',
+    participantsAndProviders: 'Participants and providers',
+    beneficiaries: 'Beneficiaries',
+    operationsEvaluationAndLearning: 'Operations, evaluation, and learning',
+    payment: 'Payment',
+    itTools: 'IT tools',
+    team: 'Team',
+    discussions: 'Discussions',
+    documents: 'Documents',
+    crsAndTdls: 'CRs and TDLs'
   }
 };
 
