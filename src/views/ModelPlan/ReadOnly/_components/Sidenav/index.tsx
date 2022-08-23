96ba6324db2a8b35afd1768d96372069f28e1cc1
Gary Zhao <gary.j.zhao@gmail.com>
2022-08-23 15:16:53 -0400

    Simplify the object

--- src/views/ModelPlan/ReadOnly/_components/Sidenav/index.tsx
+++ src/views/ModelPlan/ReadOnly/_components/Sidenav/index.tsx
@@ -9,78 +9,35 @@ interface SideNavProps {
   modelID: string;
 }
 
-type SubpageKey =
-  | 'model-basics'
-  | 'general-characteristics'
-  | 'participants-and-providers'
-  | 'beneficiaries'
-  | 'operations-evaluation-and-learning'
-  | 'payment'
-  | 'it-tools'
-  | 'team'
-  | 'discussions'
-  | 'documents'
-  | 'crs-and-tdl';
-
-type sideNavItemProps = {
-  groupEnd?: boolean; // Value used to designate end of sidenav subgrouping / border-bottom
-  component?: React.ReactNode;
-  route: string;
-  componentId?: string;
-};
-
 interface sideNavProps {
-  [key: string]: sideNavItemProps;
+  [key: string]: string;
 }
 
 const SideNav = ({ modelID }: SideNavProps) => {
   const { t } = useTranslation('modelSummary');
 
   const sideNavItems: sideNavProps = {
-    modelBasics: {
-      route: `/models/${modelID}/read-only/`
-    },
-    generalCharacteristics: {
-      route: `/models/${modelID}/read-only/general-characteristics`
-    },
-    participantsAndProviders: {
-      route: `/models/${modelID}/read-only/participants-and-providers`
-    },
-    beneficiaries: {
-      route: `/models/${modelID}/read-only/beneficiaries`
-    },
-    operationsEvaluationAndLearning: {
-      route: `/models/${modelID}/read-only/operations-evaluation-and-learning`
-    },
-    payment: {
-      route: `/models/${modelID}/read-only/payment`
-    },
-    itTools: {
-      groupEnd: true,
-      route: `/models/${modelID}/read-only/it-tools`
-    },
-    team: {
-      route: `/models/${modelID}/read-only/team`
-    },
-    discussions: {
-      route: `/models/${modelID}/read-only/discussions`
-    },
-    documents: {
-      route: `/models/${modelID}/read-only/documents`
-    },
-    crsAndTdls: {
-      route: `/models/${modelID}/read-only/crs-and-tdl`
-    }
+    modelBasics: `/models/${modelID}/read-only/`,
+    generalCharacteristics: `/models/${modelID}/read-only/general-characteristics`,
+    participantsAndProviders: `/models/${modelID}/read-only/participants-and-providers`,
+    beneficiaries: `/models/${modelID}/read-only/beneficiaries`,
+    operationsEvaluationAndLearning: `/models/${modelID}/read-only/operations-evaluation-and-learning`,
+    payment: `/models/${modelID}/read-only/payment`,
+    itTools: `/models/${modelID}/read-only/it-tools`,
+    team: `/models/${modelID}/read-only/team`,
+    discussions: `/models/${modelID}/read-only/discussions`,
+    documents: `/models/${modelID}/read-only/documents`,
+    crsAndTdls: `/models/${modelID}/read-only/crs-and-tdl`
   };
 
   // Mapping of all sub navigation links
   const subNavigationLinks: React.ReactNode[] = Object.keys(sideNavItems).map(
     (key: string) => (
       <NavLink
-        to={sideNavItems[key].route}
+        to={sideNavItems[key]}
         key={key}
         activeClassName="usa-current"
-        className={sideNavItems[key].groupEnd ? 'nav-group-border' : ''}
+        className={key === 'itTools' ? 'nav-group-border' : ''}
       >
         {t(`navigation.${key}`)}
       </NavLink>
