2b766ab7ad23c30a8351f05d1597b94e7b80de30
Steven Wade <steven.wade@oddball.io>
2022-08-25 15:23:35 -0700

    update model plan update to return isFavorite as well

--- pkg/storage/SQL/model_plan_update.sql
+++ pkg/storage/SQL/model_plan_update.sql
@@ -1,17 +1,30 @@
-UPDATE model_plan
-SET
-    model_name = :model_name,
-    status = :status,
-    archived = :archived,
-    modified_by = :modified_by,
-    modified_dts = CURRENT_TIMESTAMP
-WHERE model_plan.id = :id
-RETURNING
-id,
-model_name,
-archived,
-status,
-created_by,
-created_dts,
-modified_by,
-modified_dts;
+WITH cte_update AS (
+    UPDATE model_plan
+    SET model_name = :model_name,
+        status = :status,
+        archived = :archived,
+        modified_by = :modified_by,
+        modified_dts = CURRENT_TIMESTAMP
+    WHERE model_plan.id = :id
+    RETURNING id,
+        model_name,
+        archived,
+        status,
+        created_by,
+        created_dts,
+        modified_by,
+        modified_dts
+)
+
+SELECT
+    cte.model_name,
+    cte.archived,
+    cte.status,
+    cte.created_by,
+    cte.created_dts,
+    cte.modified_by,
+    cte.modified_dts,
+    plan_favorite.id IS NOT NULL AS is_favorite
+FROM cte_update AS cte --noqa
+LEFT JOIN plan_favorite ON cte.id = plan_favorite.model_plan_id
+    AND plan_favorite.user_id = cte.modified_by
