a49dac2b7d383f9724e20b10d978f86479d01d74
Gary Zhao <gary.j.zhao@gmail.com>
2022-08-30 13:18:29 -0400

    Copy updates

---
+++ src/i18n/en-US/readOnly/readOnlyModelPlan.ts
@@ -0,0 +1,30 @@
+const readOnlyModelPlan = {
+  heading: 'CMMI Models and Demonstrations',
+  subheading:
+    'Here you can find information about all CMMI models and demonstrations.',
+  allModelsLink: 'Jump to all models',
+  following: {
+    heading: 'Models you’re following',
+    subheading:
+      'You will receive email notifications when models you’re following are updated.',
+    alert: {
+      heading: 'You are not following any models yet.',
+      subheading:
+        'Click the star icon (STAR) for any model to add it to this section.'
+    }
+  },
+  allModels: {
+    heading: 'All models',
+    subheading:
+      'Follow models that you want to stay up to date on by clicking the star icon.',
+    tableHeading: {
+      modelName: 'Model name',
+      category: 'Category',
+      status: 'Status',
+      startDate: 'Model start date',
+      crsAndTdls: 'CRs and TDLs'
+    }
+  }
+};
+
+export default readOnlyModelPlan;
