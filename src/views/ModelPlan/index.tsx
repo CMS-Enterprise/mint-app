8c5fd63e9daddea7c327e83cfc695dcf0b4d5402
Gary Zhao <gary.j.zhao@gmail.com>
2022-08-30 13:18:42 -0400

    Add the header for Model Plan page

--- src/views/ModelPlan/index.tsx
+++ src/views/ModelPlan/index.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Grid, GridContainer, SummaryBox } from '@trussworks/react-uswds';
+
+import UswdsReactLink from 'components/LinkWrapper';
+import MainContent from 'components/MainContent';
+import PageHeading from 'components/PageHeading';
 
 const ModelPlan = () => {
-  return <></>;
+  const { t } = useTranslation('readOnlyModelPlan');
+  const { t: h } = useTranslation('home');
+
+  return (
+    <MainContent>
+      <GridContainer>
+        <Grid className="padding-bottom-6 margin-bottom-4 border-bottom border-base-light">
+          <PageHeading className="margin-bottom-1">{t('heading')}</PageHeading>
+          <p className="line-height-body-5 font-body-lg text-light margin-bottom-05 margin-top-0">
+            {t('subheading')}
+          </p>
+          <UswdsReactLink variant="unstyled" to="/models#all-models">
+            {t('allModelsLink')}
+          </UswdsReactLink>
+          <SummaryBox
+            heading=""
+            className="bg-base-lightest border-0 radius-0 padding-2 padding-bottom-3 margin-top-3 "
+          >
+            <p className="margin-0 margin-bottom-1">
+              {h('newModelSummaryBox.copy')}
+            </p>
+            <UswdsReactLink
+              className="usa-button usa-button--outline"
+              variant="unstyled"
+              to="/models/steps-overview"
+            >
+              {h('newModelSummaryBox.cta')}
+            </UswdsReactLink>
+          </SummaryBox>
+        </Grid>
+      </GridContainer>
+    </MainContent>
+  );
 };
 
 export default ModelPlan;
