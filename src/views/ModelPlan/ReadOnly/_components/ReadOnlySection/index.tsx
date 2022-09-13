99b60028fbedc977305e0cb50c31b1526e0fe79a
Gary Zhao <gary.j.zhao@gmail.com>
2022-09-13 15:49:34 -0400

    more spliting of code

--- src/views/ModelPlan/ReadOnly/_components/ReadOnlySection/index.tsx
+++ src/views/ModelPlan/ReadOnly/_components/ReadOnlySection/index.tsx
@@ -19,6 +19,23 @@ const ReadOnlySection = ({
   const { t } = useTranslation('basics');
   const sectionName = heading.toLowerCase().replaceAll(' ', '-');
 
+  if (!list) {
+    return (
+      <div
+        className={`read-only-section read-only-section--${sectionName} margin-bottom-3 ${
+          className ?? ''
+        }`}
+      >
+        <p className="text-bold margin-y-0 font-sans-md line-height-sans-4">
+          {heading}
+        </p>
+        <p className="margin-y-0 font-sans-md line-height-sans-4">
+          {copy ?? t('na')}
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div
       className={`read-only-section read-only-section--${sectionName} margin-bottom-3 ${
@@ -28,19 +45,22 @@ const ReadOnlySection = ({
       <p className="text-bold margin-y-0 font-sans-md line-height-sans-4">
         {heading}
       </p>
-      {!list ? (
-        <p className="margin-y-0 font-sans-md line-height-sans-4">
-          {copy ?? t('na')}
-        </p>
-      ) : (
-        <ul className="margin-y-0 padding-left-3">
-          {!listItems
-            ? t('na')
-            : listItems?.map(item => (
-                <li className="font-sans-md line-height-sans-4">{item}</li>
-              ))}
-        </ul>
-      )}
+      <ul className="margin-y-0 padding-left-3">
+        {listItems ? (
+          listItems?.map(item => (
+            <>
+              <li className="font-sans-md line-height-sans-4">{item}</li>
+              {item === 'Other' && (
+                <ul>
+                  <li className="font-sans-md line-height-sans-4">{copy}</li>
+                </ul>
+              )}
+            </>
+          ))
+        ) : (
+          <li className="font-sans-md line-height-sans-4">{t('na')}</li>
+        )}
+      </ul>
     </div>
 
     // TODO: Ask Natasha how does Other show on page
