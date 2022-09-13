31fd32148f4d242c3b8b1a8f709995b2748a9106
Gary Zhao <gary.j.zhao@gmail.com>
2022-09-13 13:44:23 -0400

    GetAllBasics query and type

---
+++ src/queries/ReadOnly/GetAllBasics.ts
@@ -0,0 +1,38 @@
+import { gql } from '@apollo/client';
+
+export default gql`
+  query GetAllBasics($id: UUID!) {
+    modelPlan(id: $id) {
+      basics {
+        modelCategory
+        cmsCenters
+        cmsOther
+        cmmiGroups
+        modelType
+        problem
+        goal
+        testInterventions
+        note
+        completeICIP
+        clearanceStarts
+        clearanceEnds
+        announced
+        applicationsStart
+        applicationsEnd
+        performancePeriodStarts
+        performancePeriodEnds
+        wrapUpEnds
+        highLevelNote
+        phasedIn
+        phasedInNote
+        createdBy
+        createdDts
+        modifiedBy
+        modifiedDts
+        readyForReviewBy
+        readyForReviewDts
+        status
+      }
+    }
+  }
+`;
---
+++ src/queries/ReadOnly/types/GetAllBasics.ts
@@ -0,0 +1,55 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import { ModelCategory, CMSCenter, CMMIGroup, ModelType, TaskStatus } from "./../../../types/graphql-global-types";
+
+// ====================================================
+// GraphQL query operation: GetAllBasics
+// ====================================================
+
+export interface GetAllBasics_modelPlan_basics {
+  __typename: "PlanBasics";
+  modelCategory: ModelCategory | null;
+  cmsCenters: CMSCenter[];
+  cmsOther: string | null;
+  cmmiGroups: CMMIGroup[];
+  modelType: ModelType | null;
+  problem: string | null;
+  goal: string | null;
+  testInterventions: string | null;
+  note: string | null;
+  completeICIP: Time | null;
+  clearanceStarts: Time | null;
+  clearanceEnds: Time | null;
+  announced: Time | null;
+  applicationsStart: Time | null;
+  applicationsEnd: Time | null;
+  performancePeriodStarts: Time | null;
+  performancePeriodEnds: Time | null;
+  wrapUpEnds: Time | null;
+  highLevelNote: string | null;
+  phasedIn: boolean | null;
+  phasedInNote: string | null;
+  createdBy: string;
+  createdDts: Time;
+  modifiedBy: string | null;
+  modifiedDts: Time | null;
+  readyForReviewBy: string | null;
+  readyForReviewDts: Time | null;
+  status: TaskStatus;
+}
+
+export interface GetAllBasics_modelPlan {
+  __typename: "ModelPlan";
+  basics: GetAllBasics_modelPlan_basics;
+}
+
+export interface GetAllBasics {
+  modelPlan: GetAllBasics_modelPlan;
+}
+
+export interface GetAllBasicsVariables {
+  id: UUID;
+}
