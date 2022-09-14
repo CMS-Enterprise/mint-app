6628cc45c0e10833b40c65fe41afd38e6eb91e2f
Gary Zhao <gary.j.zhao@gmail.com>
2022-09-14 14:10:12 -0400

    add more tests

--- src/views/ModelPlan/ReadOnly/_components/ReadOnlySection/index.test.tsx
+++ src/views/ModelPlan/ReadOnly/_components/ReadOnlySection/index.test.tsx
@@ -4,27 +4,40 @@ import { render, screen } from '@testing-library/react';
 import ReadOnlySection from './index';
 
 describe('The Read Only Section', () => {
-  const defaultCopyProps = {
-    heading: 'React Testing is Great',
-    copy: 'Lorem ipsum dolor sit amet.'
-  };
-  const defaultListProps = {
-    heading: 'React Testing is Great',
-    list: true,
-    listItems: ['Center for Medicare (CM)', 'Spiderman']
-  };
+  describe('As a Non-list Component', () => {
+    const defaultCopyProps = {
+      heading: 'React Testing is Great',
+      copy: 'Lorem ipsum dolor sit amet.'
+    };
 
-  it('renders non-list component without crashing', async () => {
-    render(<ReadOnlySection {...defaultCopyProps} />);
+    it('renders without crashing', async () => {
+      render(<ReadOnlySection {...defaultCopyProps} />);
 
-    expect(screen.getByText(defaultCopyProps.heading)).toBeInTheDocument();
-    expect(screen.getByText(defaultCopyProps.copy)).toBeInTheDocument();
+      expect(screen.getByText(defaultCopyProps.heading)).toBeInTheDocument();
+      expect(screen.getByText(defaultCopyProps.copy)).toBeInTheDocument();
+    });
+
+    it('renders "No Answer Entered" if copy is empty', async () => {
+      render(<ReadOnlySection {...defaultCopyProps} copy={null} />);
+
+      expect(screen.getByText(defaultCopyProps.heading)).toBeInTheDocument();
+      expect(screen.getByText('No answer entered')).toBeInTheDocument();
+    });
   });
 
-  it('renders list component without crashing', async () => {
-    render(<ReadOnlySection {...defaultListProps} />);
+  describe('As a List Component', () => {
+    const defaultListProps = {
+      heading: 'React Testing is Great',
+      list: true,
+      listItems: ['Center for Medicare (CM)', 'Spiderman']
+    };
+    it('renders without crashing', async () => {
+      render(<ReadOnlySection {...defaultListProps} />);
 
-    expect(screen.getByText(defaultListProps.heading)).toBeInTheDocument();
-    expect(screen.getByText(defaultListProps.listItems[0])).toBeInTheDocument();
+      expect(screen.getByText(defaultListProps.heading)).toBeInTheDocument();
+      expect(
+        screen.getByText(defaultListProps.listItems[0])
+      ).toBeInTheDocument();
+    });
   });
 });
