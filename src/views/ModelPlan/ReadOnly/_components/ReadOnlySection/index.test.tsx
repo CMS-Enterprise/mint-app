db563ee7a5f913ee730e516c6dafc4d50b070a35
Gary Zhao <gary.j.zhao@gmail.com>
2022-09-14 14:03:06 -0400

    testing init

---
+++ src/views/ModelPlan/ReadOnly/_components/ReadOnlySection/index.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import ReadOnlySection from './index';
+
+describe('The Read Only Section', () => {
+  const defaultCopyProps = {
+    heading: 'React Testing is Great',
+    copy: 'Lorem ipsum dolor sit amet.'
+  };
+  const defaultListProps = {
+    heading: 'React Testing is Great',
+    copy: 'Lorem ipsum dolor sit amet.'
+  };
+
+  it('renders non-list component without crashing', async () => {
+    render(<ReadOnlySection {...defaultCopyProps} />);
+
+    expect(screen.getByText(defaultCopyProps.heading)).toBeInTheDocument();
+    expect(screen.getByText(defaultCopyProps.copy)).toBeInTheDocument();
+  });
+
+  it('renders list component without crashing', async () => {
+    render(<ReadOnlySection {...defaultListProps} />);
+
+    expect(screen.getByText(defaultListProps.heading)).toBeInTheDocument();
+    expect(screen.getByText(defaultListProps.copy)).toBeInTheDocument();
+  });
+});
