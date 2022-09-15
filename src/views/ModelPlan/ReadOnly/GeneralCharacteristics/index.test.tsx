9c1b729d88d4c73a9f420520f2e8c69c66d1cb8f
Gary Zhao <gary.j.zhao@gmail.com>
2022-09-15 14:56:01 -0400

    wip

---
+++ src/views/ModelPlan/ReadOnly/GeneralCharacteristics/index.test.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { MockedProvider } from '@apollo/client/testing';
+import { render, screen, waitFor } from '@testing-library/react';
+
+import ReadOnlyGeneralCharacteristics from './index';
