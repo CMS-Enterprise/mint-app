dbeb1476384e13b714474edf9ebbd08f72d0eb2d
Gary Zhao <gary.j.zhao@gmail.com>
2022-09-15 14:56:09 -0400

    wip

--- src/views/ModelPlan/ReadOnly/GeneralCharacteristics/index.test.tsx
+++ src/views/ModelPlan/ReadOnly/GeneralCharacteristics/index.test.tsx
@@ -4,3 +4,65 @@ import { MockedProvider } from '@apollo/client/testing';
 import { render, screen, waitFor } from '@testing-library/react';
 
 import ReadOnlyGeneralCharacteristics from './index';
+
+const modelID = 'f11eb129-2c80-4080-9440-439cbe1a286f';
+
+const mocks = [
+  {
+    request: {
+      query: GetAllBasics,
+      variables: { id: modelID }
+    },
+    result: {
+      data: {
+        modelPlan: {
+          id: modelID,
+          basics: basicMockData
+        }
+      }
+    }
+  }
+];
+
+describe('Model Plan Documents page', () => {
+  it('renders without errors', async () => {
+    render(
+      <MemoryRouter
+        initialEntries={[`/models/${modelID}/read-only/model-basics`]}
+      >
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <Route path="/models/:modelID/read-only/model-basics">
+            <ReadOnlyModelBasics modelID={modelID} />
+          </Route>
+        </MockedProvider>
+      </MemoryRouter>
+    );
+
+    await waitFor(() => {
+      expect(
+        screen.getByTestId('read-only-model-plan--model-basics')
+      ).toBeInTheDocument();
+      expect(
+        screen.getByText(
+          translateModelCategory(ModelCategory.PRIMARY_CARE_TRANSFORMATION)
+        )
+      ).toBeInTheDocument();
+    });
+  });
+  it('matches snapshot', async () => {
+    const { asFragment } = render(
+      <MemoryRouter
+        initialEntries={[`/models/${modelID}/read-only/model-basics`]}
+      >
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <Route path="/models/:modelID/read-only/model-basics">
+            <ReadOnlyModelBasics modelID={modelID} />
+          </Route>
+        </MockedProvider>
+      </MemoryRouter>
+    );
+    await waitFor(() => {
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+});
