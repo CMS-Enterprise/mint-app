702e3c1bc0c7eda2dc8020bc1191d83ac6d27607
Gary Zhao <gary.j.zhao@gmail.com>
2022-09-15 13:11:36 -0400

    add notes sections

--- src/views/ModelPlan/ReadOnly/_components/ReadOnlySection/index.tsx
+++ src/views/ModelPlan/ReadOnly/_components/ReadOnlySection/index.tsx
@@ -6,7 +6,7 @@ type ReadOnlySectionProps = {
   heading: string;
   list?: boolean;
   listItems?: string[];
-  notes?: string;
+  notes?: string | null;
 };
 
 const ReadOnlySection = ({
@@ -22,44 +22,50 @@ const ReadOnlySection = ({
   // If component is not a list, render a basics component
   if (!list) {
     return (
-      <div
-        className={`read-only-section read-only-section--${sectionName} margin-bottom-3`}
-      >
-        <p className="text-bold margin-y-0 font-body-sm line-height-sans-4">
-          {heading}
-        </p>
-        <p className="margin-y-0 font-body-md line-height-sans-4">
-          {copy || <em className="text-base">{t('na')}</em>}
-        </p>
-      </div>
+      <>
+        <div
+          className={`read-only-section read-only-section--${sectionName} margin-bottom-3`}
+        >
+          <p className="text-bold margin-y-0 font-body-sm line-height-sans-4">
+            {heading}
+          </p>
+          <p className="margin-y-0 font-body-md line-height-sans-4">
+            {copy || <em className="text-base">{t('na')}</em>}
+          </p>
+        </div>
+        {notes && <ReadOnlySection heading={t('notes')} copy={notes} />}
+      </>
     );
   }
 
   // If component is a list, render a more complicated list component
   return (
-    <div
-      className={`read-only-section read-only-section--${sectionName} margin-bottom-3`}
-    >
-      <p className="text-bold margin-y-0 font-sans-md line-height-sans-4">
-        {heading}
-      </p>
-      <ul className="margin-y-0 padding-left-3">
-        {listItems ? (
-          listItems?.map(item => (
-            <React.Fragment key={`${sectionName}--${item}`}>
-              <li className="font-sans-md line-height-sans-4">{item}</li>
-              {item === 'Other' && (
-                <ul data-testid="other-entry">
-                  <li className="font-sans-md line-height-sans-4">{copy}</li>
-                </ul>
-              )}
-            </React.Fragment>
-          ))
-        ) : (
-          <li className="font-sans-md line-height-sans-4">{t('na')}</li>
-        )}
-      </ul>
-    </div>
+    <>
+      <div
+        className={`read-only-section read-only-section--${sectionName} margin-bottom-3`}
+      >
+        <p className="text-bold margin-y-0 font-sans-md line-height-sans-4">
+          {heading}
+        </p>
+        <ul className="margin-y-0 padding-left-3">
+          {listItems ? (
+            listItems?.map(item => (
+              <React.Fragment key={`${sectionName}--${item}`}>
+                <li className="font-sans-md line-height-sans-4">{item}</li>
+                {item === 'Other' && (
+                  <ul data-testid="other-entry">
+                    <li className="font-sans-md line-height-sans-4">{copy}</li>
+                  </ul>
+                )}
+              </React.Fragment>
+            ))
+          ) : (
+            <li className="font-sans-md line-height-sans-4">{t('na')}</li>
+          )}
+        </ul>
+      </div>
+      {notes && <ReadOnlySection heading={t('notes')} copy={notes} />}
+    </>
   );
 };
 
