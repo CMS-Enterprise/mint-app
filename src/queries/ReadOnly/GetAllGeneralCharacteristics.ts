2e1d3b08e78856a38648c8711329ba632be4b7a4
Gary Zhao <gary.j.zhao@gmail.com>
2022-09-15 11:53:43 -0400

    add back the copy

--- src/queries/ReadOnly/GetAllGeneralCharacteristics.ts
+++ src/queries/ReadOnly/GetAllGeneralCharacteristics.ts
@@ -9,6 +9,7 @@ export default gql`
         resemblesExistingModel
         resemblesExistingModelWhich
         resemblesExistingModelHow
+        resemblesExistingModelNote
         hasComponentsOrTracks
         hasComponentsOrTracksDiffer
         hasComponentsOrTracksNote
