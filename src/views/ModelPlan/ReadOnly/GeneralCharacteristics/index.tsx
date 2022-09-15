ecf346a641f0db6f0eed01d964fc14eaa7074cdb
Gary Zhao <gary.j.zhao@gmail.com>
2022-09-15 10:41:39 -0400

    GeneralCharacteristics init

---
+++ src/views/ModelPlan/ReadOnly/GeneralCharacteristics/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ReadOnlyGeneralCharacteristics = ({ modelID }: { modelID: string }) => {
+  return <div>ReadOnlyGeneralCharacteristics</div>;
+};
+
+export default ReadOnlyGeneralCharacteristics;
