04b0a677a1e2327dd75e8048af0fcd16c2e25118
Gary Zhao <gary.j.zhao@gmail.com>
2022-09-15 15:19:31 -0400

    Read Only General Characterstics test

--- src/views/ModelPlan/ReadOnly/GeneralCharacteristics/index.test.tsx
+++ src/views/ModelPlan/ReadOnly/GeneralCharacteristics/index.test.tsx
@@ -3,36 +3,110 @@ import { MemoryRouter, Route } from 'react-router-dom';
 import { MockedProvider } from '@apollo/client/testing';
 import { render, screen, waitFor } from '@testing-library/react';
 
+import GetAllGeneralCharacteristics from 'queries/ReadOnly/GetAllGeneralCharacteristics';
+import { GetAllGeneralCharacteristics_modelPlan_generalCharacteristics as GetAllGeneralCharacteristicsTypes } from 'queries/ReadOnly/types/GetAllGeneralCharacteristics';
+import {
+  AlternativePaymentModelType,
+  KeyCharacteristic,
+  TaskStatus
+} from 'types/graphql-global-types';
+
 import ReadOnlyGeneralCharacteristics from './index';
 
 const modelID = 'f11eb129-2c80-4080-9440-439cbe1a286f';
 
+const mockData: GetAllGeneralCharacteristicsTypes = {
+  __typename: 'PlanGeneralCharacteristics',
+  isNewModel: false,
+  existingModel: 'Accountable Care Organizations (ACOs): General Information',
+  resemblesExistingModel: true,
+  resemblesExistingModelWhich: [],
+  resemblesExistingModelHow: null,
+  resemblesExistingModelNote: 'THIS IS A NEW NOTE',
+  hasComponentsOrTracks: false,
+  hasComponentsOrTracksDiffer: null,
+  hasComponentsOrTracksNote: null,
+  alternativePaymentModel: true,
+  alternativePaymentModelTypes: [
+    AlternativePaymentModelType.REGULAR,
+    AlternativePaymentModelType.MIPS
+  ],
+  alternativePaymentModelNote: 'asdfasdfasdfasdf asdf asdf asdf asdf ',
+  keyCharacteristics: [
+    KeyCharacteristic.POPULATION_BASED,
+    KeyCharacteristic.PAYMENT,
+    KeyCharacteristic.SERVICE_DELIVERY,
+    KeyCharacteristic.OTHER
+  ],
+  keyCharacteristicsOther: null,
+  keyCharacteristicsNote: 'test',
+  collectPlanBids: null,
+  collectPlanBidsNote: null,
+  managePartCDEnrollment: null,
+  managePartCDEnrollmentNote: null,
+  planContactUpdated: null,
+  planContactUpdatedNote: null,
+  careCoordinationInvolved: false,
+  careCoordinationInvolvedDescription: null,
+  careCoordinationInvolvedNote: null,
+  additionalServicesInvolved: true,
+  additionalServicesInvolvedDescription: 'Lots of additional services',
+  additionalServicesInvolvedNote: null,
+  communityPartnersInvolved: true,
+  communityPartnersInvolvedDescription: 'Are community partners involved?\n\n',
+  communityPartnersInvolvedNote: 'frwegqergqgrqwg planContactUpdatedNote',
+  geographiesTargeted: false,
+  geographiesTargetedTypes: [],
+  geographiesTargetedTypesOther: null,
+  geographiesTargetedAppliedTo: [],
+  geographiesTargetedAppliedToOther: null,
+  geographiesTargetedNote: null,
+  participationOptions: false,
+  participationOptionsNote: null,
+  agreementTypes: [],
+  agreementTypesOther: null,
+  multiplePatricipationAgreementsNeeded: null,
+  multiplePatricipationAgreementsNeededNote: null,
+  rulemakingRequired: true,
+  rulemakingRequiredDescription: 'Lots of rules',
+  rulemakingRequiredNote: null,
+  authorityAllowances: [],
+  authorityAllowancesOther: null,
+  authorityAllowancesNote: null,
+  waiversRequired: false,
+  waiversRequiredTypes: [],
+  waiversRequiredNote: null,
+  status: TaskStatus.IN_PROGRESS
+};
+
 const mocks = [
   {
     request: {
-      query: GetAllBasics,
+      query: GetAllGeneralCharacteristics,
       variables: { id: modelID }
     },
     result: {
       data: {
         modelPlan: {
           id: modelID,
-          basics: basicMockData
+          generalCharacteristics: mockData
         }
       }
     }
   }
 ];
 
-describe('Model Plan Documents page', () => {
+describe('Read Only Model Plan Summary -- General Characteristics', () => {
   it('renders without errors', async () => {
     render(
       <MemoryRouter
-        initialEntries={[`/models/${modelID}/read-only/model-basics`]}
+        initialEntries={[
+          `/models/${modelID}/read-only/general-characteristics`
+        ]}
       >
         <MockedProvider mocks={mocks} addTypename={false}>
-          <Route path="/models/:modelID/read-only/model-basics">
-            <ReadOnlyModelBasics modelID={modelID} />
+          <Route path="/models/:modelID/read-only/general-characteristics">
+            <ReadOnlyGeneralCharacteristics modelID={modelID} />
           </Route>
         </MockedProvider>
       </MemoryRouter>
@@ -40,28 +114,28 @@ describe('Model Plan Documents page', () => {
 
     await waitFor(() => {
       expect(
-        screen.getByTestId('read-only-model-plan--model-basics')
-      ).toBeInTheDocument();
-      expect(
-        screen.getByText(
-          translateModelCategory(ModelCategory.PRIMARY_CARE_TRANSFORMATION)
-        )
+        screen.getByTestId('read-only-model-plan--general-characteristics')
       ).toBeInTheDocument();
     });
   });
   it('matches snapshot', async () => {
     const { asFragment } = render(
       <MemoryRouter
-        initialEntries={[`/models/${modelID}/read-only/model-basics`]}
+        initialEntries={[
+          `/models/${modelID}/read-only/general-characteristics`
+        ]}
       >
         <MockedProvider mocks={mocks} addTypename={false}>
-          <Route path="/models/:modelID/read-only/model-basics">
-            <ReadOnlyModelBasics modelID={modelID} />
+          <Route path="/models/:modelID/read-only/general-characteristics">
+            <ReadOnlyGeneralCharacteristics modelID={modelID} />
           </Route>
         </MockedProvider>
       </MemoryRouter>
     );
     await waitFor(() => {
+      expect(
+        screen.getByTestId('read-only-model-plan--general-characteristics')
+      ).toBeInTheDocument();
       expect(asFragment()).toMatchSnapshot();
     });
   });
