d955a8ea892343115edbfab01334fd44b02b691a
Gary Zhao <gary.j.zhao@gmail.com>
2022-09-15 11:22:00 -0400

    major work

--- src/views/ModelPlan/ReadOnly/GeneralCharacteristics/index.tsx
+++ src/views/ModelPlan/ReadOnly/GeneralCharacteristics/index.tsx
@@ -1,7 +1,135 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useQuery } from '@apollo/client';
+
+import GetAllGeneralCharacteristics from 'queries/ReadOnly/GetAllGeneralCharacteristics';
+import { GetAllGeneralCharacteristics as GetAllGeneralCharacteristicsTypes } from 'queries/ReadOnly/types/GetAllGeneralCharacteristics';
+import { TaskListStatusTag } from 'views/ModelPlan/TaskList/_components/TaskListItem';
+import { NotFoundPartial } from 'views/NotFound';
+
+import ReadOnlySection from '../_components/ReadOnlySection';
 
 const ReadOnlyGeneralCharacteristics = ({ modelID }: { modelID: string }) => {
-  return <div>ReadOnlyGeneralCharacteristics</div>;
+  const { t } = useTranslation('generalCharacteristics');
+  const { t: h } = useTranslation('draftModelPlan');
+
+  const { data, loading, error } = useQuery<GetAllGeneralCharacteristicsTypes>(
+    GetAllGeneralCharacteristics,
+    {
+      variables: {
+        id: modelID
+      }
+    }
+  );
+
+  if ((!loading && error) || (!loading && !data?.modelPlan)) {
+    return <NotFoundPartial />;
+  }
+
+  const {
+    isNewModel,
+    existingModel,
+    resemblesExistingModel,
+    resemblesExistingModelWhich,
+    resemblesExistingModelHow,
+    resemblesExistingModelNote,
+    hasComponentsOrTracks,
+    hasComponentsOrTracksDiffer,
+    hasComponentsOrTracksNote,
+    alternativePaymentModel,
+    alternativePaymentModelTypes,
+    alternativePaymentModelNote,
+    keyCharacteristics,
+    keyCharacteristicsOther,
+    keyCharacteristicsNote,
+    collectPlanBids,
+    collectPlanBidsNote,
+    managePartCDEnrollment,
+    managePartCDEnrollmentNote,
+    planContactUpdated,
+    planContactUpdatedNote,
+    careCoordinationInvolved,
+    careCoordinationInvolvedDescription,
+    careCoordinationInvolvedNote,
+    additionalServicesInvolved,
+    additionalServicesInvolvedDescription,
+    additionalServicesInvolvedNote,
+    communityPartnersInvolved,
+    communityPartnersInvolvedDescription,
+    communityPartnersInvolvedNote,
+    geographiesTargeted,
+    geographiesTargetedTypes,
+    geographiesTargetedTypesOther,
+    geographiesTargetedAppliedTo,
+    geographiesTargetedAppliedToOther,
+    geographiesTargetedNote,
+    participationOptions,
+    participationOptionsNote,
+    agreementTypes,
+    agreementTypesOther,
+    multiplePatricipationAgreementsNeeded,
+    multiplePatricipationAgreementsNeededNote,
+    rulemakingRequired,
+    rulemakingRequiredDescription,
+    rulemakingRequiredNote,
+    authorityAllowances,
+    authorityAllowancesOther,
+    authorityAllowancesNote,
+    waiversRequired,
+    waiversRequiredTypes,
+    waiversRequiredNote,
+    status
+  } = data?.modelPlan?.generalCharacteristics || {};
+
+  return (
+    <div
+      className="read-only-model-plan--model-basics"
+      data-testid="read-only-model-plan--model-basics"
+    >
+      <div className="display-flex flex-justify flex-align-start">
+        <h2 className="margin-top-0 margin-bottom-4">{t('heading')}</h2>
+        {status && <TaskListStatusTag status={status} />}
+      </div>
+
+      <ReadOnlySection
+        heading={t('isNewModel')}
+        copy={isNewModel ? t('newModel') : t('newTrack')}
+      />
+
+      <ReadOnlySection heading={t('whichExistingModel')} copy={existingModel} />
+
+      <ReadOnlySection
+        heading={t('resembleModel')}
+        copy={resemblesExistingModel ? h('yes') : h('no')}
+      />
+
+      <ReadOnlySection
+        heading={t('modelResemblance')}
+        list
+        listItems={resemblesExistingModelWhich}
+      />
+
+      <ReadOnlySection
+        heading={t('waysResembleModel')}
+        copy={resemblesExistingModelHow}
+      />
+
+      <ReadOnlySection
+        heading={t('differentComponents')}
+        copy={hasComponentsOrTracks ? h('yes') : h('no')}
+      />
+
+      <ReadOnlySection
+        heading={t('differentComponents')}
+        copy={hasComponentsOrTracks ? h('yes') : h('no')}
+      />
+
+      <ReadOnlySection
+        heading={t('tracksDiffer')}
+        copy={hasComponentsOrTracksDiffer}
+      />
+    </div>
+  );
 };
 
 export default ReadOnlyGeneralCharacteristics;
