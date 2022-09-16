6279e9fd5e7190b993cce644683b54be8a258c97
ClayBenson94 <clay.benson@oddball.io>
2022-09-16 13:36:31 -0400

    Single query to get cr/tdl

--- pkg/graph/generated/generated.go
+++ pkg/graph/generated/generated.go
@@ -701,6 +701,7 @@ type ComplexityRoot struct {
 
 	Query struct {
 		CedarPersonsByCommonName  func(childComplexity int, commonName string) int
+		CrTdl                     func(childComplexity int, id uuid.UUID) int
 		CurrentUser               func(childComplexity int) int
 		ExistingModelCollection   func(childComplexity int) int
 		ModelPlan                 func(childComplexity int, id uuid.UUID) int
@@ -940,6 +941,7 @@ type QueryResolver interface {
 	TaskListSectionLocks(ctx context.Context, modelPlanID uuid.UUID) ([]*model.TaskListSectionLockStatus, error)
 	PlanPayments(ctx context.Context, id uuid.UUID) (*models.PlanPayments, error)
 	NdaInfo(ctx context.Context) (*model.NDAInfo, error)
+	CrTdl(ctx context.Context, id uuid.UUID) (*models.PlanCrTdl, error)
 }
 type SubscriptionResolver interface {
 	OnTaskListSectionLocksChanged(ctx context.Context, modelPlanID uuid.UUID) (<-chan *model.TaskListSectionLockStatusChanged, error)
@@ -5156,6 +5158,18 @@ func (e *executableSchema) Complexity(typeName, field string, childComplexity in
 
 		return e.complexity.Query.CedarPersonsByCommonName(childComplexity, args["commonName"].(string)), true
 
+	case "Query.crTdl":
+		if e.complexity.Query.CrTdl == nil {
+			break
+		}
+
+		args, err := ec.field_Query_crTdl_args(context.TODO(), rawArgs)
+		if err != nil {
+			return 0, false
+		}
+
+		return e.complexity.Query.CrTdl(childComplexity, args["id"].(uuid.UUID)), true
+
 	case "Query.currentUser":
 		if e.complexity.Query.CurrentUser == nil {
 			break
@@ -6824,6 +6838,7 @@ type Query {
   taskListSectionLocks(modelPlanID: UUID!): [TaskListSectionLockStatus!]!
   planPayments(id: UUID!): PlanPayments!
   ndaInfo: NDAInfo!
+  crTdl(id: UUID!): PlanCrTdl!
 }
 
 """
@@ -8196,6 +8211,21 @@ func (ec *executionContext) field_Query_cedarPersonsByCommonName_args(ctx contex
 		}
 	}
 	args["commonName"] = arg0
+	return args, nil
+}
+
+func (ec *executionContext) field_Query_crTdl_args(ctx context.Context, rawArgs map[string]interface{}) (map[string]interface{}, error) {
+	var err error
+	args := map[string]interface{}{}
+	var arg0 uuid.UUID
+	if tmp, ok := rawArgs["id"]; ok {
+		ctx := graphql.WithPathContext(ctx, graphql.NewPathWithField("id"))
+		arg0, err = ec.unmarshalNUUID2githubᚗcomᚋgoogleᚋuuidᚐUUID(ctx, tmp)
+		if err != nil {
+			return nil, err
+		}
+	}
+	args["id"] = arg0
 	return args, nil
 }
 
@@ -36955,6 +36985,83 @@ func (ec *executionContext) fieldContext_Query_ndaInfo(ctx context.Context, fiel
 	return fc, nil
 }
 
+func (ec *executionContext) _Query_crTdl(ctx context.Context, field graphql.CollectedField) (ret graphql.Marshaler) {
+	fc, err := ec.fieldContext_Query_crTdl(ctx, field)
+	if err != nil {
+		return graphql.Null
+	}
+	ctx = graphql.WithFieldContext(ctx, fc)
+	defer func() {
+		if r := recover(); r != nil {
+			ec.Error(ctx, ec.Recover(ctx, r))
+			ret = graphql.Null
+		}
+	}()
+	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
+		ctx = rctx // use context from middleware stack in children
+		return ec.resolvers.Query().CrTdl(rctx, fc.Args["id"].(uuid.UUID))
+	})
+	if err != nil {
+		ec.Error(ctx, err)
+		return graphql.Null
+	}
+	if resTmp == nil {
+		if !graphql.HasFieldError(ctx, fc) {
+			ec.Errorf(ctx, "must not be null")
+		}
+		return graphql.Null
+	}
+	res := resTmp.(*models.PlanCrTdl)
+	fc.Result = res
+	return ec.marshalNPlanCrTdl2ᚖgithubᚗcomᚋcmsgovᚋmintᚑappᚋpkgᚋmodelsᚐPlanCrTdl(ctx, field.Selections, res)
+}
+
+func (ec *executionContext) fieldContext_Query_crTdl(ctx context.Context, field graphql.CollectedField) (fc *graphql.FieldContext, err error) {
+	fc = &graphql.FieldContext{
+		Object:     "Query",
+		Field:      field,
+		IsMethod:   true,
+		IsResolver: true,
+		Child: func(ctx context.Context, field graphql.CollectedField) (*graphql.FieldContext, error) {
+			switch field.Name {
+			case "id":
+				return ec.fieldContext_PlanCrTdl_id(ctx, field)
+			case "modelPlanID":
+				return ec.fieldContext_PlanCrTdl_modelPlanID(ctx, field)
+			case "idNumber":
+				return ec.fieldContext_PlanCrTdl_idNumber(ctx, field)
+			case "dateInitiated":
+				return ec.fieldContext_PlanCrTdl_dateInitiated(ctx, field)
+			case "title":
+				return ec.fieldContext_PlanCrTdl_title(ctx, field)
+			case "note":
+				return ec.fieldContext_PlanCrTdl_note(ctx, field)
+			case "createdBy":
+				return ec.fieldContext_PlanCrTdl_createdBy(ctx, field)
+			case "createdDts":
+				return ec.fieldContext_PlanCrTdl_createdDts(ctx, field)
+			case "modifiedBy":
+				return ec.fieldContext_PlanCrTdl_modifiedBy(ctx, field)
+			case "modifiedDts":
+				return ec.fieldContext_PlanCrTdl_modifiedDts(ctx, field)
+			}
+			return nil, fmt.Errorf("no field named %q was found under type PlanCrTdl", field.Name)
+		},
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			err = ec.Recover(ctx, r)
+			ec.Error(ctx, err)
+		}
+	}()
+	ctx = graphql.WithFieldContext(ctx, fc)
+	if fc.Args, err = ec.field_Query_crTdl_args(ctx, field.ArgumentMap(ec.Variables)); err != nil {
+		ec.Error(ctx, err)
+		return
+	}
+	return fc, nil
+}
+
 func (ec *executionContext) _Query___type(ctx context.Context, field graphql.CollectedField) (ret graphql.Marshaler) {
 	fc, err := ec.fieldContext_Query___type(ctx, field)
 	if err != nil {
@@ -44551,6 +44658,29 @@ func (ec *executionContext) _Query(ctx context.Context, sel ast.SelectionSet) gr
 					}
 				}()
 				res = ec._Query_ndaInfo(ctx, field)
+				if res == graphql.Null {
+					atomic.AddUint32(&invalids, 1)
+				}
+				return res
+			}
+
+			rrm := func(ctx context.Context) graphql.Marshaler {
+				return ec.OperationContext.RootResolverMiddleware(ctx, innerFunc)
+			}
+
+			out.Concurrently(i, func() graphql.Marshaler {
+				return rrm(innerCtx)
+			})
+		case "crTdl":
+			field := field
+
+			innerFunc := func(ctx context.Context) (res graphql.Marshaler) {
+				defer func() {
+					if r := recover(); r != nil {
+						ec.Error(ctx, ec.Recover(ctx, r))
+					}
+				}()
+				res = ec._Query_crTdl(ctx, field)
 				if res == graphql.Null {
 					atomic.AddUint32(&invalids, 1)
 				}
