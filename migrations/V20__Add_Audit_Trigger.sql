52a9e9a784f6bbe1bfbf04c0a0fdf7322918257a
Steven Wade <steven.wade@oddball.io>
2022-09-20 16:36:16 -0700

    WIP no longer write to old or new, just fields. Debating if  should show all fields on insert and delete

--- migrations/V20__Add_Audit_Trigger.sql
+++ migrations/V20__Add_Audit_Trigger.sql
@@ -13,7 +13,9 @@ DECLARE
     created_by_f TEXT;
     modified_by_f TEXT;
     h_changed HSTORE;
+    h_changedOld HSTORE;
     unchanged_keys text[] = ARRAY[]::text[];
+    changeJSON JSONB;
 BEGIN
 
     IF TG_WHEN <> 'AFTER' THEN
@@ -43,13 +45,28 @@ BEGIN
     h_old= hstore(OLD.*);
     h_changed = (h_new - h_old) - excluded_cols; --remove matching values and excluded columns
     unchanged_keys = akeys(h_old - akeys(h_changed));
+    h_changedOld = h_old - unchanged_keys;
 
     IF TG_OP = 'UPDATE' AND h_changed = hstore('') THEN
        RETURN NULL;
     END IF;  -- All changed fields are ignored. Skip this update.
-            
-  
+        WITH NEWval AS
+        (
+            SELECT (EACH(h_changed)).*
+        )
+        ,RESULTSet AS 
+        (
+            SELECT 
+            NEWval.key AS field,
+            NEWval.value AS new,
+            h_changedOld -> NEWval.key AS old --TODO, do we even need to get just these values if we are displaying in this way?
 
+            FROM NEWval
+        )
+        SELECT jsonb_object_agg(field,(to_jsonb(r) - 'field'))
+        INTO changeJSON
+        -- INTO audit_row.fields
+            FROM RESULTSet r;
     audit_row = ROW (
     nextval('audit.change_id_seq'), --id ---TODO make this happen after the check to skip, we don't want to increment the series otherwise
         table_id, --table_id
@@ -58,50 +75,31 @@ BEGIN
         substring(TG_OP,1,1), --action
         NULL, --old
         NULL, --new
-        NULL, --fields
+        changeJSON, --fields
         NEW.modified_by, --modified_by
         CURRENT_TIMESTAMP --modified_dts
     );
     IF (TG_OP = 'UPDATE' AND TG_LEVEL = 'ROW') THEN
         audit_row.primary_key = h_new -> pkey_f;
         audit_row.foreign_key = h_new -> fkey_f;
-        -- audit_row.old = hstore(OLD.*) - excluded_cols;
-        -- audit_row.new =  (h_new - h_old) - excluded_cols; --remove matching values and excluded columns
-        audit_row.new =  h_changed; --remove matching values and excluded columns
+        -- audit_row.new =  h_changed; --remove matching values and excluded columns
 
-        audit_row.old = h_old - unchanged_keys; --remove any key not in new
+        -- audit_row.old = h_changedOld; --remove any key not in new
+        -- audit_row.fields = changeJSON;
 
-        WITH NEWval AS
-        (
-            SELECT (EACH(audit_row.new)).*
-        )
-        ,RESULTSet AS 
-        (
-            SELECT 
-            NEWval.key AS field,
-            NEWval.value AS new,
-            audit_row.old -> NEWval.key AS old
 
-            FROM NEWval
-        )
-        SELECT jsonb_object_agg(field,(to_jsonb(r) - 'field'))
-        INTO audit_row.fields
-            FROM RESULTSet r;
-        IF audit_row.new = hstore('') THEN
-            -- All changed fields are ignored. Skip this update.
-            RETURN NULL;
-        END IF;
+
     ELSIF (TG_OP = 'DELETE' AND TG_LEVEL = 'ROW') THEN
         audit_row.modified_by = OLD.modified_by; --Should this be the case?
         audit_row.primary_key = h_old -> pkey_f;
         audit_row.foreign_key = h_old -> fkey_f;
-        audit_row.old = hstore(OLD.*) - excluded_cols;
+        -- audit_row.old = hstore(OLD.*) - excluded_cols;
     ELSIF (TG_OP = 'INSERT' AND TG_LEVEL = 'ROW') THEN
         audit_row.primary_key = h_new -> pkey_f;
         audit_row.foreign_key = h_new -> fkey_f;
         audit_row.modified_by = NEW.created_by;
         audit_row.modified_dts = NEW.created_dts;
-        audit_row.new = hstore(NEW.*) - excluded_cols;
+        -- audit_row.new = hstore(NEW.*) - excluded_cols;
     END IF;
 
     INSERT INTO audit.change VALUES(audit_row.*);
