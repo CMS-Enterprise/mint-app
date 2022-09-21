efe94a12549069e8d46c8f15dfea5895a09db005
Steven Wade <99294987+StevenWadeOddball@users.noreply.github.com>
2022-09-21 09:07:24 -0700

    Noref/embedded struct constructors (#199)
    * Make base struct, modelplanrelation, and discussion relation private
    
    * make baseTaskList Section private to models package
    
    * update crtdl to use constructors
    
    * remove commented code blocks
    
    * improved constructor for ModelPlan
    
    * expand plan collaborator constructor
    
    * discussion and reply constructor enhancements
    
    * Clarify the comments on NewStruct

--- pkg/models/plan_discussion.go
+++ pkg/models/plan_discussion.go
@@ -1,23 +1,44 @@
 package models
 
-// PlanDiscussion represents a discussion that a user has about a model plan
+import "github.com/google/uuid"
+
+//PlanDiscussion represents a discussion that a user has about a model plan
 type PlanDiscussion struct {
-	BaseStruct
-	ModelPlanRelation
+	baseStruct
+	modelPlanRelation
 	Content string           `json:"content" db:"content"`
 	Status  DiscussionStatus `json:"status" db:"status"`
 }
 
-// DiscussionReply represents a comment that was made on the PlanDiscussion
+//NewPlanDiscussion returns a New PlanDiscussion with a status of UNANSWERED
+func NewPlanDiscussion(createdBy string, modelPlanID uuid.UUID, content string) *PlanDiscussion {
+	return &PlanDiscussion{
+		Content:           content,
+		Status:            DiscussionUnAnswered,
+		modelPlanRelation: NewModelPlanRelation(modelPlanID),
+		baseStruct:        NewBaseStruct(createdBy),
+	}
+}
+
+//DiscussionReply represents a comment that was made on the PlanDiscussion
 type DiscussionReply struct {
-	BaseStruct
-	DiscussionRelation
-	// DiscussionID uuid.UUID `json:"discussionID" db:"discussion_id"`
+	baseStruct
+	discussionRelation
 	Content    string `json:"content" db:"content"`
 	Resolution bool   `json:"resolution" db:"resolution"` //default to false
 }
 
-// DiscussionStatus is an enum that represents the status of a Discussion
+//NewDiscussionReply returns a new Discussion Reply
+func NewDiscussionReply(createdBy string, discussionID uuid.UUID, content string, resolution bool) *DiscussionReply {
+	return &DiscussionReply{
+		Content:            content,
+		Resolution:         resolution,
+		discussionRelation: NewDiscussionRelation(discussionID),
+		baseStruct:         NewBaseStruct(createdBy),
+	}
+}
+
+//DiscussionStatus is an enum that represents the status of a Discussion
 type DiscussionStatus string
 
 // These constants represent the possible values of a DiscussionStatus
