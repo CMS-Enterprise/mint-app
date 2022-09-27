d432bf149b9a2edcf2f404b0b2c4e5d6e4982fa7
Gary Zhao <gary.j.zhao@gmail.com>
2022-09-27 14:13:33 -0400

    work

--- src/views/ModelPlan/ReadOnly/Team/index.tsx
+++ src/views/ModelPlan/ReadOnly/Team/index.tsx
@@ -12,14 +12,56 @@ import SectionWrapper from 'components/shared/SectionWrapper';
 import GetModelPlanCollaborators from 'queries/Collaborators/GetModelCollaborators';
 import {
   GetModelCollaborators,
-  GetModelCollaborators_modelPlan_collaborators as GetCollaboratorsType
+  GetModelCollaborators_modelPlan_collaborators as CollaboratorsType
 } from 'queries/Collaborators/types/GetModelCollaborators';
 import { TeamRole } from 'types/graphql-global-types';
+import { translateTeamRole } from 'utils/modelPlan';
 import { NotFoundPartial } from 'views/NotFound';
 
-const ReadOnlyTeamInfo = ({ modelID }: { modelID: string }) => {
-  const { t } = useTranslation('modelPlan');
+const TeamGroupings = ({
+  role,
+  collaborators
+}: {
+  role: TeamRole;
+  collaborators: CollaboratorsType[];
+}) => {
+  const { t } = useTranslation('generalReadOnly');
+  return (
+    <SectionWrapper className="padding-bottom-3 border-bottom-1px border-base-light margin-bottom-4">
+      <h2 className="margin-top-0 margin-bottom-4">
+        {role === TeamRole.MODEL_LEAD
+          ? t('contactInfo.modelLeads')
+          : translateTeamRole(role)}
+      </h2>
+      {collaborators
+        .filter(c => c.teamRole === role)
+        .map(collaborator => {
+          return (
+            <Card
+              containerProps={{
+                className: 'radius-md padding-2 margin-bottom-3 margin-x-0'
+              }}
+            >
+              <CardHeader className="padding-0">
+                <h3 className="margin-0">{collaborator.fullName}</h3>
+                <Link
+                  aria-label={collaborator.email}
+                  className="margin-0 line-height-body-5"
+                  href={`mailto:${collaborator.email}`}
+                  target="_blank"
+                >
+                  {collaborator.email}
+                  <IconMailOutline className="margin-left-05 margin-bottom-2px text-tbottom" />
+                </Link>
+              </CardHeader>
+            </Card>
+          );
+        })}
+    </SectionWrapper>
+  );
+};
 
+const ReadOnlyTeamInfo = ({ modelID }: { modelID: string }) => {
   const { data, loading, error } = useQuery<GetModelCollaborators>(
     GetModelPlanCollaborators,
     {
@@ -34,42 +76,18 @@ const ReadOnlyTeamInfo = ({ modelID }: { modelID: string }) => {
   }
 
   const collaborators = (data?.modelPlan?.collaborators ??
-    []) as GetCollaboratorsType[];
+    []) as CollaboratorsType[];
 
   return (
     <div
-      className="read-only-model-plan--participants-and-providers"
-      data-testid="read-only-model-plan--participants-and-providers"
+      className="read-only-model-plan--team-info"
+      data-testid="read-only-model-plan--team-info"
     >
-      <SectionWrapper className="padding-bottom-3 border-bottom-1px border-base-light margin-bottom-4">
-        <h2 className="margin-top-0 margin-bottom-4">
-          {t('teamRoles.modelLead')}
-        </h2>
-        {collaborators
-          .filter(c => c.teamRole === TeamRole.MODEL_LEAD)
-          .map(collaborator => {
-            return (
-              <Card
-                containerProps={{
-                  className: 'radius-md padding-2 margin-bottom-3'
-                }}
-              >
-                <CardHeader className="padding-0">
-                  <h3 className="margin-0">{collaborator.fullName}</h3>
-                  <Link
-                    aria-label={collaborator.email}
-                    className="margin-0 line-height-body-5"
-                    href={`mailto:${collaborator.email}`}
-                    target="_blank"
-                  >
-                    {collaborator.email}
-                    <IconMailOutline className="margin-left-05 margin-bottom-2px text-tbottom" />
-                  </Link>
-                </CardHeader>
-              </Card>
-            );
-          })}
-      </SectionWrapper>
+      {collaborators.map(c => {
+        return (
+          <TeamGroupings role={c.teamRole} collaborators={collaborators} />
+        );
+      })}
     </div>
   );
 };
