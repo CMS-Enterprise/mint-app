84e114dfe74f75a9974e27738f1331d438f62654
Gary Zhao <gary.j.zhao@gmail.com>
2022-09-29 11:23:58 -0400

    wip 🚧

--- src/views/ModelPlan/ReadOnly/Payments/index.tsx
+++ src/views/ModelPlan/ReadOnly/Payments/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useQuery } from '@apollo/client';
 
+import GetAllPayments from 'queries/ReadOnly/GetAllPayments';
+import {
+  GetAllPayments as GetModelPlanPaymentType,
+  GetAllPayments_modelPlan_payments as PaymentTypes
+} from 'queries/ReadOnly/types/GetAllPayments';
 import { TaskListStatusTag } from 'views/ModelPlan/TaskList/_components/TaskListItem';
 import { NotFoundPartial } from 'views/NotFound';
 
@@ -8,8 +14,8 @@ const ReadOnlyPayments = ({ modelID }: { modelID: string }) => {
   const { t } = useTranslation('payments');
   const { t: h } = useTranslation('draftModelPlan');
 
-  const { data, loading, error } = useQuery<GetAllParticipantsTypes>(
-    GetAllParticipants,
+  const { data, loading, error } = useQuery<GetModelPlanPaymentType>(
+    GetAllPayments,
     {
       variables: {
         id: modelID
@@ -17,9 +23,73 @@ const ReadOnlyPayments = ({ modelID }: { modelID: string }) => {
     }
   );
 
-  if ((!loading && error) || (!loading && !data?.modelPlan)) {
+  if ((!loading && error) || (!loading && !data) || data === undefined) {
     return <NotFoundPartial />;
   }
+  // debugger;
+
+  const {
+    fundingSource,
+    fundingSourceTrustFund,
+    fundingSourceOther,
+    fundingSourceNote,
+    fundingSourceR,
+    fundingSourceRTrustFund,
+    fundingSourceROther,
+    fundingSourceRNote,
+    payRecipients,
+    payRecipientsOtherSpecification,
+    payRecipientsNote,
+    payType,
+    payTypeNote,
+    payClaims,
+    payClaimsOther,
+    payClaimsNote,
+    shouldAnyProvidersExcludedFFSSystems,
+    shouldAnyProviderExcludedFFSSystemsNote,
+    changesMedicarePhysicianFeeSchedule,
+    changesMedicarePhysicianFeeScheduleNote,
+    affectsMedicareSecondaryPayerClaims,
+    affectsMedicareSecondaryPayerClaimsHow,
+    affectsMedicareSecondaryPayerClaimsNote,
+    payModelDifferentiation,
+    creatingDependenciesBetweenServices,
+    creatingDependenciesBetweenServicesNote,
+    needsClaimsDataCollection,
+    needsClaimsDataCollectionNote,
+    providingThirdPartyFile,
+    isContractorAwareTestDataRequirements,
+    beneficiaryCostSharingLevelAndHandling,
+    waiveBeneficiaryCostSharingForAnyServices,
+    waiveBeneficiaryCostSharingServiceSpecification,
+    waiverOnlyAppliesPartOfPayment,
+    waiveBeneficiaryCostSharingNote,
+    nonClaimsPayments,
+    nonClaimsPaymentOther,
+    paymentCalculationOwner,
+    numberPaymentsPerPayCycle,
+    numberPaymentsPerPayCycleNote,
+    sharedSystemsInvolvedAdditionalClaimPayment,
+    sharedSystemsInvolvedAdditionalClaimPaymentNote,
+    planningToUseInnovationPaymentContractor,
+    planningToUseInnovationPaymentContractorNote,
+    fundingStructure,
+    expectedCalculationComplexityLevel,
+    expectedCalculationComplexityLevelNote,
+    canParticipantsSelectBetweenPaymentMechanisms,
+    canParticipantsSelectBetweenPaymentMechanismsHow,
+    canParticipantsSelectBetweenPaymentMechanismsNote,
+    anticipatedPaymentFrequency,
+    anticipatedPaymentFrequencyOther,
+    anticipatedPaymentFrequencyNote,
+    willRecoverPayments,
+    willRecoverPaymentsNote,
+    anticipateReconcilingPaymentsRetrospectively,
+    anticipateReconcilingPaymentsRetrospectivelyNote,
+    paymentStartDate,
+    paymentStartDateNote,
+    status
+  }: PaymentTypes = data.modelPlan.payments;
 
   return (
     <div
@@ -28,7 +98,7 @@ const ReadOnlyPayments = ({ modelID }: { modelID: string }) => {
     >
       <div className="display-flex flex-justify flex-align-start">
         <h2 className="margin-top-0 margin-bottom-4">{t('heading')}</h2>
-        {/* {status && <TaskListStatusTag status={status} />} */}
+        <TaskListStatusTag status={status} />
       </div>
     </div>
   );
