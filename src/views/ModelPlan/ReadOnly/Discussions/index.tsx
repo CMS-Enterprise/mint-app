ae8afa379f528fd5f2dc9cfcac5c4db2560b5682
Gary Zhao <gary.j.zhao@gmail.com>
2022-10-06 10:35:32 -0400

    testing

--- src/views/ModelPlan/ReadOnly/Discussions/index.tsx
+++ src/views/ModelPlan/ReadOnly/Discussions/index.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+
+import Discussions from 'views/ModelPlan/Discussions';
 
 const ReadOnlyDiscussions = ({ modelID }: { modelID: string }) => {
   const { t } = useTranslation('discussions');
+  const [isDiscussionOpen, setIsDiscussionOpen] = useState(true);
 
   return (
     <div
@@ -12,6 +15,12 @@ const ReadOnlyDiscussions = ({ modelID }: { modelID: string }) => {
       <div className="display-flex flex-justify flex-align-start">
         <h2 className="margin-top-0 margin-bottom-4">{t('heading')}</h2>
       </div>
+
+      <Discussions
+        modelID={modelID}
+        isOpen={isDiscussionOpen}
+        closeModal={() => setIsDiscussionOpen(false)}
+      />
     </div>
   );
 };
