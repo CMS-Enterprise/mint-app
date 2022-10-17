0b9120e6fdd1d371d9887c6eb661ed12f3a30fce
Gary Zhao <gary.j.zhao@gmail.com>
2022-10-17 13:37:37 -0400

    Read Only CRTDLs page

---
+++ src/views/ModelPlan/ReadOnly/CRTDLs/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ReadOnlyCRTDLs = ({ modelID }: { modelID: string }) => {
+  return (
+    <div
+      className="read-only-model-plan--discussions"
+      data-testid="read-only-model-plan--discussions"
+    >
+      <h1>hello CRTDLs</h1>
+    </div>
+  );
+};
+
+export default ReadOnlyCRTDLs;
