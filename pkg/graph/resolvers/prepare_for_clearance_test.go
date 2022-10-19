fec388abc709ef91b043623a6ec4573b739afe57
ClayBenson94 <clay.benson@oddball.io>
2022-10-19 09:15:06 -0400

    Added go integration test for ready for clearance

--- pkg/graph/resolvers/prepare_for_clearance_test.go
+++ pkg/graph/resolvers/prepare_for_clearance_test.go
@@ -29,7 +29,131 @@ func getTestDates() *testDates {
 
 // TestReadyForClearanceRead tests ReadyForClearanceRead
 func (suite *ResolverSuite) TestReadyForClearanceRead() {
-	// TODO: Write this out :)
+	plan := suite.createModelPlan("Plan for Clearance")
+
+	// No updates to model plan - shouldn't be ready to start
+	planClearance, err := ReadyForClearanceRead(suite.testConfigs.Logger, suite.testConfigs.Store, plan.ID)
+	suite.NoError(err)
+	suite.EqualValues(model.PrepareForClearanceStatusCannotStart, planClearance.Status)
+	suite.Nil(planClearance.LatestClearanceDts)
+
+	// Update the basics to have a clearance date set that's too far out (more than 20 days)
+	basics, err := PlanBasicsGetByModelPlanID(suite.testConfigs.Logger, plan.ID, suite.testConfigs.Store)
+	suite.NoError(err)
+	_, err = UpdatePlanBasics(suite.testConfigs.Logger, basics.ID, map[string]interface{}{
+		"clearanceStarts": time.Now().Add(time.Hour * 24 * 21).Format(time.RFC3339), // 21 days from now
+	}, suite.testConfigs.Principal, suite.testConfigs.Store)
+	suite.NoError(err)
+	// Clearance start date is 21 days out, still shouldn't be ready to start
+	planClearance, err = ReadyForClearanceRead(suite.testConfigs.Logger, suite.testConfigs.Store, plan.ID)
+	suite.NoError(err)
+	suite.EqualValues(model.PrepareForClearanceStatusCannotStart, planClearance.Status)
+	suite.Nil(planClearance.LatestClearanceDts)
+
+	// Update the basics to have a clearance date set that's within the date range (15 days)
+	basics, err = PlanBasicsGetByModelPlanID(suite.testConfigs.Logger, plan.ID, suite.testConfigs.Store)
+	suite.NoError(err)
+	_, err = UpdatePlanBasics(suite.testConfigs.Logger, basics.ID, map[string]interface{}{
+		"clearanceStarts": time.Now().Add(time.Hour * 24 * 15).Format(time.RFC3339), // 15 days from now
+	}, suite.testConfigs.Principal, suite.testConfigs.Store)
+	suite.NoError(err)
+	// Clearance start date is 15 days out - should be "Ready to Start"
+	planClearance, err = ReadyForClearanceRead(suite.testConfigs.Logger, suite.testConfigs.Store, plan.ID)
+	suite.NoError(err)
+	suite.EqualValues(model.PrepareForClearanceStatusReady, planClearance.Status)
+	suite.Nil(planClearance.LatestClearanceDts)
+
+	// Update the basics to be marked ready for clearance
+	basics, err = PlanBasicsGetByModelPlanID(suite.testConfigs.Logger, plan.ID, suite.testConfigs.Store)
+	suite.NoError(err)
+	_, err = UpdatePlanBasics(suite.testConfigs.Logger, basics.ID, map[string]interface{}{
+		"status": model.TaskStatusInputReadyForClearance,
+	}, suite.testConfigs.Principal, suite.testConfigs.Store)
+	suite.NoError(err)
+	// Clearance start date is 15 days out & we've started by marking Basics as ready for clearance - should be "In Progress"
+	planClearance, err = ReadyForClearanceRead(suite.testConfigs.Logger, suite.testConfigs.Store, plan.ID)
+	suite.NoError(err)
+	suite.EqualValues(model.PrepareForClearanceStatusInProgress, planClearance.Status)
+	suite.NotNil(planClearance.LatestClearanceDts)
+	// Save the latest clearance DTS, as the next time we update one, it should be later than this one
+	// Yes, this _IS_ sketchy, as it's possible this test flakes if both the previous setting of the clearanceDts and the next
+	// happen within the same millisecond. I just assume that's not going to happen :)
+	previousLatestClearanceDts := *planClearance.LatestClearanceDts
+
+	// Update the general characteristics to be marked ready for clearance
+	genChar, err := FetchPlanGeneralCharacteristicsByModelPlanID(suite.testConfigs.Logger, plan.ID, suite.testConfigs.Store)
+	suite.NoError(err)
+	_, err = UpdatePlanGeneralCharacteristics(suite.testConfigs.Logger, genChar.ID, map[string]interface{}{
+		"status": model.TaskStatusInputReadyForClearance,
+	}, suite.testConfigs.Principal, suite.testConfigs.Store)
+	suite.NoError(err)
+	// Clearance start date is 15 days out & we've marked General Characteristics as ready for clearance - should still be "In Progress"
+	planClearance, err = ReadyForClearanceRead(suite.testConfigs.Logger, suite.testConfigs.Store, plan.ID)
+	suite.NoError(err)
+	suite.EqualValues(model.PrepareForClearanceStatusInProgress, planClearance.Status)
+	suite.NotNil(planClearance.LatestClearanceDts)
+	suite.True(planClearance.LatestClearanceDts.After(previousLatestClearanceDts)) // this new clearanceDts should be after the last one we saw
+	previousLatestClearanceDts = *planClearance.LatestClearanceDts
+
+	// Update the participants and providers to be marked ready for clearance
+	participants, err := PlanParticipantsAndProvidersGetByModelPlanID(suite.testConfigs.Logger, plan.ID, suite.testConfigs.Store)
+	suite.NoError(err)
+	_, err = PlanParticipantsAndProvidersUpdate(suite.testConfigs.Logger, participants.ID, map[string]interface{}{
+		"status": model.TaskStatusInputReadyForClearance,
+	}, suite.testConfigs.Principal, suite.testConfigs.Store)
+	suite.NoError(err)
+	// Clearance start date is 15 days out & we've marked Participants & Providers as ready for clearance - should still be "In Progress"
+	planClearance, err = ReadyForClearanceRead(suite.testConfigs.Logger, suite.testConfigs.Store, plan.ID)
+	suite.NoError(err)
+	suite.EqualValues(model.PrepareForClearanceStatusInProgress, planClearance.Status)
+	suite.NotNil(planClearance.LatestClearanceDts)
+	suite.True(planClearance.LatestClearanceDts.After(previousLatestClearanceDts)) // this new clearanceDts should be after the last one we saw
+	previousLatestClearanceDts = *planClearance.LatestClearanceDts
+
+	// Update the participants and providers to be marked ready for clearance
+	benes, err := PlanBeneficiariesGetByModelPlanID(suite.testConfigs.Logger, plan.ID, suite.testConfigs.Store)
+	suite.NoError(err)
+	_, err = PlanBeneficiariesUpdate(suite.testConfigs.Logger, benes.ID, map[string]interface{}{
+		"status": model.TaskStatusInputReadyForClearance,
+	}, suite.testConfigs.Principal, suite.testConfigs.Store)
+	suite.NoError(err)
+	// Clearance start date is 15 days out & we've marked Beneficiaries as ready for clearance - should still be "In Progress"
+	planClearance, err = ReadyForClearanceRead(suite.testConfigs.Logger, suite.testConfigs.Store, plan.ID)
+	suite.NoError(err)
+	suite.EqualValues(model.PrepareForClearanceStatusInProgress, planClearance.Status)
+	suite.NotNil(planClearance.LatestClearanceDts)
+	suite.True(planClearance.LatestClearanceDts.After(previousLatestClearanceDts)) // this new clearanceDts should be after the last one we saw
+	previousLatestClearanceDts = *planClearance.LatestClearanceDts
+
+	// Update the ops, eval, & learning to be marked ready for clearance
+	opsEvalLearning, err := PlanOpsEvalAndLearningGetByModelPlanID(suite.testConfigs.Logger, plan.ID, suite.testConfigs.Store)
+	suite.NoError(err)
+	_, err = PlanOpsEvalAndLearningUpdate(suite.testConfigs.Logger, opsEvalLearning.ID, map[string]interface{}{
+		"status": model.TaskStatusInputReadyForClearance,
+	}, suite.testConfigs.Principal, suite.testConfigs.Store)
+	suite.NoError(err)
+	// Clearance start date is 15 days out & we've marked ops, eval, & learning as ready for clearance - should still be "In Progress"
+	planClearance, err = ReadyForClearanceRead(suite.testConfigs.Logger, suite.testConfigs.Store, plan.ID)
+	suite.NoError(err)
+	suite.EqualValues(model.PrepareForClearanceStatusInProgress, planClearance.Status)
+	suite.NotNil(planClearance.LatestClearanceDts)
+	suite.True(planClearance.LatestClearanceDts.After(previousLatestClearanceDts)) // this new clearanceDts should be after the last one we saw
+	previousLatestClearanceDts = *planClearance.LatestClearanceDts
+
+	// Update the payments to be marked ready for clearance
+	// NOTE: This is the final section, so we expect the status to change!
+	payments, err := PlanPaymentsReadByModelPlan(suite.testConfigs.Logger, suite.testConfigs.Store, plan.ID)
+	suite.NoError(err)
+	_, err = PlanPaymentsUpdate(suite.testConfigs.Logger, suite.testConfigs.Store, payments.ID, map[string]interface{}{
+		"status": model.TaskStatusInputReadyForClearance,
+	}, suite.testConfigs.Principal)
+	suite.NoError(err)
+	// Clearance start date is 15 days out & we've marked payments as ready for clearance - should finally be "Ready for Clearance"
+	planClearance, err = ReadyForClearanceRead(suite.testConfigs.Logger, suite.testConfigs.Store, plan.ID)
+	suite.NoError(err)
+	suite.EqualValues(model.PrepareForClearanceStatusReadyForClearance, planClearance.Status)
+	suite.NotNil(planClearance.LatestClearanceDts)
+	suite.True(planClearance.LatestClearanceDts.After(previousLatestClearanceDts)) // this new clearanceDts should be after the last one we saw
 }
 
 // TestCalculateStatusNoClearanceDate tests calculateStatus in the case that it should return a status of
