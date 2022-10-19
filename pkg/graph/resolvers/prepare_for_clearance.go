afa4a870033c9371527bb0de7941f7505f7acb2f
Clay Benson <clay.benson@oddball.io>
2022-10-19 14:33:44 -0400

    [NOREF] Prepare for clearance statuses + GQL (#270)
    * update SQL fluff rules
    
    * Query to get ready for clearance information by model_plan_id
    
    * Prepare for clearance GQL + tests
    
    * Added go integration test for ready for clearance
    
    Co-authored-by: Steven Wade <steven.wade@oddball.io>

---
+++ pkg/graph/resolvers/prepare_for_clearance.go
@@ -0,0 +1,58 @@
+package resolvers
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"go.uber.org/zap"
+
+	"github.com/cmsgov/mint-app/pkg/graph/model"
+	"github.com/cmsgov/mint-app/pkg/models"
+	"github.com/cmsgov/mint-app/pkg/storage"
+)
+
+// ReadyForClearanceRead handles requests to read a information about a Model Plan's "Ready for Clearance" section
+func ReadyForClearanceRead(
+	logger *zap.Logger,
+	store *storage.Store,
+	modelPlanID uuid.UUID,
+) (*model.PrepareForClearance, error) {
+	clearanceData, err := store.ReadyForClearanceGetByModelPlanID(logger, modelPlanID)
+	if err != nil {
+		return nil, err
+	}
+
+	result := model.PrepareForClearance{
+		Status:             calculateStatus(clearanceData, time.Now()),
+		LatestClearanceDts: clearanceData.MostRecentClearanceDts,
+	}
+
+	return &result, nil
+}
+
+func calculateStatus(clearanceData *models.PrepareForClearanceResponse, now time.Time) model.PrepareForClearanceStatus {
+	// If we don't yet have a clearance date, we cannot start this section yet!
+	if clearanceData.BasicsClearanceStarts == nil {
+		return model.PrepareForClearanceStatusCannotStart
+	}
+
+	// We have a clearance date, so we can calculate 20 days prior to that date
+	// If we're before that calculated date, we can't start the section.
+	twentyDaysBeforeClearance := clearanceData.BasicsClearanceStarts.Add(-time.Hour * 24 * 20)
+	if now.Before(twentyDaysBeforeClearance) {
+		return model.PrepareForClearanceStatusCannotStart
+	}
+
+	// If every section is ready for clearance, the status is ready for clearance no matter what!
+	if clearanceData.AllReadyForClearance {
+		return model.PrepareForClearanceStatusReadyForClearance
+	}
+
+	// If there's anything in-progress (i.e. our latest clearance-time is not null) we're in progress
+	if clearanceData.MostRecentClearanceDts != nil {
+		return model.PrepareForClearanceStatusInProgress
+	}
+
+	// Otherwise, we haven't started yet!
+	return model.PrepareForClearanceStatusReady
+}
