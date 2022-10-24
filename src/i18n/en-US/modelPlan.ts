7cfd90b118f0f485b92b1d66e7baf76dc1105522
Gary Zhao <gary.j.zhao@gmail.com>
2022-10-24 15:29:12 -0400

    Add email unfollow success or failure copy

--- src/i18n/en-US/modelPlan.ts
+++ src/i18n/en-US/modelPlan.ts
@@ -33,7 +33,11 @@ const modelPlan = {
     toBeDetermined: 'To be determined',
     noneEntered: 'None entered',
     follow: 'Follow',
-    following: 'Following'
+    following: 'Following',
+    success:
+      'Success! You are no longer following {{-requestName}} and will not receive email notifications about this model.',
+    failure:
+      'There was an error unfollowing {{-requestName}}. Please try unfollowing the model again if you no longer wish to receive email notifications about it.'
   }
 };
 
