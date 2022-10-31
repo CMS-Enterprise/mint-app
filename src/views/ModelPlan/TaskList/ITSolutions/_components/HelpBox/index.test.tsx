f7a814bbc2503679397d54e58b7bf111438e29c0
Patrick Segura <patrick.segura@oddball.io>
2022-10-31 15:37:18 -0400

    Added unit tests

---
+++ src/views/ModelPlan/TaskList/ITSolutions/_components/HelpBox/index.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { render } from '@testing-library/react';
+
+import HelpBox from '.';
+
+describe('IT Solutions HelpBox', () => {
+  it('matches snapshot', async () => {
+    const { asFragment } = render(
+      <MemoryRouter
+        initialEntries={[
+          '/models/ce3405a0-3399-4e3a-88d7-3cfc613d2905/task-list/it-solutions'
+        ]}
+      >
+        <Route path="/models/:modelID/task-list/it-solutions">
+          <HelpBox />
+        </Route>
+      </MemoryRouter>
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
