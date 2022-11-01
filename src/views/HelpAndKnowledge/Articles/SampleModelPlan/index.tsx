89534930563d1ef4b9c7337fd63ee0c6225d7e98
Gary Zhao <gary.j.zhao@gmail.com>
2022-11-01 15:09:39 -0400

    Doing work on the sample model plan

--- src/views/HelpAndKnowledge/Articles/SampleModelPlan/index.tsx
+++ src/views/HelpAndKnowledge/Articles/SampleModelPlan/index.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Grid, GridContainer } from '@trussworks/react-uswds';
+
+import HelpBreadcrumb from 'components/HelpBreadcrumb';
+import MainContent from 'components/MainContent';
+import PageHeading from 'components/PageHeading';
+import RelatedArticles from 'components/RelatedArticles';
+import ReadOnly from 'views/ModelPlan/ReadOnly';
 
 export const SampleModelPlan = () => {
-  return <></>;
+  const { t } = useTranslation('sampleModelPlan');
+
+  return (
+    <>
+      <MainContent>
+        <GridContainer>
+          <Grid>
+            <HelpBreadcrumb text={t('title')} />
+            <PageHeading>{t('title')}</PageHeading>
+            <p className="font-body-lg line-height-sans-5 margin-top-0 margin-bottom-6">
+              {t('description')}
+            </p>
+            {/* insert read only model plan here */}
+          </Grid>
+        </GridContainer>
+      </MainContent>
+      <ReadOnly isHelpArticle />
+      <RelatedArticles currentArticle={t('title')} />
+    </>
+  );
 };
 
 export default SampleModelPlan;
