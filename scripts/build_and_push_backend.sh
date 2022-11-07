d3d0c51000a12d22b7852811e3c6516f22751a09
ClayBenson94 <clay.benson@oddball.io>
2022-11-07 08:30:15 -0500

    Build for linux/amd64

--- scripts/build_and_push_backend.sh
+++ scripts/build_and_push_backend.sh
@@ -16,5 +16,5 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 # Build the image
 APPLICATION_DATETIME="$(gdate --rfc-3339='seconds' --utc)"
 APPLICATION_TS="$(gdate --date="$APPLICATION_DATETIME" '+%s')"
-docker image build --quiet --build-arg ARG_APPLICATION_VERSION="$git_hash" --build-arg ARG_APPLICATION_DATETIME="$APPLICATION_DATETIME" --build-arg ARG_APPLICATION_TS="$APPLICATION_TS" --no-cache --tag "$ECR_REGISTRY/$ECR_REPOSITORY:$git_hash" .
+docker image build --platform=linux/amd64 --quiet --build-arg ARG_APPLICATION_VERSION="$git_hash" --build-arg ARG_APPLICATION_DATETIME="$APPLICATION_DATETIME" --build-arg ARG_APPLICATION_TS="$APPLICATION_TS" --no-cache --tag "$ECR_REGISTRY/$ECR_REPOSITORY:$git_hash" .
 docker image push "$ECR_REGISTRY/$ECR_REPOSITORY:$git_hash"
