365703210e01460b386c006ab788aed8ecd3bdf1
“AOS <aosnotes77@gmail.com>
2022-11-10 15:44:02 -0500

    updated the "update_ecs_task_definition" file

--- scripts/update_ecs_task_definition.sh
+++ scripts/update_ecs_task_definition.sh
@@ -21,6 +21,7 @@ TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition "$TASK_FAMI
 # Transform existing task definition by performing the following actions:
 # 1. Update the `containerDefinitions[0].image` to the new image we want to deploy
 # 2. Remove fields from the task definition that are not compatibile with `register-task-definition`'s --cli-input-json
+# Please https://github.com/aws/aws-cli/issues/3064#issuecomment-514214738
 NEW_TASK_DEFINTIION=$(echo "$TASK_DEFINITION" | jq --arg IMAGE "$ECR_IMAGE" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities) | del(.registeredAt) | del(.registeredBy)')
 
 # Register the new task, capture the output as JSON
@@ -33,3 +34,12 @@ NEW_REVISION=$(echo "$NEW_TASK_INFO" | jq '.taskDefinition.revision')
 aws ecs update-service --cluster "${ECS_CLUSTER}" \
                        --service "${SERVICE_NAME}" \
                        --task-definition "${TASK_FAMILY}:${NEW_REVISION}"
+
+# Run the healthcheck script 
+./healthcheck.sh
+
+# Grab the old revision from TASK_DEFINITION
+OLD_REVISION=$(echo "$TASK_DEFINITION" | jq '.taskDefinition.revision')
+
+# deregister the previous task definition
+aws ecs deregister-task-definition --task-definition "${TASK_FAMILY}:${OLD_REVISION}"
