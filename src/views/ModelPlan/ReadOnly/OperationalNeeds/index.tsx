17833c993f9c58efe541c4cf83e2618d3ab64550
Gary Zhao <gary.j.zhao@gmail.com>
2022-11-15 13:56:00 -0500

    ReadOnlyOperationalNeeds component init

---
+++ src/views/ModelPlan/ReadOnly/OperationalNeeds/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useQuery } from '@apollo/client';
+
+import GetOperationalNeeds from 'queries/ITSolutions/GetOperationalNeeds';
+import {
+  GetOperationalNeeds as GetOperationalNeedsType,
+  GetOperationalNeeds_modelPlan_operationalNeeds as OperationalNeedsType
+} from 'queries/ITSolutions/types/GetOperationalNeeds';
+import { TaskStatus } from 'types/graphql-global-types';
+import { TaskListStatusTag } from 'views/ModelPlan/TaskList/_components/TaskListItem';
+import { NotFoundPartial } from 'views/NotFound';
+
+const ReadOnlyOperationalNeeds = ({ modelID }: { modelID: string }) => {
+  const { t } = useTranslation('itSolutions');
+
+  const { data, loading, error } = useQuery<GetOperationalNeedsType>(
+    GetOperationalNeeds,
+    {
+      variables: {
+        id: modelID
+      }
+    }
+  );
+
+  if ((!loading && error) || (!loading && !data?.modelPlan)) {
+    return <NotFoundPartial />;
+  }
+
+  const getITSolutionsStatus = (
+    operationalNeedsArray: OperationalNeedsType[]
+  ) => {
+    const inProgress = operationalNeedsArray.find(need => need.modifiedDts);
+    return inProgress ? TaskStatus.IN_PROGRESS : TaskStatus.READY;
+  };
+
+  const { operationalNeeds } = data?.modelPlan || {};
+
+  return (
+    <div
+      className="read-only-model-plan--operational-needs"
+      data-testid="read-only-model-plan--operational-needs"
+    >
+      <div className="display-flex flex-justify flex-align-start">
+        <h2 className="margin-top-0 margin-bottom-4">{t('headingReadOnly')}</h2>
+
+        {operationalNeeds && (
+          <TaskListStatusTag status={getITSolutionsStatus(operationalNeeds)} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ReadOnlyOperationalNeeds;
