46f10926293cb4934318f7f74ec496d33b28629a
Steven Wade <steven.wade@oddball.io>
2022-11-16 14:31:27 -0800

    FEAT: add foreign keys and unique constraints

--- migrations/V36__Add_Plan_Document_Solution_Link.sql
+++ migrations/V36__Add_Plan_Document_Solution_Link.sql
@@ -9,3 +9,18 @@ CREATE TABLE plan_document_solution_link (
     modified_by EUA_ID,
     modified_dts TIMESTAMP WITH TIME ZONE
 );
+
+ALTER TABLE plan_document_solution_link
+ADD CONSTRAINT fk_doc_sol_sol FOREIGN KEY (solution_id)
+REFERENCES public.operational_solution (id) MATCH SIMPLE
+ON UPDATE NO ACTION
+ON DELETE NO ACTION;
+
+ALTER TABLE plan_document_solution_link
+ADD CONSTRAINT fk_doc_sol_doc FOREIGN KEY (document_id)
+REFERENCES public.plan_document (id) MATCH SIMPLE
+ON UPDATE NO ACTION
+ON DELETE NO ACTION;
+
+ALTER TABLE plan_document_solution_link
+ADD CONSTRAINT unique_document_id_and_solution_id UNIQUE (solution_id, document_id);
