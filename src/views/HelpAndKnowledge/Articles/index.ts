db5d52609153909348c4b2d7273a0459418def0d
Gary Zhao <gary.j.zhao@gmail.com>
2022-11-16 14:28:54 -0500

    Copying from #285

--- src/views/HelpAndKnowledge/Articles/index.ts
+++ src/views/HelpAndKnowledge/Articles/index.ts
@@ -2,6 +2,7 @@ export type ArticleProps = {
   name: string;
   route: string;
   translation: string;
+  tag: string;
 };
 
 // Help and Knowledge Articles
@@ -9,12 +10,20 @@ const helpAndKnowledgeArticles: ArticleProps[] = [
   {
     name: 'Model Plan Overview',
     route: '/model-plan-overview', // route for hitting rendered article component
-    translation: 'modelPlanOverview' // Should reference the translation used to index the title and description for cards
+    translation: 'modelPlanOverview', // Should reference the translation used to index the title and description for cards
+    tag: 'gettingStarted'
   },
   {
     name: 'Sample Model Plan',
     route: '/sample-model-plan',
-    translation: 'sampleModelPlan'
+    translation: 'sampleModelPlan',
+    tag: 'gettingStarted'
+  },
+  {
+    name: 'IT Implementation Placeholder',
+    route: '#',
+    translation: '',
+    tag: 'itImplementation'
   }
 ];
 
