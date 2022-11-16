9a43464772e5ab37eed653867802fbb7f1f34781
Gary Zhao <gary.j.zhao@gmail.com>
2022-11-16 15:14:49 -0500

    use type instead of tag

--- src/views/HelpAndKnowledge/Articles/index.ts
+++ src/views/HelpAndKnowledge/Articles/index.ts
@@ -2,28 +2,31 @@ export type ArticleProps = {
   name: string;
   route: string;
   translation: string;
-  tag: string;
+};
+
+export type ArticleTypeProps = {
+  type: 'gettingStarted' | 'itImplementation';
 };
 
 // Help and Knowledge Articles
-const helpAndKnowledgeArticles: ArticleProps[] = [
+const helpAndKnowledgeArticles: (ArticleProps & ArticleTypeProps)[] = [
   {
     name: 'Model Plan Overview',
     route: '/model-plan-overview', // route for hitting rendered article component
     translation: 'modelPlanOverview', // Should reference the translation used to index the title and description for cards
-    tag: 'gettingStarted'
+    type: 'gettingStarted'
   },
   {
     name: 'Sample Model Plan',
     route: '/sample-model-plan',
     translation: 'sampleModelPlan',
-    tag: 'gettingStarted'
+    type: 'gettingStarted'
   },
   {
     name: 'IT Implementation Placeholder',
     route: '#',
     translation: '',
-    tag: 'itImplementation'
+    type: 'itImplementation'
   }
 ];
 
