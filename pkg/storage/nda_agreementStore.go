e0c037ce7f876cfe995ff6f4ccc32d883b520441
Steven Wade <steven.wade@oddball.io>
2022-11-23 11:18:06 -0800

    First pass ability to sign an NDA as a MAC user

--- pkg/storage/nda_agreementStore.go
+++ pkg/storage/nda_agreementStore.go
@@ -5,9 +5,10 @@ import (
 
 	"go.uber.org/zap"
 
+	"github.com/google/uuid"
+
 	"github.com/cmsgov/mint-app/pkg/models"
 	"github.com/cmsgov/mint-app/pkg/shared/utilityUUID"
-	"github.com/cmsgov/mint-app/pkg/storage/genericmodel"
 )
 
 //go:embed SQL/nda_agreement_get_by_euaid.sql
@@ -19,8 +20,8 @@ var ndaAgreementUpdateSQL string
 //go:embed SQL/nda_agreement_insert.sql
 var ndaAgreementInsertSQL string
 
-// NDAAgreementGetByEUA returns an NDA based on an euaID
-func (s *Store) NDAAgreementGetByEUA(logger *zap.Logger, euaID string) (*models.NDAAgreement, error) {
+// NDAAgreementGetByUserID returns an NDA based on a UserID
+func (s *Store) NDAAgreementGetByUserID(logger *zap.Logger, userID uuid.UUID) (*models.NDAAgreement, error) {
 	nda := models.NDAAgreement{}
 
 	statement, err := s.db.PrepareNamed(ndaAgreementGetByEUAIDSQL)
@@ -28,7 +29,7 @@ func (s *Store) NDAAgreementGetByEUA(logger *zap.Logger, euaID string) (*models.
 		return nil, err
 	}
 	arg := map[string]interface{}{
-		"user_id": euaID,
+		"user_id": userID,
 	}
 
 	err = statement.Get(&nda, arg)
@@ -48,12 +49,12 @@ func (s *Store) NDAAgreementUpdate(logger *zap.Logger, nda *models.NDAAgreement)
 
 	statement, err := s.db.PrepareNamed(ndaAgreementUpdateSQL)
 	if err != nil {
-		return nil, genericmodel.HandleModelUpdateError(logger, err, nda)
+		return nil, err
 	}
 
 	err = statement.Get(nda, nda)
 	if err != nil {
-		return nil, genericmodel.HandleModelUpdateError(logger, err, nda)
+		return nil, err
 	}
 
 	return nda, nil
@@ -67,12 +68,12 @@ func (s *Store) NDAAgreementCreate(logger *zap.Logger, nda *models.NDAAgreement)
 
 	statement, err := s.db.PrepareNamed(ndaAgreementInsertSQL)
 	if err != nil {
-		return nil, genericmodel.HandleModelCreationError(logger, err, nda)
+		return nil, err
 	}
 
 	err = statement.Get(nda, &nda)
 	if err != nil {
-		return nil, genericmodel.HandleModelCreationError(logger, err, nda)
+		return nil, err
 	}
 
 	return nda, nil
