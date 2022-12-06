711561fe656eb0dcd84e93115fa5081c5c13293a
Riley Anderson <5150r.d.anderson@gmail.com>
2022-12-06 07:51:23 -0700

    start on template

---
+++ pkg/email/daily_digest.go
@@ -0,0 +1,10 @@
+package email
+
+import (
+	"github.com/cmsgov/mint-app/pkg/models"
+)
+
+// DailyDigestBodyContent defines the parameters necessary for the corresponding email body
+type DailyDigestBodyContent struct {
+	AnalyzedAudits []models.AnalyzedAudit
+}
