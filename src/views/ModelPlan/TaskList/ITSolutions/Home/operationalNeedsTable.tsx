8c39768a17c4a0478a675c82ce77d5fbfd582229
Gary Zhao <gary.j.zhao@gmail.com>
2022-12-12 15:01:12 -0500

    Adding needID

--- src/views/ModelPlan/TaskList/ITSolutions/Home/operationalNeedsTable.tsx
+++ src/views/ModelPlan/TaskList/ITSolutions/Home/operationalNeedsTable.tsx
@@ -61,6 +61,7 @@ import {
 interface GetOperationalNeedsTableType
   extends GetOperationalNeedsOperationalNeedsType {
   status: OperationalNeedsSolutionsStatus;
+  needID: string;
 }
 
 type OperationalNeedsTableProps = {
