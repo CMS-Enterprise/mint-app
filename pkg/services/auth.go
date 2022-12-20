aca6aef79baaee088d6b386e1c36a58deb8da95a
Tom Brooks <tom.brooks@oddball.io>
2022-12-19 20:11:27 -0500

    feat: mac role and authentication

--- pkg/services/auth.go
+++ pkg/services/auth.go
@@ -37,6 +37,13 @@ func HasRole(ctx context.Context, role model.Role) (bool, error) {
 		}
 		logger.Info("user authorized as ADMIN member", zap.Bool("Authorized", true))
 		return true, nil
+	case model.RoleMintMac:
+		if !principal.AllowMAC() {
+			logger.Info("does not have MAC job code")
+			return false, nil
+		}
+		logger.Info("user authorized as MAC member", zap.Bool("Authorized", true))
+		return true, nil
 	default:
 		logger.With(zap.String("Role", role.String())).Info("Unrecognized user role")
 		return false, nil
