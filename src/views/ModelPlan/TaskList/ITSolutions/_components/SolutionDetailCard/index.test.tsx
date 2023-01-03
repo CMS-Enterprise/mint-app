7e053916eedc4d95de62791b4fa28c557d7e0a89
Patrick Segura <patrick.segura@oddball.io>
2023-01-03 11:13:50 -0500

    Reimplement snapshot

--- src/views/ModelPlan/TaskList/ITSolutions/_components/SolutionDetailCard/index.test.tsx
+++ src/views/ModelPlan/TaskList/ITSolutions/_components/SolutionDetailCard/index.test.tsx
@@ -32,7 +32,7 @@ const solution: GetOperationalSolutionType = {
 };
 
 describe('SolutionDetailsCard', () => {
-  xit('matches snapshot', async () => {
+  it('matches snapshot', async () => {
     const { asFragment, getByText } = render(
       <MemoryRouter
         initialEntries={[
