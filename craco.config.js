f85a7b4d31ed70907a264c627864969ba4c31ccd
Patrick Segura <patrick.segura@oddball.io>
2023-01-11 10:40:42 -0500

    Reworked craco config

--- craco.config.js
+++ craco.config.js
@@ -1,42 +1,17 @@
-const StyleAliasesFixPlugin = {
-  overrideWebpackConfig: (data) => {
-      const config = data.webpackConfig;
-      const rules = config.module.rules[1];
-
-      const locations = rules.oneOf.reduce((rulesAcc, rule, ruleIndex) => {
-          const use = rule.use || [];
-          const useIndex = use.findIndex(u => u.loader?.includes('sass-loader'));
-          return (useIndex < 0) ? rulesAcc : {
-              ...rulesAcc,
-              [ruleIndex]: useIndex,
-          };
-      }, {});
-
-      const resolvePathsOptions = {
-          sassOptions: {
-              includePaths: [
-                "./src/stylesheets",
-                // "./node_modules/@uswds",
-                "./node_modules/@uswds/uswds/packages",
-                "./node_modules/uswds/src/stylesheets/theme"
-              ]
-          },
-      };
-
-      Object.entries(locations)
-          .forEach(([ruleIndex, useIndex]) => rules.oneOf[+ruleIndex].use[useIndex].options = {
-              ...rules.oneOf[+ruleIndex].use[useIndex].options,
-              ...resolvePathsOptions
-          });
-          console.log(JSON.stringify(config, null, 4));
-      return config;
-  },
-};
-
 module.exports = {
-  plugins: [
-      {
-          plugin: StyleAliasesFixPlugin,
-      },
-  ],
+  style: {
+    sass: {
+      loaderOptions: {
+        sourceMap: true,
+        implementation: require("sass"),
+        sassOptions: {
+          includePaths: [
+            "./src/stylesheets",
+            "./node_modules/@uswds/uswds/packages",
+            "./node_modules/uswds/src/stylesheets/theme"
+          ]
+        }
+      }
+    }
+  }
 };
