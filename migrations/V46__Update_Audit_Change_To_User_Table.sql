c8bf8f7b025f250cbece94ac8f36b44c5f4c4167
Steven Wade <steven.wade@oddball.io>
2023-01-23 10:46:37 -0800

    split up migrations to multiple

--- migrations/V46__Update_Audit_Change_To_User_Table.sql
+++ migrations/V46__Update_Audit_Change_To_User_Table.sql
@@ -5,11 +5,6 @@ RENAME COLUMN modified_by TO modified_by_old;
 ALTER TABLE audit.change
 ADD COLUMN modified_by UUID REFERENCES public.user_account (id) MATCH SIMPLE;
 
-ALTER TABLE audit.table_config
-ADD COLUMN uses_user_id BOOLEAN NOT NULL DEFAULT FALSE;
-COMMENT ON COLUMN audit.table_config.uses_user_id IS
-'Uses_user_id is meant to facilitate the migration of all tables to use the user table. It will serve as a record for the audit trigger to distinguish when the table has been migrated.
-Once all tables have been migrate, this column should be removed as it is no longer needed.';
 /***
 * UPDATE THE TABLEs existing data to point to EUAID
 
@@ -19,20 +14,13 @@ Once all tables have been migrate, this column should be removed as it is no lon
 *
 */
 WITH
-UnMatched AS (
+UnMatched_UserNames AS (
     SELECT
-        audit.change.modified_by_old,
+        audit.change.modified_by_old AS USER_NAME,
         user_account_modified.id AS modified_by
     FROM audit.change
     LEFT JOIN user_account AS user_account_modified ON audit.change.modified_by_old = user_account_modified.username
-    WHERE user_account_modified.id IS NULL
-),
-
---TODO simplify to 1 cte
-UnMatched_UserNames AS (
-    SELECT modified_by_old AS USER_NAME FROM Unmatched
-    WHERE modified_by IS NULL AND modified_by_old IS NOT NULL
-
+    WHERE user_account_modified.id IS NULL AND audit.change.modified_by_old IS NOT NULL
 )
 
 -- Insert any missing user_accounts
