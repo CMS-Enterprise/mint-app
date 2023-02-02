28e21eee2978593e42f171d7d5129df233471603
Tom Brooks <tom.brooks@oddball.io>
2023-02-02 13:00:19 -0500

    chore: moved operational solution subtask status enum into operational_solution_subtask.go

--- pkg/models/operational_solution_subtask.go
+++ pkg/models/operational_solution_subtask.go
@@ -4,6 +4,23 @@ import (
 	"github.com/google/uuid"
 )
 
+// OperationalSolutionSubtaskStatus is an enum that represents the possible operational solution subtask statuses
+type OperationalSolutionSubtaskStatus string
+
+// These constants represent the different values of OperationalSolutionSubtaskStatus
+const (
+	OperationalSolutionSubtaskStatusTodo       OperationalSolutionSubtaskStatus = "TODO"
+	OperationalSolutionSubtaskStatusInProgress OperationalSolutionSubtaskStatus = "IN_PROGRESS"
+	OperationalSolutionSubtaskStatusDone       OperationalSolutionSubtaskStatus = "DONE"
+)
+
+// AllOperationalSolutionSubtaskStatus is a collection of all possible values for OperationalSolutionSubtaskStatus
+var AllOperationalSolutionSubtaskStatus = []OperationalSolutionSubtaskStatus{
+	OperationalSolutionSubtaskStatusTodo,
+	OperationalSolutionSubtaskStatusInProgress,
+	OperationalSolutionSubtaskStatusDone,
+}
+
 // OperationalSolutionSubtask represents the specific implemented solution to an OperationalSolutionSubtask
 type OperationalSolutionSubtask struct {
 	baseStructUserTable
