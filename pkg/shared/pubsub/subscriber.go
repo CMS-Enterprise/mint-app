ebac88a027c483e4091be9150ba387aeae877ccc
Tom Brooks <tom.brooks@oddball.io>
2023-02-12 18:59:57 -0500

    fix: resolved race condition where unlocking task list sections and unsubscribing from pubsub on disconnect were not happening in sequence by adding a callback when a user is unsubscribed from pubsub and using it in the lock task list section context to then unlock all task list sections held by this user on this model plan ID

--- pkg/shared/pubsub/subscriber.go
+++ pkg/shared/pubsub/subscriber.go
@@ -1,5 +1,7 @@
 package pubsub
 
+import "github.com/google/uuid"
+
 // Subscriber is an abstract interface defining the necessary functionality for a subscription model
 //
 //	It is intended for the user to define their own version of a Subscriber to assign when subscribing to an EventType
@@ -7,4 +9,5 @@ type Subscriber interface {
 	GetID() string
 	GetPrincipal() string
 	Notify(payload interface{})
+	NotifyUnsubscribed(ps *ServicePubSub, sessionID uuid.UUID)
 }
