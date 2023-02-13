532f32e5d226bb71e533d731a84bf37deb0e1cd4
Steven Wade <steven.wade@oddball.io>
2023-02-13 14:02:45 -0800

    chore: disable opneeds trigger on beneficiaries.

--- migrations/V61__Update_Plan_Beneficiaries_To_User_Account_Table.sql
+++ migrations/V61__Update_Plan_Beneficiaries_To_User_Account_Table.sql
@@ -73,3 +73,8 @@ WHERE name = 'plan_beneficiaries';
 
 ALTER TABLE plan_beneficiaries
 ENABLE TRIGGER audit_trigger;
+
+/* disable need trigger as it is currently non-functional until needs are migrated */
+
+ALTER TABLE plan_beneficiaries
+DISABLE TRIGGER operational_need_trigger;
