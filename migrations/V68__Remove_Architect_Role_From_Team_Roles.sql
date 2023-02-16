52a6b26983325609a2d06b03a934d88165d07c47
ClayBenson94 <clay.benson@oddball.io>
2023-02-16 10:10:21 -0500

    Disable and enable trigger

--- migrations/V68__Remove_Architect_Role_From_Team_Roles.sql
+++ migrations/V68__Remove_Architect_Role_From_Team_Roles.sql
@@ -1,3 +1,5 @@
+DISABLE TRIGGER collaborator_lead_req_update;
+
 UPDATE plan_collaborator SET team_role = 'IT_LEAD' WHERE team_role = 'ARCHITECT';
 
 ALTER TYPE TEAM_ROLE RENAME TO TEAM_ROLE_OLD;
@@ -14,3 +16,5 @@ CREATE TYPE TEAM_ROLE AS ENUM (
 ALTER TABLE plan_collaborator ALTER COLUMN team_role TYPE TEAM_ROLE USING team_role::TEXT::TEAM_ROLE;
 
 DROP TYPE TEAM_ROLE_OLD;
+
+ENABLE TRIGGER collaborator_lead_req_update;
