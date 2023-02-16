95d175a6c0f949ac15e8f35ba1cb067a88dc8c56
ClayBenson94 <clay.benson@oddball.io>
2023-02-16 12:28:49 -0500

    Fix migration that removes architect to set modified_by and modified_dts

--- migrations/V68__Remove_Architect_Role_From_Team_Roles.sql
+++ migrations/V68__Remove_Architect_Role_From_Team_Roles.sql
@@ -1,7 +1,11 @@
 DROP TRIGGER collaborator_lead_req_update ON plan_collaborator;
 DROP TRIGGER collaborator_lead_req_delete ON plan_collaborator;
 
-UPDATE plan_collaborator SET team_role = 'IT_LEAD' WHERE team_role = 'ARCHITECT';
+UPDATE plan_collaborator SET
+    team_role = 'IT_LEAD',
+    modified_by = '00000001-0001-0001-0001-000000000001', -- System Account
+    modified_dts = CURRENT_TIMESTAMP
+WHERE team_role = 'ARCHITECT';
 
 ALTER TYPE TEAM_ROLE RENAME TO TEAM_ROLE_OLD;
 
@@ -23,11 +27,11 @@ CREATE TRIGGER collaborator_lead_req_update
 BEFORE UPDATE ON plan_collaborator
 FOR EACH ROW
 WHEN ((old.team_role = 'MODEL_LEAD') AND (new.team_role != 'MODEL_LEAD'))
-EXECUTE FUNCTION collaborator_role_check_trigger();
+EXECUTE FUNCTION COLLABORATOR_ROLE_CHECK_TRIGGER();
 
 
 CREATE TRIGGER collaborator_lead_req_delete
 BEFORE DELETE ON plan_collaborator
 FOR EACH ROW
 WHEN (old.team_role = 'MODEL_LEAD')
-EXECUTE FUNCTION collaborator_role_check_trigger();
+EXECUTE FUNCTION COLLABORATOR_ROLE_CHECK_TRIGGER();
