99d0106d6b99fdc583c400470a086231c451344d
Tom Brooks <tom.brooks@oddball.io>
2023-02-24 01:36:37 -0500

    chore: removed update_existing_model_table_tool per request

--- scripts/update_existing_model_table_tool/header_column_mapping.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-
-def map_headers_to_columns(headers):
-    with open('column_map.yaml') as f:
-        column_map = yaml.safe_load(f)
-
-    return [column_map.get(header, "") for header in headers]
