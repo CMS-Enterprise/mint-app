fc1b1649a483ed7f418fd853af0d217129dff818
Patrick Segura <95709965+patrickseguraoddball@users.noreply.github.com>
2023-03-06 13:43:30 -0500

    [EASI-2703] - Added banner and wrapper to display task list lock on routes (#490)
    Added banner and wrapper to display task list lock on routes

---
+++ src/views/TaskListBannerAlert/index.test.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { render } from '@testing-library/react';
+
+import TaskListBannerAlert from '.';
+
+describe('The Task List Alert Banner', () => {
+  it('renders banner on task list page', async () => {
+    const { findByTestId } = render(
+      <MemoryRouter
+        initialEntries={[
+          `/models/0272ca43-1ec1-45a6-a06f-8e2def7f6888/task-list/beneficiaries`
+        ]}
+      >
+        <Route
+          path="/models/:modelID/task-list"
+          component={TaskListBannerAlert}
+        />
+      </MemoryRouter>
+    );
+
+    expect(await findByTestId('task-list-alert')).toBeInTheDocument();
+  });
+
+  it('does not banner when not on task list page', async () => {
+    const { queryByTestId } = render(
+      <MemoryRouter
+        initialEntries={[
+          `/models/0272ca43-1ec1-45a6-a06f-8e2def7f6888/task-list`
+        ]}
+      >
+        <Route
+          path="/models/:modelID/task-list"
+          component={TaskListBannerAlert}
+        />
+      </MemoryRouter>
+    );
+
+    expect(queryByTestId('task-list-alert')).not.toBeInTheDocument();
+  });
+});
