069b321d1158b93c75e954d2c2a5d85cf0ec3468
Gary Zhao <gary.j.zhao@gmail.com>
2023-03-07 14:34:20 -0500

    basic scaffolding

--- src/views/ModelPlan/TaskList/ITSolutions/Subtasks/index.tsx
+++ src/views/ModelPlan/TaskList/ITSolutions/Subtasks/index.tsx
@@ -1,7 +1,71 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useHistory, useParams } from 'react-router-dom';
+import { Grid } from '@trussworks/react-uswds';
+
+import Breadcrumbs from 'components/Breadcrumbs';
+import PageHeading from 'components/PageHeading';
+import useMessage from 'hooks/useMessage';
+import { ModelInfoContext } from 'views/ModelInfoWrapper';
+
+import ITSolutionsSidebar from '../_components/ITSolutionSidebar';
 
 const Subtasks = () => {
-  return <div>Subtasks</div>;
+  const { modelID, operationalNeedID, operationalSolutionID } = useParams<{
+    modelID: string;
+    operationalNeedID: string;
+    operationalSolutionID: string;
+  }>();
+
+  // const history = useHistory();
+
+  const { t } = useTranslation('itSolutions');
+  const { t: h } = useTranslation('draftModelPlan');
+
+  // const { showMessageOnNextPage, message } = useMessage();
+
+  const { modelName } = useContext(ModelInfoContext);
+
+  const breadcrumbs = [
+    { text: h('home'), url: '/' },
+    { text: h('tasklistBreadcrumb'), url: `/models/${modelID}/task-list/` },
+    { text: t('breadcrumb'), url: `/models/${modelID}/task-list/it-solutions` },
+    {
+      text: t('solutionDetails'),
+      url: `/models/${modelID}/task-list/it-solutions/${operationalNeedID}/${operationalSolutionID}/solution-details`
+    },
+    { text: t('subtasks.addSubtask') }
+  ];
+
+  return (
+    <>
+      <Breadcrumbs items={breadcrumbs} />
+
+      <Grid row gap>
+        <Grid tablet={{ col: 9 }}>
+          <PageHeading className="margin-top-4 margin-bottom-2">
+            {t('subtasks.addSubtask')}
+          </PageHeading>
+
+          <p
+            className="margin-top-0 margin-bottom-1 font-body-lg"
+            data-testid="model-plan-name"
+          >
+            {h('for')} {modelName}
+          </p>
+
+          <p className="line-height-body-4">{t('subtasks.addSubtaskInfo')}</p>
+        </Grid>
+        <Grid tablet={{ col: 3 }} className="padding-x-1">
+          <ITSolutionsSidebar
+            modelID={modelID}
+            renderTextFor="need"
+            helpfulLinks={false}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default Subtasks;
