e7ac943516524e090f77a073f181173305aa78a4
Gary Zhao <gary.j.zhao@gmail.com>
2023-03-07 16:04:35 -0500

    moved subtask page copy to its own translation file

---
+++ src/i18n/en-US/draftModelPlan/subtasks.ts
@@ -0,0 +1,16 @@
+const subtasks = {
+  header: 'Subtasks',
+  todo: 'To do',
+  inProgress: 'In progress',
+  done: 'Done',
+  addSubtask: 'Add subtask',
+  addSubtasks: 'Add subtasks',
+  manageSubtasks: 'Manage subtasks',
+  noSubtasks: 'No subtasks added yet',
+  addSubtaskInfo:
+    'Add important subtasks to stay organized throughout the implementation process.',
+  manageSubtaskInfo:
+    'Track the status of your subtasks throughout the implementation process to stay organized.'
+};
+
+export default subtasks;
--- src/views/ModelPlan/TaskList/ITSolutions/Subtasks/index.tsx
+++ src/views/ModelPlan/TaskList/ITSolutions/Subtasks/index.tsx
@@ -1,21 +1,30 @@
 import React, { useContext, useMemo, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useHistory, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import { Button, Fieldset, Grid, IconArrowBack } from '@trussworks/react-uswds';
-import { Form, Formik, FormikProps } from 'formik';
+import {
+  Button,
+  Grid,
+  IconArrowBack,
+  Label,
+  TextInput
+} from '@trussworks/react-uswds';
+import { Field, Form, Formik, FormikProps } from 'formik';
 
 import Breadcrumbs from 'components/Breadcrumbs';
 import PageHeading from 'components/PageHeading';
 import { ErrorAlert, ErrorAlertMessage } from 'components/shared/ErrorAlert';
+import FieldErrorMsg from 'components/shared/FieldErrorMsg';
+import FieldGroup from 'components/shared/FieldGroup';
+import RequiredAsterisk from 'components/shared/RequiredAsterisk';
 // import useMessage from 'hooks/useMessage';
 import GetOperationalSolution from 'queries/ITSolutions/GetOperationalSolution';
 import {
   GetOperationalSolution as GetOperationalSolutionType,
   GetOperationalSolution_operationalSolution as GetOperationalSolutionOperationalSolutionType,
-  GetOperationalSolution_operationalSolution_operationalSolutionSubtasks as SubtaskType,
   GetOperationalSolutionVariables
 } from 'queries/ITSolutions/types/GetOperationalSolution';
+import { GetOperationalSolutionSubtasks_operationalSolution_operationalSolutionSubtasks as SubtasksType } from 'queries/ITSolutions/types/GetOperationalSolutionSubtasks';
 import { OperationalSolutionSubtaskStatus } from 'types/graphql-global-types';
 import flattenErrors from 'utils/flattenErrors';
 import { ModelInfoContext } from 'views/ModelInfoWrapper';
@@ -33,7 +42,7 @@ const Subtasks = () => {
 
   // const history = useHistory();
 
-  const { t } = useTranslation('itSolutions');
+  const { t } = useTranslation('subtasks');
   const { t: h } = useTranslation('draftModelPlan');
 
   // const { showMessageOnNextPage, message } = useMessage();
@@ -64,15 +73,11 @@ const Subtasks = () => {
       text: t('solutionDetails'),
       url: `/models/${modelID}/task-list/it-solutions/${operationalNeedID}/${operationalSolutionID}/solution-details`
     },
-    { text: t('subtasks.addSubtask') }
+    { text: t('addSubtask') }
   ];
 
-  type InitialValueType = Omit<
-    SubtaskType,
-    'createdBy' | 'createdDts' | 'modifiedBy' | 'modifiedDts'
-  >;
-  const formikRef = useRef<FormikProps<InitialValueType>>(null);
-  const initialValues: InitialValueType = {
+  const formikRef = useRef<FormikProps<SubtasksType>>(null);
+  const initialValues: SubtasksType = {
     __typename: 'OperationalSolutionSubtask',
     id: '',
     solutionID: operationalSolutionID,
@@ -91,7 +96,7 @@ const Subtasks = () => {
       <Grid row gap>
         <Grid tablet={{ col: 9 }}>
           <PageHeading className="margin-top-4 margin-bottom-2">
-            {t('subtasks.addSubtask')}
+            {t('addSubtask')}
           </PageHeading>
 
           <p
@@ -101,7 +106,7 @@ const Subtasks = () => {
             {h('for')} {modelName}
           </p>
 
-          <p className="line-height-body-4">{t('subtasks.addSubtaskInfo')}</p>
+          <p className="line-height-body-4">{t('addSubtaskInfo')}</p>
 
           <Grid tablet={{ col: 8 }}>
             <NeedQuestionAndAnswer
@@ -120,7 +125,7 @@ const Subtasks = () => {
               enableReinitialize
               innerRef={formikRef}
             >
-              {(formikProps: FormikProps<InitialValueType>) => {
+              {(formikProps: FormikProps<SubtasksType>) => {
                 const { errors, setErrors, handleSubmit, values } = formikProps;
 
                 const flatErrors = flattenErrors(errors);
@@ -152,40 +157,53 @@ const Subtasks = () => {
                         handleSubmit(e);
                       }}
                     >
-                      <Fieldset>
-                        <div className="margin-top-6 margin-bottom-3">
-                          <Button
-                            type="button"
-                            className="usa-button usa-button--outline margin-bottom-1"
-                            onClick={() => {
-                              // handleFormSubmit(values, 'back');
-                              console.log(values);
-                            }}
-                          >
-                            {h('back')}
-                          </Button>
-
-                          <Button
-                            type="submit"
-                            id="submit-solutions"
-                            onClick={() => setErrors({})}
-                          >
-                            {t('saveSolutions')}
-                          </Button>
-                        </div>
-
+                      <FieldGroup
+                        scrollElement="name"
+                        error={!!flatErrors.name}
+                        className="margin-top-4"
+                      >
+                        <Label htmlFor="subtask-name">
+                          {t('modelName')}
+                          <RequiredAsterisk />
+                        </Label>
+                        <FieldErrorMsg>{flatErrors.name}</FieldErrorMsg>
+                        <Field
+                          as={TextInput}
+                          error={!!flatErrors.name}
+                          id="subtask-name"
+                          maxLength={50}
+                          name="name"
+                        />
+                      </FieldGroup>
+                      <div className="margin-top-6 margin-bottom-3">
                         <Button
                           type="button"
-                          className="usa-button usa-button--unstyled display-flex flex-align-center margin-bottom-6"
-                          onClick={() => {}}
+                          className="usa-button usa-button--outline margin-bottom-1"
+                          onClick={() => {
+                            // handleFormSubmit(values, 'back');
+                            console.log(values);
+                          }}
                         >
-                          <IconArrowBack
-                            className="margin-right-1"
-                            aria-hidden
-                          />
-                          {h('saveAndReturn')}
+                          {h('back')}
                         </Button>
-                      </Fieldset>
+
+                        <Button
+                          type="submit"
+                          id="submit-solutions"
+                          onClick={() => setErrors({})}
+                        >
+                          {t('saveSolutions')}
+                        </Button>
+                      </div>
+
+                      <Button
+                        type="button"
+                        className="usa-button usa-button--unstyled display-flex flex-align-center margin-bottom-6"
+                        onClick={() => {}}
+                      >
+                        <IconArrowBack className="margin-right-1" aria-hidden />
+                        {h('saveAndReturn')}
+                      </Button>
                     </Form>
                   </>
                 );
