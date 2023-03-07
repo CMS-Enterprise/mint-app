56c93ad847c201041db6c26f0660363c5e5f16b1
Gary Zhao <gary.j.zhao@gmail.com>
2023-03-07 16:03:36 -0500

    moved subtask page copy to its own translation file

--- src/i18n/en-US/draftModelPlan/itSolutions.ts
+++ src/i18n/en-US/draftModelPlan/itSolutions.ts
@@ -163,14 +163,9 @@ const itSolutions = {
     todo: 'To do',
     inProgress: 'In progress',
     done: 'Done',
-    addSubtask: 'Add subtask',
     addSubtasks: 'Add subtasks',
     manageSubtasks: 'Manage subtasks',
-    noSubtasks: 'No subtasks added yet',
-    addSubtaskInfo:
-      'Add important subtasks to stay organized throughout the implementation process.',
-    manageSubtaskInfo:
-      'Track the status of your subtasks throughout the implementation process to stay organized.'
+    noSubtasks: 'No subtasks added yet'
   },
   documents: 'Documents',
   links: {
