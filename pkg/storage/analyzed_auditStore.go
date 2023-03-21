ba4d0cf68eed3ed213722786b45fbd7fdde6b24b
Tom Brooks <100007843+OddTomBrooks@users.noreply.github.com>
2023-03-21 13:34:13 -0400

    [EASI-2557] Daily Digest Email to MINT Team Mailbox (#502)
    * wip: first pass at creating a MINT email to the MINT dev team whenever a model plan is created
    
    * chore: resolved css to match figma spec
    
    * chore: resolved css to match figma spec
    
    * fix: restored mistakenly deleted folder
    
    * chore: tailoring html to match closer to Figma spec
    
    * chore: updated email sent to dev team on model plan to not block model plan creation on failure
    
    * chore: improved CSS to remove redundant tags and better match Figma spec
    
    * chore: cleaned up unused CSS files
    
    * wip: new question email to MINT dev team, committing now to merge in work from other branch
    
    * feat: email sent to dev team when new question added
    
    * [NOREF] Email Templates Changing Extension to HTML (#485)
    
    * chore: changed added_as_collaborator email templates from .template to .html extension
    
    * chore: changed daily_digest email templates from .template to .html extension
    
    * chore: changed model_plan_created email templates from .template to .html extension
    
    * chore: moved model plan created email sending into an asynchronous function
    
    * chore: made final changes to match email to Figma view
    
    * chore: linting changes, updated code to use address book
    
    * chore: made sending email to MINT Team on plan discussion creation async
    
    * feat: aggregated daily digest email
    
    * fix: corrected aggregated digest tests
    
    * chore: fix unit test, fetch 3rd job and complete it to get callback job
    
    ---------
    
    Co-authored-by: Steven Wade <steven.wade@oddball.io>
    Co-authored-by: Steven Wade <99294987+StevenWadeOddball@users.noreply.github.com>

--- pkg/storage/analyzed_auditStore.go
+++ pkg/storage/analyzed_auditStore.go
@@ -24,6 +24,9 @@ var analyzedAuditGetByModelPlanIDAndDate string
 //go:embed SQL/analyzed_audit/get_collection_by_model_plan_ids_and_date.sql
 var analyzedAuditGetByModelPlanIDsAndDate string
 
+//go:embed SQL/analyzed_audit/get_by_date.sql
+var analyzedAuditGetByDate string
+
 // AnalyzedAuditCreate creates and returns an AnalyzedAudit object
 func (s *Store) AnalyzedAuditCreate(logger *zap.Logger, AnalyzedAudit *models.AnalyzedAudit) (*models.AnalyzedAudit, error) {
 
@@ -103,6 +106,26 @@ func (s *Store) AnalyzedAuditGetByModelPlanIDsAndDate(logger *zap.Logger, modelP
 	arg := map[string]interface{}{
 		"model_plan_ids": pq.Array(modelPlanIDs),
 		"date":           date.Format("2006-01-02"),
+	}
+
+	err = stmt.Select(&analyzedAudits, arg)
+
+	if err != nil {
+		return nil, err
+	}
+	return analyzedAudits, nil
+}
+
+// AnalyzedAuditGetByDate gets and returns all AnalyzedAudits by date
+func (s *Store) AnalyzedAuditGetByDate(_ *zap.Logger, date time.Time) ([]*models.AnalyzedAudit, error) {
+	var analyzedAudits []*models.AnalyzedAudit
+
+	stmt, err := s.db.PrepareNamed(analyzedAuditGetByDate)
+	if err != nil {
+		return nil, err
+	}
+	arg := map[string]interface{}{
+		"date": date.Format("2006-01-02"),
 	}
 
 	err = stmt.Select(&analyzedAudits, arg)
