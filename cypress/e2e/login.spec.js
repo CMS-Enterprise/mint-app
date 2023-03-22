0e370d6795fefba54f51601515c0e38e9196cc0f
ClayBenson94 <clay.benson@oddball.io>
2023-03-22 12:57:31 -0400

    Retry login test

--- cypress/e2e/login.spec.js
+++ cypress/e2e/login.spec.js
@@ -1,11 +1,30 @@
+const maxAttempts = 3;
+
 describe('Logging in', () => {
-  it('logs in with okta', () => {
-    cy.login();
-    cy.location('pathname', { timeout: 20000 }).should(
-      'equal',
-      '/pre-decisional-notice'
-    );
-  });
+  it(
+    'logs in with okta',
+    {
+      retries: {
+        runMode: maxAttempts - 1, // 2 retries when running from `cypress run` (3 total attempts)
+        openMode: 0 // 0 retries when running from `cypress open` (1 total attempt)
+      }
+    },
+    () => {
+      // Get the current number of retries and sleep before running the test to make sure the One-Time-Password is new
+      const currentRetry = cy.state('runnable')._currentRetry; // eslint-disable-line no-underscore-dangle
+      if (currentRetry > 0) {
+        cy.log(
+          `[Attempt ${currentRetry + 1}/${maxAttempts}] Sleeping 30s for OTP`
+        );
+        cy.wait(30000);
+      }
+      cy.login();
+      cy.location('pathname', { timeout: 20000 }).should(
+        'equal',
+        '/pre-decisional-notice'
+      );
+    }
+  );
 
   it('logs in with local auth an verifies NDA', () => {
     cy.localLogin({ name: 'MINT', role: 'MINT_USER_NONPROD' });
