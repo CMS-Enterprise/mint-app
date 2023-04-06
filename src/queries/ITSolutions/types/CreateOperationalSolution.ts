06d84582e1483c5940621026d4a6f556e3e2d3fe
Gary Zhao <gary.j.zhao@gmail.com>
2023-04-06 13:13:27 -0400

    add CreateOperationalSolution types

---
+++ src/queries/ITSolutions/types/CreateOperationalSolution.ts
@@ -0,0 +1,25 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import { OperationalSolutionKey, OperationalSolutionChanges } from "./../../../types/graphql-global-types";
+
+// ====================================================
+// GraphQL mutation operation: CreateOperationalSolution
+// ====================================================
+
+export interface CreateOperationalSolution_createOperationalSolution {
+  __typename: "OperationalSolution";
+  id: UUID;
+}
+
+export interface CreateOperationalSolution {
+  createOperationalSolution: CreateOperationalSolution_createOperationalSolution;
+}
+
+export interface CreateOperationalSolutionVariables {
+  operationalNeedID: UUID;
+  solutionType?: OperationalSolutionKey | null;
+  changes: OperationalSolutionChanges;
+}
