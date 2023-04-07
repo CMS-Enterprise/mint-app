cdd44dbcad01d3d625a7af3cdcf2c08b0e09531b
Gary Zhao <gary.j.zhao@gmail.com>
2023-04-07 14:42:24 -0400

    change where it is looking for the id

--- src/views/ModelPlan/TaskList/ITSolutions/AddCustomSolution/index.tsx
+++ src/views/ModelPlan/TaskList/ITSolutions/AddCustomSolution/index.tsx
@@ -184,9 +184,9 @@ const AddCustomSolution = () => {
 
       if (!operationalSolutionID) {
         history.push(
-          // If this block of code is hit, property addOrUpdateCustomOperationalSolution will always exist - ts doesn't know this
+          // If this block of code is hit, property createOperationalSolution will always exist - ts doesn't know this
           // @ts-ignore
-          `/models/${modelID}/task-list/it-solutions/${operationalNeedID}/add-solution/${updateMutation.data.addOrUpdateCustomOperationalSolution.id}`,
+          `/models/${modelID}/task-list/it-solutions/${operationalNeedID}/add-solution/${updateMutation.data.createOperationalSolution.id}`,
           { isCustomNeed: false }
         );
       } else {
