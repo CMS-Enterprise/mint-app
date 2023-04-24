e1787f4b3630f3df720f8bfbace0364583a0ac31
Gary Zhao <gary.j.zhao@gmail.com>
2023-04-24 14:28:26 -0400

    add aria live to results

--- src/components/TableResults/index.tsx
+++ src/components/TableResults/index.tsx
@@ -47,14 +47,14 @@ const TableResults = ({
       <span>
         {rows === 0 ? (
           showNoResults && (
-            <div role="status">
+            <div role="status" aria-live="polite">
               {t('tableAndPagination:results.noResults')}{' '}
               {/* Displays the search input even if there are no results */}
               {displayResult(globalFilter)}
             </div>
           )
         ) : (
-          <div role="status">
+          <div role="status" aria-live="polite">
             <Trans i18nKey="tableAndPagination:results.results">
               indexZero {{ currentPage }} indexOne {{ pageRange }} indexTwo{' '}
               {{ rows }}
