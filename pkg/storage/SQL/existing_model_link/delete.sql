a52be56e160a44a742970c12728c6a926723b041
Steven Wade <steven.wade@oddball.io>
2023-04-27 12:53:32 -0700

    feat: add returning statment to delete SQL

--- pkg/storage/SQL/existing_model_link/delete.sql
+++ pkg/storage/SQL/existing_model_link/delete.sql
@@ -1,2 +1,11 @@
 DELETE FROM existing_model_link
-WHERE id = :id;
+WHERE id = :id
+RETURNING
+id,
+model_plan_id,
+existing_model_id,
+current_model_plan_id,
+created_by,
+created_dts,
+modified_by,
+modified_dts;
