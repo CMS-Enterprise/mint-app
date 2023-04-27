4ad67f44dc817e8f927dcd561bd77dc3fc93e648
Gary Zhao <gary.j.zhao@gmail.com>
2023-04-27 11:31:49 -0400

    update the solution card to display a few more things in the solution implementation details page

--- src/views/ModelPlan/TaskList/ITSolutions/_components/SolutionCard/index.tsx
+++ src/views/ModelPlan/TaskList/ITSolutions/_components/SolutionCard/index.tsx
@@ -20,6 +20,8 @@ import classNames from 'classnames';
 import UswdsReactLink from 'components/LinkWrapper';
 import Divider from 'components/shared/Divider';
 import { GetOperationalNeed_operationalNeed_solutions as GetOperationalNeedSolutionsType } from 'queries/ITSolutions/types/GetOperationalNeed';
+import { OperationalSolutionKey } from 'types/graphql-global-types';
+import { translateOperationalSolutionKey } from 'utils/modelPlan';
 
 import './index.scss';
 
@@ -57,58 +59,66 @@ const SolutionCard = ({
     pocEmail: 'john.doe@oddball.io'
   };
 
+  const treatAsOtherSolutions = [
+    OperationalSolutionKey.CONTRACTOR,
+    OperationalSolutionKey.CROSS_MODEL_CONTRACT,
+    OperationalSolutionKey.EXISTING_CMS_DATA_AND_PROCESS,
+    OperationalSolutionKey.INTERNAL_STAFF
+  ];
+
+  const isDefaultSolutionOptions =
+    solution.name !== null && solution.pocEmail === null;
+
   return (
     <CardGroup className="flex-column flex-no-wrap">
       <Card className={classNames('solution-card', { shadow }, className)}>
         <div className="padding-3">
-          <h3
-            className="margin-bottom-2 margin-top-0 solutions-checkbox__header"
-            style={{ wordBreak: 'break-word' }}
-          >
-            {solution.nameOther || solution.name}
-          </h3>
+          {solution.key &&
+            treatAsOtherSolutions.includes(solution.key) &&
+            !isDefaultSolutionOptions && (
+              <>
+                <h3 className="margin-y-0 solutions-checkbox__header">
+                  {solution.otherHeader}
+                </h3>
+                <h5 className="text-normal margin-top-0 margin-bottom-2">
+                  {translateOperationalSolutionKey(solution.key)}
+                </h5>
+              </>
+            )}
 
-          {solution.pocName ? (
-            <Grid
-              tablet={{ col: 6 }}
-              className={classNames({ 'margin-bottom-2': solution.name })}
+          {(!solution.key ||
+            !treatAsOtherSolutions.includes(solution.key) ||
+            isDefaultSolutionOptions) && (
+            <h3
+              className="margin-bottom-2 margin-top-0 solutions-checkbox__header"
+              style={{ wordBreak: 'break-word' }}
             >
-              <p className="text-bold margin-bottom-0">{t('contact')}</p>
-
-              <p className="margin-y-0">{solution.pocName}</p>
-
-              <Link
-                aria-label={h('contactInfo.sendAnEmail')}
-                className="line-height-body-5 display-flex flex-align-center"
-                href={`mailto:${solution.pocEmail}`}
-                target="_blank"
-              >
-                <div>{solution.pocEmail}</div>
-                <IconMailOutline className="margin-left-05 text-tbottom" />
-              </Link>
-            </Grid>
-          ) : (
-            <Grid
-              tablet={{ col: 6 }}
-              className={classNames({ 'margin-bottom-2': solution.name })}
-            >
-              <p className="text-bold margin-bottom-0">{t('contact')}</p>
-
-              <p className="margin-y-0">{tempSolutionPOC.pocName}</p>
-
-              <Link
-                aria-label={h('contactInfo.sendAnEmail')}
-                className="line-height-body-5 display-flex flex-align-center"
-                href={`mailto:${tempSolutionPOC.pocEmail}`}
-                target="_blank"
-              >
-                <div>{tempSolutionPOC.pocEmail}</div>
-                <IconMailOutline className="margin-left-05 text-tbottom" />
-              </Link>
-            </Grid>
+              {solution.nameOther || solution.name}
+            </h3>
           )}
 
-          {/* Show 'About Dettails' link if not updating solution details and not a custom solution */}
+          <Grid
+            tablet={{ col: 6 }}
+            className={classNames({ 'margin-bottom-2': solution.name })}
+          >
+            <p className="text-bold margin-bottom-0">{t('contact')}</p>
+
+            <p className="margin-y-0">
+              {solution.pocName ?? tempSolutionPOC.pocName}
+            </p>
+
+            <Link
+              aria-label={h('contactInfo.sendAnEmail')}
+              className="line-height-body-5 display-flex flex-align-center"
+              href={`mailto:${solution.pocEmail ?? tempSolutionPOC.pocEmail}`}
+              target="_blank"
+            >
+              <div>{solution.pocEmail ?? tempSolutionPOC.pocEmail}</div>
+              <IconMailOutline className="margin-left-05 text-tbottom" />
+            </Link>
+          </Grid>
+
+          {/* Show 'About Details' link if not updating solution details and not a custom solution */}
           {renderSolutionCardLinks && solution.name && (
             <>
               <Divider />
