30928908a2d9fa57bf86b6471aeeb2d3ce489b6f
Gary Zhao <gary.j.zhao@gmail.com>
2023-05-24 16:01:48 -0400

    progress

--- src/i18n/en-US/readOnly/filterView.ts
+++ src/i18n/en-US/readOnly/filterView.ts
@@ -1,5 +1,13 @@
 const filterView = {
-  group: 'Gruop'
+  group: 'Group',
+  content: 'Filter down to the content your group cares the most about.',
+  alert: {
+    heading: 'Don’t see your group listed?',
+    content:
+      'Contact the MINT Team at <1>MINTTeam@cms.hhs.gov</1>, so we can learn more about your group.'
+  },
+  viewAll: 'View all content',
+  viewFiltered: 'View filtered content'
 };
 
 export default filterView;
--- src/views/ModelPlan/ReadOnly/_components/FilterViewModal/index.tsx
+++ src/views/ModelPlan/ReadOnly/_components/FilterViewModal/index.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import { Trans, useTranslation } from 'react-i18next';
+import { Alert, Button, Link } from '@trussworks/react-uswds';
 
 const FilterViewModal = () => {
-  return <div>FilterViewModal</div>;
+  const { t } = useTranslation('filterView');
+
+  return (
+    <>
+      <div className="filter-view__body padding-top-3 padding-bottom-4">
+        <h3 className="margin-y-0">{t('group')}</h3>
+        <p className="margin-y-0 font-body-sm text-base">{t('content')}</p>
+        <Alert noIcon type="info">
+          <p className="margin-y-0 font-body-sm text-bold">
+            {t('alert.heading')}
+          </p>
+          <p className="margin-y-0 font-body-sm">
+            <Trans i18nKey="filterView:alert.content">
+              indexOne
+              <Link href="mailto:MINTTeam@cms.hhs.gov">helpTextEmail</Link>
+              indexTwo
+            </Trans>
+          </p>
+        </Alert>
+      </div>
+      <div className="filter-view__footer margin-x-neg-4 border-top-1px border-base-lighter">
+        <div className="padding-x-4 padding-top-2 display-flex flex-justify">
+          <Button type="button" unstyled>
+            {t('viewAll')}
+          </Button>
+          <Button type="button" disabled className="margin-x-0">
+            {t('viewFiltered')}
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default FilterViewModal;
