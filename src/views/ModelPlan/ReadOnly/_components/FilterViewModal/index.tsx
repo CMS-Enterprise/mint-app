dacc48fbadbc5988946ad6778cde044c4395647a
Gary Zhao <gary.j.zhao@gmail.com>
2023-05-30 11:29:09 -0400

    Add group options

--- src/views/ModelPlan/ReadOnly/_components/FilterViewModal/index.tsx
+++ src/views/ModelPlan/ReadOnly/_components/FilterViewModal/index.tsx
@@ -17,8 +17,24 @@ const FilterViewModal = ({ closeModal }: FilterViewModalProps) => {
   const { t } = useTranslation('filterView');
 
   const groupOptions = [
-    { value: 'test', label: 'test' },
-    { value: 'Gary', label: 'Gary' }
+    { value: 'ccw', label: 'Chronic Conditions Warehouse (CCW)' },
+    { value: 'cmmi', label: 'CMMI Cost Estimate' },
+    {
+      value: 'cbosc',
+      label: 'Consolidated Business Operations Support Center (CBOSC)'
+    },
+    {
+      value: 'dfsdm',
+      label: 'Division of Financial Services and Debt Management (DFSDM)'
+    },
+    { value: 'ipc', label: 'Innovation Payment Contractor (IPC)' },
+    {
+      value: 'iddoc',
+      label: 'Innovative Design, Development, and Operations Contract (IDDOC)'
+    },
+    { value: 'mdm', label: 'Master Data Management (MDM)' },
+    { value: 'oact', label: 'Office of the Actuary (OACT)' },
+    { value: 'pbg', label: 'Provider Billing Group (PBG)' }
   ];
 
   return (
