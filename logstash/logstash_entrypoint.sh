c9ef49ea0e3f2291b3fc7903d9b32646af6dd9ed
Justin Woodson <jdwoodson@gmail.com>
2023-06-01 16:51:59 -0400

    EASI-2993 build logstash image

---
+++ logstash/logstash_entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Ensure FAKTORY_CRON_TOML_BASE64 is set, if not, we shouldn't start the service.
+if [ -z "${LOGSTASH_CONF_BASE64}" ]; then
+	echo "Unable to start Logstash without setting LOGSTASH_CONF_BASE64"
+	exit 1
+fi
+
+# Install Opensearch output plugin
+logstash-plugin install logstash-output-opensearch
+
+# Copy Logstash config from environment variable into file for Logstash service to read
+echo "$LOGSTASH_CONF_BASE64" | base64 -d > /usr/share/logstash/pipeline/logstash.conf
+
+# Start Faktory
+logstash -f /usr/share/logstash/pipeline/logstash.conf
