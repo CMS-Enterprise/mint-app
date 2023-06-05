2b50710d348b23db899e15247cb1bcd5a70a1410
Justin Woodson <jdwoodson@gmail.com>
2023-06-05 13:37:07 -0400

    EASI-2993 (fix) incorrect Faktory reference

--- logstash/logstash_entrypoint.sh
+++ logstash/logstash_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Ensure FAKTORY_CRON_TOML_BASE64 is set, if not, we shouldn't start the service.
+# Ensure LOGSTASH_CONF_BASE64 is set, if not, we shouldn't start the service.
 if [ -z "${LOGSTASH_CONF_BASE64}" ]; then
 	echo "Unable to start Logstash without setting LOGSTASH_CONF_BASE64"
 	exit 1
@@ -9,5 +9,5 @@ fi
 # Copy Logstash config from environment variable into file for Logstash service to read
 echo "$LOGSTASH_CONF_BASE64" | base64 -d > /usr/share/logstash/pipeline/logstash.conf
 
-# Start Faktory
+# Start Logstash
 logstash -f /usr/share/logstash/pipeline/logstash.conf
