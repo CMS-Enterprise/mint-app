31468829634750afe7883b17756b229f93657d9d
Patrick Segura <patrick.segura@oddball.io>
2023-06-08 11:30:29 -0400

    Lint alignment

--- src/types/translation.ts
+++ src/types/translation.ts
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* eslint-disable camelcase */
 
 export type TranslationFieldProperties = {
   gqlField: string;
@@ -37,12 +37,21 @@ export type TranslationPlanBasics = {
   cmsCenters: TranslationFieldPropertiesWithOptions;
   cmmiGroups: TranslationFieldPropertiesWithOptions;
   modelType: TranslationFieldPropertiesWithOptions;
-  problem: TranslationFieldProperties
-  goal: TranslationFieldProperties
-  testInterventions: TranslationFieldProperties
+  problem: TranslationFieldProperties;
+  goal: TranslationFieldProperties;
+  testInterventions: TranslationFieldProperties;
 };
 
-export type PlanSection = 'model_plan' | 'plan_basics' | 'plan_general_characteristics' | 'plan_participants_and_providers' | 'plan_beneficiaries' | 'plan_ops_eval_and_learning' | 'plan_payments' | 'operational_need' | 'operational_solution';
+export type PlanSection =
+  | 'model_plan'
+  | 'plan_basics'
+  | 'plan_general_characteristics'
+  | 'plan_participants_and_providers'
+  | 'plan_beneficiaries'
+  | 'plan_ops_eval_and_learning'
+  | 'plan_payments'
+  | 'operational_need'
+  | 'operational_solution';
 
 export type TranslationPlan<> = {
   model_plan: TranslationModelPlan;
