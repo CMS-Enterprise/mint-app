b72784b7ec140c1455401c3123239e2262cf233c
ClayBenson94 <clay.benson@oddball.io>
2023-06-29 12:05:10 -0400

    Rename migration

---
+++ migrations/V98__Update_Plan_Discussion_Add_Role.sql
@@ -0,0 +1,38 @@
+CREATE TYPE DISCUSSION_USER_ROLE AS ENUM (
+  'CMS_SYSTEM_SERVICE_TEAM',
+  'IT_ARCHITECT',
+  'LEADERSHIP',
+  'MEDICARE_ADMINISTRATIVE_CONTRACTOR',
+  'MINT_TEAM',
+  'MODEL_IT_LEAD',
+  'MODEL_TEAM',
+  'SHARED_SYSTEM_MAINTAINER',
+  'NONE_OF_THE_ABOVE'
+  );
+
+ALTER TABLE plan_discussion
+  ADD COLUMN user_role DISCUSSION_USER_ROLE NOT NULL DEFAULT 'NONE_OF_THE_ABOVE'::DISCUSSION_USER_ROLE,
+  ADD COLUMN user_role_description ZERO_STRING;
+
+-- Add a CHECK constraint to enforce the user_role and user_role_description relationship
+-- If user_role is 'NONE_OF_THE_ABOVE', user_role_description must not be empty
+ALTER TABLE plan_discussion
+  ADD CONSTRAINT plan_discussion_user_role_check CHECK (
+        (user_role = 'NONE_OF_THE_ABOVE' AND user_role_description IS NOT NULL)
+        OR
+        (user_role != 'NONE_OF_THE_ABOVE' AND (user_role_description IS NULL))
+      );
+
+
+ALTER TABLE discussion_reply
+  ADD COLUMN user_role DISCUSSION_USER_ROLE NOT NULL DEFAULT 'NONE_OF_THE_ABOVE'::DISCUSSION_USER_ROLE,
+  ADD COLUMN user_role_description ZERO_STRING;
+
+-- Add a CHECK constraint to enforce the user_role and user_role_description relationship
+-- If user_role is 'NONE_OF_THE_ABOVE', user_role_description must not be empty
+ALTER TABLE discussion_reply
+  ADD CONSTRAINT plan_discussion_user_role_check CHECK (
+      (user_role = 'NONE_OF_THE_ABOVE' AND user_role_description IS NOT NULL)
+      OR
+      (user_role != 'NONE_OF_THE_ABOVE' AND (user_role_description IS NULL))
+    );
