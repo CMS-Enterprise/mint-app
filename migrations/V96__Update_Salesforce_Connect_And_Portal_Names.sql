81ebba1def48527917716ed31eecf7fc4fb1b6af
Tom Brooks <tom.brooks@oddball.io>
2023-06-28 23:32:25 -0400

    feat: update salesforce names in possible operational solutions

---
+++ migrations/V96__Update_Salesforce_Connect_And_Portal_Names.sql
@@ -0,0 +1,7 @@
+UPDATE possible_operational_solution
+SET sol_name = 'Salesforce CONNECT'
+WHERE id = 32;
+
+UPDATE possible_operational_solution
+SET sol_name = 'Salesforce Project Officer Support Tool or Portal'
+WHERE id = 34;
