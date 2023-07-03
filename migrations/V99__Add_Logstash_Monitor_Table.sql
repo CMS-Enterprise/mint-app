453262477226e8d003c1c337846c1415fa782a8d
Tom Brooks <tom.brooks@oddball.io>
2023-07-03 17:08:25 -0400

    feat: make logstash resilient against clearing the database

---
+++ migrations/V99__Add_Logstash_Monitor_Table.sql
@@ -0,0 +1,8 @@
+CREATE SCHEMA IF NOT EXISTS config;
+
+CREATE TABLE config.logstash_monitor (
+    table_name VARCHAR(255) NOT NULL PRIMARY KEY,
+    has_ingested BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+INSERT INTO config.logstash_monitor (table_name) VALUES ('change');
