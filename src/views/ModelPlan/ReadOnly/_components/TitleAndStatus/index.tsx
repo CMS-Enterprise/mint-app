8d8860c17ee3047e65edca24b662dd15a10ffafd
Gary Zhao <gary.j.zhao@gmail.com>
2023-07-18 13:28:23 -0400

    add new title and status component to DRY code

---
+++ src/views/ModelPlan/ReadOnly/_components/TitleAndStatus/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { TaskStatus } from 'types/graphql-global-types';
+import { TaskListStatusTag } from 'views/ModelPlan/TaskList/_components/TaskListItem';
+
+type TitleAndStatusProps = {
+  clearance: boolean | undefined;
+  clearanceTitle: string;
+  heading: string;
+  isViewingFilteredView?: boolean;
+  status: TaskStatus | undefined;
+};
+
+const TitleAndStatus = ({
+  clearance,
+  clearanceTitle,
+  heading,
+  isViewingFilteredView,
+  status
+}: TitleAndStatusProps) => {
+  return (
+    <div className="display-flex flex-justify flex-align-start">
+      <h2 className="margin-top-0 margin-bottom-4">
+        {clearance ? clearanceTitle : heading}
+      </h2>
+      {!isViewingFilteredView && status && (
+        <TaskListStatusTag status={status} />
+      )}
+    </div>
+  );
+};
+
+export default TitleAndStatus;
