535d8db41e0c616bfd0b345ab243cab6ece238c5
Gary Zhao <gary.j.zhao@gmail.com>
2023-07-18 12:10:08 -0400

    Add two e2e tests

--- cypress/e2e/filterGroups.spec.js
+++ cypress/e2e/filterGroups.spec.js
@@ -1,9 +1,9 @@
-describe('Email Unfollow Link', () => {
+describe('Filter Group in Read Only Sections', () => {
   beforeEach(() => {
     cy.localLogin({ name: 'MINT', role: 'MINT_USER_NONPROD' });
   });
 
-  it('follows a model plan and then unfollow via email link', () => {
+  it('Viewing the CCW filtered group view', () => {
     cy.visit('/models');
 
     cy.get('[data-testid="table"] a')
@@ -19,32 +19,58 @@ describe('Email Unfollow Link', () => {
     });
 
     cy.get('[data-testid="filter-view-modal"').should('exist');
+    cy.get('[data-testid="button"')
+      .contains('View filtered content')
+      .should('be.disabled');
 
     cy.get('[data-testid="combo-box-input"')
       .type('Chron{downArrow}{enter}')
       .should('have.value', 'Chronic Conditions Warehouse (CCW)');
 
-    // cy.get('#filter-group').contains('Chronic Conditions Warehouse (CCW)');
+    cy.get('[data-testid="button"')
+      .contains('View filtered content')
+      .should('not.be.disabled')
+      .click();
 
-    // cy.get('[data-testid="table"] button > svg[data-cy="unfavorited"]')
-    //   .first()
-    //   .click();
+    cy.url().should('include', '?filter-view=ccw');
 
-    // cy.get('#favorite-table').within(() => {
-    //   cy.contains('Empty Plan').then($el => {
-    //     const modelID = $el[0].pathname.replace(
-    //       /.*models\/(.*)\/read.*/g,
-    //       '$1'
-    //     );
+    cy.get('[data-testid="group-filter-banner"]')
+      .should('exist')
+      .contains('You are viewing CCW information.');
 
-    //     cy.visit(`/unfollow/?modelID=${modelID}`);
-    //   });
-    // });
+    cy.get('#read-only-side-nav__wrapper').should('not.exist');
+    cy.get('.filtered-view-section--model-team').should('exist');
+  });
 
-    // cy.url().should('include', '/models');
+  it('Clear filter after viewing the CCW filtered group view', () => {
+    cy.visit('/models');
 
-    // cy.get('#favorite-table').should('not.exist');
+    cy.get('[data-testid="table"] a')
+      .contains('Enhancing Oncology Model')
+      .click();
 
-    // cy.get('[data-testid="mandatory-fields-alert"]').contains('Empty Plan');
+    cy.get('[data-testid="group-filter-banner"]').within(() => {
+      cy.get('button').contains('Filter').click();
+    });
+
+    cy.get('[data-testid="combo-box-input"')
+      .type('Chron{downArrow}{enter}')
+      .should('have.value', 'Chronic Conditions Warehouse (CCW)');
+
+    cy.get('[data-testid="button"').contains('View filtered content').click();
+
+    cy.url().should('include', '?filter-view=ccw');
+
+    cy.get('[data-testid="group-filter-banner"]').contains(
+      'You are viewing CCW information.'
+    );
+
+    cy.get('[data-testid="group-filter-banner"]').within(() => {
+      cy.get('button').contains('Clear filter').click();
+    });
+
+    cy.get('[data-testid="group-filter-banner"]').contains(
+      'You are viewing all information about this model.'
+    );
   });
 });
