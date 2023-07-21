743f314646ec8cdfdcf8cebbb1d64ed70feb9bd2
Gary Zhao <gary.j.zhao@gmail.com>
2023-07-21 13:09:44 -0400

    add TwoPageMeeting component to get started page

---
+++ src/i18n/en-US/helpAndKnowledge/Articles/twoPageMeeting.ts
@@ -0,0 +1,7 @@
+const twoPageMeeting = {
+  title: 'How to have a successful 2-pager meeting',
+  description:
+    'Use this article to ensure you’re fully prepared for your 2-page concept review meeting with CMMI Front Office (FO).'
+};
+
+export default twoPageMeeting;
