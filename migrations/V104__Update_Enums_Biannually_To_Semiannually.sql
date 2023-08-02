d6e4f7317b49116ff238a77d73a64eda73a1ef8c
Tom Brooks <tom.brooks@oddball.io>
2023-08-02 18:08:37 -0400

    fix: resolved many broken elements of the enum array updates

---
+++ migrations/V104__Update_Enums_Biannually_To_Semiannually.sql
@@ -0,0 +1,22 @@
+BEGIN;
+  -- Update the columns that use the old enum values to use the new ones
+  UPDATE plan_beneficiaries
+  SET beneficiary_selection_frequency = 'SEMIANNUALLY'
+  WHERE beneficiary_selection_frequency = 'BIANNUALLY';
+
+  UPDATE plan_participants_and_providers
+  SET provider_addition_frequency = 'SEMIANNUALLY'
+  WHERE provider_addition_frequency = 'BIANNUALLY';
+
+  UPDATE plan_ops_eval_and_learning
+  SET data_sharing_frequency = replace_element(data_sharing_frequency, 'BIANNUALLY', 'SEMIANNUALLY')
+  WHERE 'BIANNUALLY' = ANY(data_sharing_frequency);
+
+  UPDATE plan_ops_eval_and_learning
+  SET data_collection_frequency = replace_element(data_collection_frequency, 'BIANNUALLY', 'SEMIANNUALLY')
+  WHERE 'BIANNUALLY' = ANY(data_collection_frequency);
+
+  UPDATE plan_payments
+  SET anticipated_payment_frequency = replace_element(anticipated_payment_frequency, 'BIANNUALLY', 'SEMIANNUALLY')
+  WHERE 'BIANNUALLY' = ANY(anticipated_payment_frequency);
+COMMIT;
--- migrations/V104__Update_Plan_Payments_Fields.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE plan_payments
-  DROP COLUMN funding_structure;
