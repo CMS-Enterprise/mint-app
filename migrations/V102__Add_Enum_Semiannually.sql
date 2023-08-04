08b47fc730e3dd03625114907b96cafd9319cc49
Tom Brooks <tom.brooks@oddball.io>
2023-08-04 14:41:41 -0400

    chore: per feedback, removed enum changes in favor of FE translation masking

--- migrations/V102__Add_Enum_Semiannually.sql
+++
@@ -1,3 +0,0 @@
-ALTER TYPE frequency_type ADD VALUE 'SEMIANNUALLY';
-ALTER TYPE pp_anticipated_payment_frequency_type ADD VALUE 'SEMIANNUALLY';
-ALTER TYPE data_frequency_type ADD VALUE 'SEMIANNUALLY';
