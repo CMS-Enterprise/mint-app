b11c65d1b94721312a5e8f5738cdb50acbe63557
Gary Zhao <gary.j.zhao@gmail.com>
2023-08-15 11:34:28 -0400

    update more tests

--- src/views/ModelPlan/TaskList/Basics/index.test.tsx
+++ src/views/ModelPlan/TaskList/Basics/index.test.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { MemoryRouter, Route } from 'react-router-dom';
 import { MockedProvider } from '@apollo/client/testing';
 import { render, screen, waitFor } from '@testing-library/react';
+import { mount } from 'enzyme';
+import toJson, { OutputMapper } from 'enzyme-to-json';
 
 import GetModelPlanInfo from 'queries/Basics/GetModelPlanInfo';
 import { GetModelPlanInfo_modelPlan as GetModelPlanInfoType } from 'queries/Basics/types/GetModelPlanInfo';
@@ -10,6 +12,7 @@ import {
   CMSCenter,
   ModelCategory
 } from 'types/graphql-global-types';
+import renameTooltipAriaAndID from 'utils/testing/snapshotSerializeReplacements';
 
 import Basics from './index';
 
@@ -49,7 +52,7 @@ const mocks = [
   }
 ];
 
-describe('Model Plan Documents page', () => {
+describe('Model Plan Task List Basics page', () => {
   it('renders without errors', async () => {
     render(
       <MemoryRouter
@@ -73,7 +76,7 @@ describe('Model Plan Documents page', () => {
     });
   });
   it('matches snapshot', async () => {
-    const { asFragment } = render(
+    const component = mount(
       <MemoryRouter
         initialEntries={[
           '/models/f11eb129-2c80-4080-9440-439cbe1a286f/task-list/basics'
@@ -86,12 +89,16 @@ describe('Model Plan Documents page', () => {
         </MockedProvider>
       </MemoryRouter>
     );
+
     await waitFor(() => {
-      expect(screen.getByTestId('model-plan-basics')).toBeInTheDocument();
-      expect(
-        screen.getByTestId('summary-box--previous-name')
-      ).toBeInTheDocument();
+      expect(component.text().includes('State-Based')).toBe(true);
     });
-    expect(asFragment()).toMatchSnapshot();
+
+    expect(
+      toJson(component, {
+        mode: 'deep',
+        map: renameTooltipAriaAndID as OutputMapper
+      })
+    ).toMatchSnapshot();
   });
 });
