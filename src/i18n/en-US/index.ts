a913f5241fa5860b617ffaba06630bc5220138eb
Gary Zhao <gary.j.zhao@gmail.com>
2023-08-16 14:43:21 -0400

    all the initial six pager meeting

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -15,6 +15,7 @@ import subtasks from './draftModelPlan/subtasks';
 import getAccess from './helpAndKnowledge/Articles/getAccess';
 import modelPlanOverview from './helpAndKnowledge/Articles/modelPlanOverview';
 import sampleModelPlan from './helpAndKnowledge/Articles/sampleModelPlan';
+import sixPageMeeting from './helpAndKnowledge/Articles/sixPageMeeting';
 import twoPageMeeting from './helpAndKnowledge/Articles/twoPageMeeting';
 import helpAndKnowledge from './helpAndKnowledge/helpAndKnowledge';
 import { basics, basicsMisc } from './modelPlan/basics';
@@ -104,6 +105,7 @@ const enUS = {
   readOnlyModelPlan,
   sampleModelPlan,
   twoPageMeeting,
+  sixPageMeeting,
   subtasks,
   tableAndPagination,
   termsAndConditions
