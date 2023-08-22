c0fa88b55ab84ab1e16100718cf4f89ac4b05c2d
Tom Brooks <tom.brooks@oddball.io>
2023-08-22 18:04:49 -0400

    fix: corrected thread-safety issues

--- pkg/storage/namedstatements/cache.go
+++ pkg/storage/namedstatements/cache.go
@@ -10,37 +10,70 @@ import (
 // Cache is a cache of prepared statements.
 type Cache struct {
 	db                 *sqlx.DB
-	preparedStatements sync.Map
+	preparedStatements map[string]*sqlx.NamedStmt
+	lock               sync.RWMutex
 }
 
 // NewCache initializes a new Cache instance with the provided database.
 func NewCache(db *sqlx.DB) *Cache {
 	return &Cache{
 		db:                 db,
-		preparedStatements: sync.Map{},
+		preparedStatements: make(map[string]*sqlx.NamedStmt),
 	}
 }
 
 // Get returns a prepared statement for the provided query.
 func (n *Cache) Get(query string) (*sqlx.NamedStmt, error) {
-	if stmtInterface, ok := n.preparedStatements.Load(query); ok {
-		stmt, ok := stmtInterface.(*sqlx.NamedStmt)
-		if !ok {
-			return nil, errors.New("failed to cast stmtInterface to *sqlx.Stmt")
-		}
+	n.lock.RLock()
+	storedStatement, foundPreparedStatement := n.preparedStatements[query]
+	n.lock.RUnlock()
 
-		return stmt, nil
+	if foundPreparedStatement {
+		return storedStatement, nil
 	}
 
-	return n.prepareNamed(query)
-}
+	n.lock.Lock()
+	defer n.lock.Unlock()
 
-func (n *Cache) prepareNamed(query string) (*sqlx.NamedStmt, error) {
-	stmt, err := n.db.PrepareNamed(query)
+	// Check again to ensure no other goroutine prepared the statement since our previous check.
+	// We must assert this as there is a potential for a prepared statement overwrite between the
+	// first check and the write lock acquisition, however minor the chance.
+	if storedStatement, foundPreparedStatement = n.preparedStatements[query]; foundPreparedStatement {
+		return storedStatement, nil
+	}
+
+	preparedStatement, err := n.db.PrepareNamed(query)
 	if err != nil {
 		return nil, err
 	}
 
-	n.preparedStatements.Store(query, stmt)
-	return stmt, nil
+	n.preparedStatements[query] = preparedStatement
+	return preparedStatement, nil
+}
+
+// Close releases all resources and closes all prepared statements.
+func (n *Cache) Close() error {
+	n.lock.Lock()
+	defer n.lock.Unlock()
+
+	var errs []error
+
+	for _, stmt := range n.preparedStatements {
+		if err := stmt.Close(); err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	// Clear the map after attempting to close all statements
+	n.preparedStatements = make(map[string]*sqlx.NamedStmt)
+
+	if len(errs) > 0 {
+		msg := "Errors while closing prepared statements: "
+		for _, err := range errs {
+			msg += err.Error() + "; "
+		}
+		return errors.New(msg)
+	}
+
+	return nil
 }
