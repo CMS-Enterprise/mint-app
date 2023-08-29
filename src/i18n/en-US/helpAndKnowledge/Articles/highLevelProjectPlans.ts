46b41d86da8d8fc26256e308d5fafe15af3e24e4
Gary Zhao <gary.j.zhao@gmail.com>
2023-08-29 15:18:35 -0400

    Add high level project plans translations

---
+++ src/i18n/en-US/helpAndKnowledge/Articles/highLevelProjectPlans.ts
@@ -0,0 +1,23 @@
+const highLevelProjectPlans = {
+  title: 'High-level project plans',
+  description:
+    'Use this high-level project plan to understand and track the key pieces of work involved in developing a model. The MINT Team can assist model teams in completing this for upcoming models once collaboration begins.',
+  downloadExcel: 'Download project plan as an Excel file',
+  accordionHelp:
+    'Click to expand each accordion to reveal more information about each phase within the project plan.',
+  accordionItems: {
+    titles: [
+      'CMMI Model Operational Planning',
+      'CMMI Internal Clearance Process',
+      'Clearance of Documents',
+      'Legal',
+      'Participants',
+      'Model Operations',
+      'Payment',
+      'Learning & Diffusion',
+      'Evaluation'
+    ]
+  }
+};
+
+export default highLevelProjectPlans;
