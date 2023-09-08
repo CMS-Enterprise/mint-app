a669b2ae2a6abdc41cf9257ac73a76bf60ff55e3
Gary Zhao <gary.j.zhao@gmail.com>
2023-09-08 14:12:39 -0400

    add xslx as file declaration and spacing/prettier edits

--- src/global.d.ts
+++ src/global.d.ts
@@ -15,17 +15,18 @@ declare global {
   declare module '*.doc';
   declare module '*.pdf';
   declare module '*.docx';
+  declare module '*.xlsx';
 
-  // These types are defined in GQL generated types.  
+  // These types are defined in GQL generated types.
   // Defining globally here for FE to recognize generated types.
   //
   // Until we add better scalar mapping with graphql-codegen, anything added here should
   // also be added to codegen.ts
-   type UUID = string;
-   type Time = string;
-   type Upload = File;
+  type UUID = string;
+  type Time = string;
+  type Upload = File;
 
-   interface Window {
+  interface Window {
     Cypress: any;
     Beacon: any;
     store: any;
