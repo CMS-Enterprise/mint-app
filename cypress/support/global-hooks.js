286fc7337900ef527820a2f21187b59b0c7917cc
ClayBenson94 <clay.benson@oddball.io>
2023-09-15 13:30:22 -0400

    Clean then seed

--- cypress/support/global-hooks.js
+++ cypress/support/global-hooks.js
@@ -1,12 +1,13 @@
 beforeEach(() => {
-  cy.exec('scripts/dev db:clean').then(result => {
-    cy.log('db:clean code', result.code);
-    cy.log('db:clean stdout', result.stdout);
-    cy.log('db:clean stderr', result.stderr);
-  });
-  cy.exec('scripts/dev db:seed').then(result => {
-    cy.log('db:seed code', result.code);
-    cy.log('db:seed stdout', result.stdout);
-    cy.log('db:seed stderr', result.stderr);
+  cy.exec('scripts/dev db:clean').then(cleanResult => {
+    cy.log('db:clean code', cleanResult.code);
+    cy.log('db:clean stdout', cleanResult.stdout);
+    cy.log('db:clean stderr', cleanResult.stderr);
+
+    cy.exec('scripts/dev db:seed').then(seedResult => {
+      cy.log('db:seed code', seedResult.code);
+      cy.log('db:seed stdout', seedResult.stdout);
+      cy.log('db:seed stderr', seedResult.stderr);
+    });
   });
 });
