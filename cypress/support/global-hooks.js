dd4889aa9290cc201f20af85d9a9ac11a6278bca
Patrick Segura <patrick.segura@oddball.io>
2023-09-18 11:06:12 -0500

    Added cy.exec assertions and timeout

--- cypress/support/global-hooks.js
+++ cypress/support/global-hooks.js
@@ -1,13 +1,19 @@
 beforeEach(() => {
-  cy.exec('scripts/dev db:clean').then(cleanResult => {
-    cy.log('db:clean code', cleanResult.code);
-    cy.log('db:clean stdout', cleanResult.stdout);
-    cy.log('db:clean stderr', cleanResult.stderr);
+  cy.exec('scripts/dev db:clean', { timeout: 20000 })
+    .its('code')
+    .should('eq', 0)
+    .then(cleanResult => {
+      cy.log('db:clean code', cleanResult.code);
+      cy.log('db:clean stdout', cleanResult.stdout);
+      cy.log('db:clean stderr', cleanResult.stderr);
 
-    cy.exec('scripts/dev db:seed').then(seedResult => {
-      cy.log('db:seed code', seedResult.code);
-      cy.log('db:seed stdout', seedResult.stdout);
-      cy.log('db:seed stderr', seedResult.stderr);
+      cy.exec('scripts/dev db:seed', { timeout: 20000 })
+        .its('code')
+        .should('eq', 0)
+        .then(seedResult => {
+          cy.log('db:seed code', seedResult.code);
+          cy.log('db:seed stdout', seedResult.stdout);
+          cy.log('db:seed stderr', seedResult.stderr);
+        });
     });
-  });
 });
