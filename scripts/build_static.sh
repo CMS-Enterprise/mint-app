c466cc4c797fdd0d6a8ff5e37d2c3b54355b8080
Patrick Segura <95709965+patrickseguraoddball@users.noreply.github.com>
2023-09-18 13:14:29 -0500

    [EASI-3250] - Migrate cra to vite (#693)
    * Init csv translation, update packages
    
    * Implemented dynamic header translations
    
    * Added date translations
    
    * Reverted team member name translation
    
    * Remove unwind transform
    
    * Added recursive function to translate data
    
    * Added support for parent level translations, added comments
    
    * Added better support for discussion/replies with users, fixed heading parsing
    
    * Added ignore pattern for json2csv node module file
    
    * Revert ignore pattern config
    
    * Added ignore pattern, updated snaps
    
    * Added global timezone to jest config
    
    * Added unit tests
    
    * Added comment for clarity
    
    * Added slash to allowed characters/ removed unneeded field
    
    * Init an mimic EASI vite config/changes
    
    * Updated widget config, address/deleted unneeded unit tests
    
    * Updated cypress config
    
    * Push updates to snapshots and lock file
    
    * Revert run_tests.yml to main
    
    * Extend cypress timeout on login to account for vite sass
    
    * Add warmup
    
    * Revert "Add warmup"
    
    This reverts commit 64b1df3a6acce5f805bdd7f9ffc36d1adc653335.
    
    * Clean then seed
    
    * Added cy.exec assertions and timeout
    
    * Add failOnNonZeroExit: false
    
    ---------
    
    Co-authored-by: Justin Woodson <Jdwoodson@gmail.com>
    Co-authored-by: ClayBenson94 <clay.benson@oddball.io>

--- scripts/build_static.sh
+++ scripts/build_static.sh
@@ -6,19 +6,19 @@ set -eux -o pipefail
 case "$APP_ENV" in
   "dev")
     MINT_URL="https://dev.mint.cms.gov"
-    export REACT_APP_OKTA_DOMAIN="https://test.idp.idm.cms.gov"
+    export VITE_OKTA_DOMAIN="https://test.idp.idm.cms.gov"
     ;;
   "test")
     MINT_URL="https://test.mint.cms.gov"
-    export REACT_APP_OKTA_DOMAIN="https://test.idp.idm.cms.gov"
+    export VITE_OKTA_DOMAIN="https://test.idp.idm.cms.gov"
     ;;
   "impl")
     MINT_URL="https://impl.mint.cms.gov"
-    export REACT_APP_OKTA_DOMAIN="https://impl.idp.idm.cms.gov"
+    export VITE_OKTA_DOMAIN="https://impl.idp.idm.cms.gov"
     ;;
   "prod")
     MINT_URL="https://mint.cms.gov"
-    export REACT_APP_OKTA_DOMAIN="https://idm.cms.gov"
+    export VITE_OKTA_DOMAIN="https://idm.cms.gov"
     ;;
   *)
     echo "APP_ENV value not recognized: ${APP_ENV:-unset}"
@@ -27,16 +27,16 @@ case "$APP_ENV" in
     ;;
 esac
 
-# Export REACT_APP_ environment variables required to build the app.
-export REACT_APP_APP_ENV="$APP_ENV"
-export REACT_APP_OKTA_CLIENT_ID="$OKTA_CLIENT_ID"
-export REACT_APP_OKTA_SERVER_ID="$OKTA_SERVER_ID"
-export REACT_APP_LD_CLIENT_ID="$LD_CLIENT_ID"
-export REACT_APP_OKTA_ISSUER="${REACT_APP_OKTA_DOMAIN}/oauth2/${REACT_APP_OKTA_SERVER_ID}"
-export REACT_APP_OKTA_REDIRECT_URI="${MINT_URL}/implicit/callback"
-export REACT_APP_API_ADDRESS="${MINT_URL}/api/v1"
-export REACT_APP_GRAPHQL_ADDRESS="${MINT_URL}/api/graph/query"
-export REACT_APP_LOCAL_AUTH_ENABLED="false" # Disable deploying local auth
+# Export VITE_ environment variables required to build the app.
+export VITE_APP_ENV="$APP_ENV"
+export VITE_OKTA_CLIENT_ID="$OKTA_CLIENT_ID"
+export VITE_OKTA_SERVER_ID="$OKTA_SERVER_ID"
+export VITE_LD_CLIENT_ID="$LD_CLIENT_ID"
+export VITE_OKTA_ISSUER="${VITE_OKTA_DOMAIN}/oauth2/${VITE_OKTA_SERVER_ID}"
+export VITE_OKTA_REDIRECT_URI="${MINT_URL}/implicit/callback"
+export VITE_API_ADDRESS="${MINT_URL}/api/v1"
+export VITE_GRAPHQL_ADDRESS="${MINT_URL}/api/graph/query"
+export VITE_LOCAL_AUTH_ENABLED="false" # Disable deploying local auth
 
 ( set -x -u ;
   yarn install --frozen-lockfile
