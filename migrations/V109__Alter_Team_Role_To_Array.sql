5609cee7814d1a4658ad3f953c223f49c33bace6
Tom Brooks <tom.brooks@oddball.io>
2023-10-19 02:02:55 -0400

    chore: fixed plan corraborator team roles trigger

--- migrations/V109__Alter_Team_Role_To_Array.sql
+++ migrations/V109__Alter_Team_Role_To_Array.sql
@@ -1,24 +1,31 @@
-DROP TRIGGER collaborator_lead_req_update ON plan_collaborator;
-DROP TRIGGER collaborator_lead_req_delete ON plan_collaborator;
-DROP FUNCTION collaborator_role_check_trigger();
+-- Drop existing triggers and functions
+DROP TRIGGER IF EXISTS collaborator_lead_req_update ON plan_collaborator;
+DROP TRIGGER IF EXISTS collaborator_lead_req_delete ON plan_collaborator;
+DROP FUNCTION IF EXISTS collaborator_role_check_trigger();
 
+-- Rename team_role column to team_roles
 ALTER TABLE plan_collaborator RENAME COLUMN team_role TO team_roles;
 
--- Alter the team_roles column to be an array of team_role enum and convert existing values to array format
+-- Convert the team_roles column to an array of team_role enum
 ALTER TABLE plan_collaborator
   ALTER COLUMN team_roles TYPE team_role[] USING ARRAY[team_roles];
 
-CREATE FUNCTION collaborator_role_check_trigger() RETURNS TRIGGER AS $role_check$
+-- Create or replace trigger function
+CREATE OR REPLACE FUNCTION collaborator_role_check_trigger() RETURNS TRIGGER AS $role_check$
 BEGIN
-  -- Check if 'MODEL_LEAD' exists in the team_roles array for the given model_plan_id
-  IF (
-       SELECT count(*)
-       FROM plan_collaborator
-       WHERE 'MODEL_LEAD' = ANY(team_roles) AND model_plan_id = OLD.model_plan_id
-     ) < 2 THEN
-    RAISE EXCEPTION 'There must be at least one MODEL_LEAD assigned to each model plan';
+  -- Check if the old roles contained "MODEL_LEAD" but the new roles do not
+  IF 'MODEL_LEAD' = ANY(OLD.team_roles) AND NOT ('MODEL_LEAD' = ANY(NEW.team_roles)) THEN
+    -- Ensure there's another collaborator with the "MODEL_LEAD" role
+    IF (
+         SELECT count(*)
+         FROM plan_collaborator
+         WHERE 'MODEL_LEAD' = ANY(team_roles) AND model_plan_id = OLD.model_plan_id AND id != NEW.id
+       ) = 0 THEN
+      RAISE EXCEPTION 'There must be at least one MODEL_LEAD assigned to each model plan';
+    END IF;
   END IF;
 
+  -- Return value based on operation type
   IF (TG_OP = 'DELETE') THEN
     RETURN OLD;
   ELSE
@@ -27,20 +34,21 @@ BEGIN
 END
 $role_check$ LANGUAGE plpgsql;
 
+-- Create triggers
 CREATE TRIGGER collaborator_lead_req_update
   BEFORE UPDATE ON plan_collaborator
   FOR EACH ROW
-  WHEN ('MODEL_LEAD' = ANY(old.team_roles) AND 'MODEL_LEAD' != ANY(new.team_roles))
-EXECUTE FUNCTION COLLABORATOR_ROLE_CHECK_TRIGGER();
+  WHEN ('MODEL_LEAD' = ANY(OLD.team_roles) AND NOT ('MODEL_LEAD' = ANY(NEW.team_roles)))
+EXECUTE FUNCTION collaborator_role_check_trigger();
 
 CREATE TRIGGER collaborator_lead_req_delete
   BEFORE DELETE ON plan_collaborator
   FOR EACH ROW
-  WHEN ('MODEL_LEAD' = ANY(old.team_roles))
-EXECUTE FUNCTION COLLABORATOR_ROLE_CHECK_TRIGGER();
+  WHEN ('MODEL_LEAD' = ANY(OLD.team_roles))
+EXECUTE FUNCTION collaborator_role_check_trigger();
 
--- Trigger function to check for unique roles within team_roles array
-CREATE FUNCTION ENSURE_UNIQUE_ROLES_TRIGGER() RETURNS TRIGGER AS $unique_roles$
+-- Trigger function to check for unique roles in team_roles array
+CREATE FUNCTION ensure_unique_roles_trigger() RETURNS TRIGGER AS $unique_roles$
 BEGIN
   IF ARRAY_LENGTH(NEW.team_roles, 1) != (SELECT COUNT(DISTINCT unnest_val) FROM unnest(NEW.team_roles) AS unnest_val) THEN
     RAISE EXCEPTION 'Duplicate roles are not allowed in team_roles';
@@ -49,8 +57,8 @@ BEGIN
 END
 $unique_roles$ LANGUAGE plpgsql;
 
--- Trigger to call the function before each INSERT or UPDATE on plan_collaborator
+-- Create trigger to ensure unique roles
 CREATE TRIGGER ensure_unique_roles
   BEFORE INSERT OR UPDATE ON plan_collaborator
   FOR EACH ROW
-EXECUTE FUNCTION ENSURE_UNIQUE_ROLES_TRIGGER();
+EXECUTE FUNCTION ensure_unique_roles_trigger();
