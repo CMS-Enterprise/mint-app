e5238e7e8c6b54da4aeb9ebe2d8a730a9e9f06fd
Gary Zhao <gary.j.zhao@gmail.com>
2023-10-30 13:47:11 -0400

    update compy

--- src/i18n/en-US/modelPlan/collaborators.ts
+++ src/i18n/en-US/modelPlan/collaborators.ts
@@ -49,7 +49,7 @@ export const collaboratorsMisc: Record<string, string | NestedTranslation> = {
   teamInfo:
     'Team members are all eligible to view and edit all sections of a Model Plan, including uploading and removing documents as well as adding or removing team members.',
   searchMemberInfo:
-    'This team member will be able to view and edit anything about a model plan. Please make sure this individual should be able to do this before you proceed.',
+    'This new team member will be able to view and edit anything about a model plan. Please make sure this individual should be able to do this before you proceed.',
   lastModelLeadMemberInfo:
     'If this team member is no longer the Model Lead, you must add a new Model Lead before removing that role for this team member.',
   dontAddTeamMember: 'Don’t add a team member and return to the previous page',
