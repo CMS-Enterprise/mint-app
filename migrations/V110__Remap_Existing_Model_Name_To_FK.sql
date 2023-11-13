4fd65733ad79bac993b632c9a99498594920abfc
Tom Brooks <tom.brooks@oddball.io>
2023-11-13 10:29:40 -0500

    feat: updated to use dual column solution in plan_general_characteristics as discussed with Steven

--- migrations/V110__Remap_Existing_Model_Name_To_FK.sql
+++ migrations/V110__Remap_Existing_Model_Name_To_FK.sql
@@ -1,19 +1,50 @@
--- Flyway migration V110
+-- Adding new columns to the plan_general_characteristics table
 ALTER TABLE plan_general_characteristics
-  ADD COLUMN existing_model_id UUID NULL,
-  ADD CONSTRAINT fk_existing_model
-    FOREIGN KEY (existing_model_id)
+  ADD COLUMN existing_model_plan_id UUID NULL,
+  ADD COLUMN existing_model_external_id INT NULL;
+
+-- Adding foreign key constraints
+ALTER TABLE plan_general_characteristics
+  ADD CONSTRAINT fk_existing_model_plan
+    FOREIGN KEY (existing_model_plan_id)
       REFERENCES model_plan(id)
       ON DELETE SET NULL;
 
+ALTER TABLE plan_general_characteristics
+  ADD CONSTRAINT fk_existing_model_external
+    FOREIGN KEY (existing_model_external_id)
+      REFERENCES public.existing_model(id)
+      ON DELETE SET NULL;
+
+-- Ensuring that existing_model_plan_id and existing_model_external_id are mutually exclusive
+ALTER TABLE plan_general_characteristics
+  ADD CONSTRAINT plan_general_characteristics_check_exclusive_model_ids CHECK (
+      (existing_model_plan_id IS NULL) OR (existing_model_external_id IS NULL)
+    );
+
+-- Data migration for existing_model_plan_id
 UPDATE plan_general_characteristics pc
-SET existing_model_id = (
+SET existing_model_plan_id = (
   SELECT mp.id
   FROM model_plan mp
   WHERE mp.model_name = pc.existing_model
   LIMIT 1
 )
 WHERE EXISTS (
+  SELECT 1
+  FROM model_plan mp
+  WHERE mp.model_name = pc.existing_model
+);
+
+-- Data migration for existing_model_external_id
+UPDATE plan_general_characteristics pc
+SET existing_model_external_id = (
+  SELECT em.id
+  FROM public.existing_model em
+  WHERE em.model_name = pc.existing_model
+  LIMIT 1
+)
+WHERE NOT EXISTS ( -- Ensuring no conflict with existing_model_plan_id
   SELECT 1
   FROM model_plan mp
   WHERE mp.model_name = pc.existing_model
