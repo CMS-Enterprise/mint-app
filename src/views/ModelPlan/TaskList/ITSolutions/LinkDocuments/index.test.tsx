be5687b1985cce28c4f1c6d4622bafc5c97c598b
Patrick Segura <patrick.segura@oddball.io>
2023-11-20 08:17:02 -0600

    Undo act on link test

--- src/views/ModelPlan/TaskList/ITSolutions/LinkDocuments/index.test.tsx
+++ src/views/ModelPlan/TaskList/ITSolutions/LinkDocuments/index.test.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { MemoryRouter, Route } from 'react-router-dom';
 import {
-  act,
   render,
   waitFor,
   waitForElementToBeRemoved
@@ -11,15 +10,14 @@ import userEvent from '@testing-library/user-event';
 import configureMockStore from 'redux-mock-store';
 
 import { ASSESSMENT } from 'constants/jobCodes';
-import {
-  needQuestionAndAnswerMock,
-  possibleSolutionsMock
-} from 'data/mock/solutions';
+import { possibleSolutionsMock } from 'data/mock/solutions';
 import { MessageProvider } from 'hooks/useMessage';
 import GetOperationalSolution from 'queries/ITSolutions/GetOperationalSolution';
 import { OpSolutionStatus } from 'types/graphql-global-types';
 import VerboseMockedProvider from 'utils/testing/MockedProvider';
 import documentMocks from 'views/ModelPlan/Documents/index.test';
+
+import needQuestionAndAnswerMock from '../_components/NeedQuestionAndAnswer/mocks';
 
 import LinkDocuments from '.';
 
@@ -102,52 +100,82 @@ const mockStore = configureMockStore();
 const store = mockStore({ auth: mockAuthReducer });
 
 describe('IT Solutions Link Documents', () => {
-  it('renders correctly and matches snapshot', async () => {
-    await act(async () => {
-      const { getByTestId, getByRole, asFragment } = render(
-        <MemoryRouter
-          initialEntries={[
-            {
-              pathname: `/models/${modelID}/task-list/it-solutions/${operationalNeedID}/${operationalSolutionID}/link-documents`
-            }
-          ]}
-        >
-          <VerboseMockedProvider mocks={mocks} addTypename={false}>
-            <Provider store={store}>
-              <Route path="/models/:modelID/task-list/it-solutions/:operationalNeedID/:operationalSolutionID/link-documents">
-                <MessageProvider>
-                  <LinkDocuments />
-                </MessageProvider>
-              </Route>
-            </Provider>
-          </VerboseMockedProvider>
-        </MemoryRouter>
-      );
+  it('renders correctly', async () => {
+    const { getByTestId } = render(
+      <MemoryRouter
+        initialEntries={[
+          {
+            pathname: `/models/${modelID}/task-list/it-solutions/${operationalNeedID}/${operationalSolutionID}/link-documents`
+          }
+        ]}
+      >
+        <VerboseMockedProvider mocks={mocks} addTypename={false}>
+          <Provider store={store}>
+            <Route path="/models/:modelID/task-list/it-solutions/:operationalNeedID/:operationalSolutionID/link-documents">
+              <MessageProvider>
+                <LinkDocuments />
+              </MessageProvider>
+            </Route>
+          </Provider>
+        </VerboseMockedProvider>
+      </MemoryRouter>
+    );
 
-      await waitForElementToBeRemoved(() => getByRole('progressbar'));
+    // Link mutation button disabled if state === original state of selections
+    const linkButton = getByTestId('link-documents-button');
+    expect(linkButton).toHaveAttribute('disabled');
 
-      // Link mutation button disabled if state === original state of selections
-      const linkButton = getByTestId('link-documents-button');
-      expect(linkButton).toHaveAttribute('disabled');
+    // Wait for page to load
+    await waitForElementToBeRemoved(() => getByTestId('page-loading'));
 
-      // Click checkbox table cell to toggle document selection
-      const solutionDocument1 = getByTestId(
-        'link-document-9d828454-9ecd-42a0-ad84-bc8c8ddea634'
-      );
+    // Click checkbox table cell to toggle document selection
+    const solutionDocument1 = getByTestId(
+      'link-document-9d828454-9ecd-42a0-ad84-bc8c8ddea634'
+    );
 
-      userEvent.click(solutionDocument1);
+    userEvent.click(solutionDocument1);
 
-      const solutionDocument2 = getByTestId(
-        'link-document-07d0d06f-9ecd-42a0-ad84-bc8c8ddea084'
-      );
+    const solutionDocument2 = getByTestId(
+      'link-document-07d0d06f-9ecd-42a0-ad84-bc8c8ddea084'
+    );
 
-      await waitFor(() => {
-        expect(solutionDocument1).not.toBeChecked();
-        expect(solutionDocument2).toBeChecked();
-        expect(linkButton).not.toHaveAttribute('disabled');
-      });
-
-      expect(asFragment()).toMatchSnapshot();
+    await waitFor(() => {
+      expect(solutionDocument1).not.toBeChecked();
+      expect(solutionDocument2).toBeChecked();
+      expect(linkButton).not.toHaveAttribute('disabled');
     });
+  });
+
+  it('matches snapshot', async () => {
+    const { asFragment, getByTestId } = render(
+      <MemoryRouter
+        initialEntries={[
+          {
+            pathname: `/models/${modelID}/task-list/it-solutions/${operationalNeedID}/${operationalSolutionID}/link-documents`
+          }
+        ]}
+      >
+        <VerboseMockedProvider mocks={mocks} addTypename={false}>
+          <Provider store={store}>
+            <Route path="/models/:modelID/task-list/it-solutions/:operationalNeedID/:operationalSolutionID/link-documents">
+              <MessageProvider>
+                <LinkDocuments />
+              </MessageProvider>
+            </Route>
+          </Provider>
+        </VerboseMockedProvider>
+      </MemoryRouter>
+    );
+
+    // Wait for page to load
+    await waitForElementToBeRemoved(() => getByTestId('page-loading'));
+
+    // Click checkbox table cell to toggle document selection
+    const solutionDocument1 = getByTestId(
+      'link-document-9d828454-9ecd-42a0-ad84-bc8c8ddea634'
+    );
+    userEvent.click(solutionDocument1);
+
+    expect(asFragment()).toMatchSnapshot();
   });
 });
