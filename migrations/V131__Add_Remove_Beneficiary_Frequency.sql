5c99d85301d6370cfefbec92ef37fc04d19160d1
Patrick Segura <patrick.segura@oddball.io>
2024-01-10 10:34:23 -0600

    Updated postman collection, removed unneeded null migration default

--- migrations/V131__Add_Remove_Beneficiary_Frequency.sql
+++ migrations/V131__Add_Remove_Beneficiary_Frequency.sql
@@ -2,4 +2,4 @@ ALTER TABLE plan_beneficiaries
   ADD COLUMN beneficiary_removal_frequency_continually ZERO_STRING,
   ADD COLUMN beneficiary_removal_frequency_other ZERO_STRING,
   ADD COLUMN beneficiary_removal_frequency_note ZERO_STRING,
-  ADD COLUMN beneficiary_removal_frequency FREQUENCY_TYPE_NEW[] DEFAULT NULL;
+  ADD COLUMN beneficiary_removal_frequency FREQUENCY_TYPE_NEW[];
