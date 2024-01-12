8d51dd5bf876355c7b2940311ce1221af5f68a4d
Tom Brooks <tom.brooks@oddball.io>
2024-01-12 15:56:13 -0500

    chore: updated _other and _continually notes to append on existing notes, added modification_dts and modified_by assignment where appropriate in migration

--- migrations/V127__Update_Data_Frequency_Type.sql
+++ migrations/V127__Update_Data_Frequency_Type.sql
@@ -3,26 +3,68 @@ ALTER TABLE plan_ops_eval_and_learning
   ALTER COLUMN data_sharing_frequency TYPE TEXT[] USING data_sharing_frequency::TEXT[],
   ALTER COLUMN data_collection_frequency TYPE TEXT[] USING data_collection_frequency::TEXT[];
 
+-- Set the modified_dts and modified_by in the case where we perform any modifications
+UPDATE plan_ops_eval_and_learning
+SET
+  modified_dts = now(),
+  modified_by = '00000001-0001-0001-0001-000000000001'
+WHERE
+  'BIANNUALLY' = ANY(data_sharing_frequency) OR
+  'DAILY' = ANY(data_sharing_frequency) OR
+  'WEEKLY' = ANY(data_sharing_frequency) OR
+  'SEMI_MONTHLY' = ANY(data_sharing_frequency) OR
+  'NOT_PLANNING_TO_DO_THIS' = ANY(data_sharing_frequency) OR
+  'BIANNUALLY' = ANY(data_collection_frequency) OR
+  'DAILY' = ANY(data_collection_frequency) OR
+  'WEEKLY' = ANY(data_collection_frequency) OR
+  'SEMI_MONTHLY' = ANY(data_collection_frequency) OR
+  'NOT_PLANNING_TO_DO_THIS' = ANY(data_collection_frequency);
+
 -- Alter plan_ops_eval_and_learning.data_sharing_frequency to use this new type
 UPDATE plan_ops_eval_and_learning
-SET data_sharing_frequency_continually = (
-  SELECT string_agg(CASE
-                      WHEN val = 'DAILY' THEN 'Daily'
-                      WHEN val = 'WEEKLY' THEN 'Weekly'
-                      END, ', ')
-  FROM unnest(data_sharing_frequency) AS val
-  WHERE val IN ('DAILY', 'WEEKLY')
-);
+SET data_sharing_frequency_continually =
+      CASE
+        WHEN data_sharing_frequency_continually IS NOT NULL AND data_sharing_frequency_continually <> '' THEN
+          data_sharing_frequency_continually || ', ' || (
+            SELECT string_agg(CASE
+                                WHEN val = 'DAILY' THEN 'Daily'
+                                WHEN val = 'WEEKLY' THEN 'Weekly'
+                                END, ', ')
+            FROM unnest(data_sharing_frequency) AS val
+            WHERE val IN ('DAILY', 'WEEKLY')
+          ) ELSE (
+            SELECT string_agg(CASE
+                                WHEN val = 'DAILY' THEN 'Daily'
+                                WHEN val = 'WEEKLY' THEN 'Weekly'
+                                END, ', ')
+            FROM unnest(data_sharing_frequency) AS val
+            WHERE val IN ('DAILY', 'WEEKLY'))
+        END
+WHERE 'DAILY' = ANY(data_sharing_frequency) OR 'WEEKLY' = ANY(data_sharing_frequency);
 
 UPDATE plan_ops_eval_and_learning
-SET data_sharing_frequency_other = (
-  SELECT string_agg(CASE
-                      WHEN val = 'SEMI_MONTHLY' THEN 'Semi-monthly'
-                      WHEN val = 'NOT_PLANNING_TO_DO_THIS' THEN 'Not planning to do this'
-                      END, ', ')
-  FROM unnest(data_sharing_frequency) AS val
-  WHERE val IN ('SEMI_MONTHLY', 'NOT_PLANNING_TO_DO_THIS')
-);
+SET data_sharing_frequency_other =
+      CASE
+        WHEN data_sharing_frequency_other IS NOT NULL AND data_sharing_frequency_other <> '' THEN
+          data_sharing_frequency_other || ', ' || (
+            SELECT string_agg(CASE
+                                WHEN val = 'SEMI_MONTHLY' THEN 'Semi-monthly'
+                                WHEN val = 'NOT_PLANNING_TO_DO_THIS' THEN 'Not planning to do this'
+                                END, ', ')
+            FROM unnest(data_sharing_frequency) AS val
+            WHERE val IN ('SEMI_MONTHLY', 'NOT_PLANNING_TO_DO_THIS')
+          )
+        ELSE
+          (
+            SELECT string_agg(CASE
+                                WHEN val = 'SEMI_MONTHLY' THEN 'Semi-monthly'
+                                WHEN val = 'NOT_PLANNING_TO_DO_THIS' THEN 'Not planning to do this'
+                                END, ', ')
+            FROM unnest(data_sharing_frequency) AS val
+            WHERE val IN ('SEMI_MONTHLY', 'NOT_PLANNING_TO_DO_THIS')
+          )
+        END
+WHERE 'SEMI_MONTHLY' = ANY(data_sharing_frequency) OR 'NOT_PLANNING_TO_DO_THIS' = ANY(data_sharing_frequency);
 
 ALTER TABLE plan_ops_eval_and_learning
   ADD COLUMN data_sharing_frequency_new TEXT[];
@@ -45,24 +87,52 @@ ALTER TABLE plan_ops_eval_and_learning
 
 -- Alter plan_ops_eval_and_learning.data_collection_frequency to use this new type
 UPDATE plan_ops_eval_and_learning
-SET data_collection_frequency_continually = (
-  SELECT string_agg(CASE
-                      WHEN val = 'DAILY' THEN 'Daily'
-                      WHEN val = 'WEEKLY' THEN 'Weekly'
-                      END, ', ')
-  FROM unnest(data_collection_frequency) AS val
-  WHERE val IN ('DAILY', 'WEEKLY')
-);
+SET data_collection_frequency_continually =
+      CASE
+        WHEN data_collection_frequency_continually IS NOT NULL AND data_collection_frequency_continually <> '' THEN
+          data_collection_frequency_continually || ', ' || (
+            SELECT string_agg(CASE
+                                WHEN val = 'DAILY' THEN 'Daily'
+                                WHEN val = 'WEEKLY' THEN 'Weekly'
+                                END, ', ')
+            FROM unnest(data_collection_frequency) AS val
+            WHERE val IN ('DAILY', 'WEEKLY')
+          )
+        ELSE
+          (
+            SELECT string_agg(CASE
+                                WHEN val = 'DAILY' THEN 'Daily'
+                                WHEN val = 'WEEKLY' THEN 'Weekly'
+                                END, ', ')
+            FROM unnest(data_collection_frequency) AS val
+            WHERE val IN ('DAILY', 'WEEKLY')
+          )
+        END
+WHERE 'DAILY' = ANY(data_collection_frequency) OR 'WEEKLY' = ANY(data_collection_frequency);
 
 UPDATE plan_ops_eval_and_learning
-SET data_collection_frequency_other = (
-  SELECT string_agg(CASE
-                      WHEN val = 'SEMI_MONTHLY' THEN 'Semi-monthly'
-                      WHEN val = 'NOT_PLANNING_TO_DO_THIS' THEN 'Not planning to do this'
-                      END, ', ')
-  FROM unnest(data_collection_frequency) AS val
-  WHERE val IN ('SEMI_MONTHLY', 'NOT_PLANNING_TO_DO_THIS')
-);
+SET data_collection_frequency_other =
+      CASE
+        WHEN data_collection_frequency_other IS NOT NULL AND data_collection_frequency_other <> '' THEN
+          data_collection_frequency_other || ', ' || (
+            SELECT string_agg(CASE
+                                WHEN val = 'SEMI_MONTHLY' THEN 'Semi-monthly'
+                                WHEN val = 'NOT_PLANNING_TO_DO_THIS' THEN 'Not planning to do this'
+                                END, ', ')
+            FROM unnest(data_collection_frequency) AS val
+            WHERE val IN ('SEMI_MONTHLY', 'NOT_PLANNING_TO_DO_THIS')
+          )
+        ELSE
+          (
+            SELECT string_agg(CASE
+                                WHEN val = 'SEMI_MONTHLY' THEN 'Semi-monthly'
+                                WHEN val = 'NOT_PLANNING_TO_DO_THIS' THEN 'Not planning to do this'
+                                END, ', ')
+            FROM unnest(data_collection_frequency) AS val
+            WHERE val IN ('SEMI_MONTHLY', 'NOT_PLANNING_TO_DO_THIS')
+          )
+        END
+WHERE 'SEMI_MONTHLY' = ANY(data_collection_frequency) OR 'NOT_PLANNING_TO_DO_THIS' = ANY(data_collection_frequency);
 
 ALTER TABLE plan_ops_eval_and_learning
   ADD COLUMN data_collection_frequency_new TEXT[];
@@ -93,7 +163,7 @@ SET data_collection_frequency = ARRAY(
            ELSE val
            END
   FROM unnest(data_collection_frequency) AS val
-                                )
+  WHERE val IN ('DAILY', 'WEEKLY'))
 WHERE 'BIANNUALLY' = ANY(data_collection_frequency) OR
   'DAILY' = ANY(data_collection_frequency) OR
   'WEEKLY' = ANY(data_collection_frequency) OR
--- migrations/V128__Update_PP_Anticipated_Payment_Frequency.sql
+++ migrations/V128__Update_PP_Anticipated_Payment_Frequency.sql
@@ -2,37 +2,73 @@
 ALTER TABLE plan_payments
   ALTER COLUMN anticipated_payment_frequency TYPE TEXT[] USING anticipated_payment_frequency::TEXT[];
 
+-- Set the modified_dts and modified_by in the case where we perform any modifications
+UPDATE plan_payments
+SET
+  modified_dts = now(),
+  modified_by = '00000001-0001-0001-0001-000000000001'
+WHERE 'DAILY' = ANY(anticipated_payment_frequency) OR
+  'WEEKLY' = ANY(anticipated_payment_frequency) OR
+  'SEMIMONTHLY' = ANY(anticipated_payment_frequency);
+
 -- Alter plan_ops_eval_and_learning.data_sharing_frequency to use this new type
 UPDATE plan_payments
-SET anticipated_payment_frequency_continually = (
-  SELECT string_agg(CASE
-                      WHEN val = 'DAILY' THEN 'Daily'
-                      WHEN val = 'WEEKLY' THEN 'Weekly'
-                      END, ', ')
-  FROM unnest(anticipated_payment_frequency) AS val
-  WHERE val IN ('DAILY', 'WEEKLY')
-);
+SET anticipated_payment_frequency_continually =
+      CASE
+        WHEN anticipated_payment_frequency_continually IS NOT NULL AND anticipated_payment_frequency_continually <> '' THEN
+          anticipated_payment_frequency_continually || ', ' || (
+            SELECT string_agg(CASE
+                                WHEN val = 'DAILY' THEN 'Daily'
+                                WHEN val = 'WEEKLY' THEN 'Weekly'
+                                END, ', ')
+            FROM unnest(anticipated_payment_frequency) AS val
+            WHERE val IN ('DAILY', 'WEEKLY')
+          )
+        ELSE
+          (
+            SELECT string_agg(CASE
+                                WHEN val = 'DAILY' THEN 'Daily'
+                                WHEN val = 'WEEKLY' THEN 'Weekly'
+                                END, ', ')
+            FROM unnest(anticipated_payment_frequency) AS val
+            WHERE val IN ('DAILY', 'WEEKLY')
+          )
+        END
+WHERE 'DAILY' = ANY(anticipated_payment_frequency) OR 'WEEKLY' = ANY(anticipated_payment_frequency);
 
 UPDATE plan_payments
-SET anticipated_payment_frequency_other = (
-  SELECT string_agg(CASE
-                      WHEN val = 'SEMI_MONTHLY' THEN 'Semi-monthly'
-                      END, ', ')
-  FROM unnest(anticipated_payment_frequency) AS val
-  WHERE val IN ('SEMI_MONTHLY')
-);
+SET anticipated_payment_frequency_other =
+      CASE
+        WHEN anticipated_payment_frequency_other IS NOT NULL AND anticipated_payment_frequency_other <> '' THEN
+          anticipated_payment_frequency_other || ', ' || (
+            SELECT string_agg(CASE
+                                WHEN val = 'SEMIMONTHLY' THEN 'Semi-monthly'
+                                END, ', ')
+            FROM unnest(anticipated_payment_frequency) AS val
+            WHERE val IN ('SEMIMONTHLY')
+          )
+        ELSE
+          (
+            SELECT string_agg(CASE
+                                WHEN val = 'SEMIMONTHLY' THEN 'Semi-monthly'
+                                END, ', ')
+            FROM unnest(anticipated_payment_frequency) AS val
+            WHERE val IN ('SEMIMONTHLY')
+          )
+        END
+WHERE 'SEMIMONTHLY' = ANY(anticipated_payment_frequency);
 
 ALTER TABLE plan_payments
   ADD COLUMN anticipated_payment_frequency_new TEXT[];
 
 -- Append new values to data_sharing_frequency_new based on conditions
 UPDATE plan_payments
-SET anticipated_payment_frequency_new = array_append(anticipated_payment_frequency_new, 'CONTINUALLY')
-WHERE 'DAILY' = ANY(anticipated_payment_frequency) OR 'WEEKLY' = ANY(anticipated_payment_frequency);
+  SET anticipated_payment_frequency_new = array_append(anticipated_payment_frequency_new, 'CONTINUALLY')
+  WHERE 'DAILY' = ANY(anticipated_payment_frequency) OR 'WEEKLY' = ANY(anticipated_payment_frequency);
 
 UPDATE plan_payments
-SET anticipated_payment_frequency_new = array_append(anticipated_payment_frequency_new, 'OTHER')
-WHERE 'SEMI_MONTHLY' = ANY(anticipated_payment_frequency);
+  SET anticipated_payment_frequency_new = array_append(anticipated_payment_frequency_new, 'OTHER')
+  WHERE 'SEMIMONTHLY' = ANY(anticipated_payment_frequency);
 
 -- Drop old column and rename the new one
 ALTER TABLE plan_payments
@@ -40,17 +76,6 @@ ALTER TABLE plan_payments
 
 ALTER TABLE plan_payments
   RENAME COLUMN anticipated_payment_frequency_new TO anticipated_payment_frequency;
-
--- Alter plan_payments to use this new type
-UPDATE plan_payments
-SET anticipated_payment_frequency_continually = (
-  SELECT string_agg(CASE
-                      WHEN val = 'DAILY' THEN 'Daily'
-                      WHEN val = 'WEEKLY' THEN 'Weekly'
-                      END, ', ')
-  FROM unnest(anticipated_payment_frequency) AS val
-  WHERE val IN ('DAILY', 'WEEKLY')
-);
 
 -- Alter plan_payments anticipated_payment_frequency to use the common
 -- frequency_type enum
