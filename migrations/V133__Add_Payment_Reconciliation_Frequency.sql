6b8c03d46ecf107fbe70e7130474b217d8cbafd7
Patrick Segura <patrick.segura@oddball.io>
2024-01-19 11:03:34 -0600

    updated snaps, resolved migrations

--- migrations/V133__Add_Payment_Reconciliation_Frequency.sql
+++ migrations/V133__Add_Payment_Reconciliation_Frequency.sql
@@ -2,4 +2,4 @@ ALTER TABLE plan_payments
   ADD COLUMN payment_reconciliation_frequency_continually ZERO_STRING,
   ADD COLUMN payment_reconciliation_frequency_other ZERO_STRING,
   ADD COLUMN payment_reconciliation_frequency_note ZERO_STRING,
-  ADD COLUMN payment_reconciliation_frequency FREQUENCY_TYPE_NEW[];
+  ADD COLUMN payment_reconciliation_frequency FREQUENCY_TYPE[];
