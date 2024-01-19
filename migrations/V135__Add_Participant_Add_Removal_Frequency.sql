904028d2a6dc3a69585953c8a4f882ce584a9abe
Patrick Segura <patrick.segura@oddball.io>
2024-01-19 14:14:55 -0600

    Resolved migration naming - new

--- migrations/V135__Add_Participant_Add_Removal_Frequency.sql
+++ migrations/V135__Add_Participant_Add_Removal_Frequency.sql
@@ -3,10 +3,10 @@ ALTER TABLE plan_participants_and_providers
   ADD COLUMN participant_added_frequency_continually ZERO_STRING,
   ADD COLUMN participant_added_frequency_other ZERO_STRING,
   ADD COLUMN participant_added_frequency_note ZERO_STRING,
-  ADD COLUMN participant_added_frequency FREQUENCY_TYPE_NEW[],
+  ADD COLUMN participant_added_frequency FREQUENCY_TYPE[],
 
 -- participant_removed_frequency columns
   ADD COLUMN participant_removed_frequency_continually ZERO_STRING,
   ADD COLUMN participant_removed_frequency_other ZERO_STRING,
   ADD COLUMN participant_removed_frequency_note ZERO_STRING,
-  ADD COLUMN participant_removed_frequency FREQUENCY_TYPE_NEW[];
+  ADD COLUMN participant_removed_frequency FREQUENCY_TYPE[];
