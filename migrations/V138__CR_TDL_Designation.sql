14c55d9eeb85db6b4468d26db88e5e3b0ea64b8e
ClayBenson94 <clay.benson@oddball.io>
2024-01-22 11:20:25 -0500

    Update migration to properly handle existing data

--- migrations/V138__CR_TDL_Designation.sql
+++ migrations/V138__CR_TDL_Designation.sql
@@ -1,7 +1,7 @@
 -- https://jiraent.cms.gov/browse/EASI-3495
 CREATE TYPE CR_TDL_TYPE AS ENUM ('CR', 'TDL');
 
-ALTER TABLE plan_cr_tdl ADD COLUMN type CR_TDL_TYPE NOT NULL;
+ALTER TABLE plan_cr_tdl ADD COLUMN type CR_TDL_TYPE;
 ALTER TABLE plan_cr_tdl ADD COLUMN date_implemented TIMESTAMP;
 
 -- Initial constraint ensures that date_implemented isn't set on TDLs
@@ -12,4 +12,10 @@ ALTER TABLE plan_cr_tdl ADD COLUMN date_implemented TIMESTAMP;
 ALTER TABLE plan_cr_tdl ADD CONSTRAINT date_implemented_not_set_on_tdl CHECK (type = 'CR' OR (type = 'TDL' AND date_implemented IS NULL));
 
 -- Set all existing entries to CRs by default
-UPDATE plan_cr_tdl SET type = 'CR';
+UPDATE plan_cr_tdl SET
+	type = 'CR',
+	modified_by = '00000001-0001-0001-0001-000000000001',
+	modified_dts = now();
+
+-- Set NOT NULL constraint on type, now that it's been set across the board
+ALTER TABLE plan_cr_tdl ALTER COLUMN type SET NOT NULL; 
