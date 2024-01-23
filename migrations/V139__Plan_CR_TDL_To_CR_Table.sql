520d561df04d18323f3ca60f182be46fff9041b5
Steven Wade <steven.wade@oddball.io>
2024-01-23 14:17:03 -0800

    chore: remove accidentally commented out migrations

--- migrations/V139__Plan_CR_TDL_To_CR_Table.sql
+++ migrations/V139__Plan_CR_TDL_To_CR_Table.sql
@@ -1,8 +1,8 @@
--- -- https://jiraent.cms.gov/browse/EASI-3495
+-- https://jiraent.cms.gov/browse/EASI-3495
 
--- ALTER TABLE plan_cr_tdl ADD COLUMN date_implemented TIMESTAMP;
+ALTER TABLE plan_cr_tdl ADD COLUMN date_implemented TIMESTAMP;
 
--- ALTER TABLE plan_cr_tdl RENAME TO plan_cr;
+ALTER TABLE plan_cr_tdl RENAME TO plan_cr;
 
--- /* Update audit table to rename cr_tdl to plan_cr */
--- UPDATE "audit"."table_config" SET "name"='plan_cr' WHERE "name"='plan_cr_tdl' 
+/* Update audit table to rename cr_tdl to plan_cr */
+UPDATE "audit"."table_config" SET "name"='plan_cr' WHERE "name"='plan_cr_tdl' 
