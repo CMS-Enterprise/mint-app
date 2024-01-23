88b11453d24eff37064cffec326922c9ea7319e3
Tom Brooks <tom.brooks@oddball.io>
2024-01-23 13:01:50 -0500

    chore: formatted sql

--- migrations/V138__IPC_Updates.sql
+++ migrations/V138__IPC_Updates.sql
@@ -1,20 +1,51 @@
 /* Remove relevant points of contact */
-WITH poc_solkeys_to_remove(SolutionKey) AS (VALUES('IPC')),
-     pocs_to_remove(POCName) AS (VALUES('Sue Nonemaker'), ('Philip Tennant'))
+WITH poc_solkeys_to_remove(SolutionKey) AS (
+  VALUES('IPC')
+),
+     pocs_to_remove(POCName) AS (
+       VALUES('Sue Nonemaker'),
+             ('Philip Tennant')
+     )
 DELETE FROM possible_operational_solution_contact
 WHERE possible_operational_solution_id IN (
   SELECT id
   FROM possible_operational_solution
-  WHERE CAST(sol_key as TEXT) IN (SELECT SolutionKey FROM poc_solkeys_to_remove)
+  WHERE CAST(sol_key as TEXT) IN (
+    SELECT SolutionKey
+    FROM poc_solkeys_to_remove
+  )
 )
-  AND name = ANY(SELECT POCName FROM pocs_to_remove);
+  AND name = ANY(
+  SELECT POCName
+  FROM pocs_to_remove
+);
 
 /* Set up new POCs */
-WITH pocs(SolutionName, SolutionKey, Name, Email, Role, IsTeam) AS (VALUES
-                                                                      ('Innovation Payment Contractor (IPC)', 'IPC', 'Alistair Quatre', 'alistair.quatre@cms.hhs.gov', 'Subject Matter Expert', FALSE),
-                                                                      ('Innovation Payment Contractor (IPC)', 'IPC', 'Brandon Cooper', 'brandon.cooper@cms.hhs.gov', 'Subject Matter Expert', FALSE)
+WITH pocs(
+          SolutionName,
+          SolutionKey,
+          Name,
+          Email,
+          Role,
+          IsTeam
+  ) AS (
+  VALUES (
+           'Innovation Payment Contractor (IPC)',
+           'IPC',
+           'Alistair Quatre',
+           'alistair.quatre@cms.hhs.gov',
+           'Subject Matter Expert',
+           FALSE
+         ),
+         (
+           'Innovation Payment Contractor (IPC)',
+           'IPC',
+           'Brandon Cooper',
+           'brandon.cooper@cms.hhs.gov',
+           'Subject Matter Expert',
+           FALSE
+         )
 )
-
 INSERT INTO possible_operational_solution_contact(
   id,
   possible_operational_solution_id,
@@ -25,16 +56,13 @@ INSERT INTO possible_operational_solution_contact(
   created_by,
   created_dts
 )
-
-SELECT
-  gen_random_uuid() AS id,
-  pos.id as possible_operational_solution_id,
-  pocs.name AS name,
-  pocs.email AS email,
-  pocs.role AS role,
-  pocs.IsTeam as is_team,
-  '00000001-0001-0001-0001-000000000001' AS created_by, --System account
-  current_timestamp AS created_dts
-
+SELECT gen_random_uuid() AS id,
+       pos.id as possible_operational_solution_id,
+       pocs.name AS name,
+       pocs.email AS email,
+       pocs.role AS role,
+       pocs.IsTeam as is_team,
+       '00000001-0001-0001-0001-000000000001' AS created_by, --System account
+       current_timestamp AS created_dts
 FROM pocs
        JOIN possible_operational_solution pos on CAST(pos.sol_key as TEXT) = pocs.solutionkey
