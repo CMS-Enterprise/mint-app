c5329144ff6d2ce3b71dac087377eb18718c82ea
Steven Wade <steven.wade@oddball.io>
2024-02-01 14:27:08 -0800

    feat: rename tables to be called activity, and notifications note that its related to a user

--- migrations/V144__Add_Notification_tables.sql
+++ migrations/V144__Add_Notification_tables.sql
@@ -1,14 +1,14 @@
 -- Create the new type
-CREATE TYPE EVENT_TYPE AS ENUM (
+CREATE TYPE ACTIVITY_TYPE AS ENUM (
   'DAILY_DIGEST_COMPLETE',
   'NEW_PLAN_DISCUSSION',
   'NEW_DISCUSSION_REPLY'
 );
 
-CREATE TABLE publication ( --TODO settle on naming. The word event is reserved
+CREATE TABLE activity ( --TODO settle on naming. The word event is reserved
     id UUID PRIMARY KEY NOT NULL,
     actor UUID NOT NULL REFERENCES user_account(id), --foreign key to user table
-    event_type EVENT_TYPE NOT NULL,
+    activity_type ACTIVITY_TYPE NOT NULL,
 
     --META DATA
     created_by UUID NOT NULL REFERENCES user_account(id), --Who wrote this row, not necessarily the actor, though it could be the same
@@ -20,7 +20,7 @@ CREATE TABLE publication ( --TODO settle on naming. The word event is reserved
 
 --NOTE these tables likely doesn't need to be audited. It is an audit table by itself. If we want, we can audit it though
 
-CREATE TABLE notification ( --TODO settle on naming. The word notification is reserved
+CREATE TABLE user_notification ( --TODO settle on naming. The word notification is reserved
     id UUID PRIMARY KEY NOT NULL,
     publication_id UUID NOT NULL REFERENCES publication(id), --foreign key to user table
     user_id UUID NOT NULL REFERENCES user_account(id),
