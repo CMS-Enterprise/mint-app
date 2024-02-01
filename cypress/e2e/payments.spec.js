aa7d959d70c2128a79a521669c8af07674c389b4
Gary Zhao <gary.j.zhao@gmail.com>
2024-02-01 14:43:53 -0500

    add e2e test

--- cypress/e2e/payments.spec.js
+++ cypress/e2e/payments.spec.js
@@ -274,6 +274,18 @@ describe('The Model Plan Payment Form', () => {
       .type('Payment Frequency Payments Other')
       .should('have.value', 'Payment Frequency Payments Other');
 
+    cy.get('#payment-demand-recoupment-frequency-quarterly')
+      .check({ force: true })
+      .should('be.checked');
+
+    cy.get('#payment-demand-recoupment-frequency-other')
+      .check({ force: true })
+      .should('be.checked');
+
+    cy.get('#payment-demand-recoupment-frequency-other-text')
+      .type('Payment Demand Recoupment Frequency Other')
+      .should('have.value', 'Payment Demand Recoupment Frequency Other');
+
     cy.get('#payment-payment-start-date')
       .type('10/26/2028')
       .should('have.value', '10/26/2028');
