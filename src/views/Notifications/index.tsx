f2c4755fa1dbd241f0283107188ae37d2a41a815
Gary Zhao <gary.j.zhao@gmail.com>
2024-02-29 13:55:02 -0500

    clean up rebase confusion

--- src/views/Notifications/index.tsx
+++ src/views/Notifications/index.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-// import NotificationsHome from './Home';
 import NotificationsHome from './Home';
 import NotificationSettings from './Settings';
 
 const Notifications = () => {
   return (
     <Switch>
-      {/* <Route path="/notifications" component={NotificationsHome} exact /> */}
+      <Route path="/notifications" component={NotificationsHome} exact />
 
       <Route
         path="/notifications/settings"
         component={NotificationSettings}
         exact
       />
-      <Route path="/notifications" component={NotificationsHome} />
     </Switch>
   );
 };
