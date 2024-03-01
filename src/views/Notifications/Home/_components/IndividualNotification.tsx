88fc53f3d38e8a13235434c6b82d23d8a2ddf765
Gary Zhao <gary.j.zhao@gmail.com>
2024-03-01 14:45:08 -0500

    add date

--- src/views/Notifications/Home/_components/IndividualNotification.tsx
+++ src/views/Notifications/Home/_components/IndividualNotification.tsx
@@ -10,20 +10,24 @@ import {
 
 import IconInitial from 'components/shared/IconInitial';
 import MentionTextArea from 'components/shared/MentionTextArea';
+import { getTimeElapsed } from 'utils/date';
 
 type IndividualNotificationProps = {
   isRead: boolean;
+  createdDts: string;
   activity: NotificationActivityType;
 };
 
 const IndividualNotification = ({
   isRead,
+  createdDts,
   activity: {
     metaData,
     actorUserAccount: { commonName }
   }
 }: IndividualNotificationProps) => {
   const { t: notificationsT } = useTranslation('notifications');
+  const { t: discussionT } = useTranslation('discussions');
 
   // Type guard to check union type
   const isTaggedInDiscussion = (
@@ -39,29 +43,34 @@ const IndividualNotification = ({
   return (
     <Grid row>
       <Grid desktop={{ col: 12 }} className="position-relative">
+        {/* Notification Red Dot */}
         {!isRead && (
           <div className="circle-1 bg-error position-absolute margin-top-3 margin-left-1" />
         )}
 
-        <div className="padding-3">
+        <div className="padding-3 display-flex flex-justify">
           {isTaggedInDiscussion(metaData) && (
-            <>
+            <div className="flex-fill">
               <div className="display-flex flex-align-center margin-bottom-05">
                 <IconInitial
                   className="margin-right-05"
                   user={commonName}
                   hasBoldUsername
                 />
+
                 {notificationsT('index.activityType.taggedInDiscussion.text', {
                   modelName: metaData.modelPlan.modelName
                 })}
               </div>
               <div className="margin-left-5">
-                <MentionTextArea
-                  id={`mention-${metaData.discussionID}`}
-                  editable={false}
-                  initialContent={metaData.content}
-                />
+                <span>
+                  <MentionTextArea
+                    className="text-base-darker"
+                    id={`mention-${metaData.discussionID}`}
+                    editable={false}
+                    initialContent={`“${metaData.content}”`}
+                  />
+                </span>
                 <Button
                   type="button"
                   unstyled
@@ -72,8 +81,13 @@ const IndividualNotification = ({
                   <Icon.ArrowForward className="margin-left-1" aria-hidden />
                 </Button>
               </div>
-            </>
+            </div>
           )}
+          <span className="flex-auto text-base-darker text-right">
+            {getTimeElapsed(createdDts)
+              ? getTimeElapsed(createdDts) + discussionT('ago')
+              : discussionT('justNow')}
+          </span>
         </div>
       </Grid>
     </Grid>
