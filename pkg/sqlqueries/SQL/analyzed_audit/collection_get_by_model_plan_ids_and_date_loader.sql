a580b092c759f7c5a50105327651bbf3696c00c6
Steven Wade <steven.wade@oddball.io>
2024-03-05 07:19:59 -0800

    feat: update dataloader script to get unique result set by date and model plan ids

--- pkg/sqlqueries/SQL/analyzed_audit/collection_get_by_model_plan_ids_and_date_loader.sql
+++ pkg/sqlqueries/SQL/analyzed_audit/collection_get_by_model_plan_ids_and_date_loader.sql
@@ -1,8 +1,8 @@
 WITH QUERIED_IDS AS (
     /*Translate the input to a table */
-    SELECT 
+    SELECT DISTINCT -- The JSON could potentially have duplicate date and model plan combinations. This ensures that they are only fetched once per analyzed audit
         date,
-        model_plan_ids
+        UNNEST(model_plan_ids) AS model_plan_id -- Each row is a combination of date and a model_plan_id
     FROM
         JSON_TO_RECORDSET(:paramTableJSON)
         AS x("date" date, "model_plan_ids" UUID[]) --noqa
@@ -19,5 +19,5 @@ SELECT
     AA.modified_by,
     AA.modified_dts
 FROM analyzed_audit AS AA
-INNER JOIN QUERIED_IDS AS qIDs  ON AA.date = qIDs.date AND AA.model_plan_id = ANY(qIDs.model_plan_ids)
+INNER JOIN QUERIED_IDS AS qIDs  ON AA.date = qIDs.date AND AA.model_plan_id = qIDs.model_plan_id
 ORDER BY AA.model_name ASC;
