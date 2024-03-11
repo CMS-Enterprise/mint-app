4456694187a5ed4a89dc70217a5c2e66c5dd45e5
Steven Wade <steven.wade@oddball.io>
2024-03-11 11:45:59 -0700

    feat: ActivityAddedAsCollaboratorCreate and unit test

--- pkg/notifications/activity.go
+++ pkg/notifications/activity.go
@@ -93,6 +93,14 @@ func parseRawActivityMetaData(activityType models.ActivityType, rawMetaDataJSON 
 			return nil, err
 		}
 		return &meta, nil
+	case models.ActivityAddedAsCollaborator:
+		// Deserialize the raw JSON into AddedAsCollaboratorMeta
+		meta := models.AddedAsCollaboratorMeta{}
+		if err := json.Unmarshal(rawData, &meta); err != nil {
+
+			return nil, err
+		}
+		return &meta, nil
 
 	// Add cases for other ActivityTypes as needed
 
---
+++ pkg/notifications/added_as_collaborator_meta.go.go
@@ -0,0 +1,43 @@
+package notifications
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/uuid"
+
+	"github.com/cmsgov/mint-app/pkg/models"
+	"github.com/cmsgov/mint-app/pkg/sqlutils"
+)
+
+// ActivityTAddedAsCollaboratorMetaCreate creates an activity for when a User is added as a collaborator for a model plan.
+// It also creates all the relevant notifications
+func ActivityAddedAsCollaboratorCreate(
+	ctx context.Context,
+	np sqlutils.NamedPreparer,
+	actorID uuid.UUID,
+	modelPlanID uuid.UUID,
+	collaboratorID uuid.UUID,
+	collaboratorAccountID uuid.UUID,
+	getPreferencesFunc GetUserNotificationPreferencesFunc) (*models.Activity, error) {
+
+	activity := models.NewAddedAsCollaboratorActivity(actorID, modelPlanID, collaboratorID)
+
+	retActivity, actErr := activityCreate(ctx, np, activity)
+	if actErr != nil {
+		return nil, actErr
+	}
+
+	pref, err := getPreferencesFunc(ctx, collaboratorAccountID)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get user notification preference, Notification not created %w", err)
+	}
+
+	_, err = userNotificationCreate(ctx, np, retActivity, collaboratorAccountID, pref.AddedAsCollaborator)
+	if err != nil {
+		return nil, err
+	}
+
+	return retActivity, nil
+
+}
