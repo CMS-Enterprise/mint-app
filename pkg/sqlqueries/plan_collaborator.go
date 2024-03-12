273e9211f1c10729753a01819accc64927b5e4fa
Steven Wade <steven.wade@oddball.io>
2024-03-12 13:31:41 -0700

    feat: add comments to the embedded SQL files for plan collaborator

--- pkg/sqlqueries/plan_collaborator.go
+++ pkg/sqlqueries/plan_collaborator.go
@@ -2,21 +2,33 @@ package sqlqueries
 
 import _ "embed"
 
+// planCollaboratorCreateSQL creates a Plan Collaborator entry in the database
+//
 //go:embed SQL/plan_collaborator/create.sql
 var planCollaboratorCreateSQL string
 
+// planCollaboratorUpdateSQL updates a Plan Collaborator entry in the database
+//
 //go:embed SQL/plan_collaborator/update.sql
 var planCollaboratorUpdateSQL string
 
+// planCollaboratorDeleteSQL deletes a Plan Collaborator entry in the database
+//
 //go:embed SQL/plan_collaborator/delete.sql
 var planCollaboratorDeleteSQL string
 
+// planCollaboratorGetByIDSQL returns a Plan Collaborator entry in the database. When possible, the data loader version should be preferred
+//
 //go:embed SQL/plan_collaborator/get_by_id.sql
 var planCollaboratorGetByIDSQL string
 
+// planCollaboratorGetByIDLoaderSQL returns a Collection of Plan Collaborator entries from the database. It expects a JSON array of IDs, which represent the id of the entry
+//
 //go:embed SQL/plan_collaborator/get_by_id_loader.sql
 var planCollaboratorGetByIDLoaderSQL string
 
+// planCollaboratorGetByModelPlanIDLoaderSQL  returns a Collection of Plan Collaborator entries from the database. It expects a JSON array of model_plan_ids, the result can be processed to return all collaborators for various model plans
+//
 //go:embed SQL/plan_collaborator/get_by_model_plan_id_LOADER.sql
 var planCollaboratorGetByModelPlanIDLoaderSQL string
 
