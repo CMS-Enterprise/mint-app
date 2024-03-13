ea853e0d89ba9eea9307e20c7c9b4f6e19433655
Gary Zhao <gary.j.zhao@gmail.com>
2024-03-13 14:31:43 -0400

    add utils file

---
+++ src/views/Notifications/Home/_components/_utils.ts
@@ -0,0 +1,40 @@
+import {
+  GetNotifications_currentUser_notifications_notifications_activity_metaData_ActivityMetaBaseStruct as BaseStructActivityType,
+  GetNotifications_currentUser_notifications_notifications_activity_metaData_DailyDigestCompleteActivityMeta as DailyDigestCompleteActivityType,
+  GetNotifications_currentUser_notifications_notifications_activity_metaData_TaggedInDiscussionReplyActivityMeta as TaggedInDiscussionReplyActivityType,
+  GetNotifications_currentUser_notifications_notifications_activity_metaData_TaggedInPlanDiscussionActivityMeta as TaggedInDiscussionActivityType
+} from 'gql/gen/types/GetNotifications';
+
+// Type guard to check union type
+export const isTaggedInDiscussion = (
+  data:
+    | TaggedInDiscussionReplyActivityType
+    | TaggedInDiscussionActivityType
+    | DailyDigestCompleteActivityType
+    | BaseStructActivityType
+): data is TaggedInDiscussionActivityType => {
+  /* eslint no-underscore-dangle: 0 */
+  return data.__typename === 'TaggedInPlanDiscussionActivityMeta';
+};
+
+export const isTaggedInDiscussionReply = (
+  data:
+    | TaggedInDiscussionReplyActivityType
+    | TaggedInDiscussionActivityType
+    | DailyDigestCompleteActivityType
+    | BaseStructActivityType
+): data is TaggedInDiscussionReplyActivityType => {
+  /* eslint no-underscore-dangle: 0 */
+  return data.__typename === 'TaggedInDiscussionReplyActivityMeta';
+};
+
+export const isDailyDigest = (
+  data:
+    | TaggedInDiscussionReplyActivityType
+    | TaggedInDiscussionActivityType
+    | DailyDigestCompleteActivityType
+    | BaseStructActivityType
+): data is DailyDigestCompleteActivityType => {
+  /* eslint no-underscore-dangle: 0 */
+  return data.__typename === 'DailyDigestCompleteActivityMeta';
+};
