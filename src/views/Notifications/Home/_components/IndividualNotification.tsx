5bf0625d9b6b429a610ac9f950359711bdb4d34d
Gary Zhao <gary.j.zhao@gmail.com>
2024-03-20 11:29:54 -0400

    default state of isExpanded to be false

--- src/views/Notifications/Home/_components/IndividualNotification.tsx
+++ src/views/Notifications/Home/_components/IndividualNotification.tsx
@@ -40,7 +40,7 @@ const IndividualNotification = ({
 }: IndividualNotificationProps) => {
   const { t: discussionT } = useTranslation('discussions');
 
-  const [isExpanded, setIsExpanded] = useState(true);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   const history = useHistory();
   const isMobile = useCheckResponsiveScreen('mobile');
