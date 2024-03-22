087b5036a96847c6f68f3bf7b87fa764c03307b7
Gary Zhao <garyjzhao@users.noreply.github.com>
2024-03-22 11:39:20 -0400

    [EASI-4025] FE work for the Daily Digest Notification (#1016)
    * drying up the code
    
    * better drying methods
    
    * add utils file
    
    * Add DailyDigestCompleteActivityMeta fragment to GetNotifications gql
    
    * remove bang and add double pipes
    
    * Adding more conditional renders for daily digest
    
    * paired with Steven to remove unused type
    
    * Adding daily digest component
    
    * change Mint System Account to MINT
    
    * fixing the type error
    
    * add handleMarkAsReadAndToggleDailyDigest functionality
    
    * check to see if it is read yet before making a mutation call
    
    * changed to tsx file && moved helper functions to util file
    
    * add namespace
    
    * adding daily digest
    
    * basic scaoffolding of the daily digest component
    
    * passing down the metadata to dailydigest
    
    * working the audit changes
    
    * add more translations
    
    * add plan sections
    
    * add arrow to cta
    
    * add translations for the plan section strings
    
    * adjust some width
    
    * wip
    
    * pass in the correct value
    
    * pass the correct translation
    
    * move things back in the component
    
    * add status change translations
    
    * Add translation status change
    
    * clean up the code more
    
    * removed unused code
    
    * Add more changes translation and keys
    
    * adding logic for the +x more changes
    
    * add boolean to changesArray && showFirstFiveChanges variable
    
    * add a few more logic to keep track of unique edge cases
    
    * moved the function to util folder
    
    * default state of isExpanded to be false
    
    * moved translatePlanSections to utils folder
    
    * add daily digest test
    
    * Add Daily Digest to e2e tests
    
    * quick QA Feeback
    
    * removed TranslateStatusChange function in utils
    
    * removed mroe unused util function
    
    * add comments to describe function better
    
    * have changesArray be instantiated inside the function
    
    * add more description to the function in utils folder
    
    * appeasing the type errors and adding util testing file

--- src/gql/gen/graphql.ts
+++ src/gql/gen/graphql.ts
@@ -54,14 +54,8 @@ export type Activity = {
   modifiedDts?: Maybe<Scalars['Time']['output']>;
 };
 
-export type ActivityMetaBaseStruct = {
-  __typename: 'ActivityMetaBaseStruct';
-  type: ActivityType;
-  version: Scalars['Int']['output'];
-};
-
 /** ActivityMetaData is a type that represents all the data that can be captured in an Activity */
-export type ActivityMetaData = ActivityMetaBaseStruct | DailyDigestCompleteActivityMeta | TaggedInDiscussionReplyActivityMeta | TaggedInPlanDiscussionActivityMeta;
+export type ActivityMetaData = DailyDigestCompleteActivityMeta | TaggedInDiscussionReplyActivityMeta | TaggedInPlanDiscussionActivityMeta;
 
 /** ActivityType represents the possible activities that happen in application that might result in a notification */
 export enum ActivityType {
@@ -3249,7 +3243,7 @@ export type GetNotificationSettingsQuery = { __typename: 'Query', currentUser: {
 export type GetNotificationsQueryVariables = Exact<{ [key: string]: never; }>;
 
 
-export type GetNotificationsQuery = { __typename: 'Query', currentUser: { __typename: 'CurrentUser', notifications: { __typename: 'UserNotifications', numUnreadNotifications: number, notifications: Array<{ __typename: 'UserNotification', id: UUID, isRead: boolean, inAppSent: boolean, emailSent: boolean, createdDts: Time, activity: { __typename: 'Activity', activityType: ActivityType, entityID: UUID, actorID: UUID, actorUserAccount: { __typename: 'UserAccount', commonName: string }, metaData: { __typename: 'ActivityMetaBaseStruct' } | { __typename: 'TaggedInDiscussionReplyActivityMeta', version: number, type: ActivityType, modelPlanID: UUID, discussionID: UUID, replyID: UUID, content: string, modelPlan: { __typename: 'ModelPlan', modelName: string } } | { __typename: 'TaggedInPlanDiscussionActivityMeta', version: number, type: ActivityType, modelPlanID: UUID, discussionID: UUID, content: string, modelPlan: { __typename: 'ModelPlan', modelName: string } } } }> } } };
+export type GetNotificationsQuery = { __typename: 'Query', currentUser: { __typename: 'CurrentUser', notifications: { __typename: 'UserNotifications', numUnreadNotifications: number, notifications: Array<{ __typename: 'UserNotification', id: UUID, isRead: boolean, inAppSent: boolean, emailSent: boolean, createdDts: Time, activity: { __typename: 'Activity', activityType: ActivityType, entityID: UUID, actorID: UUID, actorUserAccount: { __typename: 'UserAccount', commonName: string }, metaData: { __typename: 'DailyDigestCompleteActivityMeta', version: number, type: ActivityType, modelPlanIDs: Array<UUID>, date: Time, analyzedAudits: Array<{ __typename: 'AnalyzedAudit', id: UUID, modelPlanID: UUID, modelName: string, date: Time, changes: { __typename: 'AnalyzedAuditChange', modelPlan?: { __typename: 'AnalyzedModelPlan', oldName?: string | null, statusChanges?: Array<string | null> | null } | null, documents?: { __typename: 'AnalyzedDocuments', count?: number | null } | null, crTdls?: { __typename: 'AnalyzedCrTdls', activity?: boolean | null } | null, planSections?: { __typename: 'AnalyzedPlanSections', updated: Array<string>, readyForReview: Array<string>, readyForClearance: Array<string> } | null, modelLeads?: { __typename: 'AnalyzedModelLeads', added: Array<{ __typename: 'AnalyzedModelLeadInfo', id: UUID, commonName: string }> } | null, planDiscussions?: { __typename: 'AnalyzedPlanDiscussions', activity?: boolean | null } | null } }> } | { __typename: 'TaggedInDiscussionReplyActivityMeta', version: number, type: ActivityType, modelPlanID: UUID, discussionID: UUID, replyID: UUID, content: string, modelPlan: { __typename: 'ModelPlan', modelName: string } } | { __typename: 'TaggedInPlanDiscussionActivityMeta', version: number, type: ActivityType, modelPlanID: UUID, discussionID: UUID, content: string, modelPlan: { __typename: 'ModelPlan', modelName: string } } } }> } } };
 
 export type GetPollNotificationsQueryVariables = Exact<{ [key: string]: never; }>;
 
@@ -5722,6 +5716,44 @@ export const GetNotificationsDocument = gql`
               discussionID
               replyID
               content
+            }
+            ... on DailyDigestCompleteActivityMeta {
+              version
+              type
+              modelPlanIDs
+              date
+              analyzedAudits {
+                id
+                modelPlanID
+                modelName
+                date
+                changes {
+                  modelPlan {
+                    oldName
+                    statusChanges
+                  }
+                  documents {
+                    count
+                  }
+                  crTdls {
+                    activity
+                  }
+                  planSections {
+                    updated
+                    readyForReview
+                    readyForClearance
+                  }
+                  modelLeads {
+                    added {
+                      id
+                      commonName
+                    }
+                  }
+                  planDiscussions {
+                    activity
+                  }
+                }
+              }
             }
           }
         }
