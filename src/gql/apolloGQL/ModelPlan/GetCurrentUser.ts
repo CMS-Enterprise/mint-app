f3d7395c0881407fcc6c3ea8743fc76609777f1f
Gary Zhao <gary.j.zhao@gmail.com>
2024-03-22 13:36:36 -0400

    Convert GetCurrentUser

--- src/gql/apolloGQL/ModelPlan/GetCurrentUser.ts
+++ src/gql/apolloGQL/ModelPlan/GetCurrentUser.ts
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-export default gql`
+export default gql(/* GraphQL */ `
   query GetCurrentUser {
     currentUser {
       launchDarkly {
@@ -9,4 +9,4 @@ export default gql`
       }
     }
   }
-`;
+`);
