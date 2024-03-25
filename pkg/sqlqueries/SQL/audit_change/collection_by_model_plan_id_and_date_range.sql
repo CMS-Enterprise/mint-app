5f7dbcccb076dff826ab81f7a9856c4d310bc796
Steven Wade <steven.wade@oddball.io>
2024-03-25 13:09:37 -0700

    feat: add model_plan_changes to the query

--- pkg/sqlqueries/SQL/audit_change/collection_by_model_plan_id_and_date_range.sql
+++ pkg/sqlqueries/SQL/audit_change/collection_by_model_plan_id_and_date_range.sql
@@ -10,7 +10,6 @@ WITH PLAN AS (
 DISCUSSION AS ( 
     SELECT 
         PLAN.id AS model_plan_id,
-
         discussion.id AS id,
         'discussion_id' AS fk_field
     FROM PLAN
@@ -21,7 +20,6 @@ DISCUSSION AS (
 NEED AS ( 
     SELECT 
         PLAN.id AS model_plan_id,
-
         OpNd.id AS id,
         'operational_need_id' AS fk_field
     FROM PLAN
@@ -77,7 +75,22 @@ SELECT
 FROM audit.change
 INNER JOIN audit.table_config ON audit.table_config.id = audit.change.table_id
 INNER JOIN FIELDS ON (FIELDS.fk_field = audit.table_config.fkey_field AND FIELDS.id = audit.change.foreign_key)
--- Ticket (ChChCh Changes!) This should also get the model changes, this just gets the child fields currently because of the inner join.)
 WHERE
-    (audit.change.foreign_key = :foreign_key OR audit.change.primary_key = :primary_key)
+    audit.change.modified_dts >= :time_start AND audit.change.modified_dts < :time_end
+UNION
+SELECT --This selects changes for the model plan
+    audit.change.id,
+    audit.change.table_id,
+    audit.change.primary_key,
+    audit.change.foreign_key,
+    audit.table_config.name AS table_name,
+    audit.change.action,
+    audit.change.fields,
+    audit.change.modified_by,
+    audit.change.modified_dts
+FROM audit.change
+INNER JOIN audit.table_config ON audit.table_config.id = audit.change.table_id
+WHERE
+    (audit.change.primary_key = (SELECT id FROM PLAN) AND audit.table_config.name = 'model_plan') 
+    -- TODO (ChChCh Changes!) Perhaps using the provided variable instead of selecting from a CTE?
     AND audit.change.modified_dts >= :time_start AND audit.change.modified_dts < :time_end
