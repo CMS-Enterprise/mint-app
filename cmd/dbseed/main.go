173f2d754d3a05b2f6954fb8febdde43dd6e6a2c
Steven Wade <steven.wade@oddball.io>
2024-03-26 08:16:59 -0700

    feat: split analyze audit to its own command in dbseed

--- cmd/dbseed/main.go
+++ cmd/dbseed/main.go
@@ -1,12 +1,12 @@
+// Package main is a utility command to provide dev data for the mint application
 package main
 
 import (
-	"context"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/lib/pq"
 
 	"github.com/guregu/null/zero"
 
@@ -27,24 +27,36 @@ import (
 	ld "github.com/launchdarkly/go-server-sdk/v6"
 )
 
+var viperConfig = viper.New()
+
+var rootCmd = &cobra.Command{
+	Use:   "dbseed",
+	Short: "Seed the DB",
+	Long:  "Seeds the Database with Model Plans and associated data",
+	Run: func(cmd *cobra.Command, args []string) {
+		seed(viperConfig)
+	},
+}
+
+func init() {
+	viperConfig.AutomaticEnv()
+	rootCmd.AddCommand(analyzeAuditCommand)
+
+}
+
+func execute() {
+	// get Variables once instead of in multiple places
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
 // The main entrypoint for the dbseed command.
 // Invoke with "go run cmd/dbseed"
 func main() {
-	var rootCmd = &cobra.Command{
-		Use:   "dbseed",
-		Short: "Seed the DB",
-		Long:  "Seeds the Database with Model Plans and associated data",
-		Run: func(cmd *cobra.Command, args []string) {
-			config := viper.New()
-			config.AutomaticEnv()
-			seed(config)
-		},
-	}
+	execute()
 
-	err := rootCmd.Execute()
-	if err != nil {
-		panic(err)
-	}
 }
 
 // getResolverDependencies takes a Viper config and returns a Store and Logger object to be used
@@ -256,53 +268,4 @@ func (s *Seeder) SeedData() {
 			},
 		},
 	)
-}
-
-// CreateAnalyzedAuditData uses the seeder to generate analyzed audits. It will make one record for all changes just seeded
-func (s *Seeder) CreateAnalyzedAuditData() {
-	dayToAnalyze := time.Now()
-
-	// Step 1. Get all model plans
-	modelPlans, err := s.Config.Store.ModelPlanCollection(s.Config.Logger, false)
-	if err != nil {
-		panic(fmt.Errorf("couldn't retrieve model plan collection"))
-	}
-
-	// Step 2. Iterate through all model plans, and generate analyzed audit data
-	for _, mp := range modelPlans {
-		_, err2 := resolvers.AnalyzeModelPlanForAnalyzedAudit(s.Config.Context, s.Config.Store, s.Config.Logger, dayToAnalyze, mp.ID)
-		// Notice:  that this will create an error if you run this a second time, because there is already an analyzed audit record.
-		// For simplicity, we check if it is that error, and if so just continue.
-		if err2 != nil {
-			if pqErr, ok := err2.(*pq.Error); ok {
-
-				if pqErr.Code.Name() == "unique_violation" {
-					continue
-				}
-				fmt.Printf("pq error: Severity: %s, Code: %s, Message: %s\n", pqErr.Severity, pqErr.Code, pqErr.Message)
-
-			} else {
-				fmt.Printf("there was an issue analyzing model plan: %s, ID: %s. Err: %v", mp.ModelName, mp.ID, err2)
-			}
-
-		}
-	}
-
-	// Step 4. Get all Users who have a favorited model (See worker.DigestEmailBatchJob in pkg/worker/digest_email_job.go)
-
-	// Try to send the emails for the daily digest (which also generates a notification)
-	userIDs, err := s.Config.Store.PlanFavoriteCollectionGetUniqueUserIDs()
-	if err != nil {
-		panic(fmt.Errorf("couldn't get user ids for users with plan favorites, %w", err))
-	}
-	preferenceFunctions := func(ctx context.Context, user_id uuid.UUID) (*models.UserNotificationPreferences, error) {
-		return storage.UserNotificationPreferencesGetByUserID(s.Config.Store, user_id)
-	}
-	for _, id := range userIDs {
-		err := resolvers.DailyDigestNotificationSend(s.Config.Context, s.Config.Store, s.Config.Logger, dayToAnalyze, id, preferenceFunctions, s.Config.EmailService, s.Config.EmailTemplateService, s.Config.AddressBook)
-		if err != nil {
-			fmt.Printf("there was an issue sending digest emails for userID: %s", id)
-		}
-	}
-
 }
