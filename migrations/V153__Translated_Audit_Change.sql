c757dab914f114f3cb20d7ae04c1229fe80a731d
Steven Wade <steven.wade@oddball.io>
2024-04-04 07:56:52 -0700

    feat: humanized to translated

--- migrations/V153__Translated_Audit_Change.sql
+++ migrations/V153__Translated_Audit_Change.sql
@@ -5,7 +5,7 @@ CREATE TYPE DATABASE_OPERATION AS ENUM (
 COMMENT ON TYPE DATABASE_OPERATION IS 'The possible types of operations that can cause an audit entry.
 Currently they are represented in the audit.change table as the first letter of the action EG I, D, U, T.';
 
-CREATE TABLE humanized_audit_change (
+CREATE TABLE translated_audit_change (
     id UUID PRIMARY KEY,
     model_plan_id UUID NOT NULL REFERENCES model_plan(id),
     actor_id UUID NOT NULL REFERENCES user_account(id), --foreign key to user table
@@ -35,21 +35,21 @@ CREATE TABLE humanized_audit_change (
 --Ticket (ChChCh Changes!)  Consider removing time start and time end and just use date
 
 -- Comment for the table
-COMMENT ON TABLE humanized_audit_change IS 'Table storing human-readable audit trail of changes made to model plans.';
+COMMENT ON TABLE translated_audit_change IS 'Table storing human-readable audit trail of changes made to model plans.';
 
 -- Comments for each column
-COMMENT ON COLUMN humanized_audit_change.id IS 'Unique identifier for the audit trail change record.';
-COMMENT ON COLUMN humanized_audit_change.model_plan_id IS 'Identifier referencing the model plan associated with this audit trail change.';
-COMMENT ON COLUMN humanized_audit_change.actor_id IS 'Identifier of the user who performed the changes (actor).';
-COMMENT ON COLUMN humanized_audit_change.change_id IS 'Foreign key to the untranslated change source of this translation';
-COMMENT ON COLUMN humanized_audit_change.date IS 'Timestamp indicating the exact time of the change.';
+COMMENT ON COLUMN translated_audit_change.id IS 'Unique identifier for the audit trail change record.';
+COMMENT ON COLUMN translated_audit_change.model_plan_id IS 'Identifier referencing the model plan associated with this audit trail change.';
+COMMENT ON COLUMN translated_audit_change.actor_id IS 'Identifier of the user who performed the changes (actor).';
+COMMENT ON COLUMN translated_audit_change.change_id IS 'Foreign key to the untranslated change source of this translation';
+COMMENT ON COLUMN translated_audit_change.date IS 'Timestamp indicating the exact time of the change.';
 
-COMMENT ON COLUMN humanized_audit_change.meta_data IS 'JSONB data storing the details of the changes made.';
-COMMENT ON COLUMN humanized_audit_change.model_name IS 'String representing the model name associated with the change.';
-COMMENT ON COLUMN humanized_audit_change.created_by IS 'Unique identifier of the user who created the audit trail change.';
-COMMENT ON COLUMN humanized_audit_change.created_dts IS 'Timestamp with time zone indicating the creation time of the audit trail change.';
-COMMENT ON COLUMN humanized_audit_change.modified_by IS 'Unique identifier of the user who last modified the audit trail change.';
-COMMENT ON COLUMN humanized_audit_change.modified_dts IS 'Timestamp with time zone indicating the last modification time of the audit trail change.';
+COMMENT ON COLUMN translated_audit_change.meta_data IS 'JSONB data storing the details of the changes made.';
+COMMENT ON COLUMN translated_audit_change.model_name IS 'String representing the model name associated with the change.';
+COMMENT ON COLUMN translated_audit_change.created_by IS 'Unique identifier of the user who created the audit trail change.';
+COMMENT ON COLUMN translated_audit_change.created_dts IS 'Timestamp with time zone indicating the creation time of the audit trail change.';
+COMMENT ON COLUMN translated_audit_change.modified_by IS 'Unique identifier of the user who last modified the audit trail change.';
+COMMENT ON COLUMN translated_audit_change.modified_dts IS 'Timestamp with time zone indicating the last modification time of the audit trail change.';
 
 -- Ticket: (ChChCh Changes!) ADD MORE  comments for the new fields
 
