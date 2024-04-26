72ac59419b54338b9ce34f3254e7495fc618ad17
Tom Brooks <tom.brooks@oddball.io>
2024-04-26 00:42:55 -0400

    Update frontend NDA messages

--- src/i18n/en-US/nda.ts
+++ src/i18n/en-US/nda.ts
@@ -7,6 +7,7 @@ const nda = {
   accepted: 'Accepted on ',
   header:
     'Pre-Decisional: Information not releasable to the public unless authorized by law.',
+  caveat: 'INFORMATION NOT RELEASABLE TO THE PUBLIC UNLESS AUTHORIZED BY LAW:',
   body:
     'This information has not been publicly disclosed and may be privileged and confidential. It is for internal government use only and must not be disseminated, distributed, or copied to persons not authorized to receive the information. Unauthorized disclosure may result in prosecution to the full extent of the law.'
 };
