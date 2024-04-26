e2dcd691125cbc10d40a8ffceb589597ceebeb87
Steven Wade <steven.wade@oddball.io>
2024-04-26 12:52:39 -0700

    feat: add table_reference column

--- pkg/graph/resolvers/translation.resolvers.go
+++ pkg/graph/resolvers/translation.resolvers.go
@@ -6,26 +6,10 @@ package resolvers
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/cmsgov/mint-app/pkg/graph/generated"
 	"github.com/cmsgov/mint-app/pkg/models"
 )
-
-// TableReference is the resolver for the tableReference field.
-func (r *translationFieldResolver) TableReference(ctx context.Context, obj *models.TranslationField) (*string, error) {
-	panic(fmt.Errorf("not implemented: TableReference - tableReference"))
-}
-
-// TableReference is the resolver for the tableReference field.
-func (r *translationFieldWithOptionsResolver) TableReference(ctx context.Context, obj *models.TranslationFieldWithOptions) (*string, error) {
-	panic(fmt.Errorf("not implemented: TableReference - tableReference"))
-}
-
-// TableReference is the resolver for the tableReference field.
-func (r *translationFieldWithOptionsAndChildrenResolver) TableReference(ctx context.Context, obj *models.TranslationFieldWithOptionsAndChildren) (*string, error) {
-	panic(fmt.Errorf("not implemented: TableReference - tableReference"))
-}
 
 // ChildRelation is the resolver for the childRelation field.
 func (r *translationFieldWithOptionsAndChildrenResolver) ChildRelation(ctx context.Context, obj *models.TranslationFieldWithOptionsAndChildren) (map[string]interface{}, error) {
@@ -35,21 +19,6 @@ func (r *translationFieldWithOptionsAndChildrenResolver) ChildRelation(ctx conte
 		resultMap[key] = value
 	}
 	return resultMap, nil
-}
-
-// TableReference is the resolver for the tableReference field.
-func (r *translationFieldWithOptionsAndParentResolver) TableReference(ctx context.Context, obj *models.TranslationFieldWithOptionsAndParent) (*string, error) {
-	panic(fmt.Errorf("not implemented: TableReference - tableReference"))
-}
-
-// TableReference is the resolver for the tableReference field.
-func (r *translationFieldWithParentResolver) TableReference(ctx context.Context, obj *models.TranslationFieldWithParent) (*string, error) {
-	panic(fmt.Errorf("not implemented: TableReference - tableReference"))
-}
-
-// TableReference is the resolver for the tableReference field.
-func (r *translationFieldWithParentAndChildrenResolver) TableReference(ctx context.Context, obj *models.TranslationFieldWithParentAndChildren) (*string, error) {
-	panic(fmt.Errorf("not implemented: TableReference - tableReference"))
 }
 
 // ChildRelation is the resolver for the childRelation field.
@@ -62,29 +31,9 @@ func (r *translationFieldWithParentAndChildrenResolver) ChildRelation(ctx contex
 	return resultMap, nil
 }
 
-// TranslationField returns generated.TranslationFieldResolver implementation.
-func (r *Resolver) TranslationField() generated.TranslationFieldResolver {
-	return &translationFieldResolver{r}
-}
-
-// TranslationFieldWithOptions returns generated.TranslationFieldWithOptionsResolver implementation.
-func (r *Resolver) TranslationFieldWithOptions() generated.TranslationFieldWithOptionsResolver {
-	return &translationFieldWithOptionsResolver{r}
-}
-
 // TranslationFieldWithOptionsAndChildren returns generated.TranslationFieldWithOptionsAndChildrenResolver implementation.
 func (r *Resolver) TranslationFieldWithOptionsAndChildren() generated.TranslationFieldWithOptionsAndChildrenResolver {
 	return &translationFieldWithOptionsAndChildrenResolver{r}
-}
-
-// TranslationFieldWithOptionsAndParent returns generated.TranslationFieldWithOptionsAndParentResolver implementation.
-func (r *Resolver) TranslationFieldWithOptionsAndParent() generated.TranslationFieldWithOptionsAndParentResolver {
-	return &translationFieldWithOptionsAndParentResolver{r}
-}
-
-// TranslationFieldWithParent returns generated.TranslationFieldWithParentResolver implementation.
-func (r *Resolver) TranslationFieldWithParent() generated.TranslationFieldWithParentResolver {
-	return &translationFieldWithParentResolver{r}
 }
 
 // TranslationFieldWithParentAndChildren returns generated.TranslationFieldWithParentAndChildrenResolver implementation.
@@ -92,16 +41,5 @@ func (r *Resolver) TranslationFieldWithParentAndChildren() generated.Translation
 	return &translationFieldWithParentAndChildrenResolver{r}
 }
 
-type translationFieldResolver struct{ *Resolver }
-type translationFieldWithOptionsResolver struct{ *Resolver }
 type translationFieldWithOptionsAndChildrenResolver struct{ *Resolver }
-type translationFieldWithOptionsAndParentResolver struct{ *Resolver }
-type translationFieldWithParentResolver struct{ *Resolver }
 type translationFieldWithParentAndChildrenResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//   - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//     it when you're done.
-//   - You have helper methods in this file. Move them out to keep these resolver files clean.
