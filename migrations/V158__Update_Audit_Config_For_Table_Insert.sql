8d8cfe421e9f928372ae588bc8102cd5536ab1f4
Steven Wade <steven.wade@oddball.io>
2024-05-22 10:49:55 -0700

    feat: allow explicitly inserting key fields in an audit

--- migrations/V158__Update_Audit_Config_For_Table_Insert.sql
+++ migrations/V158__Update_Audit_Config_For_Table_Insert.sql
@@ -19,5 +19,16 @@ WHERE name IN (
     'plan_collaborator'
 );
 
+UPDATE audit.table_config
+SET 
+    insert_fields = '{*,solution_id}', -- insert solution_id even though it is a foreign key
+    modified_by = '00000001-0001-0001-0001-000000000001', -- system account
+    modified_dts = CURRENT_TIMESTAMP
+
+
+WHERE name IN (
+    'plan_document_solution_link'
+);
+
 /* remove it_tools table config as the table no longer exists*/
 DELETE FROM audit.table_config WHERE name = 'plan_it_tools';
