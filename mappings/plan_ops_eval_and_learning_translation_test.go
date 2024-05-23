baae142e49f3a49cb26e24371ec4e8e4def46c52
Steven Wade <steven.wade@oddball.io>
2024-05-23 13:21:19 -0700

    feat: translation field coverage for PlanOpsEvalAndLearning

--- mappings/plan_ops_eval_and_learning_translation_test.go
+++ mappings/plan_ops_eval_and_learning_translation_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/cmsgov/mint-app/pkg/models"
 )
 
 func TestPlanOpsEvalAndLearningTranslation(t *testing.T) {
@@ -29,4 +31,17 @@ func TestPlanOpsEvalAndLearningTranslationVerifyFieldsArePopulated(t *testing.T)
 
 	assertTranslationFields(t, translation)
 
+}
+
+func TestPlanOpsEvalAndLearningTranslationCoverage(t *testing.T) {
+	translation, err := PlanOpsEvalAndLearningTranslation()
+
+	assert.NoError(t, err)
+	assert.NotNil(t, translation)
+
+	tMap, err := translation.ToMap()
+	assert.NoError(t, err)
+	assert.NotNil(t, tMap)
+
+	assertTranslationStructCoverage(t, tMap, models.PlanOpsEvalAndLearning{}, taskListStructExcludeFields)
 }
