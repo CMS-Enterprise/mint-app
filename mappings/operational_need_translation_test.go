08eb7a504c24fce267162c0a86b9105c27c6935e
Steven Wade <steven.wade@oddball.io>
2024-05-23 13:11:23 -0700

    feat: translation field coverage for operational need

--- mappings/operational_need_translation_test.go
+++ mappings/operational_need_translation_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/cmsgov/mint-app/pkg/models"
 )
 
 func TestOperationalNeedTranslation(t *testing.T) {
@@ -29,4 +31,17 @@ func TestOperationalNeedTranslationVerifyFieldsArePopulated(t *testing.T) {
 
 	assertTranslationFields(t, translation)
 
+}
+
+func TestOperationalNeedTranslationCoverage(t *testing.T) {
+	translation, err := OperationalNeedTranslation()
+
+	assert.NoError(t, err)
+	assert.NotNil(t, translation)
+
+	tMap, err := translation.ToMap()
+	assert.NoError(t, err)
+	assert.NotNil(t, tMap)
+
+	assertTranslationStructCoverage(t, tMap, models.OperationalNeed{}, taskListStructExcludeFields)
 }
