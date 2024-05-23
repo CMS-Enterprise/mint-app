9ecd9140a14d1551e9edc64113c1c3f15dfad357
Steven Wade <steven.wade@oddball.io>
2024-05-23 13:25:22 -0700

    feat: translation field coverage for PlanTDL

--- mappings/plan_tdl_translation_test.go
+++ mappings/plan_tdl_translation_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/cmsgov/mint-app/pkg/models"
 )
 
 func TestPlanTDLTranslation(t *testing.T) {
@@ -29,4 +31,17 @@ func TestPlanTDLTranslationVerifyFieldsArePopulated(t *testing.T) {
 
 	assertTranslationFields(t, translation)
 
+}
+
+func TestPlanTDLTranslationCoverage(t *testing.T) {
+	translation, err := PlanTDLTranslation()
+
+	assert.NoError(t, err)
+	assert.NotNil(t, translation)
+
+	tMap, err := translation.ToMap()
+	assert.NoError(t, err)
+	assert.NotNil(t, tMap)
+
+	assertTranslationStructCoverage(t, tMap, models.PlanTDL{}, taskListStructExcludeFields)
 }
