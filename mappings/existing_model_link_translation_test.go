769c47123002bf8c3d9338f6c6ed462c9acabcb2
Steven Wade <steven.wade@oddball.io>
2024-05-23 13:03:25 -0700

    feat: existing model link translation field coverage

--- mappings/existing_model_link_translation_test.go
+++ mappings/existing_model_link_translation_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/cmsgov/mint-app/pkg/models"
 )
 
 func TestExistingModelLinkTranslation(t *testing.T) {
@@ -29,4 +31,17 @@ func TestExistingModelLinkTranslationVerifyFieldsArePopulated(t *testing.T) {
 
 	assertTranslationFields(t, translation)
 
+}
+
+func TestExistingModelLinkTranslationCoverage(t *testing.T) {
+	translation, err := ExistingModelLinkTranslation()
+
+	assert.NoError(t, err)
+	assert.NotNil(t, translation)
+
+	tMap, err := translation.ToMap()
+	assert.NoError(t, err)
+	assert.NotNil(t, tMap)
+
+	assertTranslationStructCoverage(t, tMap, models.ExistingModelLink{}, taskListStructExcludeFields)
 }
