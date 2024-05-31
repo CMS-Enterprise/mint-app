eea1853fb37a603b94b812442a7395f070e7e13e
ClayBenson94 <clay.benson@oddball.io>
2024-05-31 09:37:16 -0400

    chore: rename migration from 157 to 158 to avoid conflicts

--- migrations/V157__Add_New_Model_Plan_Notification.sql
+++
@@ -1,5 +0,0 @@
-ALTER TYPE ACTIVITY_TYPE
-  ADD VALUE 'NEW_MODEL_PLAN' AFTER 'MODEL_PLAN_SHARED';
-
-ALTER TABLE user_notification_preferences
-  ADD COLUMN new_model_plan user_notification_preference_flag[] DEFAULT '{}'::user_notification_preference_flag[];
