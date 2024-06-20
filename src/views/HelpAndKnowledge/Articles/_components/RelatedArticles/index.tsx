11fc07c8e7266edfe11cd6dc2104f07c0cad0948
Gary Zhao <gary.j.zhao@gmail.com>
2024-06-20 16:30:51 -0400

    correct the snaps

--- src/views/HelpAndKnowledge/Articles/_components/RelatedArticles/index.tsx
+++ src/views/HelpAndKnowledge/Articles/_components/RelatedArticles/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
-import { CardGroup, GridContainer } from '@trussworks/react-uswds';
-import classnames from 'classnames';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import { CardGroup, GridContainer } from "@trussworks/react-uswds";
+import classnames from "classnames";
 
 import helpAndKnowledgeArticles, {
   ArticleCategories,
   ArticleProps,
-  HelpArticle
-} from 'views/HelpAndKnowledge/Articles';
-import ArticleCard from 'views/HelpAndKnowledge/Articles/_components/ArticleCard';
+  HelpArticle,
+} from "views/HelpAndKnowledge/Articles";
+import ArticleCard from "views/HelpAndKnowledge/Articles/_components/ArticleCard";
 
 type RelatedArticlesProps = {
   className?: string;
@@ -24,17 +24,13 @@ const RelatedArticles = ({
   currentArticle,
   specificArticles,
   type,
-  viewAllLink
+  viewAllLink,
 }: RelatedArticlesProps) => {
-  const { t } = useTranslation('helpAndKnowledge');
-
-  if (specificArticles?.length !== 3) {
-    return null;
-  }
+  const { t } = useTranslation("helpAndKnowledge");
 
   // Filter to only the category tag type
   let filteredArticles = type
-    ? helpAndKnowledgeArticles.filter(article => article.type === type)
+    ? helpAndKnowledgeArticles.filter((article) => article.type === type)
     : helpAndKnowledgeArticles;
 
   // If the only article is the current article, default to all articles
@@ -44,7 +40,7 @@ const RelatedArticles = ({
 
   // Slice to first 3 of the relevant articles
   filteredArticles = filteredArticles
-    .filter(article => article.key !== currentArticle)
+    .filter((article) => article.key !== currentArticle)
     .slice(0, 3);
 
   // It first checks if `specificArticles` is defined, and if so, it maps over each article name to find the corresponding article object in `helpAndKnowledgeArticles`.
@@ -52,8 +48,8 @@ const RelatedArticles = ({
   // If `specificArticles` is not defined, it assigns `filteredArticles` to `articlesToShow`.
   const articlesToShow: ArticleProps[] =
     specificArticles
-      ?.map(articleKey =>
-        helpAndKnowledgeArticles.find(article => article.key === articleKey)
+      ?.map((articleKey) =>
+        helpAndKnowledgeArticles.find((article) => article.key === articleKey)
       )
       .filter((article): article is ArticleProps => !!article) ?? // Filter out undefined values // Filter out undefined values
     filteredArticles;
@@ -61,15 +57,15 @@ const RelatedArticles = ({
   return (
     <div className="bg-primary-lighter">
       <GridContainer className="padding-top-4 padding-bottom-10">
-        <h2 className="margin-top-0 margin-bottom-1">{t('relatedHelp')}</h2>
-        <dt className="margin-bottom-4">{t('relatedDescription')}</dt>
+        <h2 className="margin-top-0 margin-bottom-1">{t("relatedHelp")}</h2>
+        <dt className="margin-bottom-4">{t("relatedDescription")}</dt>
         <CardGroup className={classnames(className)}>
-          {articlesToShow.map(article => (
+          {articlesToShow.map((article) => (
             <ArticleCard {...article} isLink />
           ))}
         </CardGroup>
         {viewAllLink && (
-          <Link to="/help-and-knowledge/articles">{t('viewAllRelated')}</Link>
+          <Link to="/help-and-knowledge/articles">{t("viewAllRelated")}</Link>
         )}
       </GridContainer>
     </div>
