3f21082acb1f8fadf2bdf532ffa4fb21adf28477
Tom Brooks <tom.brooks@oddball.io>
2024-06-28 17:38:03 -0400

    chore: moved existing model queries

---
+++ pkg/sqlqueries/existing_model.go
@@ -0,0 +1,20 @@
+package sqlqueries
+
+import _ "embed"
+
+//go:embed SQL/existing_model/collection_get.sql
+var existingModelCollectionGetSQL string
+
+//go:embed SQL/existing_model/get_by_id_LOADER.sql
+var existingModelGetByModelPlanIDLoaderSQL string
+
+type existingModelScripts struct {
+	CollectionGet          string
+	GetByModelPlanIDLoader string
+}
+
+// ExistingModel houses all the sql for getting data for discussion reply from the database
+var ExistingModel = existingModelScripts{
+	CollectionGet:          existingModelCollectionGetSQL,
+	GetByModelPlanIDLoader: existingModelGetByModelPlanIDLoaderSQL,
+}
