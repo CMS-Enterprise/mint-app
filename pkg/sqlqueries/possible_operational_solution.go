04e6e2d8126dcadd8c583d720030ff9980083537
Tom Brooks <tom.brooks@oddball.io>
2024-06-29 01:35:23 -0400

    chore: moved plan tdl queries and possible operational solution queries

---
+++ pkg/sqlqueries/possible_operational_solution.go
@@ -0,0 +1,35 @@
+package sqlqueries
+
+import _ "embed"
+
+//go:embed SQL/possible_operational_solution/collection_get_by_need_type.sql
+var possibleOperationalSolutionCollectionByNeedTypeSQL string
+
+//go:embed SQL/possible_operational_solution/collection_get_all.sql
+var possibleOperationalSolutionCollectionGelAllSQL string
+
+//go:embed SQL/possible_operational_solution/collection_get_by_operational_need_id.sql
+var possibleOperationalSolutionCollectionByOperationalNeedIDSQL string
+
+//go:embed SQL/possible_operational_solution/get_by_id.sql
+var possibleOperationalSolutionGetByIDSQL string
+
+//go:embed SQL/possible_operational_solution/get_by_key.sql
+var possibleOperationalSolutionGetByKeySQL string
+
+type possibleOperationalSolutionScripts struct {
+	CollectionByNeedType          string
+	CollectionGetAll              string
+	CollectionByOperationalNeedID string
+	GetByID                       string
+	GetByKey                      string
+}
+
+// PossibleOperationalSolution houses all the sql for getting data for possible operational solution from the database
+var PossibleOperationalSolution = possibleOperationalSolutionScripts{
+	CollectionByNeedType:          possibleOperationalSolutionCollectionByNeedTypeSQL,
+	CollectionGetAll:              possibleOperationalSolutionCollectionGelAllSQL,
+	CollectionByOperationalNeedID: possibleOperationalSolutionCollectionByOperationalNeedIDSQL,
+	GetByID:                       possibleOperationalSolutionGetByIDSQL,
+	GetByKey:                      possibleOperationalSolutionGetByKeySQL,
+}
