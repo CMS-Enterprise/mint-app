2e814387b093ff9a8e11bbd8621b1b2d91d7a03a
Tom Brooks <tom.brooks@oddball.io>
2024-07-10 18:57:49 -0400

    chore: removed unnecessary Scan and Value methods

--- pkg/models/data_exchange_approach.go
+++ pkg/models/data_exchange_approach.go
@@ -1,8 +1,6 @@
 package models
 
 import (
-	"database/sql/driver"
-
 	"github.com/google/uuid"
 )
 
@@ -23,14 +21,4 @@ func NewDataExchangeApproach(name string, createdBy uuid.UUID, modelPlanID uuid.
 		Name:              name,
 		IsComplete:        false,
 	}
-}
-
-// Value allows us to satisfy the valuer interface, so we can write to the database
-func (d DataExchangeApproach) Value() (driver.Value, error) {
-	return GenericValue(d)
-}
-
-// Scan implements the scanner interface, so we can translate the JSONb from the db to an object in GO
-func (d *DataExchangeApproach) Scan(src interface{}) error {
-	return GenericScan(src, d)
 }
