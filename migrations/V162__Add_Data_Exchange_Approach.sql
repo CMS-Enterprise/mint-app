4626bf81866295f3ac1edc56711db97f9ad3b486
Steven Wade <steven.wade@oddball.io>
2024-07-16 08:25:46 -0700

    fix: migration linting

--- migrations/V162__Add_Data_Exchange_Approach.sql
+++ migrations/V162__Add_Data_Exchange_Approach.sql
@@ -1,23 +1,23 @@
 CREATE TABLE data_exchange_approach (
-                                      id UUID PRIMARY KEY NOT NULL,
-                                      model_plan_id UUID NOT NULL REFERENCES model_plan(id),
-                                      high_level_overview ZERO_STRING,
-                                      new_methods ZERO_STRING,
-                                      feasibility ZERO_STRING,
-                                      participant_burden ZERO_STRING,
-                                      cmmi_impact ZERO_STRING,
-                                      additional_considerations ZERO_STRING,
+    id UUID PRIMARY KEY NOT NULL,
+    model_plan_id UUID NOT NULL REFERENCES model_plan(id),
+    high_level_overview ZERO_STRING,
+    new_methods ZERO_STRING,
+    feasibility ZERO_STRING,
+    participant_burden ZERO_STRING,
+    cmmi_impact ZERO_STRING,
+    additional_considerations ZERO_STRING,
 
-  -- META DATA
-                                      created_by UUID REFERENCES public.user_account (id) MATCH SIMPLE NOT NULL,
-                                      created_dts TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                      modified_by UUID REFERENCES public.user_account (id) MATCH SIMPLE,
-                                      modified_dts TIMESTAMP WITH TIME ZONE,
-                                      ready_for_review_by UUID REFERENCES public.user_account (id) MATCH SIMPLE,
-                                      ready_for_review_dts TIMESTAMP WITH TIME ZONE,
-                                      ready_for_clearance_by UUID REFERENCES public.user_account (id) MATCH SIMPLE,
-                                      ready_for_clearance_dts TIMESTAMP WITH TIME ZONE,
-                                      status TASK_STATUS NOT NULL DEFAULT 'READY'
+    -- META DATA
+    created_by UUID REFERENCES public.user_account (id) MATCH SIMPLE NOT NULL,
+    created_dts TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_by UUID REFERENCES public.user_account (id) MATCH SIMPLE,
+    modified_dts TIMESTAMP WITH TIME ZONE,
+    ready_for_review_by UUID REFERENCES public.user_account (id) MATCH SIMPLE,
+    ready_for_review_dts TIMESTAMP WITH TIME ZONE,
+    ready_for_clearance_by UUID REFERENCES public.user_account (id) MATCH SIMPLE,
+    ready_for_clearance_dts TIMESTAMP WITH TIME ZONE,
+    status TASK_STATUS NOT NULL DEFAULT 'READY'
 );
 
 COMMENT ON COLUMN data_exchange_approach.high_level_overview IS 'Provide a high-level overview of how you anticipate exchanging data with participants.';
