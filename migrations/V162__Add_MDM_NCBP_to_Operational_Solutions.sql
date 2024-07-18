06b7bcb9964929bd1caf188952dedd5b484be3cb
Patrick Segura <patrick.segura@oddball.io>
2024-07-18 13:53:52 -0500

    Added migration comments

--- migrations/V162__Add_MDM_NCBP_to_Operational_Solutions.sql
+++ migrations/V162__Add_MDM_NCBP_to_Operational_Solutions.sql
@@ -16,6 +16,7 @@ WITH pocs(SolutionName, SolutionKey, Name, Email, Role, IsPrimary, IsTeam) AS (
 ('Master Data Management for Non-Claims Based Payments', 'MDM_NCBP', 'Sameera Gudipati', 'sameera.gudipati1@cms.hhs.gov', 'OIT Point of Contact',FALSE, FALSE)
 )
 
+-- Insert MDM-NCBP contacts into the possible operational solution contact table
 INSERT INTO possible_operational_solution_contact(
     id,
     possible_operational_solution_id,
@@ -42,6 +43,7 @@ SELECT
 FROM pocs
 JOIN possible_operational_solution pos on CAST(pos.sol_key as TEXT) = pocs.solutionkey;
 
+-- Link solution as a default for the operational need make non-claims based payments
 SELECT CREATE_POSSIBLE_NEED_SOLUTION_LINK(
   'MAKE_NON_CLAIMS_BASED_PAYMENTS'::OPERATIONAL_NEED_KEY,
   '{MDM_NCBP}'::OPERATIONAL_SOLUTION_KEY[]
