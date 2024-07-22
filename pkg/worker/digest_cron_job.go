450a61c9213db1875b7d79b14504262d787ae5cc
Steven Wade <steven.wade@oddball.io>
2024-07-22 14:08:55 -0700

    feat: add logging to analyzed audit cron job

--- pkg/worker/digest_cron_job.go
+++ pkg/worker/digest_cron_job.go
@@ -6,6 +6,7 @@ import (
 
 	faktory "github.com/contribsys/faktory/client"
 	faktory_worker "github.com/contribsys/faktory_worker_go"
+	"go.uber.org/zap"
 )
 
 // DigestCronJob is the job the cron schedule calls
@@ -14,6 +15,7 @@ func (w *Worker) DigestCronJob(ctx context.Context, args ...interface{}) error {
 
 	// Call AnalyzedAuditBatchJob
 	helper := faktory_worker.HelperFor(ctx)
+	w.Logger.Info("creating Daily Analyzed Audit Cron Job", zap.Any("JID", helper.Jid()))
 
 	return helper.With(func(cl *faktory.Client) error {
 		job := faktory.NewJob(analyzedAuditBatchJobName, dayToAnalyze)
