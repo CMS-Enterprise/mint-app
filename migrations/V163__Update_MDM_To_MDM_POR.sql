6ba8c7073a157bc6a191573c435cf6c396e31979
Steven Wade <steven.wade@oddball.io>
2024-07-22 08:16:14 -0700

    fix: remove unnessary space in solution name

--- migrations/V163__Update_MDM_To_MDM_POR.sql
+++ migrations/V163__Update_MDM_To_MDM_POR.sql
@@ -2,7 +2,7 @@ ALTER TYPE OPERATIONAL_SOLUTION_KEY RENAME VALUE 'MDM' TO 'MDM_POR';
 
 UPDATE possible_operational_solution
 SET
-    sol_name = 'Master Data Management Program-Organization Relationship  (MDM-POR)',
+    sol_name = 'Master Data Management Program-Organization Relationship (MDM-POR)',
     modified_by = '00000001-0001-0001-0001-000000000001',
     modified_dts = NOW()
 WHERE sol_key = 'MDM_POR';
