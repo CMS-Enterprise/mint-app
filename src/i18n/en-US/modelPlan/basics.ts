958d82603632d828c8878bdf7996c5f78a4485c3
Patrick Segura <patrick.segura@oddball.io>
2024-07-29 16:19:25 -0500

    Fixed basics as well

--- src/i18n/en-US/modelPlan/basics.ts
+++ src/i18n/en-US/modelPlan/basics.ts
@@ -467,10 +467,10 @@ export const basics: TranslationBasics = {
     formType: TranslationFormType.CHECKBOX,
     order: 3.17,
     options: {
-      READY: 'ready',
-      IN_PROGRESS: 'in progress',
-      READY_FOR_REVIEW: 'ready for review',
-      READY_FOR_CLEARANCE: 'ready for clearance'
+      READY: 'Ready',
+      IN_PROGRESS: 'In progress',
+      READY_FOR_REVIEW: 'Ready for review',
+      READY_FOR_CLEARANCE: 'Ready for clearance'
     },
     hideFromReadonly: true
   }
