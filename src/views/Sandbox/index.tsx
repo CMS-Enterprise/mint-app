1bea2692cb893d0cec499978c9af603a83882959
Gary Zhao <gary.j.zhao@gmail.com>
2024-08-06 14:24:30 -0400

    revert sandbox

--- src/views/Sandbox/index.tsx
+++ src/views/Sandbox/index.tsx
@@ -1,25 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { Button } from '@trussworks/react-uswds';
+import React, { useEffect } from 'react';
 
 import MainContent from 'components/MainContent';
-import ProviderAndSupplierModal from 'views/ModelPlan/TaskList/ParticipantsAndProviders/_component/ProviderAndSupplierModal';
 
 const Sandbox = () => {
-  const [isOpen, setIsOpen] = useState(true);
-
   useEffect(() => {
     document.title = 'Sandbox';
   }, []);
 
   return (
     <MainContent>
-      <Button type="button" unstyled onClick={() => setIsOpen(true)}>
-        Click me
-      </Button>
-      <ProviderAndSupplierModal
-        isOpen={isOpen}
-        closeModal={() => setIsOpen(false)}
-      />
+      <></>
     </MainContent>
   );
 };
