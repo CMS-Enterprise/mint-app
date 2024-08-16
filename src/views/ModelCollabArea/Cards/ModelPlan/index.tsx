8791070956c5992d885ddc50640c1a1de68de6c6
Gary Zhao <gary.j.zhao@gmail.com>
2024-08-16 14:11:51 -0400

    continue working the card

--- src/views/ModelCollabArea/Cards/ModelPlan/index.tsx
+++ src/views/ModelCollabArea/Cards/ModelPlan/index.tsx
@@ -6,25 +6,44 @@ import {
   CardFooter,
   CardHeader
 } from '@trussworks/react-uswds';
-import { TaskStatus } from 'gql/gen/graphql';
+import {
+  GetModelPlanQuery,
+  TaskStatus,
+  useGetModelPlanQuery
+} from 'gql/gen/graphql';
 
 import UswdsReactLink from 'components/LinkWrapper';
+import { formatDateLocal } from 'utils/date';
 import { TaskListStatusTag } from 'views/ModelPlan/TaskList/_components/TaskListItem';
 
 import './index.scss';
 
+type GetModelPlanTypes = GetModelPlanQuery['modelPlan'];
+
 type ModelPlanCardType = {
   modelID: string;
-  modifiedDts?: string;
-  modifiedBy?: string;
 };
 
-const ModelPlanCard = ({
-  modelID,
-  modifiedDts,
-  modifiedBy
-}: ModelPlanCardType) => {
+const ModelPlanCard = ({ modelID }: ModelPlanCardType) => {
   const { t: modelPlanCardT } = useTranslation('modelPlanCard');
+  const { data } = useGetModelPlanQuery({
+    variables: {
+      id: modelID
+    }
+  });
+
+  const modelPlan = data?.modelPlan || ({} as GetModelPlanTypes);
+  const {
+    modifiedDts,
+    modifiedBy
+    // basics: { status },
+    // beneficiaries: { status },
+    // generalCharacteristics: { status },
+    // opsEvalAndLearning: { status },
+    // participantsAndProviders: { status },
+    // payments: { status },
+    // prepareForClearance: { status }
+  } = modelPlan;
 
   return (
     <Card gridLayout={{ tablet: { col: 6 } }} className="card--model-plan">
@@ -47,6 +66,14 @@ const ModelPlanCard = ({
       <CardBody>
         <p>{modelPlanCardT('body')}</p>
       </CardBody>
+
+      {modifiedDts && (
+        <p className="text-base margin-left-3">
+          {modelPlanCardT('mostRecentEdit', {
+            date: formatDateLocal(modifiedDts, 'MM/dd/yyyy')
+          })}
+        </p>
+      )}
       <CardFooter>
         <UswdsReactLink
           to={`/models/${modelID}/task-list`}
