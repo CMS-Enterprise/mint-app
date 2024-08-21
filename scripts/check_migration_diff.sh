c07323c59d1898684188e0f094cdfe41a2a007c3
ClayBenson94 <clay.benson@oddball.io>
2024-08-21 10:52:14 -0400

    Fix script to negate check

--- scripts/check_migration_diff.sh
+++ scripts/check_migration_diff.sh
@@ -5,7 +5,7 @@ set -eu -o pipefail
 diff=$(git diff --name-status "origin/${BASE_REF}..origin/${HEAD_REF}" -- ./migrations)
 printf 'Migration diff:\n%s\n' "${diff}"
 
-diffNonAdds=$(echo "${diff}" | grep -E '^A' || true) # Look for lines that start with A, and always return exit code 0 with || true
+diffNonAdds=$(echo "${diff}" | grep -Ev '^A' || true) # Look for lines that don't (-v) start with A, and always return exit code 0 with || true
 printf 'Migration diff (non-adds):\n%s\n' "${diffNonAdds}"
 
 if [ -n "${diffNonAdds}" ]; then
