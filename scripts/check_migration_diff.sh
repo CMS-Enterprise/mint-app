ad6491669e313b2e800029a11e5848e51ff39040
ClayBenson94 <clay.benson@oddball.io>
2024-08-21 10:36:46 -0400

    Fix script to handle grep's exit code

--- scripts/check_migration_diff.sh
+++ scripts/check_migration_diff.sh
@@ -3,10 +3,10 @@
 set -eu -o pipefail
 
 diff=$(git diff --name-status "origin/${BASE_REF}..origin/${HEAD_REF}" -- ./migrations)
-printf "Migration diff:\n%s", "${diff}"
+printf 'Migration diff:\n%s\n' "${diff}"
 
-diffNonAdds=$(echo "${diff}" | grep -E '^A\s')
-printf "Migration nonAdds diff:\n%s", "${diffNonAdds}"
+diffNonAdds=$(echo "${diff}" | grep -E '^A' || true) # Look for lines that start with A, and always return exit code 0 with || true
+printf 'Migration diff (non-adds):\n%s\n' "${diffNonAdds}"
 
 if [ -n "${diffNonAdds}" ]; then
   echo "Migration files can only be added in migrations folder"
