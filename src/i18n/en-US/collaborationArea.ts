8bb0d6e57a9ad1e2ad987eb983a093efd113cb86
Gary Zhao <gary.j.zhao@gmail.com>
2024-09-03 13:03:35 -0400

    Added DocumentsCard copy

--- src/i18n/en-US/collaborationArea.ts
+++ src/i18n/en-US/collaborationArea.ts
@@ -9,10 +9,17 @@ const collaborationArea = {
   goToModelPlan: 'Go to Model Plan',
   modelPlanCard: {
     heading: 'Model Plan',
-    body: 'The Model Plan will help components across CMS evaluate your model’s operational requirements and IT needs. It contains questions about payments, providers, general characteristics, and more. The Model Plan is flexible, so you may leave questions blank, add new information, and change information as you iterate on your model or learn of new dependencies.',
+    body:
+      'The Model Plan will help components across CMS evaluate your model’s operational requirements and IT needs. It contains questions about payments, providers, general characteristics, and more. The Model Plan is flexible, so you may leave questions blank, add new information, and change information as you iterate on your model or learn of new dependencies.',
     mostRecentEdit: 'Most recent edit on {{-date}} by ',
     sectionsStarted: '{{-sectionsStarted}}/7 sections started',
     shareButton: 'Share or export Model Plan'
+  },
+  documentsCard: {
+    heading: 'Documents',
+    noDocuments: 'No documents added',
+    linkAdded: '{{count}} link added',
+    linkAdded_other: '{{count}} links added'
   }
 };
 
