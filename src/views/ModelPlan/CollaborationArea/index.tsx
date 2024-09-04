0eaa2cbe435a38f935c205d68ba0a0a51f12f98c
Gary Zhao <gary.j.zhao@gmail.com>
2024-09-04 11:05:15 -0400

    Added DocumentCards test

--- src/views/ModelPlan/CollaborationArea/index.tsx
+++ src/views/ModelPlan/CollaborationArea/index.tsx
@@ -274,7 +274,7 @@ const CollaborationArea = () => {
           />
         )}
       </GridContainer>
-      <div className="bg-primary-lighter padding-y-6">
+      <div className="bg-primary-lighter padding-top-6 padding-bottom-10">
         <GridContainer>
           <CardGroup>
             <DocumentsCard documents={documents} modelID={modelID} />
