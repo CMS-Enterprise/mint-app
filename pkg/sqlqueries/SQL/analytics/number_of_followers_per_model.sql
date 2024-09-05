17c045571965b8d5bda85ce975855dcbf6048133
Steven Wade <steven.wade@oddball.io>
2024-09-05 12:35:01 -0600

    feat: number_of_followers_per_model

---
+++ pkg/sqlqueries/SQL/analytics/number_of_followers_per_model.sql
@@ -0,0 +1,8 @@
+/* Number of Followers By Model */
+SELECT
+    model_plan.model_name,
+    COUNT(favorite.*) AS number_of_followers
+FROM model_plan
+LEFT JOIN plan_favorite AS favorite ON model_plan.id = favorite.model_plan_id
+GROUP BY model_plan.model_name
+ORDER BY number_of_followers DESC
