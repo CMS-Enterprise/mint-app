fdc227bc2cfd556ef6dcc91e9b18afd72239f100
Steven Wade <steven.wade@oddball.io>
2024-09-06 10:59:33 -0600

    feat: tdl parquet data in local file

--- cmd/dbseed/echimp.go
+++ cmd/dbseed/echimp.go
@@ -8,14 +8,22 @@ import (
 //go:embed data/FFS_CR_DATA.parquet
 var fsCrDataParquet []byte
 
+//go:embed data/TDL_DATA.parquet
+var tdlDataParquet []byte
+
 // SeedEChimpBucket uploads EChimp data to MINIO if it has not yet happened
 func (s *Seeder) SeedEChimpBucket() {
 
 	crKey := "echimp_cr"
 
-	// Create a reader from the embedded byte slice
-	reader := bytes.NewReader(fsCrDataParquet)
-	err := s.Config.EChimpClient.UploadFile(reader, crKey)
+	// Create a crReader from the embedded byte slice
+	crReader := bytes.NewReader(fsCrDataParquet)
+	tldReader := bytes.NewReader(tdlDataParquet)
+	err := s.Config.EChimpClient.UploadFile(crReader, crKey)
+	if err != nil {
+		panic(err)
+	}
+	err = s.Config.EChimpClient.UploadFile(tldReader, crKey)
 	if err != nil {
 		panic(err)
 	}
