cb9cf1ddd0aee39adf01d9ff5c7fb901adce0e07
Steven Wade <steven.wade@oddball.io>
2024-10-11 11:26:23 -0600

    feat: use sorting utility methods in data exchange, add a comment to potentially replace get by id with loader calls

--- pkg/storage/loaders/plan_data_exchange_approach_loader.go
+++ pkg/storage/loaders/plan_data_exchange_approach_loader.go
@@ -2,10 +2,8 @@ package loaders
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/google/uuid"
-	"github.com/samber/lo"
 
 	"github.com/cms-enterprise/mint-app/pkg/appcontext"
 	"github.com/cms-enterprise/mint-app/pkg/models"
@@ -26,42 +24,18 @@ var PlanDataExchangeApproach = &planDataExchangeApproachLoaders{
 
 func batchPlanDataExchangeApproachByModelPlanID(ctx context.Context, modelPlanIDs []uuid.UUID) []*dataloader.Result[*models.PlanDataExchangeApproach] {
 	logger := appcontext.ZLogger(ctx)
-	output := make([]*dataloader.Result[*models.PlanDataExchangeApproach], len(modelPlanIDs))
 	loaders, err := Loaders(ctx)
 	if err != nil {
-		for index := range modelPlanIDs {
-			output[index] = &dataloader.Result[*models.PlanDataExchangeApproach]{Data: nil, Error: err}
-		}
-		return output
+		return errorPerEachKey[uuid.UUID, *models.PlanDataExchangeApproach](modelPlanIDs, err)
 	}
 
-	// TODO, when sorting and erroring utilities are merged to main, replace these functions with them
 	data, err := storage.PlanDataExchangeApproachGetByModelPlanIDLoader(loaders.DataReader.Store, logger, modelPlanIDs)
 	if err != nil {
-
-		for index := range modelPlanIDs {
-			output[index] = &dataloader.Result[*models.PlanDataExchangeApproach]{Data: nil, Error: err}
-		}
-		return output
+		return errorPerEachKey[uuid.UUID, *models.PlanDataExchangeApproach](modelPlanIDs, err)
 	}
-
-	approachByModelPlanID := lo.Associate(data, func(data *models.PlanDataExchangeApproach) (uuid.UUID, *models.PlanDataExchangeApproach) {
-		return data.ModelPlanID, data
-	})
-
-	// TODO (loaders) implement the generic sorting functionality once it is merged to main
-
-	// RETURN IN THE SAME ORDER REQUESTED
-	for index, id := range modelPlanIDs {
-
-		data, ok := approachByModelPlanID[id]
-		if ok {
-			output[index] = &dataloader.Result[*models.PlanDataExchangeApproach]{Data: data, Error: nil}
-		} else {
-			err2 := fmt.Errorf("plan data exchange approach not found for modelPlanID id %s", id)
-			output[index] = &dataloader.Result[*models.PlanDataExchangeApproach]{Data: nil, Error: err2}
-		}
+	getKeyFunc := func(data *models.PlanDataExchangeApproach) uuid.UUID {
+		return data.ModelPlanID
 	}
-	return output
+	return oneToOneDataLoaderFunc(modelPlanIDs, data, getKeyFunc)
 
 }
