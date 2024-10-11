1a80aae1ac04dc771dc0650705d4e85996dcf6a0
Gary Zhao <gary.j.zhao@gmail.com>
2024-10-11 13:03:40 -0400

    lint fix

--- cypress/e2e/crtdl.spec.js
+++ cypress/e2e/crtdl.spec.js
@@ -18,7 +18,7 @@ describe('FFS CRs and TDLs', () => {
     cy.contains('button', 'View more').click();
     cy.get('[data-testid="cr-and-tdl-sidepanel"]').contains(
       'Billing Requirements for Physician Services Rendered in Method II Critical Access Hospitals'
-    )
+    );
     cy.get('[data-testid="cr-and-tdl-sidepanel"]').within(() => {
       cy.contains('button', 'View this in ECHIMP').click();
     });
