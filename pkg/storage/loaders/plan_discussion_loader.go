73b10793ca6e3ba6a909c0736a80bb13714721a7
Steven Wade <steven.wade@oddball.io>
2024-10-11 12:25:06 -0600

    chore: remove commented out code

--- pkg/storage/loaders/plan_discussion_loader.go
+++ pkg/storage/loaders/plan_discussion_loader.go
@@ -23,51 +23,6 @@ var PlanDiscussion = &planDiscussionLoaders{
 	ByModelPlanID: NewLoaderWrapper(batchPlanDiscussionGetByModelPlanID),
 }
 
-// // GetPlanDiscussionByModelPlanID uses a DataLoader to aggreggate a SQL call and return all Plan Discussion in one query
-// func (loaders *DataLoaders) GetPlanDiscussionByModelPlanID(ctx context.Context, keys dataloaderOld.Keys) []*dataloaderOld.Result {
-// 	dr := loaders.DataReader
-
-// 	logger := appcontext.ZLogger(ctx)
-// 	arrayCK, err := ConvertToKeyArgsArray(keys)
-// 	if err != nil {
-// 		logger.Error("issue converting keys for data loader in Plan Discussion", zap.Error(*err))
-// 	}
-// 	marshaledParams, err := arrayCK.ToJSONArray()
-// 	if err != nil {
-// 		logger.Error("issue converting keys to JSON for data loader in Plan Discussion", zap.Error(*err))
-// 	}
-
-// 	discs, _ := dr.Store.PlanDiscussionGetByModelPlanIDLOADER(logger, marshaledParams)
-// 	discsByID := map[string][]*models.PlanDiscussion{}
-
-// 	for _, disc := range discs {
-
-// 		slice, ok := discsByID[string(disc.ModelPlanID.String())]
-// 		if ok {
-// 			slice = append(slice, disc) //Add to existing slice
-// 			discsByID[string(disc.ModelPlanID.String())] = slice
-// 			continue
-// 		}
-// 		discsByID[string(disc.ModelPlanID.String())] = []*models.PlanDiscussion{disc}
-// 	}
-
-// 	// RETURN IN THE SAME ORDER REQUESTED
-// 	output := make([]*dataloaderOld.Result, len(keys))
-// 	for index, key := range keys {
-// 		ck, ok := key.Raw().(KeyArgs)
-// 		if ok {
-// 			resKey := fmt.Sprint(ck.Args["model_plan_id"])
-// 			discs := discsByID[resKey] // if discussion not found, empty array
-// 			output[index] = &dataloaderOld.Result{Data: discs, Error: nil}
-// 		} else {
-// 			err := fmt.Errorf("could not retrive key from %s", key.String())
-// 			output[index] = &dataloaderOld.Result{Data: nil, Error: err}
-// 		}
-// 	}
-// 	return output
-
-// }
-
 func batchPlanDiscussionGetByModelPlanID(ctx context.Context, modelPlanIDs []uuid.UUID) []*dataloader.Result[[]*models.PlanDiscussion] {
 	loaders, err := Loaders(ctx)
 	logger := appcontext.ZLogger(ctx)
