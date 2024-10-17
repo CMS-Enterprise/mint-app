f248a8f6abd00c31706ed9eef44139a1b87af75c
Gary Zhao <gary.j.zhao@gmail.com>
2024-10-17 13:37:11 -0400

    update truncated text to have line clamp snap

--- src/components/TruncatedText/index.test.tsx
+++ src/components/TruncatedText/index.test.tsx
@@ -1,51 +1,29 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import {
-  GetOverviewDocument,
-  GetOverviewQuery,
-  ModelType
-} from 'gql/generated/graphql';
+import { GetModelPlanBaseDocument } from 'gql/generated/graphql';
 import VerboseMockedProvider from 'tests/MockedProvider';
 
 import TruncatedText from './index';
 
-type GetOverviewType = GetOverviewQuery['modelPlan']['basics'];
-
-const overviewMockData: GetOverviewType = {
-  __typename: 'PlanBasics',
-  id: '123',
-  modelType: [ModelType.MANDATORY_NATIONAL],
-  modelTypeOther: 'Other model type',
-  problem: 'My problem',
-  goal: 'A goal',
-  testInterventions: 'Intervention',
-  note: 'Test note'
-};
-
 const mocks = [
   {
     request: {
-      query: GetOverviewDocument,
+      query: GetModelPlanBaseDocument,
       variables: { id: 'f11eb129-2c80-4080-9440-439cbe1a286f' }
     },
     result: {
       data: {
-        modelPlan: {
-          __typename: 'ModelPlan',
-          id: 'f11eb129-2c80-4080-9440-439cbe1a286f',
-          modelName: 'My excellent plan that I just initiated',
-          basics: overviewMockData
-        }
+        modelPlan: {}
       }
     }
   }
 ];
 
 const textString =
-  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus sit minima dolor asperiores pariatur deleniti blanditiis sint dolorem, laboriosam corporis labore culpa tenetur est quaerat.<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>';
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus sit minima dolor asperiores pariatur deleniti blanditiis sint dolorem, laboriosam corporis labore culpa tenetur est quaerat.<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>';
 
 describe('The TruncatedText component', () => {
-  it.skip('truncates text according to character count', () => {
+  it('truncates text according to character count', () => {
     const { getByText } = render(
       <VerboseMockedProvider mocks={mocks} addTypename={false}>
         <TruncatedText id="test" text={textString} charLimit={50} />
@@ -56,18 +34,18 @@ describe('The TruncatedText component', () => {
     expect(getByText(`${textString.substring(0, 50)} ...`)).toBeInTheDocument();
   });
 
-  it('truncates text according to line clamp', () => {
-    const { getByText } = render(
+  it('truncates text according to line clamp', async () => {
+    const { getByText, getByTestId } = render(
       <VerboseMockedProvider mocks={mocks} addTypename={false}>
-        <TruncatedText id="test" text={textString} lineClamp={1} />
+        <TruncatedText id="test" text={textString} lineClamp={2} />
       </VerboseMockedProvider>
     );
 
     expect(getByText('Read more')).toBeInTheDocument();
-    // expect(getByText(`${textString.substring(0, 50)} ...`)).toBeInTheDocument();
+    expect(getByTestId('truncated-text')).toHaveStyle('--line-clamp: 2');
   });
 
-  it.skip('matches snapshot', () => {
+  it('matches charLimit snapshot', () => {
     const { getByText, asFragment } = render(
       <VerboseMockedProvider mocks={mocks} addTypename={false}>
         <TruncatedText id="test" text={textString} charLimit={50} />
@@ -75,6 +53,16 @@ describe('The TruncatedText component', () => {
     );
     expect(getByText('Read more')).toBeInTheDocument();
     expect(getByText(`${textString.substring(0, 50)} ...`)).toBeInTheDocument();
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('matches lineClamp snapshot', () => {
+    const { getByText, asFragment } = render(
+      <VerboseMockedProvider mocks={mocks} addTypename={false}>
+        <TruncatedText id="test" text={textString} lineClamp={2} />
+      </VerboseMockedProvider>
+    );
+    expect(getByText('Read more')).toBeInTheDocument();
     expect(asFragment()).toMatchSnapshot();
   });
 });
