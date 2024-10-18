078eca237bb6829e4b78103970e7fecfc6e7d037
ClayBenson94 <clay.benson@oddball.io>
2024-10-18 11:59:12 -0400

    Clean up MTO GraphQL for high-level review

--- pkg/models/mto.go
+++ pkg/models/mto.go
@@ -2,5 +2,6 @@ package models
 
 type ModelsToOperationMatrix struct {
 	// empty on purpose so we have to add resolvers
+	// we could probably swap ModelPlan out for modelPlanRelation since we only really need the ID, but this works for now
 	ModelPlan *ModelPlan
 }
