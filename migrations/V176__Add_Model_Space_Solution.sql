caa6dcaff80dc57b55d8030ac8a571766fd562a3
Steven Wade <steven.wade@oddball.io>
2024-10-21 16:30:55 -0400

    feat: add pocs in the migration

--- migrations/V176__Add_Model_Space_Solution.sql
+++ migrations/V176__Add_Model_Space_Solution.sql
@@ -6,3 +6,38 @@ COMMIT;
 -- Insert MODEL_SPACE into the possible operational solution table
 INSERT INTO possible_operational_solution("id", "sol_name", "sol_key", "created_by") VALUES
 (41, 'Model Space', 'MODEL_SPACE', '00000001-0001-0001-0001-000000000001');
+
+
+WITH poc( SolutionKey, Name, Email, Role, IsPrimary, IsTeam) AS (
+    VALUES
+
+    ('MODEL_SPACE', 'Zach Nall', 'r.nall@cms.hhs.gov', 'Product Owner', TRUE, FALSE),
+    ('MODEL_SPACE', 'Curtis Naumann', 'curtis.naumann@cms.hhs.gov', 'Product Owner', FALSE, FALSE)
+)
+
+-- Insert MODEL_SPACE contacts into the possible operational solution contact table
+INSERT INTO possible_operational_solution_contact(
+    id,
+    possible_operational_solution_id,
+    name,
+    email,
+    role,
+    is_primary,
+    is_team,
+    created_by,
+    created_dts
+)
+
+SELECT
+    gen_random_uuid() AS id,
+    pos.id AS possible_operational_solution_id,
+    poc.name,
+    poc.email,
+    poc.role,
+    poc.IsPrimary AS is_primary,
+    poc.IsTeam AS is_team,
+    '00000001-0001-0001-0001-000000000001' AS created_by, --System account
+    current_timestamp AS created_dts
+
+FROM poc
+INNER JOIN possible_operational_solution AS pos ON cast(pos.sol_key AS TEXT) = poc.SolutionKey;
