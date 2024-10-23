49821ae6ca4a5161005ea058a74ece85b73c8663
Tom Brooks <100007843+OddTomBrooks@users.noreply.github.com>
2024-10-23 18:53:39 -0400

    [MINT-2506] Daily Digest Updates with Data Exchange Approach (#1437)
    * wip: daily digest updates to support data exchange approach
    
    * chore: restored envrc to unedited
    
    * fix: broken unit test mock calls
    
    * Add data exchange marked complete humanized string
    
    * fix: updated gql
    
    * chore: updates per PR feedback
    
    * Adding assert for analyzed_audit_job_test
    
    * Re-order and re-name migrations
    
    * Rename type in resolver
    
    * Fix typo'd table name
    
    ---------
    
    Co-authored-by: ClayBenson94 <clay.benson@oddball.io>

--- src/gql/generated/graphql.ts
+++ src/gql/generated/graphql.ts
@@ -169,6 +169,7 @@ export type AnalyzedPlanDiscussions = {
 
 export type AnalyzedPlanSections = {
   __typename: 'AnalyzedPlanSections';
+  dataExchangeApproachMarkedComplete?: Maybe<Scalars['Boolean']['output']>;
   readyForClearance: Array<TableName>;
   readyForReview: Array<TableName>;
   updated: Array<TableName>;
@@ -3699,6 +3700,7 @@ export enum TableName {
   PLAN_BENEFICIARIES = 'plan_beneficiaries',
   PLAN_COLLABORATOR = 'plan_collaborator',
   PLAN_CR = 'plan_cr',
+  PLAN_DATA_EXCHANGE_APPROACH = 'plan_data_exchange_approach',
   PLAN_DISCUSSION = 'plan_discussion',
   PLAN_DOCUMENT = 'plan_document',
   PLAN_DOCUMENT_SOLUTION_LINK = 'plan_document_solution_link',
