622dc5256cfec19e4c41d3b4e57464c3bd18ba65
Steven Wade <steven.wade@oddball.io>
2024-10-28 10:35:14 -0400

    feat: add mto_solution type and poc fields

--- migrations/V181__Add_MTO_Solution.sql
+++ migrations/V181__Add_MTO_Solution.sql
@@ -2,6 +2,12 @@ CREATE TYPE MTO_SOLUTION_STATUS AS ENUM (
     'SOLUTION_STATUS_ONE',
     'SOLUTION_STATUS_TWO'
 );
+CREATE TYPE MTO_SOLUTION_TYPE AS ENUM (
+    'IT_SYSTEM',
+    'CONTRACT',
+    'CROSS_CUTTING_GROUP',
+    'OTHER',
+);
 
 /*
 TODO, finish implementing this
@@ -12,10 +18,14 @@ CREATE TABLE mto_solution(
     mto_common_solution_id UUID REFERENCES mto_common_solution(id),
     -- we allow null because this is will be from the commonMilestone table if it exists
     name ZERO_STRING,
+    -- we allow null because this is will be from the commonMilestone table if it exists
+    type MTO_SOLUTION_TYPE,
     facilitated_by MTO_FACILITATOR,
     status MTO_SOLUTION_STATUS NOT NULL,
     risk_indicator MTO_RISK_INDICATOR,
-    is_draft BOOLEAN NOT NULL DEFAULT TRUE,
+    is_draft BOOLEAN NOT NULL DEFAULT TRUE,  --TODO (mto) is this field needed?
+    poc_name ZERO_STRING NOT NULL,
+    poc_email EMAIL NOT NULL,
 
     created_by UUID NOT NULL REFERENCES user_account(id),
     created_dts TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
