a021e2c993330558b87bfa4178d1e86b0ed128b1
Tom Brooks <tom.brooks@oddball.io>
2024-10-31 12:17:53 -0400

    wip: mto common milestone and solution first pass with linking table

--- migrations/V179__Add_MTO_Common_Milestone_And_Solution.sql
+++ migrations/V179__Add_MTO_Common_Milestone_And_Solution.sql
@@ -1,6 +1,10 @@
 CREATE TABLE mto_common_milestone (
                             id UUID PRIMARY KEY,
                             category_name ZERO_STRING NOT NULL,
+                            sub_category_name ZERO_STRING NOT NULL,
+                            title ZERO_STRING NOT NULL,
+                            description ZERO_STRING NOT NULL,
+                            facilitated_by_role ZERO_STRING NOT NULL,
 
                             created_by UUID NOT NULL REFERENCES user_account(id),
                             created_dts TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -10,6 +14,20 @@ CREATE TABLE mto_common_milestone (
 
 CREATE TABLE mto_common_solution (
                             id UUID PRIMARY KEY,
+                            name ZERO_STRING NOT NULL,
+                            key ZERO_STRING NOT NULL,
+                            description ZERO_STRING NOT NULL,
+
+                            created_by UUID NOT NULL REFERENCES user_account(id),
+                            created_dts TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                            modified_by UUID REFERENCES user_account(id),
+                            modified_dts TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE mto_common_milestone_solution_link (
+                            id UUID PRIMARY KEY,
+                            mto_common_milestone_id UUID NOT NULL REFERENCES mto_common_milestone(id) UNIQUE,
+                            mto_common_solution_id UUID NOT NULL REFERENCES mto_common_solution(id) UNIQUE,
 
                             created_by UUID NOT NULL REFERENCES user_account(id),
                             created_dts TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
