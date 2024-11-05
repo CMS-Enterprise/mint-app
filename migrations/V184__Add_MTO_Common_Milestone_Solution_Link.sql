b0d5321034fd5442b0f8cc9ae57c4cd520d56771
Steven Wade <steven.wade@oddball.io>
2024-11-05 13:48:53 -0500

    feat: enforce milestone solution combination uniqueness

--- migrations/V184__Add_MTO_Common_Milestone_Solution_Link.sql
+++ migrations/V184__Add_MTO_Common_Milestone_Solution_Link.sql
@@ -1,10 +1,12 @@
 CREATE TABLE mto_common_milestone_solution_link (
     id UUID PRIMARY KEY,
-    mto_common_milestone_id UUID NOT NULL REFERENCES mto_common_milestone(id) UNIQUE,
-    mto_common_solution_id UUID NOT NULL REFERENCES mto_common_solution(id) UNIQUE,
+    mto_common_milestone_id UUID NOT NULL REFERENCES mto_common_milestone(id),
+    mto_common_solution_id UUID NOT NULL REFERENCES mto_common_solution(id),
 
     created_by UUID NOT NULL REFERENCES user_account(id),
     created_dts TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modified_by UUID REFERENCES user_account(id),
-    modified_dts TIMESTAMP WITH TIME ZONE
+    modified_dts TIMESTAMP WITH TIME ZONE,
+
+    UNIQUE (mto_common_milestone_id, mto_common_solution_id)
 );
