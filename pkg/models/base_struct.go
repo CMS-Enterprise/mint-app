533bd3fd39f85d7107f775dfaf2fe39a5e14127c
Steven Wade <steven.wade@oddball.io>
2024-11-05 11:10:10 -0500

    feat: use lo latest by instead of bespoke sort functionality

--- pkg/models/base_struct.go
+++ pkg/models/base_struct.go
@@ -1,10 +1,10 @@
 package models
 
 import (
-	"slices"
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/samber/lo"
 
 	"github.com/cms-enterprise/mint-app/pkg/authentication"
 )
@@ -79,23 +79,10 @@ func GetMostRecentTime(baseStructs []IBaseStruct) (time.Time, uuid.UUID) {
 	if len(baseStructs) < 1 {
 		return timeToReturn, uuid.Nil
 	}
-	// Sort baseStructs based on a custom comparison function
-	slices.SortStableFunc(baseStructs, func(item1, item2 IBaseStruct) int {
-		// Directly call the MostRecentModification method on IBaseStruct
-		time1, _ := item1.MostRecentModification()
-		time2, _ := item2.MostRecentModification()
 
-		// Return -1 if time1 is after time2 (more recent)
-		// Return 1 if time2 is after time1
-		// Return 0 if they are equal
-		if time1.After(time2) {
-			return -1
-		} else if time2.After(time1) {
-			return 1
-		}
-		return 0 // Default comparison if times are equal
+	latestBaseStruct := lo.LatestBy(baseStructs, func(bs IBaseStruct) time.Time {
+		modificationTime, _ := bs.MostRecentModification()
+		return modificationTime
 	})
-
-	return baseStructs[0].MostRecentModification()
-
+	return latestBaseStruct.MostRecentModification()
 }
