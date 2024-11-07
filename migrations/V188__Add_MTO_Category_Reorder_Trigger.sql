56ddb641645213b505fe6bc3c9003a6af327452e
Steven Wade <steven.wade@oddball.io>
2024-11-07 15:28:31 -0500

    feat: position adjustment tentatively working, still need to test and adjust

--- migrations/V188__Add_MTO_Category_Reorder_Trigger.sql
+++ migrations/V188__Add_MTO_Category_Reorder_Trigger.sql
@@ -1,6 +1,11 @@
 CREATE OR REPLACE FUNCTION update_position_based_on_parent_and_model_plan()
 RETURNS TRIGGER AS $$
 
+DECLARE
+    /*
+     This determines if we need to add or subtract a value from the position. It should be +1 or -1
+    */
+    position_adjustment INT;
 BEGIN
 
     -- Avoid recursion by checking pg_trigger_depth()
@@ -12,57 +17,70 @@ BEGIN
     -- Handle Insert: Adjust positions of other categories if the inserted position conflicts
     IF TG_OP = 'INSERT' THEN
         -- Move categories with the same parent_id and model_plan_id that have a position >= NEW.position
+        position_adjustment := 1;  -- For insert, we increase positions of conflicting rows
         UPDATE mto_category
-        SET position = position + 1,
-        modified_by = NEW.modified_by,
-        modified_dts = NEW.modified_dts
-        WHERE (parent_id IS NULL AND NEW.parent_id IS NULL OR parent_id = NEW.parent_id)
-          AND (model_plan_id IS NULL AND NEW.model_plan_id IS NULL OR model_plan_id = NEW.model_plan_id)
+        SET position = position + position_adjustment,
+            modified_by = NEW.modified_by,
+            modified_dts = NEW.modified_dts
+        WHERE ((parent_id IS NULL AND NEW.parent_id IS NULL) OR (parent_id = NEW.parent_id))
+          AND ((model_plan_id IS NULL AND NEW.model_plan_id IS NULL) OR (model_plan_id = NEW.model_plan_id))
           AND position >= NEW.position
           AND id != NEW.id;  -- Exclude the newly inserted row
     
     -- Handle Update: Reorder positions when a row's position is changed 
     ELSIF TG_OP = 'UPDATE' THEN
-        -- Handle position change within the same parent_id and model_plan_id
-        IF OLD.parent_id IS  DISTINCT FROM NEW.parent_id AND OLD.model_plan_id IS  DISTINCT FROM NEW.model_plan_id THEN
+        -- Determine if record moved up or down
+        IF NEW.position > OLD.position THEN
+            position_adjustment := -1;
+        ELSE
+            position_adjustment := 1;
+        END IF;
+
+        /*
+        Check if either the parent_id or model_plan_id changed. If so, adjust the old, and the new values
+        TODO (mto) --> make this work. Could also remove because it functionally shouldn't occur
+        */
+        IF OLD.parent_id IS DISTINCT FROM NEW.parent_id AND OLD.model_plan_id IS DISTINCT FROM NEW.model_plan_id THEN
             -- If the parent_id or model_plan_id changes, adjust positions for both
             -- Handle deletion of the category from the old parent
             UPDATE mto_category
             SET position = position + 1,
-            modified_by = NEW.modified_by,
-            modified_dts = NEW.modified_dts
-            WHERE (parent_id IS NULL AND OLD.parent_id IS NULL OR parent_id = OLD.parent_id)
-              AND (model_plan_id IS NULL AND OLD.model_plan_id IS NULL OR model_plan_id = OLD.model_plan_id)
+                modified_by = NEW.modified_by,
+                modified_dts = NEW.modified_dts
+            WHERE ((parent_id IS NULL AND OLD.parent_id IS NULL) OR (parent_id = OLD.parent_id))
+              AND ((model_plan_id IS NULL AND OLD.model_plan_id IS NULL) OR (model_plan_id = OLD.model_plan_id))
               AND position > OLD.position
               AND id != NEW.id;  -- Exclude the updated row
             
             -- Handle insertion of the category into the new parent
             UPDATE mto_category
             SET position = position - 1,
-            modified_by = NEW.modified_by,
-            modified_dts = NEW.modified_dts
-            WHERE (parent_id IS NULL AND NEW.parent_id IS NULL OR parent_id = NEW.parent_id)
-              AND (model_plan_id IS NULL AND NEW.model_plan_id IS NULL OR model_plan_id = NEW.model_plan_id)
-              AND position >= NEW.position
-              AND id != NEW.id;  -- Exclude the updated row
-        ELSIF OLD.parent_id IS NOT DISTINCT FROM NEW.parent_id OR OLD.model_plan_id IS NOT DISTINCT FROM NEW.model_plan_id THEN
-            -- Handle the case where the position is changed without changing parent_id or model_plan_id
-            IF NEW.position > OLD.position THEN
-                UPDATE mto_category
-                SET position = position - 1,
                 modified_by = NEW.modified_by,
                 modified_dts = NEW.modified_dts
-                WHERE (parent_id IS NULL AND NEW.parent_id IS NULL OR parent_id = NEW.parent_id)
-                  AND (model_plan_id IS NULL AND NEW.model_plan_id IS NULL OR model_plan_id = NEW.model_plan_id)
+            WHERE ((parent_id IS NULL AND NEW.parent_id IS NULL) OR (parent_id = NEW.parent_id))
+              AND ((model_plan_id IS NULL AND NEW.model_plan_id IS NULL) OR (model_plan_id = NEW.model_plan_id))
+              AND position >= NEW.position
+              AND id != NEW.id;  -- Exclude the updated row
+
+        ELSIF OLD.parent_id IS NOT DISTINCT FROM NEW.parent_id AND OLD.model_plan_id IS NOT DISTINCT FROM NEW.model_plan_id THEN
+            -- Handle the case where the position is changed without changing parent_id or model_plan_id
+            /* The category moved down */
+            IF position_adjustment = -1 THEN
+                UPDATE mto_category
+                SET position = position + position_adjustment,
+                    modified_by = NEW.modified_by,
+                    modified_dts = NEW.modified_dts
+                WHERE ((parent_id IS NULL AND NEW.parent_id IS NULL) OR (parent_id = NEW.parent_id))
+                  AND ((model_plan_id IS NULL AND NEW.model_plan_id IS NULL) OR (model_plan_id = NEW.model_plan_id))
                   AND position > OLD.position AND position <= NEW.position
                   AND id != NEW.id;  -- Exclude the updated row
-            ELSIF NEW.position < OLD.position THEN
+            ELSIF position_adjustment = 1 THEN
                 UPDATE mto_category
-                SET position = position + 1,
-            modified_by = NEW.modified_by,
-            modified_dts = NEW.modified_dts
-                WHERE (parent_id IS NULL AND NEW.parent_id IS NULL OR parent_id = NEW.parent_id)
-                  AND (model_plan_id IS NULL AND NEW.model_plan_id IS NULL OR model_plan_id = NEW.model_plan_id)
+                SET position = position + position_adjustment,
+                    modified_by = NEW.modified_by,
+                    modified_dts = NEW.modified_dts
+                WHERE ((parent_id IS NULL AND NEW.parent_id IS NULL) OR (parent_id = NEW.parent_id))
+                  AND ((model_plan_id IS NULL AND NEW.model_plan_id IS NULL) OR (model_plan_id = NEW.model_plan_id))
                   AND position < OLD.position AND position >= NEW.position
                   AND id != NEW.id;  -- Exclude the updated row
             END IF;
@@ -74,8 +92,8 @@ BEGIN
         SET position = position - 1,
             modified_by = OLD.modified_by,
             modified_dts = OLD.modified_dts
-        WHERE (parent_id IS NULL AND OLD.parent_id IS NULL OR parent_id = OLD.parent_id)
-          AND (model_plan_id IS NULL AND OLD.model_plan_id IS NULL OR model_plan_id = OLD.model_plan_id)
+        WHERE ((parent_id IS NULL AND OLD.parent_id IS NULL) OR (parent_id = OLD.parent_id))
+          AND ((model_plan_id IS NULL AND OLD.model_plan_id IS NULL) OR (model_plan_id = OLD.model_plan_id))
           AND position > OLD.position
           AND id != OLD.id;  -- Exclude the deleted row
     END IF;
@@ -83,7 +101,6 @@ BEGIN
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
-
 
 
 CREATE TRIGGER update_position_trigger
