0e7339ac61914709a1f104187a3169c0ff93ad30
Steven Wade <steven.wade@oddball.io>
2024-11-08 12:23:12 -0500

    feat: add assertion that categories in other models are unaffected by category re-order

--- pkg/graph/resolvers/mto_category_test.go
+++ pkg/graph/resolvers/mto_category_test.go
@@ -103,13 +103,19 @@ func (suite *ResolverSuite) TestMTOCategoryReordering() {
 	store := suite.testConfigs.Store
 	logger := suite.testConfigs.Logger
 
-	plan := suite.createModelPlan("testing category creation plan")
+	plan1 := suite.createModelPlan("testing category reorder plan 1")
+	plan2 := suite.createModelPlan("testing category reorder plan 2")
+	plan2CatName := "Category for plan 2"
+
+	plan2Cat, err := MTOCategoryCreate(ctx, logger, principal, store, plan2CatName, plan2.ID, nil)
+	suite.NoError(err)
+	suite.NotNil(plan2Cat)
 	// Make top level and sub categories
 	cat0Name := "Category 0"
 	cat0SubAName := "Category 0A"
 	cat0SubBName := "Category 0B"
 
-	category0, err := MTOCategoryCreate(ctx, logger, principal, store, cat0Name, plan.ID, nil)
+	category0, err := MTOCategoryCreate(ctx, logger, principal, store, cat0Name, plan1.ID, nil)
 	suite.NoError(err)
 	// Assert all fields are as expected for first model
 	suite.Equal(0, category0.Position, "Categories should be added to the next available position")
@@ -119,43 +125,43 @@ func (suite *ResolverSuite) TestMTOCategoryReordering() {
 	suite.Nil(category0.ParentID)
 	suite.Nil(category0.ModifiedBy)
 	suite.Nil(category0.ModifiedDts)
-	suite.EqualValues(plan.ID, category0.ModelPlanID)
+	suite.EqualValues(plan1.ID, category0.ModelPlanID)
 
-	category0SubA, err := MTOCategoryCreate(ctx, logger, principal, store, cat0SubAName, plan.ID, &category0.ID)
+	category0SubA, err := MTOCategoryCreate(ctx, logger, principal, store, cat0SubAName, plan1.ID, &category0.ID)
 	suite.NoError(err)
 	suite.Equal(0, category0SubA.Position, "Categories should be added to the next available position")
 	if suite.NotNil(category0SubA.ParentID) {
 		suite.EqualValues(category0.ID, *category0SubA.ParentID)
 	}
-	suite.EqualValues(plan.ID, category0SubA.ModelPlanID)
+	suite.EqualValues(plan1.ID, category0SubA.ModelPlanID)
 
-	category0SubB, err := MTOCategoryCreate(ctx, logger, principal, store, cat0SubBName, plan.ID, &category0.ID)
+	category0SubB, err := MTOCategoryCreate(ctx, logger, principal, store, cat0SubBName, plan1.ID, &category0.ID)
 	suite.NoError(err)
 	suite.Equal(1, category0SubB.Position, "Categories should be added to the next available position")
 	if suite.NotNil(category0SubB.ParentID) {
 		suite.EqualValues(category0.ID, *category0SubB.ParentID)
 	}
-	suite.EqualValues(plan.ID, category0SubB.ModelPlanID)
+	suite.EqualValues(plan1.ID, category0SubB.ModelPlanID)
 
 	cat1Name := "Category 1"
 	cat1SubAName := "Category 1A"
 	cat1SubBName := "Category 1B"
 
-	category1, err := MTOCategoryCreate(ctx, logger, principal, store, cat1Name, plan.ID, nil)
+	category1, err := MTOCategoryCreate(ctx, logger, principal, store, cat1Name, plan1.ID, nil)
 	suite.NoError(err)
 	//Second top level category
 	suite.Equal(1, category1.Position, "Categories should be added to the next available position")
 
-	category1SubA, err := MTOCategoryCreate(ctx, logger, principal, store, cat1SubAName, plan.ID, &category1.ID)
+	category1SubA, err := MTOCategoryCreate(ctx, logger, principal, store, cat1SubAName, plan1.ID, &category1.ID)
 	suite.NoError(err)
 	suite.Equal(0, category1SubA.Position, "Categories should be added to the next available position")
-	category1SubB, err := MTOCategoryCreate(ctx, logger, principal, store, cat1SubBName, plan.ID, &category1.ID)
+	category1SubB, err := MTOCategoryCreate(ctx, logger, principal, store, cat1SubBName, plan1.ID, &category1.ID)
 	suite.NoError(err)
 	suite.Equal(1, category1SubB.Position, "Categories should be added to the next available position")
 
 	cat2Name := "Category 2"
 
-	category2, err := MTOCategoryCreate(ctx, logger, principal, store, cat2Name, plan.ID, nil)
+	category2, err := MTOCategoryCreate(ctx, logger, principal, store, cat2Name, plan1.ID, nil)
 	suite.NoError(err)
 	//Third top level category
 	suite.Equal(2, category2.Position, "Categories should be added to the next available position")
@@ -167,27 +173,44 @@ func (suite *ResolverSuite) TestMTOCategoryReordering() {
 	suite.EqualValues(0, category2To0.Position)
 
 	// this returns uncategorized as well, expect 4
-	topCategories, err := MTOCategoryGetByModelPlanIDLOADER(ctx, plan.ID)
+	topCategoriesModel1, err := MTOCategoryGetByModelPlanIDLOADER(ctx, plan1.ID)
 	suite.NoError(err)
-	suite.NotNil(topCategories)
-	//TODO (mto), remove in favor of explicit checking?
-	// for index, cat := range topCategories {
-	// 	suite.EqualValues(index, cat.Position)
-	// }
-	suite.Len(topCategories, 4)
+	suite.NotNil(topCategoriesModel1)
+	suite.Len(topCategoriesModel1, 4)
+	//Assert the categories are as expected and in the expected position  (and returned in order by position)
 	// Assert 0 to 1
-	category0To1 := topCategories[1]
+	category0To1 := topCategoriesModel1[1]
 	suite.Equal(category0.ID, category0To1.ID)
 	suite.EqualValues(1, category0To1.Position)
 	//Assert 1 to 2
-	category1To2 := topCategories[2]
+	category1To2 := topCategoriesModel1[2]
 	suite.Equal(category1.ID, category1To2.ID)
 	suite.EqualValues(2, category1To2.Position)
 
-	uncategorized := topCategories[3]
+	//
+	uncategorized := topCategoriesModel1[3]
 	suite.Equal(uuid.Nil, uncategorized.ID)
-
+	suite.True(uncategorized.IsUncategorized())
 	// TODO (mto) should we add a position to the uncategorized one?
 	// suite.EqualValues(2, uncategorized.Position)
+
+	// Assert other models not affected
+	// this returns uncategorized as well, expect 4
+	topCategoriesModel2, err := MTOCategoryGetByModelPlanIDLOADER(ctx, plan2.ID)
+	suite.NoError(err)
+	suite.NotNil(topCategoriesModel2)
+	suite.Len(topCategoriesModel2, 2)
+	//Assert the categories are as expected and in the expected position  (and returned in order by position)
+	suite.EqualValues(0, topCategoriesModel2[0].Position)
+	suite.EqualValues(plan2Cat.ID, topCategoriesModel2[0].ID)
+	uncategorizedModelPlan2 := topCategoriesModel2[1]
+	suite.Equal(uuid.Nil, uncategorizedModelPlan2.ID)
+	suite.True(uncategorizedModelPlan2.IsUncategorized())
+
+	// Child categories are unaffected
+	category0To1SubCategories, err := MTOSubcategoryGetByParentIDLoader(ctx, plan1.ID, category0To1.ID)
+	suite.NoError(err)
+	suite.NotNil(category0To1SubCategories)
+	suite.Len(category0To1SubCategories, 3)
 
 }
