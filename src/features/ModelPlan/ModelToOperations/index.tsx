5c11a7776966c02af54f0481aebdaf854d6d1fff
Patrick Segura <patrick.segura@oddball.io>
2024-11-08 14:28:01 -0600

    Fixed linting errors

--- src/features/ModelPlan/ModelToOperations/index.tsx
+++ src/features/ModelPlan/ModelToOperations/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Redirect, Route, Switch, useParams } from 'react-router-dom';
 import { Grid, Icon } from '@trussworks/react-uswds';
