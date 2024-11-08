8a331157a57efa7afeb49d00cb791ea5dc358ad3
Steven Wade <steven.wade@oddball.io>
2024-11-08 10:02:09 -0500

    feat: basic implementation of reorder test

--- pkg/graph/resolvers/mto_category_test.go
+++ pkg/graph/resolvers/mto_category_test.go
@@ -98,3 +98,64 @@ func (suite *ResolverSuite) TestMTOCategoryRename() {
 	suite.EqualValues(plan.ID, renamedCategory.ModelPlanID)
 
 }
+
+func (suite *ResolverSuite) TestMTOCategoryReordering() {
+
+	plan := suite.createModelPlan("testing category creation plan")
+	// Make top level and sub categories
+	cat0Name := "Category 0"
+	cat0SubAName := "Category 0A"
+	cat0SubBName := "Category 0B"
+
+	category0, err := MTOCategoryCreate(suite.testConfigs.Context, suite.testConfigs.Logger, suite.testConfigs.Principal, suite.testConfigs.Store, cat0Name, plan.ID, nil)
+	suite.NoError(err)
+	// Assert all fields are as expected for first model
+	suite.Equal(0, category0.Position, "Categories should be added to the next available position")
+	suite.Equal(cat0Name, category0.Name)
+	suite.Equal(suite.testConfigs.Principal.UserAccount.ID, category0.CreatedBy)
+	suite.NotNil(category0.CreatedDts)
+	suite.Nil(category0.ParentID)
+	suite.Nil(category0.ModifiedBy)
+	suite.Nil(category0.ModifiedDts)
+	suite.EqualValues(plan.ID, category0.ModelPlanID)
+
+	category0SubA, err := MTOCategoryCreate(suite.testConfigs.Context, suite.testConfigs.Logger, suite.testConfigs.Principal, suite.testConfigs.Store, cat0SubAName, plan.ID, &category0.ID)
+	suite.NoError(err)
+	suite.Equal(0, category0SubA.Position, "Categories should be added to the next available position")
+	if suite.NotNil(category0SubA.ParentID) {
+		suite.EqualValues(category0.ID, *category0SubA.ParentID)
+	}
+	suite.EqualValues(plan.ID, category0SubA.ModelPlanID)
+
+	category0SubB, err := MTOCategoryCreate(suite.testConfigs.Context, suite.testConfigs.Logger, suite.testConfigs.Principal, suite.testConfigs.Store, cat0SubBName, plan.ID, &category0.ID)
+	suite.NoError(err)
+	suite.Equal(1, category0SubB.Position, "Categories should be added to the next available position")
+	if suite.NotNil(category0SubB.ParentID) {
+		suite.EqualValues(category0.ID, *category0SubB.ParentID)
+	}
+	suite.EqualValues(plan.ID, category0SubB.ModelPlanID)
+
+	cat1Name := "Category 1"
+	cat1SubAName := "Category 1A"
+	cat1SubBName := "Category 1B"
+
+	category1, err := MTOCategoryCreate(suite.testConfigs.Context, suite.testConfigs.Logger, suite.testConfigs.Principal, suite.testConfigs.Store, cat1Name, plan.ID, nil)
+	suite.NoError(err)
+	//Second top level category
+	suite.Equal(1, category1.Position, "Categories should be added to the next available position")
+
+	category1SubA, err := MTOCategoryCreate(suite.testConfigs.Context, suite.testConfigs.Logger, suite.testConfigs.Principal, suite.testConfigs.Store, cat1SubAName, plan.ID, &category1.ID)
+	suite.NoError(err)
+	suite.Equal(0, category1SubA.Position, "Categories should be added to the next available position")
+	category1SubB, err := MTOCategoryCreate(suite.testConfigs.Context, suite.testConfigs.Logger, suite.testConfigs.Principal, suite.testConfigs.Store, cat1SubBName, plan.ID, &category1.ID)
+	suite.NoError(err)
+	suite.Equal(1, category1SubB.Position, "Categories should be added to the next available position")
+
+	cat2Name := "Category 2"
+
+	category2, err := MTOCategoryCreate(suite.testConfigs.Context, suite.testConfigs.Logger, suite.testConfigs.Principal, suite.testConfigs.Store, cat2Name, plan.ID, nil)
+	suite.NoError(err)
+	//Third top level category
+	suite.Equal(2, category2.Position, "Categories should be added to the next available position")
+
+}
