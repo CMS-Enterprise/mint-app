f74f2e6446f8bb75e2c029716fdcc0f69d9d9538
Steven Wade <steven.wade@oddball.io>
2024-11-14 09:46:58 -0500

    feat: update trigger to account for parent changes

--- migrations/V188__Add_MTO_Category_Reorder_Trigger.sql
+++ migrations/V188__Add_MTO_Category_Reorder_Trigger.sql
@@ -29,17 +29,50 @@ BEGIN
     
     -- Handle Update: Reorder positions when a row's position is changed 
     ELSIF TG_OP = 'UPDATE' THEN
-        -- Determine if record moved up or down
-        IF NEW.position > OLD.position THEN
-            position_adjustment := -1;
-        ELSE
-            position_adjustment := 1;
-        END IF;
+    IF OLD.model_plan_id IS DISTINCT FROM NEW.model_plan_id THEN
+        RAISE EXCEPTION 'updating model_plan_id is not allowed. Caught in trigger function update_position_based_on_parent_and_model_plan';
+    END IF;
 
  /* Prevent updates to model_plan_id and parent_id as these are fixed for each category */
-        IF OLD.parent_id IS DISTINCT FROM NEW.parent_id AND OLD.model_plan_id IS DISTINCT FROM NEW.model_plan_id THEN
-        RAISE EXCEPTION 'updating model_plan_id or parent_id is not allows. Caught in trigger function update_position_based_on_parent_and_model_plan';
-        ELSIF OLD.parent_id IS NOT DISTINCT FROM NEW.parent_id AND OLD.model_plan_id IS NOT DISTINCT FROM NEW.model_plan_id THEN
+        IF OLD.parent_id IS DISTINCT FROM NEW.parent_id THEN
+        /*
+        Handle the old parent changes like a delete, move all lower categories up one for old parent
+        */
+        UPDATE mto_category
+        SET position = position - 1,
+            modified_by = NEW.modified_by,
+            modified_dts = NEW.modified_dts
+        WHERE ((parent_id IS NULL AND OLD.parent_id IS NULL) OR (parent_id = OLD.parent_id))
+          AND ((model_plan_id IS NULL AND OLD.model_plan_id IS NULL) OR (model_plan_id = OLD.model_plan_id))
+          AND position > OLD.position
+          AND id != OLD.id;  -- Exclude the deleted row
+
+          /* 
+          Handle the new row position like an insert
+          */
+
+        -- Move categories with the same parent_id and model_plan_id that have a position >= NEW.position
+        position_adjustment := 1;  -- For insert, we increase positions of conflicting rows
+        UPDATE mto_category
+        SET position = position + position_adjustment,
+            modified_by = NEW.modified_by,
+            modified_dts = NEW.modified_dts
+        WHERE ((parent_id IS NULL AND NEW.parent_id IS NULL) OR (parent_id = NEW.parent_id))
+          AND ((model_plan_id IS NULL AND NEW.model_plan_id IS NULL) OR (model_plan_id = NEW.model_plan_id))
+          AND position >= NEW.position
+          AND id != NEW.id;  -- Exclude the newly inserted row
+
+        -- Make this a distinct check to see if position has changed at all. If not, don't trigger this.
+        ELSIF OLD.parent_id IS NOT DISTINCT FROM NEW.parent_id  THEN
+            -- If the position is unchanged, no further action is required    
+            IF NEW.position = OLD.position THEN
+                RETURN NEW;
+            -- Determine if record moved up or down
+            ELSIF NEW.position > OLD.position THEN
+                position_adjustment := -1;
+            ELSE
+                position_adjustment := 1;
+            END IF;
             IF position_adjustment = -1 THEN
             /* Row moved down: Shift rows in the range [OLD.position + 1, NEW.position] up by 1 */
                 UPDATE mto_category
