d921a16ebd584de3b07118a60e0e35e8e1953e44
Steven Wade <steven.wade@oddball.io>
2024-11-18 14:50:30 -0500

    feat: first pass at get by id data loader for infor

---
+++ pkg/storage/loaders/mto_info.go
@@ -0,0 +1,44 @@
+package loaders
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+
+	"github.com/cms-enterprise/mint-app/pkg/appcontext"
+	"github.com/cms-enterprise/mint-app/pkg/models"
+	"github.com/cms-enterprise/mint-app/pkg/storage"
+
+	"github.com/graph-gophers/dataloader/v7"
+)
+
+// mtoInfoLoaders is a struct that holds LoaderWrappers related to MTO Infos
+type mtoInfoLoaders struct {
+
+	// ByIDOrModelPlanID Gets a list of mto Info records associated with a model plan by the supplied model plan id (which is the same as the id).
+	ByIDOrModelPlanID LoaderWrapper[uuid.UUID, *models.MTOInfo]
+}
+
+// MTOInfo is the singleton instance of all LoaderWrappers related to MTO Infos
+var MTOInfo = &mtoInfoLoaders{
+	ByIDOrModelPlanID: NewLoaderWrapper(batchMTOInfoGetByModelPlanID),
+}
+
+// batchMTOInfoGetByModelPlanID combines all provided keys and sorts and returns the results from the data loader
+func batchMTOInfoGetByModelPlanID(ctx context.Context, modelPlanIDs []uuid.UUID) []*dataloader.Result[*models.MTOInfo] {
+	loaders, err := Loaders(ctx)
+	logger := appcontext.ZLogger(ctx)
+	if err != nil {
+		return errorPerEachKey[uuid.UUID, *models.MTOInfo](modelPlanIDs, err)
+	}
+	data, err := storage.MTOInfoGetByIDOrModelPlanIDLoader(loaders.DataReader.Store, logger, modelPlanIDs)
+	if err != nil {
+		return errorPerEachKey[uuid.UUID, *models.MTOInfo](modelPlanIDs, err)
+	}
+	getKeyFunc := func(data *models.MTOInfo) uuid.UUID {
+		return data.ID
+	}
+
+	// sort and return the result
+	return oneToOneDataLoader(modelPlanIDs, data, getKeyFunc)
+}
