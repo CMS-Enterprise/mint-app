9c6e7fa8589daa78b1b43c862bb7d4568c6902e1
Steven Wade <steven.wade@oddball.io>
2024-11-21 14:59:28 -0500

    feat: adjust postman, and sql queries

--- pkg/sqlqueries/SQL/mto/common_milestone/get_by_model_plan_id_LOADER.sql
+++ pkg/sqlqueries/SQL/mto/common_milestone/get_by_model_plan_id_LOADER.sql
@@ -8,7 +8,6 @@ SELECT
     mto_common_milestone.key,
     mto_common_milestone.category_name,
     mto_common_milestone.sub_category_name,
-    mto_common_milestone.description,
     mto_common_milestone.facilitated_by_role,
     mto_common_milestone.section,
     -- These are fields in the table we may choose not to expose in the app
