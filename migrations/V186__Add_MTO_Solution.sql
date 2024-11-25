cca3050c91cd2d47ed514c171ebd79e64b08f9d2
Steven Wade <steven.wade@oddball.io>
2024-11-25 15:05:59 -0500

    feat: make milestone and solution take an array of enum for facilitated by

--- migrations/V186__Add_MTO_Solution.sql
+++ migrations/V186__Add_MTO_Solution.sql
@@ -18,7 +18,7 @@ CREATE TABLE mto_solution(
     name ZERO_STRING,
     -- we allow null because this is will be from the commonSolution table if it exists
     type MTO_SOLUTION_TYPE,
-    facilitated_by MTO_FACILITATOR,
+    facilitated_by MTO_FACILITATOR[],
     needed_by TIMESTAMP WITH TIME ZONE,
     status MTO_SOLUTION_STATUS NOT NULL,
     risk_indicator MTO_RISK_INDICATOR NOT NULL DEFAULT 'ON_TRACK',
