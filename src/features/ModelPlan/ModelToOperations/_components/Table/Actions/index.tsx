7d1549312a59fcf84d9d5bfafed3ec35206113aa
Gary Zhao <gary.j.zhao@gmail.com>
2024-12-04 14:04:33 -0500

    add the milestone buttons

--- src/features/ModelPlan/ModelToOperations/_components/Table/Actions/index.tsx
+++ src/features/ModelPlan/ModelToOperations/_components/Table/Actions/index.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
-import { Button } from '@trussworks/react-uswds';
+import { useTranslation } from 'react-i18next';
+import { Button, Icon } from '@trussworks/react-uswds';
 
 import useMessage from 'hooks/useMessage';
 
 import MTOModal from '../../Modal';
 
 const MTOTableActions = () => {
+  const { t } = useTranslation('modelToOperationsMisc');
+
   const { clearMessage } = useMessage();
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [actionsMenuOpen, setActionsMenuOpen] = useState(true);
 
   const [modalType, setModalType] = useState<
     'category' | 'milestone' | 'solution'
@@ -21,40 +25,101 @@ const MTOTableActions = () => {
         closeModal={() => setIsModalOpen(false)}
         modalType={modalType}
       />
-      <div className="margin-top-4">
-        <div className="width-fit-content">
+      <div className="border-1px radius-md border-gray-10 padding-3">
+        <div className="action-bar display-flex">
+          <p className="margin-y-0 text-bold">
+            {t('table.tableActions.tableActions')}
+          </p>
+          <div className="margin-x-2 padding-y-05">
+            <div className="border-right-2px border-base-light height-full" />
+          </div>
           <Button
             type="button"
-            onClick={() => {
-              clearMessage();
-              setModalType('category');
-              setIsModalOpen(true);
-            }}
+            unstyled
+            onClick={() => setActionsMenuOpen(!actionsMenuOpen)}
           >
-            Add custom category
-          </Button>
-          <Button
-            type="button"
-            onClick={() => {
-              clearMessage();
-              setModalType('milestone');
-              setIsModalOpen(true);
-            }}
-          >
-            Add custom milestone
-          </Button>
-          <Button
-            type="button"
-            onClick={() => {
-              clearMessage();
-              setModalType('solution');
-              setIsModalOpen(true);
-            }}
-            className="margin-bottom-4"
-          >
-            Add custom solution
+            {actionsMenuOpen ? (
+              <>
+                {t('table.tableActions.hideActions')}
+                <Icon.ExpandLess />
+              </>
+            ) : (
+              <>
+                {t('table.tableActions.showActions')}
+                <Icon.ExpandMore />
+              </>
+            )}
           </Button>
         </div>
+        {actionsMenuOpen && (
+          <div className="action-buttons margin-top-3 display-flex">
+            <div className="width-fit-content">
+              <div className="display-flex flex-align-center">
+                <Icon.Flag
+                  className="margin-right-1 bg-green-5"
+                  style={{
+                    height: '20px',
+                    width: '20px',
+                    fill: '#607F35'
+                  }}
+                />
+                <p className="margin-y-0 text-bold">
+                  {t('table.tableActions.milestones')}
+                </p>
+              </div>
+              <p className="margin-top-0">
+                {t('table.tableActions.commonMilestones', { number: 31 })}
+              </p>
+
+              <Button
+                type="button"
+                className="display-block margin-bottom-1"
+                outline
+                onClick={() => {
+                  clearMessage();
+                  setModalType('milestone');
+                  setIsModalOpen(true);
+                }}
+              >
+                {t('table.tableActions.browseMilestoneLibrary')}
+              </Button>
+              <Button
+                type="button"
+                className="display-block"
+                unstyled
+                onClick={() => {
+                  clearMessage();
+                  setModalType('milestone');
+                  setIsModalOpen(true);
+                }}
+              >
+                {t('optionsCard.milestones.linkText')}
+              </Button>
+            </div>
+            {/* <Button
+              type="button"
+              onClick={() => {
+                clearMessage();
+                setModalType('category');
+                setIsModalOpen(true);
+              }}
+            >
+              Add custom category
+            </Button>
+
+            <Button
+              type="button"
+              onClick={() => {
+                clearMessage();
+                setModalType('solution');
+                setIsModalOpen(true);
+              }}
+              className="margin-bottom-4"
+            >
+              Add custom solution
+            </Button> */}
+          </div>
+        )}
       </div>
     </>
   );
