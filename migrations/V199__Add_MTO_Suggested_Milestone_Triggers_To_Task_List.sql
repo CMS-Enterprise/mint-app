85865df99bf866e8645aba752aa8b8c127fbe7b6
Steven Wade <steven.wade@oddball.io>
2024-12-06 10:25:54 -0500

    feat: add migration to add suggestions for existing models

---
+++ migrations/V199__Add_MTO_Suggested_Milestone_Triggers_To_Task_List.sql
@@ -0,0 +1,11 @@
+-- SELECT public.ADD_MTO_SUGGESTED_MILESTONE_SUGGESTION_TRIGGER('public', 'plan_basics'); -- no records currently are based on basics
+
+-- SELECT public.ADD_MTO_SUGGESTED_MILESTONE_SUGGESTION_TRIGGER('public', 'plan_beneficiaries');
+
+-- SELECT public.ADD_MTO_SUGGESTED_MILESTONE_SUGGESTION_TRIGGER('public', 'plan_general_characteristics');
+
+-- SELECT public.ADD_MTO_SUGGESTED_MILESTONE_SUGGESTION_TRIGGER('public', 'plan_ops_eval_and_learning');
+
+-- SELECT public.ADD_MTO_SUGGESTED_MILESTONE_SUGGESTION_TRIGGER('public', 'plan_participants_and_providers');
+
+-- SELECT public.ADD_MTO_SUGGESTED_MILESTONE_SUGGESTION_TRIGGER('public', 'plan_payments');
