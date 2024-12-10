2d6403e43f55f738e8ce2654c54edbff027abf71
Steven Wade <steven.wade@oddball.io>
2024-12-10 11:24:24 -0500

    feat: clarify unit test with comments

--- pkg/graph/resolvers/mto_milestone_test.go
+++ pkg/graph/resolvers/mto_milestone_test.go
@@ -260,22 +260,63 @@ func (suite *ResolverSuite) TestMTOMilestoneGetBySolutionIDLoader() {
 		models.MTOCSKApps,
 	})
 
-	// validate the created solutions
-	solutions, err := MTOSolutionGetByModelPlanIDLOADER(suite.testConfigs.Context, plan1.ID)
-	suite.NoError(err)
-	suite.Len(solutions, 2)
-	sol1 := solutions[0]
-	sol2 := solutions[1]
-
 	commonMilestoneKey2 := models.MTOCommonMilestoneKeyCommWPart
 	milestone2 := suite.createMilestoneCommon(plan1.ID, commonMilestoneKey2, []models.MTOCommonSolutionKey{
 		models.MTOCSKCcw,
 		models.MTOCSKApps,
 	})
+	commonMilestoneKey3 := models.MTOCommonMilestoneKeyAcquireALearnCont
+	milestone3 := suite.createMilestoneCommon(plan1.ID, commonMilestoneKey3, []models.MTOCommonSolutionKey{
+		models.MTOCSKBcda,
+		models.MTOCSKCmsBox,
+		models.MTOCSKCcw,
+	})
+	/***
+	Validate the created solutions and assign them by key
+	***/
+
+	solutions, err := MTOSolutionGetByModelPlanIDLOADER(suite.testConfigs.Context, plan1.ID)
+	suite.NoError(err)
+	suite.Len(solutions, 4)
+
+	solMTOCSKCcw, ok := lo.Find(solutions, func(solution *models.MTOSolution) bool {
+		if solution.Key != nil {
+			return *solution.Key == models.MTOCSKCcw
+		}
+		return false
+	})
+	suite.True(ok)
+	solMTOCSKApps, ok := lo.Find(solutions, func(solution *models.MTOSolution) bool {
+		if solution.Key != nil {
+			return *solution.Key == models.MTOCSKApps
+		}
+		return false
+	})
+	suite.True(ok)
+	solMTOCSKBcda, ok := lo.Find(solutions, func(solution *models.MTOSolution) bool {
+		if solution.Key != nil {
+			return *solution.Key == models.MTOCSKBcda
+		}
+		return false
+	})
+	suite.True(ok)
+	solMTOCSKCmsBox, ok := lo.Find(solutions, func(solution *models.MTOSolution) bool {
+		if solution.Key != nil {
+			return *solution.Key == models.MTOCSKCmsBox
+		}
+		return false
+	})
+	suite.True(ok)
+	/***
+	end select solutions
+	***/
 
 	expectedResults := []loaders.KeyAndExpected[uuid.UUID, []uuid.UUID]{
-		{Key: sol1.ID, Expected: []uuid.UUID{milestone1.ID, milestone2.ID}},
-		{Key: sol2.ID, Expected: []uuid.UUID{milestone1.ID, milestone2.ID}},
+		// All three milestones use this solution
+		{Key: solMTOCSKCcw.ID, Expected: []uuid.UUID{milestone1.ID, milestone2.ID, milestone3.ID}},
+		{Key: solMTOCSKApps.ID, Expected: []uuid.UUID{milestone1.ID, milestone2.ID}},
+		{Key: solMTOCSKBcda.ID, Expected: []uuid.UUID{milestone3.ID}},
+		{Key: solMTOCSKCmsBox.ID, Expected: []uuid.UUID{milestone3.ID}},
 	}
 
 	verifyFunc := func(data []*models.MTOMilestone, expected []uuid.UUID) bool {
@@ -285,6 +326,7 @@ func (suite *ResolverSuite) TestMTOMilestoneGetBySolutionIDLoader() {
 		})
 		return suite.ElementsMatch(dataIDs, expected)
 	}
+	// Call the helper method to validate all results
 	loaders.VerifyLoaders[uuid.UUID, []*models.MTOMilestone, []uuid.UUID](suite.testConfigs.Context, &suite.Suite, loaders.MTOMilestone.BySolutionID,
 		expectedResults, verifyFunc)
 
