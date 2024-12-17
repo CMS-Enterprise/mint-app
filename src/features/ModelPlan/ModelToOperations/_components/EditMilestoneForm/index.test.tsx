bc1619e5eab7a087b1966351b8f6d38f0f3d6e9e
Patrick Segura <patrick.segura@oddball.io>
2024-12-17 09:55:19 -0600

    Added unit test

--- src/features/ModelPlan/ModelToOperations/_components/EditMilestoneForm/index.test.tsx
+++ src/features/ModelPlan/ModelToOperations/_components/EditMilestoneForm/index.test.tsx
@@ -1,42 +1,43 @@
 import React from 'react';
 import { MemoryRouter, Route } from 'react-router-dom';
-import { render } from '@testing-library/react';
-import { categoryMock, milestoneMock } from 'tests/mock/mto';
-import { modelID } from 'tests/mock/readonly';
-import VerboseMockedProvider from 'tests/MockedProvider';
+import { MockedProvider } from '@apollo/client/testing';
+import { render, screen, waitFor } from '@testing-library/react';
+import { categoryMock, milestoneMock, modelID } from 'tests/mock/mto';
 
 import MessageProvider from 'contexts/MessageContext';
 
 import EditMilestoneForm from '.';
 
-describe('Custom Catergory form', () => {
+describe('EditMilestoneForm', () => {
   it('matches snapshot', async () => {
     const { asFragment } = render(
-      <MemoryRouter initialEntries={[`/models/${modelID}/`]}>
+      <MemoryRouter
+        initialEntries={[
+          `/models/${modelID}/collaboration-area/model-to-operations/matrix?view=milestones&edit-milestone=123`
+        ]}
+      >
         <MessageProvider>
-          <VerboseMockedProvider
+          <MockedProvider
             mocks={[...milestoneMock, ...categoryMock]}
             addTypename={false}
           >
-            <Route path="/models/:modelID/collaboration-area/model-to-operations/matrix?edit-milestone=123">
+            <Route path="/models/:modelID/collaboration-area/model-to-operations/matrix">
               <EditMilestoneForm
                 closeModal={() => {}}
                 setIsDirty={() => {}}
                 submitted={{ current: false }}
               />
             </Route>
-          </VerboseMockedProvider>
+          </MockedProvider>
         </MessageProvider>
       </MemoryRouter>
     );
 
-    // await waitFor(() => {
-    //   expect(
-    //     screen.getByText(
-    //       'Choose a primary category if you are adding a sub-category, or choose "None" if you are adding a primary category.'
-    //     )
-    //   ).toBeInTheDocument();
-    // });
+    await waitFor(() => {
+      expect(screen.getByText('Milestone 1')).toBeInTheDocument();
+
+      expect(screen.getByText('Suggested')).toBeInTheDocument();
+    });
 
     expect(asFragment()).toMatchSnapshot();
   });
