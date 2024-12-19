c11c204297fa273ca7af497b65979f434fa3f4e0
Steven Wade <steven.wade@oddball.io>
2024-12-19 13:30:01 -0500

    feat: insert common solutions allow conflicts in batched manner

--- pkg/sqlqueries/SQL/mto/solution/create_common_solutions_allow_conflicts.sql
+++ pkg/sqlqueries/SQL/mto/solution/create_common_solutions_allow_conflicts.sql
@@ -34,22 +34,55 @@ insert_attempt AS ( --noqa
         created_dts,
         modified_by,
         modified_dts
+),
+
+JOINED AS (
+    SELECT
+        id,
+        model_plan_id,
+        mto_common_solution_key,
+        name,
+        type,
+        facilitated_by,
+        needed_by,
+        status,
+        risk_indicator,
+        poc_name,
+        poc_email,
+        created_by
+    FROM insert_attempt
+    UNION ALL
+    SELECT
+        mto_solution.id,
+        mto_solution.model_plan_id,
+        mto_solution.mto_common_solution_key,
+        mto_solution.name,
+        mto_solution.type,
+        mto_solution.facilitated_by,
+        mto_solution.needed_by,
+        mto_solution.status,
+        mto_solution.risk_indicator,
+        mto_solution.poc_name,
+        mto_solution.poc_email,
+        mto_solution.created_by
+    FROM mto_solution
+    JOIN data_to_insert ON data_to_insert.model_plan_id = mto_solution.model_plan_id AND data_to_insert.mto_common_solution_key = mto_solution.mto_common_solution_key
 )
 
--- Join and return inserted and existing data
+-- Left Join on common solution for name and type info and return inserted and existing data
 SELECT
-    mto_solution.id,
-    mto_solution.model_plan_id,
-    mto_solution.mto_common_solution_key,
-    COALESCE(mto_solution.name, mto_common_solution.name) AS "name",
-    COALESCE(mto_solution.type, mto_common_solution.type) AS "type",
-    mto_solution.facilitated_by,
-    mto_solution.needed_by,
-    mto_solution.status,
-    mto_solution.risk_indicator,
-    mto_solution.poc_name,
-    mto_solution.poc_email,
-    mto_solution.created_by
-FROM mto_solution
-JOIN data_to_insert ON data_to_insert.model_plan_id = mto_solution.model_plan_id AND data_to_insert.mto_common_solution_key = mto_solution.mto_common_solution_key
-LEFT JOIN mto_common_solution ON mto_solution.mto_common_solution_key = mto_common_solution.key
+    JOINED.id,
+    JOINED.model_plan_id,
+    JOINED.mto_common_solution_key,
+    COALESCE(JOINED.name, mto_common_solution.name) AS "name",
+    COALESCE(JOINED.type, mto_common_solution.type) AS "type",
+    JOINED.facilitated_by,
+    JOINED.needed_by,
+    JOINED.status,
+    JOINED.risk_indicator,
+    JOINED.poc_name,
+    JOINED.poc_email,
+    JOINED.created_by
+FROM JOINED
+JOIN data_to_insert ON data_to_insert.model_plan_id = JOINED.model_plan_id AND data_to_insert.mto_common_solution_key = JOINED.mto_common_solution_key
+LEFT JOIN mto_common_solution ON JOINED.mto_common_solution_key = mto_common_solution.key
