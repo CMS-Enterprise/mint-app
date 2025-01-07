513383b4f82f5581d993b9f3be906d99db383821
Steven Wade <steven.wade@oddball.io>
2025-01-07 09:38:51 -0600

    feat: category table inserts all fields on insert

--- migrations/V203__MTO_Tables_Enable_Auditing.sql
+++ migrations/V203__MTO_Tables_Enable_Auditing.sql
@@ -1,5 +1,5 @@
 --  TODO, do we need to include other FKey fields? Do we need to include all fields on create since this can be deleted?
-SELECT audit.AUDIT_TABLE('public', 'mto_category', 'id', 'model_plan_id', '{created_by,created_dts,modified_by,modified_dts}'::TEXT[], '{}'::TEXT[]);
+SELECT audit.AUDIT_TABLE('public', 'mto_category', 'id', 'model_plan_id', '{created_by,created_dts,modified_by,modified_dts}'::TEXT[], '{*}'::TEXT[]);
 SELECT audit.AUDIT_TABLE('public', 'mto_milestone', 'id', 'model_plan_id', '{created_by,created_dts,modified_by,modified_dts}'::TEXT[], '{}'::TEXT[]);
 SELECT audit.AUDIT_TABLE('public', 'mto_solution', 'id', 'model_plan_id', '{created_by,created_dts,modified_by,modified_dts}'::TEXT[], '{}'::TEXT[]);
 SELECT audit.AUDIT_TABLE('public', 'mto_milestone_solution_link', 'id', 'model_plan_id', '{created_by,created_dts,modified_by,modified_dts}'::TEXT[], '{}'::TEXT[]);
