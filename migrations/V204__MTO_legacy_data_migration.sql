b56e4695c11f324ce84f3b40af70454f8429c604
Steven Wade <steven.wade@oddball.io>
2025-01-13 15:15:01 -0600

    feat: gather need and solution data in CTES

--- migrations/V204__MTO_legacy_data_migration.sql
+++ migrations/V204__MTO_legacy_data_migration.sql
@@ -12,3 +12,36 @@ This migration migrates operational solution data to mto solution data. It does 
    a. solution sub tasks
    b. document solution links
 **/
+
+
+WITH needs AS (
+    SELECT 
+        need.id,
+        need.model_plan_id,
+        possible.need_key AS possible_need_type,
+        need.name_other
+        
+    FROM operational_need AS need 
+    LEFT JOIN possible_operational_need AS possible ON need.need_type = possible.id
+    WHERE need.needed = TRUE
+),
+
+solutions AS (
+    SELECT 
+        solution.operational_need_id,
+        need.model_plan_id,
+        possible.sol_key AS possible_solution_type,
+        solution.name_other,
+        solution.is_other,
+        solution.other_header
+        
+    FROM operational_solution AS solution
+    JOIN operational_need AS need ON solution.operational_need_id = need.id -- TODO should we disregard solutions that are for needs that are not needed? or still add any solutions that are needed? They might currently be hidden in the front end 
+    LEFT JOIN possible_operational_solution AS possible ON solution.solution_type = possible.id
+    WHERE solution.needed = TRUE
+)
+
+
+--SELECT * FROM needs;
+SELECT solutions.* FROM solutions
+LEFT JOIN needs ON solutions.operational_need_id = needs.id;
