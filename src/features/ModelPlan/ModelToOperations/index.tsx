5f6e3ff57a1fe7b2ca19ad9c0da08cd42cc54f7d
Gary Zhao <gary.j.zhao@gmail.com>
2025-01-14 13:45:23 -0500

    moved MTOModalProvider & MTOModal to parent file

--- src/features/ModelPlan/ModelToOperations/index.tsx
+++ src/features/ModelPlan/ModelToOperations/index.tsx
@@ -4,7 +4,9 @@ import { NotFoundPartial } from 'features/NotFound';
 
 import MainContent from 'components/MainContent';
 import ProtectedRoute from 'components/ProtectedRoute';
+import { MTOModalProvider } from 'contexts/MTOModalContext';
 
+import MTOModal from './_components/FormModal';
 import MTOHome from './Home';
 import MilestoneLibrary from './MilestoneLibrary';
 import SolutionLibrary from './SolutionLibrary';
@@ -14,33 +16,36 @@ import './index.scss';
 const ModelToOperations = () => {
   return (
     <MainContent className="mint-body-normal" data-testid="model-to-operations">
-      <Switch>
-        <ProtectedRoute
-          path="/models/:modelID/collaboration-area/model-to-operations/matrix"
-          component={MTOHome}
-          exact
-        />
+      <MTOModalProvider>
+        <MTOModal />
+        <Switch>
+          <ProtectedRoute
+            path="/models/:modelID/collaboration-area/model-to-operations/matrix"
+            component={MTOHome}
+            exact
+          />
 
-        <ProtectedRoute
-          path="/models/:modelID/collaboration-area/model-to-operations/milestone-library"
-          component={MilestoneLibrary}
-          exact
-        />
+          <ProtectedRoute
+            path="/models/:modelID/collaboration-area/model-to-operations/milestone-library"
+            component={MilestoneLibrary}
+            exact
+          />
 
-        <ProtectedRoute
-          path="/models/:modelID/collaboration-area/model-to-operations/solution-library"
-          component={SolutionLibrary}
-          exact
-        />
+          <ProtectedRoute
+            path="/models/:modelID/collaboration-area/model-to-operations/solution-library"
+            component={SolutionLibrary}
+            exact
+          />
 
-        <Redirect
-          exact
-          from="/models/:modelID/collaboration-area/model-to-operations"
-          to="/models/:modelID/collaboration-area/model-to-operations/matrix"
-        />
+          <Redirect
+            exact
+            from="/models/:modelID/collaboration-area/model-to-operations"
+            to="/models/:modelID/collaboration-area/model-to-operations/matrix"
+          />
 
-        <Route path="*" render={() => <NotFoundPartial />} />
-      </Switch>
+          <Route path="*" render={() => <NotFoundPartial />} />
+        </Switch>
+      </MTOModalProvider>
     </MainContent>
   );
 };
