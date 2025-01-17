87fbe3fa643c7b6f8f273bd8d31fe8572df18f93
Gary Zhao <gary.j.zhao@gmail.com>
2025-01-17 15:17:53 -0500

    moved button group to its own sepearate component

---
+++ src/features/ModelPlan/ModelToOperations/SolutionLibrary/_components/ViewSelector.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
+import { Button, ButtonGroup, Label, Select } from '@trussworks/react-uswds';
+
+import useCheckResponsiveScreen from 'hooks/useCheckMobile';
+
+import { SolutionCardType, SolutionViewType } from '..';
+
+const ViewSelector = ({
+  viewParam,
+  allSolutions,
+  itSystemsSolutions,
+  contractsSolutions,
+  crossCutSolutions
+}: {
+  viewParam: SolutionViewType;
+  allSolutions: SolutionCardType[];
+  itSystemsSolutions: SolutionCardType[];
+  contractsSolutions: SolutionCardType[];
+  crossCutSolutions: SolutionCardType[];
+}) => {
+  const { t } = useTranslation('modelToOperationsMisc');
+  const isTablet = useCheckResponsiveScreen('tablet', 'smaller');
+  const history = useHistory();
+  const params = new URLSearchParams(history.location.search);
+
+  return (
+    <>
+      {isTablet ? (
+        <div>
+          <Label htmlFor="select-view" className="text-normal maxw-none">
+            {t('documentsMisc:documentTable.view')}
+          </Label>
+          <Select id="select-view" name="select-view" className="maxw-none">
+            <option value="all">
+              {t('solutionLibrary.tabs.allSolutions', {
+                count: allSolutions.length
+              })}
+            </option>
+            <option value="it-systems">
+              {t('solutionLibrary.tabs.itSystems', {
+                count: itSystemsSolutions.length
+              })}
+            </option>
+            <option value="contracts">
+              {t('solutionLibrary.tabs.contracts', {
+                count: contractsSolutions.length
+              })}
+            </option>
+            <option value="cross-cut">
+              {t('solutionLibrary.tabs.crossCutting', {
+                count: crossCutSolutions.length
+              })}
+            </option>
+          </Select>
+        </div>
+      ) : (
+        <ButtonGroup type="segmented" className="margin-right-3">
+          <Button
+            type="button"
+            outline={viewParam !== 'all'}
+            onClick={() => {
+              params.set('view', 'all');
+              history.replace({ search: params.toString() });
+            }}
+          >
+            {t('solutionLibrary.tabs.allSolutions', {
+              count: allSolutions.length
+            })}
+          </Button>
+          <Button
+            type="button"
+            outline={viewParam !== 'it-systems'}
+            onClick={() => {
+              params.set('view', 'it-systems');
+              history.replace({ search: params.toString() });
+            }}
+          >
+            {t('solutionLibrary.tabs.itSystems', {
+              count: itSystemsSolutions.length
+            })}
+          </Button>
+          <Button
+            type="button"
+            outline={viewParam !== 'contracts'}
+            onClick={() => {
+              params.set('view', 'contracts');
+              history.replace({ search: params.toString() });
+            }}
+          >
+            {t('solutionLibrary.tabs.contracts', {
+              count: contractsSolutions.length
+            })}
+          </Button>
+          <Button
+            type="button"
+            outline={viewParam !== 'cross-cut'}
+            onClick={() => {
+              params.set('view', 'cross-cut');
+              history.replace({ search: params.toString() });
+            }}
+          >
+            {t('solutionLibrary.tabs.crossCutting', {
+              count: crossCutSolutions.length
+            })}
+          </Button>
+        </ButtonGroup>
+      )}
+    </>
+  );
+};
+
+export default ViewSelector;
