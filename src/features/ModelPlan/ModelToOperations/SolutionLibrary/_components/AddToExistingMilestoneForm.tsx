7189f11f09b5ca66165ef0199dda71e1220f24a4
Gary Zhao <gary.j.zhao@gmail.com>
2025-01-21 15:18:57 -0500

    query the milestones

--- src/features/ModelPlan/ModelToOperations/SolutionLibrary/_components/AddToExistingMilestoneForm.tsx
+++ src/features/ModelPlan/ModelToOperations/SolutionLibrary/_components/AddToExistingMilestoneForm.tsx
@@ -1,40 +1,60 @@
 import React from 'react';
-// import {
-//   Controller,
-//   FormProvider,
-//   SubmitHandler,
-//   useForm
-// } from 'react-hook-form';
+import {
+  // Controller,
+  FormProvider,
+  // SubmitHandler,
+  useForm
+} from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
-// import { useParams } from 'react-router-dom';
-// import { MtoCommonSolutionKey } from 'gql/generated/graphql';
+import { useParams } from 'react-router-dom';
+import {
+  MtoCommonSolutionKey,
+  useGetMtoAllMilestonesQuery
+} from 'gql/generated/graphql';
 
 // import useMessage from 'hooks/useMessage';
 
-// type FormValues = {
-//   linkedSolutions: MtoCommonSolutionKey[] | string[] | undefined;
-// };
+type FormValues = {
+  linkedSolutions: MtoCommonSolutionKey[] | string[] | undefined;
+};
 
 const AddToExistingMilestoneForm = () => {
   const { t } = useTranslation('modelToOperationsMisc');
 
-  // const { modelID } = useParams<{ modelID: string }>();
+  const { modelID } = useParams<{ modelID: string }>();
 
   // const { message, showMessage, clearMessage } = useMessage();
 
-  // Variables for the form
-  // const methods = useForm<FormValues>({
-  //   defaultValues: {
-  //     linkedSolutions: []
-  //   },
-  //   mode: 'onBlur'
-  // });
+  const { data } = useGetMtoAllMilestonesQuery({
+    variables: {
+      id: modelID
+    }
+  });
+
+  const commonMilestones = data?.modelPlan?.mtoMatrix?.commonMilestones;
+  const milestones = data?.modelPlan?.mtoMatrix?.milestones;
+  const allMilestones = [
+    ...(commonMilestones || []),
+    ...(milestones || [])
+  ].sort((a, b) => a.name.localeCompare(b.name));
+
+  console.log(allMilestones);
+
+  const methods = useForm<FormValues>({
+    defaultValues: {
+      linkedSolutions: []
+    },
+    mode: 'onBlur'
+  });
 
   return (
     <>
       <p className="mint-body-normal">
         {t('modal.addToExistingMilestone.description')}
       </p>
+      <FormProvider {...methods}>
+        <p>temp</p>
+      </FormProvider>
     </>
   );
 };
