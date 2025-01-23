653b616797092a7046e7ffc0f36e55b10ef2e49f
Gary Zhao <gary.j.zhao@gmail.com>
2025-01-23 11:34:16 -0500

    update tests and snaps to include 0 milestones warning alert

--- src/features/ModelPlan/ModelToOperations/SolutionLibrary/_components/AddToExistingMilestoneForm/AddToExistingMilestoneForm.test.tsx
+++ src/features/ModelPlan/ModelToOperations/SolutionLibrary/_components/AddToExistingMilestoneForm/AddToExistingMilestoneForm.test.tsx
@@ -37,4 +37,35 @@ describe('Custom Catergory form', () => {
 
     expect(asFragment()).toMatchSnapshot();
   });
+
+  it('shows warning message when zero milestones and matches snapshot', async () => {
+    // Set milestone array to empty
+    mtoMatrixMock[0].result.data.modelPlan.mtoMatrix.milestones = [];
+
+    const { asFragment } = render(
+      <MemoryRouter initialEntries={[`/models/${modelID}/`]}>
+        <MessageProvider>
+          <VerboseMockedProvider mocks={[...mtoMatrixMock]} addTypename={false}>
+            <Route path="/models/:modelID/">
+              <AddToExistingMilestoneForm
+                closeModal={() => {}}
+                solutionName="Test Solution"
+                solutionKey={MtoCommonSolutionKey.ACO_OS}
+              />
+            </Route>
+          </VerboseMockedProvider>
+        </MessageProvider>
+      </MemoryRouter>
+    );
+
+    await waitFor(() => {
+      expect(
+        screen.getByText(
+          'You have not yet added any milestones to your MTO. You may do so from the milestone library or by adding a custom milestone.'
+        )
+      ).toBeInTheDocument();
+    });
+
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
