0f2b875a31915e916ae2e67a78edfa7b641248da
Patrick Segura <patrick.segura@oddball.io>
2025-04-14 10:59:41 -0500

    Changed query name

--- src/gql/operations/ModelToOperations/GetGlobalMTOCommonMilestones.ts
+++ src/gql/operations/ModelToOperations/GetGlobalMTOCommonMilestones.ts
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export default gql(/* GraphQL */ `
-  query GetGlobalMTOCommonMilestones() {
+  query GetGlobalMTOCommonSolutions() {
     mtoCommonSolutions {
       name
       key
