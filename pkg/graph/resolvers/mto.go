75644e31afbe8379b0da3062e10cb9c48bb4a17e
Steven Wade <steven.wade@oddball.io>
2025-04-15 10:51:28 -0700

    chore: remove recent modification type

--- pkg/graph/resolvers/mto.go
+++ pkg/graph/resolvers/mto.go
@@ -11,7 +11,6 @@ import (
 	"github.com/cms-enterprise/mint-app/pkg/authentication"
 	"github.com/cms-enterprise/mint-app/pkg/models"
 	"github.com/cms-enterprise/mint-app/pkg/storage"
-	"github.com/cms-enterprise/mint-app/pkg/storage/loaders"
 )
 
 // MTOStatusGet returns the overall status of an MTO
@@ -58,46 +57,6 @@ func MTOMostRecentTranslatedAudit(ctx context.Context, store *storage.Store, log
 	}
 
 	return records[0], nil
-}
-
-// MTOLastUpdatedGet returns the most recent update to an MTO overall.
-func MTOLastUpdatedGet(ctx context.Context, modelPlanID uuid.UUID) (*models.RecentModification, error) {
-	// TODO  (mto) restructure this to use change history
-
-	// Call the loader directly so we don't get uncategorized included
-	categories, err := loaders.MTOCategory.ByModelPlanID.Load(ctx, modelPlanID)
-	if err != nil {
-		return nil, err
-	}
-	milestones, err := MTOMilestoneGetByModelPlanIDLOADER(ctx, modelPlanID)
-	if err != nil {
-		return nil, err
-	}
-	solutions, err := MTOSolutionGetByModelPlanIDLOADER(ctx, modelPlanID)
-	if err != nil {
-		return nil, err
-	}
-
-	var baseStructs []models.IBaseStruct
-	for _, category := range categories {
-		baseStructs = append(baseStructs, category)
-	}
-	for _, milestone := range milestones {
-		baseStructs = append(baseStructs, milestone)
-	}
-	for _, solution := range solutions {
-		baseStructs = append(baseStructs, solution)
-	}
-	mostRecentTime, mostRecentUserUUID := models.GetMostRecentTime(baseStructs)
-	if mostRecentUserUUID == uuid.Nil {
-		// There is no recent edit, nil is returned for the user
-		return nil, nil
-	}
-
-	recentModified := models.NewRecentModification(mostRecentUserUUID, mostRecentTime)
-
-	return &recentModified, nil
-
 }
 
 func MTOToggleReadyForReview(ctx context.Context, logger *zap.Logger, principal authentication.Principal, store *storage.Store, modelPlanID uuid.UUID, isReadyForReview bool) (*models.MTOInfo, error) {
