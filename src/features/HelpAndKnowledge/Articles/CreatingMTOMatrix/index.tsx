fadbf04a376589d8e19e0909d54ddd610a235b59
Patrick Segura <patrick.segura@oddball.io>
2025-05-05 14:06:57 -0500

    Finished initial markup

--- src/features/HelpAndKnowledge/Articles/CreatingMTOMatrix/index.tsx
+++ src/features/HelpAndKnowledge/Articles/CreatingMTOMatrix/index.tsx
@@ -4,23 +4,23 @@ import {
   Card,
   Grid,
   GridContainer,
+  Icon,
   Link,
   ProcessList,
   ProcessListHeading,
-  ProcessListItem,
-  Table
+  ProcessListItem
 } from '@trussworks/react-uswds';
-import classNames from 'classnames';
 import HelpBreadcrumb from 'features/HelpAndKnowledge/Articles/_components/HelpBreadcrumb';
+import i18next from 'i18next';
 
-import Alert from 'components/Alert';
+import ExternalLink from 'components/ExternalLink';
 import UswdsReactLink from 'components/LinkWrapper';
 import MainContent from 'components/MainContent';
 import PageHeading from 'components/PageHeading';
 import { tArray } from 'utils/translation';
 
 import HelpCategoryTag from '../_components/HelpCategoryTag';
-import { ArticleCategories } from '..';
+import helpAndKnowledgeArticles, { ArticleCategories } from '..';
 
 export const CreatingMTOMatrix = () => {
   const { t } = useTranslation('creatingMtoMatrix');
@@ -29,126 +29,235 @@ export const CreatingMTOMatrix = () => {
   const readViewList = tArray<string>('creatingMtoMatrix:readViewList');
   const itLeadList = tArray<string>('creatingMtoMatrix:itLeadList');
   const modelTeamList = tArray<string>('creatingMtoMatrix:modelTeamList');
+  const recommendProcessList = tArray<any>(
+    'creatingMtoMatrix:recommendProcessList'
+  );
+
+  const mtoTutorialArticles = helpAndKnowledgeArticles
+    .filter(article => article.type === ArticleCategories.MTO_TUTORIALS)
+    .sort((a, b) => {
+      if (a.key === 'STARTING_MTO') return -1; // Move STARTING_MTO to the top
+      if (b.key === 'STARTING_MTO') return 1; // Keep other articles below STARTING_MTO
+      return 0; // Maintain the order of other articles
+    });
 
   return (
     <MainContent>
       <GridContainer>
         <Grid tablet={{ col: 12 }}>
-          <HelpBreadcrumb text={t('breadcrumb')} home />
-          <div className="usa-in-page-nav-container">
-            <main className="mint-body-medium">
-              <PageHeading className="line-height-large margin-bottom-2 margin-top-4">
-                {t('title')}
-              </PageHeading>
+          <HelpBreadcrumb text={t('breadcrumb')} />
+          <div className="mint-body-medium">
+            <PageHeading className="line-height-large margin-bottom-2 margin-top-4">
+              {t('title')}
+            </PageHeading>
 
-              <HelpCategoryTag type={ArticleCategories.IT_IMPLEMENTATION} />
+            <HelpCategoryTag type={ArticleCategories.IT_IMPLEMENTATION} />
 
-              <p className="mint-body-large margin-top-1 margin-bottom-4">
-                {t('description')}
-              </p>
+            <p className="mint-body-large margin-top-1 margin-bottom-4">
+              {t('description')}
+            </p>
 
-              <h2 className="margin-top-4 margin-bottom-1">{t('whatIsMTO')}</h2>
+            <h2 className="margin-top-4 margin-bottom-1">{t('whatIsMTO')}</h2>
 
-              <p className="margin-bottom-3 margin-top-1">
-                {t('whatIsMTODescription')}
-              </p>
+            <p className="margin-bottom-3 margin-top-1">
+              {t('whatIsMTODescription')}
+            </p>
 
-              <h3 className="margin-top-3 margin-bottom-1">
-                {t('whyCreateMTO')}
+            <h3 className="margin-top-3 margin-bottom-1">
+              {t('whyCreateMTO')}
+            </h3>
+
+            <p className="margin-bottom-2 margin-top-1">
+              {t('whyCreateMTODescription')}
+            </p>
+
+            <Grid row gap={2}>
+              <Grid tablet={{ col: 6 }} className="margin-bottom-1">
+                <Card
+                  className="margin-0 height-full"
+                  containerProps={{
+                    className: 'padding-2 radius-md margin-0'
+                  }}
+                >
+                  <h4 className="margin-y-0">{t('editAccess')}</h4>
+
+                  <p className="margin-y-1">{t('editAccessDescription')}</p>
+
+                  <ul className="margin-0">
+                    {editAccessList.map(item => (
+                      <li>{item}</li>
+                    ))}
+                  </ul>
+                </Card>
+              </Grid>
+
+              <Grid tablet={{ col: 6 }} className="margin-bottom-1">
+                <Card
+                  className="margin-0 height-full"
+                  containerProps={{
+                    className: 'padding-2 radius-md margin-0'
+                  }}
+                >
+                  <h4 className="margin-y-0">{t('readView')}</h4>
+
+                  <p className="margin-y-1">{t('readViewDescription')}</p>
+
+                  <ul className="margin-0">
+                    {readViewList.map(item => (
+                      <li>{item}</li>
+                    ))}
+                  </ul>
+                </Card>
+              </Grid>
+            </Grid>
+
+            <h3 className="margin-top-3 margin-bottom-1">
+              {t('whyCreateMTO')}
+            </h3>
+
+            <p className="margin-bottom-2 margin-top-1">
+              {t('whyCreateMTODescription')}
+            </p>
+
+            <Grid row gap={2}>
+              <Grid tablet={{ col: 6 }} className="margin-bottom-1">
+                <Card
+                  className="margin-0 height-full"
+                  containerProps={{
+                    className: 'padding-2 radius-md margin-0'
+                  }}
+                >
+                  <h4 className="margin-y-0">{t('itLead')}</h4>
+
+                  <p className="margin-y-1">{t('itLeadDescription')}</p>
+
+                  <ul className="margin-0">
+                    {itLeadList.map(item => (
+                      <li>{item}</li>
+                    ))}
+                  </ul>
+                </Card>
+              </Grid>
+
+              <Grid tablet={{ col: 6 }} className="margin-bottom-1">
+                <Card
+                  className="margin-0 height-full"
+                  containerProps={{
+                    className: 'padding-2 radius-md margin-0'
+                  }}
+                >
+                  <h4 className="margin-y-0">{t('modelTeam')}</h4>
+
+                  <p className="margin-y-1">{t('modelTeamDescription')}</p>
+
+                  <ul className="margin-0">
+                    {modelTeamList.map(item => (
+                      <li>{item}</li>
+                    ))}
+                  </ul>
+                </Card>
+              </Grid>
+            </Grid>
+
+            <h2 className="margin-top-4 margin-bottom-1">{t('partsOfMTO')}</h2>
+
+            <p className="margin-bottom-3 margin-top-1">
+              {t('partsOfMTODescription')}
+            </p>
+
+            <h3 className="margin-top-3 margin-bottom-1">{t('milestones')}</h3>
+
+            <p className="margin-bottom-2 margin-top-1">
+              {t('milestonesDescription')}
+            </p>
+
+            <h3 className="margin-top-3 margin-bottom-1">{t('categories')}</h3>
+
+            <p className="margin-bottom-2 margin-top-1">
+              {t('categoriesDescription')}
+            </p>
+
+            <h3 className="margin-top-3 margin-bottom-1">{t('itSystems')}</h3>
+
+            <p className="margin-bottom-2 margin-top-1">
+              <Trans
+                i18nKey="creatingMtoMatrix:itSystemsDescription"
+                components={{
+                  link1: (
+                    <UswdsReactLink to="/help-and-knowledge/operational-solutions">
+                      {' '}
+                    </UswdsReactLink>
+                  )
+                }}
+              />
+            </p>
+
+            <h3 className="margin-top-3 margin-bottom-1">{t('templates')}</h3>
+
+            <p className="margin-bottom-2 margin-top-1">
+              {t('templatesDescription')}
+            </p>
+
+            <h2 className="margin-top-4 margin-bottom-1 line-height-large">
+              {t('recommendProcess')}
+            </h2>
+
+            <ProcessList>
+              {recommendProcessList.map(item => (
+                <ProcessListItem key={item.title} className="maxw-none">
+                  <ProcessListHeading type="h3">
+                    {item.title}
+                  </ProcessListHeading>
+                  <p>{item.description}</p>
+                </ProcessListItem>
+              ))}
+            </ProcessList>
+
+            <div className="bg-base-lightest padding-2">
+              <h3 className="margin-top-1 margin-bottom-1">
+                {t('mtoTutorials')}
               </h3>
 
               <p className="margin-bottom-2 margin-top-1">
-                {t('whyCreateMTODescription')}
+                {t('mtoTutorialsDescription')}
               </p>
 
-              <Grid row gap={2}>
-                <Grid tablet={{ col: 6 }} className="margin-bottom-1">
-                  <Card
-                    className="margin-0 height-full"
-                    containerProps={{
-                      className: 'padding-2 radius-md margin-0'
-                    }}
-                  >
-                    <h4 className="margin-y-0">{t('editAccess')}</h4>
-
-                    <p className="margin-y-1">{t('editAccessDescription')}</p>
-
-                    <ul className="margin-0">
-                      {editAccessList.map(item => (
-                        <li>{item}</li>
-                      ))}
-                    </ul>
-                  </Card>
-                </Grid>
-
-                <Grid tablet={{ col: 6 }} className="margin-bottom-1">
-                  <Card
-                    className="margin-0 height-full"
-                    containerProps={{
-                      className: 'padding-2 radius-md margin-0'
-                    }}
-                  >
-                    <h4 className="margin-y-0">{t('readView')}</h4>
-
-                    <p className="margin-y-1">{t('readViewDescription')}</p>
-
-                    <ul className="margin-0">
-                      {readViewList.map(item => (
-                        <li>{item}</li>
-                      ))}
-                    </ul>
-                  </Card>
-                </Grid>
+              <Grid row>
+                {mtoTutorialArticles.map(article => {
+                  return (
+                    <Grid tablet={{ col: 6 }} key={article.key}>
+                      <UswdsReactLink
+                        className="fit-content display-flex flex-align-center margin-y-05"
+                        to={`/help-and-knowledge${article.route}`}
+                      >
+                        <span>
+                          {i18next.t(`${article.translation}:title`)}
+                          <Icon.ArrowForward className="margin-left-1 top-2px" />
+                        </span>
+                      </UswdsReactLink>
+                    </Grid>
+                  );
+                })}
               </Grid>
+            </div>
 
-              <h3 className="margin-top-3 margin-bottom-1">
-                {t('whyCreateMTO')}
-              </h3>
+            <h2 className="margin-top-5 margin-bottom-1 line-height-large">
+              {t('stillNeedHelp')}
+            </h2>
 
-              <p className="margin-bottom-2 margin-top-1">
-                {t('whyCreateMTODescription')}
-              </p>
-
-              <Grid row gap={2}>
-                <Grid tablet={{ col: 6 }} className="margin-bottom-1">
-                  <Card
-                    className="margin-0 height-full"
-                    containerProps={{
-                      className: 'padding-2 radius-md margin-0'
-                    }}
-                  >
-                    <h4 className="margin-y-0">{t('itLead')}</h4>
-
-                    <p className="margin-y-1">{t('itLeadDescription')}</p>
-
-                    <ul className="margin-0">
-                      {itLeadList.map(item => (
-                        <li>{item}</li>
-                      ))}
-                    </ul>
-                  </Card>
-                </Grid>
-
-                <Grid tablet={{ col: 6 }} className="margin-bottom-1">
-                  <Card
-                    className="margin-0 height-full"
-                    containerProps={{
-                      className: 'padding-2 radius-md margin-0'
-                    }}
-                  >
-                    <h4 className="margin-y-0">{t('modelTeam')}</h4>
-
-                    <p className="margin-y-1">{t('modelTeamDescription')}</p>
-
-                    <ul className="margin-0">
-                      {modelTeamList.map(item => (
-                        <li>{item}</li>
-                      ))}
-                    </ul>
-                  </Card>
-                </Grid>
-              </Grid>
-            </main>
+            <p className="margin-bottom-2 margin-top-1">
+              <Trans
+                i18nKey="creatingMtoMatrix:stillNeedHelpDescription"
+                components={{
+                  email: <Link href="mailto:MINTTeam@cms.hhs.gov"> </Link>,
+                  slack: (
+                    <ExternalLink href="https://cmsgov.slack.com/archives/C04B10ZN6A2">
+                      {' '}
+                    </ExternalLink>
+                  )
+                }}
+              />
+            </p>
           </div>
         </Grid>
       </GridContainer>
--- src/i18n/en-US/helpAndKnowledge/Articles/creatingMtoMatrix.ts
+++ src/i18n/en-US/helpAndKnowledge/Articles/creatingMtoMatrix.ts
@@ -1,3 +1,5 @@
+import { title } from 'process';
+
 const creatingMtoMatrix = {
   title: 'Creating your Model-to-operations matrix (MTO) in MINT',
   breadcrumb: 'Creating your MTO in MINT',
@@ -45,6 +47,71 @@ const creatingMtoMatrix = {
     'Meet regularly with the IT Lead to review MTO status',
     'Work with the IT Lead to ensure content in the MTO is correct',
     'Take responsibility for and update any tasks or activities assigned to them in the MTO'
-  ]
+  ],
+  partsOfMTO: 'Parts of an MTO',
+  partsOfMTODescription:
+    'An MTO in MINT is made up of a variety of different pieces, all of which help communicate different aspects of a model’s move towards becoming an active and operational model.',
+  milestones: 'Milestones',
+  milestonesDescription:
+    'A milestone is a key function or requirement for a model. Work for a milestone needs to be completed or enabled before a model can go live. MINT’s MTO feature offers two milestone capabilities. The MINT milestone library contains a set of common model milestones that can be quickly added to an MTO. MINT also offers the capability to create custom milestones, allowing for any unique characteristics of individual models.',
+  categories: 'Categories',
+  categoriesDescription:
+    'Categories are an easy way for IT leads to arrange milestones into functional groups or model phases. This can make the MTO more easily digestible for other MINT users and can help organize milestones in a more chronological manner according to operational phase.',
+  itSystems: 'IT systems and solutions',
+  itSystemsDescription:
+    'Solutions are IT systems, contracts, CMS groups and teams, and other methods to implement milestones. IT leads will use their breadth of expertise to add solutions to the MTO and specify which milestone(s) they will help to accomplish. Like milestones, MINT offers two ways of adding a solution to an MTO. The MINT solution library houses a set of information about commonly used IT systems and other solutions, enabling IT leads to quickly add them to an MTO or associate them with a milestone. MINT also allows users to create custom solutions, helpful in cases where a needed system is not included in MINT or when a model is using a bespoke solution not commonly used by other models. You can also view MINT’s available IT systems and solutions in the <link1>Operational solutions area of the Help Center</link1>. When IT Leads add solutions to an MTO, the points of contact for the chosen IT system or solution will receive an automatic notification email from MINT which includes a link to read about the model that plans to use their system or solution. ',
+  templates: 'Templates',
+  templatesDescription:
+    'Templates are combinations of milestones, categories, and solutions that give users a specific MTO starting point or set of content to add to an existing MTO. Template content is often specific to a particular type of model or a particular model characteristic.',
+  recommendProcess:
+    'Recommended high-level process for MTOs and solution implementation',
+  recommendProcessList: [
+    {
+      title: 'Create Model Plan in MINT',
+      description:
+        'The Model Lead and Model Team work to fill out the Model Plan in MINT as best they can, with assistance as needed from the MINT Team (formerly known as the Model Intake Team). They should also complete any additional Model Intake activities that are necessary.'
+    },
+    {
+      title: 'IT Lead is assigned to the model',
+      description:
+        'The IT Lead should review all materials created by the Model Team, including but not limited to: the Model Plan in MINT, 2-page and 6-page concept papers, and the ICIP or draft ICIP.'
+    },
+    {
+      title: 'Begin drafting MTO',
+      description:
+        'Using all that was learned in step 2, the IT Lead should begin an MTO in MINT. A solid draft of an MTO is usually expected within 20 days of beginning to engage with a model team, though IT leads should expect to continue updating the MTO and its statuses as work on the operational aspects of the model progresses. '
+    },
+    {
+      title: 'Share MTO with Model Team',
+      description:
+        'Once a solid draft of the MTO is complete, the IT Lead should meet with the Model Team to share the draft MTO and discuss the details and plan for moving forward. This would also be a good time to establish a plan for a regular cadence of MTO updates, whether during an existing meeting series, or as a new set of meetings. '
+    },
+    {
+      title: '(Optional) Mark MTO as Ready for Review',
+      description:
+        'Once the model’s draft MTO is generally agreed upon by the IT Lead and Model Team, set the MTO status to "Ready for review" to signal to other interested parties that the main content (milestones, solutions, etc.) is fairly well set and is open to any comments or feedback. Even if the MTO is set to "Ready for review", IT Leads should still expect to update the implementation status of individual items as work progresses.'
+    },
+    {
+      title: 'Begin discussions with IT system and solution teams',
+      description:
+        'IT Leads (or other agreed upon responsible party) should begin reaching out to IT system and solution teams identified in the MTO to begin or learn more about any necessary onboarding processes. In step 3 (above) and after, as IT Leads add solutions to an MTO, the points of contact for the chosen IT system or solution will receive an automatic notification email from MINT. These emails include a link to read about the model, and can be a good opportunity to begin conversations with the IT system or solution team. '
+    },
+    {
+      title: 'Continue to update the MTO in MINT',
+      description:
+        'As work progresses, IT Leads should continue to use the MINT MTO feature to update the status of individual milestones as well as IT systems and solutions. To asynchronously update any interested parties, use the Share feature in MINT to share a link to the model’s MTO with any colleagues or interested individuals at CMS.'
+    },
+    {
+      title: 'Final content update',
+      description:
+        'When the model goes Active (or is paused or ends for another reason), IT Leads should do a final content update of the MTO, ensuring that all statuses, milestones, and solutions are accurate. This ensures that MINT can retain historical information about how a model went operational, potentially leading to efficiencies in the future.'
+    }
+  ],
+  mtoTutorials: 'MTO tutorials',
+  mtoTutorialsDescription:
+    'Use the links below to access more specific details about how to use the MTO capabilities in MINT.',
+  stillNeedHelp: 'Still need help?',
+  stillNeedHelpDescription:
+    'If you’d like to know more about the Model-to-operations capabilities in MINT, please feel free to reach out to the MINT team at <email>MINTTeam@cms.hhs.gov</email> or <slack>chat with us on Slack</slack>.'
 };
 export default creatingMtoMatrix;
