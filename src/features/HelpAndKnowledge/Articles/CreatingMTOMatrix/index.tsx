744794ad7e1dac149488cbaa246390e2e74db289
Patrick Segura <patrick.segura@oddball.io>
2025-05-05 14:10:45 -0500

    Added related articles

--- src/features/HelpAndKnowledge/Articles/CreatingMTOMatrix/index.tsx
+++ src/features/HelpAndKnowledge/Articles/CreatingMTOMatrix/index.tsx
@@ -20,7 +20,8 @@ import PageHeading from 'components/PageHeading';
 import { tArray } from 'utils/translation';
 
 import HelpCategoryTag from '../_components/HelpCategoryTag';
-import helpAndKnowledgeArticles, { ArticleCategories } from '..';
+import RelatedArticles from '../_components/RelatedArticles';
+import helpAndKnowledgeArticles, { ArticleCategories, HelpArticle } from '..';
 
 export const CreatingMTOMatrix = () => {
   const { t } = useTranslation('creatingMtoMatrix');
@@ -42,226 +43,244 @@ export const CreatingMTOMatrix = () => {
     });
 
   return (
-    <MainContent>
-      <GridContainer>
-        <Grid tablet={{ col: 12 }}>
-          <HelpBreadcrumb text={t('breadcrumb')} />
-          <div className="mint-body-medium">
-            <PageHeading className="line-height-large margin-bottom-2 margin-top-4">
-              {t('title')}
-            </PageHeading>
+    <div>
+      <MainContent>
+        <GridContainer>
+          <Grid tablet={{ col: 12 }}>
+            <HelpBreadcrumb text={t('breadcrumb')} />
+            <div className="mint-body-medium">
+              <PageHeading className="line-height-large margin-bottom-2 margin-top-4">
+                {t('title')}
+              </PageHeading>
 
-            <HelpCategoryTag type={ArticleCategories.IT_IMPLEMENTATION} />
+              <HelpCategoryTag type={ArticleCategories.IT_IMPLEMENTATION} />
 
-            <p className="mint-body-large margin-top-1 margin-bottom-4">
-              {t('description')}
-            </p>
+              <p className="mint-body-large margin-top-1 margin-bottom-4">
+                {t('description')}
+              </p>
 
-            <h2 className="margin-top-4 margin-bottom-1">{t('whatIsMTO')}</h2>
+              <h2 className="margin-top-4 margin-bottom-1">{t('whatIsMTO')}</h2>
 
-            <p className="margin-bottom-3 margin-top-1">
-              {t('whatIsMTODescription')}
-            </p>
+              <p className="margin-bottom-3 margin-top-1">
+                {t('whatIsMTODescription')}
+              </p>
 
-            <h3 className="margin-top-3 margin-bottom-1">
-              {t('whyCreateMTO')}
-            </h3>
-
-            <p className="margin-bottom-2 margin-top-1">
-              {t('whyCreateMTODescription')}
-            </p>
-
-            <Grid row gap={2}>
-              <Grid tablet={{ col: 6 }} className="margin-bottom-1">
-                <Card
-                  className="margin-0 height-full"
-                  containerProps={{
-                    className: 'padding-2 radius-md margin-0'
-                  }}
-                >
-                  <h4 className="margin-y-0">{t('editAccess')}</h4>
-
-                  <p className="margin-y-1">{t('editAccessDescription')}</p>
-
-                  <ul className="margin-0">
-                    {editAccessList.map(item => (
-                      <li>{item}</li>
-                    ))}
-                  </ul>
-                </Card>
-              </Grid>
-
-              <Grid tablet={{ col: 6 }} className="margin-bottom-1">
-                <Card
-                  className="margin-0 height-full"
-                  containerProps={{
-                    className: 'padding-2 radius-md margin-0'
-                  }}
-                >
-                  <h4 className="margin-y-0">{t('readView')}</h4>
-
-                  <p className="margin-y-1">{t('readViewDescription')}</p>
-
-                  <ul className="margin-0">
-                    {readViewList.map(item => (
-                      <li>{item}</li>
-                    ))}
-                  </ul>
-                </Card>
-              </Grid>
-            </Grid>
-
-            <h3 className="margin-top-3 margin-bottom-1">
-              {t('whyCreateMTO')}
-            </h3>
-
-            <p className="margin-bottom-2 margin-top-1">
-              {t('whyCreateMTODescription')}
-            </p>
-
-            <Grid row gap={2}>
-              <Grid tablet={{ col: 6 }} className="margin-bottom-1">
-                <Card
-                  className="margin-0 height-full"
-                  containerProps={{
-                    className: 'padding-2 radius-md margin-0'
-                  }}
-                >
-                  <h4 className="margin-y-0">{t('itLead')}</h4>
-
-                  <p className="margin-y-1">{t('itLeadDescription')}</p>
-
-                  <ul className="margin-0">
-                    {itLeadList.map(item => (
-                      <li>{item}</li>
-                    ))}
-                  </ul>
-                </Card>
-              </Grid>
-
-              <Grid tablet={{ col: 6 }} className="margin-bottom-1">
-                <Card
-                  className="margin-0 height-full"
-                  containerProps={{
-                    className: 'padding-2 radius-md margin-0'
-                  }}
-                >
-                  <h4 className="margin-y-0">{t('modelTeam')}</h4>
-
-                  <p className="margin-y-1">{t('modelTeamDescription')}</p>
-
-                  <ul className="margin-0">
-                    {modelTeamList.map(item => (
-                      <li>{item}</li>
-                    ))}
-                  </ul>
-                </Card>
-              </Grid>
-            </Grid>
-
-            <h2 className="margin-top-4 margin-bottom-1">{t('partsOfMTO')}</h2>
-
-            <p className="margin-bottom-3 margin-top-1">
-              {t('partsOfMTODescription')}
-            </p>
-
-            <h3 className="margin-top-3 margin-bottom-1">{t('milestones')}</h3>
-
-            <p className="margin-bottom-2 margin-top-1">
-              {t('milestonesDescription')}
-            </p>
-
-            <h3 className="margin-top-3 margin-bottom-1">{t('categories')}</h3>
-
-            <p className="margin-bottom-2 margin-top-1">
-              {t('categoriesDescription')}
-            </p>
-
-            <h3 className="margin-top-3 margin-bottom-1">{t('itSystems')}</h3>
-
-            <p className="margin-bottom-2 margin-top-1">
-              <Trans
-                i18nKey="creatingMtoMatrix:itSystemsDescription"
-                components={{
-                  link1: (
-                    <UswdsReactLink to="/help-and-knowledge/operational-solutions">
-                      {' '}
-                    </UswdsReactLink>
-                  )
-                }}
-              />
-            </p>
-
-            <h3 className="margin-top-3 margin-bottom-1">{t('templates')}</h3>
-
-            <p className="margin-bottom-2 margin-top-1">
-              {t('templatesDescription')}
-            </p>
-
-            <h2 className="margin-top-4 margin-bottom-1 line-height-large">
-              {t('recommendProcess')}
-            </h2>
-
-            <ProcessList>
-              {recommendProcessList.map(item => (
-                <ProcessListItem key={item.title} className="maxw-none">
-                  <ProcessListHeading type="h3">
-                    {item.title}
-                  </ProcessListHeading>
-                  <p>{item.description}</p>
-                </ProcessListItem>
-              ))}
-            </ProcessList>
-
-            <div className="bg-base-lightest padding-2">
-              <h3 className="margin-top-1 margin-bottom-1">
-                {t('mtoTutorials')}
+              <h3 className="margin-top-3 margin-bottom-1">
+                {t('whyCreateMTO')}
               </h3>
 
               <p className="margin-bottom-2 margin-top-1">
-                {t('mtoTutorialsDescription')}
+                {t('whyCreateMTODescription')}
               </p>
 
-              <Grid row>
-                {mtoTutorialArticles.map(article => {
-                  return (
-                    <Grid tablet={{ col: 6 }} key={article.key}>
-                      <UswdsReactLink
-                        className="fit-content display-flex flex-align-center margin-y-05"
-                        to={`/help-and-knowledge${article.route}`}
-                      >
-                        <span>
-                          {i18next.t(`${article.translation}:title`)}
-                          <Icon.ArrowForward className="margin-left-1 top-2px" />
-                        </span>
-                      </UswdsReactLink>
-                    </Grid>
-                  );
-                })}
+              <Grid row gap={2}>
+                <Grid tablet={{ col: 6 }} className="margin-bottom-1">
+                  <Card
+                    className="margin-0 height-full"
+                    containerProps={{
+                      className: 'padding-2 radius-md margin-0'
+                    }}
+                  >
+                    <h4 className="margin-y-0">{t('editAccess')}</h4>
+
+                    <p className="margin-y-1">{t('editAccessDescription')}</p>
+
+                    <ul className="margin-0">
+                      {editAccessList.map(item => (
+                        <li>{item}</li>
+                      ))}
+                    </ul>
+                  </Card>
+                </Grid>
+
+                <Grid tablet={{ col: 6 }} className="margin-bottom-1">
+                  <Card
+                    className="margin-0 height-full"
+                    containerProps={{
+                      className: 'padding-2 radius-md margin-0'
+                    }}
+                  >
+                    <h4 className="margin-y-0">{t('readView')}</h4>
+
+                    <p className="margin-y-1">{t('readViewDescription')}</p>
+
+                    <ul className="margin-0">
+                      {readViewList.map(item => (
+                        <li>{item}</li>
+                      ))}
+                    </ul>
+                  </Card>
+                </Grid>
               </Grid>
+
+              <h3 className="margin-top-3 margin-bottom-1">
+                {t('whyCreateMTO')}
+              </h3>
+
+              <p className="margin-bottom-2 margin-top-1">
+                {t('whyCreateMTODescription')}
+              </p>
+
+              <Grid row gap={2}>
+                <Grid tablet={{ col: 6 }} className="margin-bottom-1">
+                  <Card
+                    className="margin-0 height-full"
+                    containerProps={{
+                      className: 'padding-2 radius-md margin-0'
+                    }}
+                  >
+                    <h4 className="margin-y-0">{t('itLead')}</h4>
+
+                    <p className="margin-y-1">{t('itLeadDescription')}</p>
+
+                    <ul className="margin-0">
+                      {itLeadList.map(item => (
+                        <li>{item}</li>
+                      ))}
+                    </ul>
+                  </Card>
+                </Grid>
+
+                <Grid tablet={{ col: 6 }} className="margin-bottom-1">
+                  <Card
+                    className="margin-0 height-full"
+                    containerProps={{
+                      className: 'padding-2 radius-md margin-0'
+                    }}
+                  >
+                    <h4 className="margin-y-0">{t('modelTeam')}</h4>
+
+                    <p className="margin-y-1">{t('modelTeamDescription')}</p>
+
+                    <ul className="margin-0">
+                      {modelTeamList.map(item => (
+                        <li>{item}</li>
+                      ))}
+                    </ul>
+                  </Card>
+                </Grid>
+              </Grid>
+
+              <h2 className="margin-top-4 margin-bottom-1">
+                {t('partsOfMTO')}
+              </h2>
+
+              <p className="margin-bottom-3 margin-top-1">
+                {t('partsOfMTODescription')}
+              </p>
+
+              <h3 className="margin-top-3 margin-bottom-1">
+                {t('milestones')}
+              </h3>
+
+              <p className="margin-bottom-2 margin-top-1">
+                {t('milestonesDescription')}
+              </p>
+
+              <h3 className="margin-top-3 margin-bottom-1">
+                {t('categories')}
+              </h3>
+
+              <p className="margin-bottom-2 margin-top-1">
+                {t('categoriesDescription')}
+              </p>
+
+              <h3 className="margin-top-3 margin-bottom-1">{t('itSystems')}</h3>
+
+              <p className="margin-bottom-2 margin-top-1">
+                <Trans
+                  i18nKey="creatingMtoMatrix:itSystemsDescription"
+                  components={{
+                    link1: (
+                      <UswdsReactLink to="/help-and-knowledge/operational-solutions">
+                        {' '}
+                      </UswdsReactLink>
+                    )
+                  }}
+                />
+              </p>
+
+              <h3 className="margin-top-3 margin-bottom-1">{t('templates')}</h3>
+
+              <p className="margin-bottom-2 margin-top-1">
+                {t('templatesDescription')}
+              </p>
+
+              <h2 className="margin-top-4 margin-bottom-1 line-height-large">
+                {t('recommendProcess')}
+              </h2>
+
+              <ProcessList>
+                {recommendProcessList.map(item => (
+                  <ProcessListItem key={item.title} className="maxw-none">
+                    <ProcessListHeading type="h3">
+                      {item.title}
+                    </ProcessListHeading>
+                    <p>{item.description}</p>
+                  </ProcessListItem>
+                ))}
+              </ProcessList>
+
+              <div className="bg-base-lightest padding-2">
+                <h3 className="margin-top-1 margin-bottom-1">
+                  {t('mtoTutorials')}
+                </h3>
+
+                <p className="margin-bottom-2 margin-top-1">
+                  {t('mtoTutorialsDescription')}
+                </p>
+
+                <Grid row>
+                  {mtoTutorialArticles.map(article => {
+                    return (
+                      <Grid tablet={{ col: 6 }} key={article.key}>
+                        <UswdsReactLink
+                          className="fit-content display-flex flex-align-center margin-y-05"
+                          to={`/help-and-knowledge${article.route}`}
+                        >
+                          <span>
+                            {i18next.t(`${article.translation}:title`)}
+                            <Icon.ArrowForward className="margin-left-1 top-2px" />
+                          </span>
+                        </UswdsReactLink>
+                      </Grid>
+                    );
+                  })}
+                </Grid>
+              </div>
+
+              <h2 className="margin-top-5 margin-bottom-1 line-height-large">
+                {t('stillNeedHelp')}
+              </h2>
+
+              <p className="margin-bottom-6 margin-top-1">
+                <Trans
+                  i18nKey="creatingMtoMatrix:stillNeedHelpDescription"
+                  components={{
+                    email: <Link href="mailto:MINTTeam@cms.hhs.gov"> </Link>,
+                    slack: (
+                      <ExternalLink href="https://cmsgov.slack.com/archives/C04B10ZN6A2">
+                        {' '}
+                      </ExternalLink>
+                    )
+                  }}
+                />
+              </p>
             </div>
+          </Grid>
+        </GridContainer>
+      </MainContent>
 
-            <h2 className="margin-top-5 margin-bottom-1 line-height-large">
-              {t('stillNeedHelp')}
-            </h2>
-
-            <p className="margin-bottom-2 margin-top-1">
-              <Trans
-                i18nKey="creatingMtoMatrix:stillNeedHelpDescription"
-                components={{
-                  email: <Link href="mailto:MINTTeam@cms.hhs.gov"> </Link>,
-                  slack: (
-                    <ExternalLink href="https://cmsgov.slack.com/archives/C04B10ZN6A2">
-                      {' '}
-                    </ExternalLink>
-                  )
-                }}
-              />
-            </p>
-          </div>
-        </Grid>
-      </GridContainer>
-    </MainContent>
+      <RelatedArticles
+        currentArticle={HelpArticle.CREATING_MTO_MATRIX}
+        specificArticles={[
+          HelpArticle.PHASES_INVOLVED,
+          HelpArticle.MODEL_SOLUTION_IMPLEMENTATION,
+          HelpArticle.UTILIZING_SOLUTIONS
+        ]}
+        viewAllLink
+      />
+    </div>
   );
 };
 
