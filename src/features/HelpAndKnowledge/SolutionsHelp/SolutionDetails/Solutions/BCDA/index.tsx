89d21960284e4e2fed860d6986e907cc2606fa5d
Patrick Segura <patrick.segura@oddball.io>
2025-05-14 13:47:34 -0500

    Removed uselocation import

--- src/features/HelpAndKnowledge/SolutionsHelp/SolutionDetails/Solutions/BCDA/index.tsx
+++ src/features/HelpAndKnowledge/SolutionsHelp/SolutionDetails/Solutions/BCDA/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Trans } from 'react-i18next';
-import { useHistory, useLocation } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import {
   Button,
   ProcessList,
