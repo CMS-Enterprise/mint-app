56e46e0f3959d18664af3356a5e3c056a3d8cc6f
Peyton Thibodeaux <peytonthibodeaux@Peytons-MacBook-Pro.local>
2025-05-21 09:37:38 -0500

    forgot early return

--- pkg/worker/okta_cron_job.go
+++ pkg/worker/okta_cron_job.go
@@ -11,10 +11,13 @@ import (
 func (w *Worker) RefreshOktaCronJob(ctx context.Context, args ...interface{}) error {
 	helper := faktory_worker.HelperFor(ctx)
 	logger := loggerWithFaktoryFieldsWithoutBatchID(w.Logger, helper)
+
 	_, err := w.OktaAPIClient.SearchByName(context.Background(), "MINT")
 	if err != nil {
 		logger.Warn("failed to refresh okta token", zap.Error(err))
+		return err
 	}
+
 	logger.Info("okta token refreshed")
 	return nil
 }
