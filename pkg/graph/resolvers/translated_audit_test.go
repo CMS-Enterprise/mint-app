9430225c3eb5bb2f02e34a520c98d725065b6c0c
Steven Wade <steven.wade@oddball.io>
2025-05-23 15:40:36 -0700

    feat: added more notes about unit test expansion

--- pkg/graph/resolvers/translated_audit_test.go
+++ pkg/graph/resolvers/translated_audit_test.go
@@ -20,6 +20,7 @@ import (
 func (suite *ResolverSuite) TestTranslatedAuditGetMostRecentByModelPlanIDAndTableNames() {
 	plan1 := suite.createModelPlan("test plan for changes1")
 	plan2 := suite.createModelPlan("test plan for changes2")
+	plan3 := suite.createModelPlan("test plan for changes3")
 
 	suite.dangerousQueueAndTranslateAllAudits()
 	// TODO verify this and expand to be a more robust validation
@@ -36,7 +37,17 @@ func (suite *ResolverSuite) TestTranslatedAuditGetMostRecentByModelPlanIDAndTabl
 			ExcludedFields: "{}",
 			IsAdmin:        false,
 		}, Expected: models.TNPlanBasics},
+		{Key: storage.MostRecentByModelPlanIDAndTableFilters{
+			ModelPlanID:    plan3.ID,
+			TableNames:     helpers.JoinStringSlice([]models.TableName{models.TNPlanGeneralCharacteristics}, true),
+			ExcludedFields: "{}",
+			IsAdmin:        false,
+		}, Expected: models.TNPlanGeneralCharacteristics},
 	}
+
+	//TODO update this to create more test data. We should verify that updates, deletes, excluded fields, etc are all accounted for.
+	// Perhaps we should also include a more expansive expected variable to show field count etc? (So we can validate if a field is excluded etc?)
+	// Can verify last suggested status is updated by including multiple tables to check, updating two, and making sure it isn't model plan if only status was updated
 	verifyFunc := func(data *models.TranslatedAudit, expected models.TableName) bool {
 		if data == nil {
 			return false
