1bdb888611b6cc2a045f5089cdbc1b8778f0fc6e
Steven Wade <steven.wade@oddball.io>
2025-05-23 15:21:27 -0700

    feat: update sql to not return only 1 record for the loader, but instead to return 1 entry per record

--- pkg/sqlqueries/SQL/translated_audit/most_recent_get_by_model_plan_id_and_table_names_loader.sql
+++ pkg/sqlqueries/SQL/translated_audit/most_recent_get_by_model_plan_id_and_table_names_loader.sql
@@ -1,6 +1,8 @@
 WITH QUERIED_IDS AS (
     /* Translate the input to a table */
     SELECT
+    -- we use input_idx to keep track of the order of the input, and ensure we return the top result per input row
+        ROW_NUMBER() OVER () AS input_idx,
         model_plan_id,
         is_admin,
         user_id,
@@ -28,61 +30,87 @@ KEYS_WITH_ACCESS_CHECK AS (
         QUERIED_IDS qID
     LEFT JOIN plan_collaborator pc
         ON qID.user_id = pc.user_id AND qID.model_plan_id = pc.model_plan_id
+),
+
+RANKED_AUDIT AS (
+    SELECT 
+        qID.input_idx,
+        tAudit.id,
+        qID.model_plan_id,
+        tAudit.actor_id,
+        actor.common_name AS actor_name,
+        tAudit.change_id,
+        tAudit.date,
+        tAudit.table_id,
+        audit.table_config.name AS table_name,
+        tAudit.primary_key,
+        tAudit.action,
+        tAudit.restricted,
+        tAudit.meta_data_type,
+        tAudit.meta_data,
+        tAudit.created_by,
+        tAudit.created_dts,
+        tAudit.modified_by,
+        tAudit.modified_dts,
+        qID.table_names,
+        qID.is_admin,
+        qID.excluded_fields,
+        ROW_NUMBER() OVER (PARTITION BY qID.input_idx ORDER BY tAudit.change_id DESC) AS rn
+    FROM
+        translated_audit AS tAudit
+    LEFT JOIN audit.table_config ON tAudit.table_id = audit.table_config.id
+    LEFT JOIN user_account AS actor ON tAudit.actor_id = actor.id
+    JOIN KEYS_WITH_ACCESS_CHECK AS qID ON tAudit.model_plan_id = qID.model_plan_id
+    WHERE
+        (
+            (tAudit.restricted = FALSE AND qID.has_restricted_access = FALSE) -- user does not have access to restricted audits, only show non-restricted
+            OR qID.has_restricted_access = TRUE
+        ) -- show all audits if the user has access to restricted audits
+        AND (
+            NOT (qID.should_filter)
+            OR  table_config.name = ANY(qID.table_names) -- filter by table names
+        )
+        AND (         
+            NOT (qID.should_exclude_fields)        
+            OR
+            (
+                -- This ensures that theres is at least one field that is not excluded, or there are no fields (like for an insert)
+                EXISTS (
+                    SELECT 1
+                    FROM translated_audit_field taf
+                    WHERE
+                        taf.translated_audit_id = tAudit.id
+                        AND (
+                            taf.field_name IS NULL
+                            OR
+                            taf.field_name <> ALL(qID.excluded_fields)
+                        )
+                )
+            )
+        )
 )
 
 SELECT 
-    tAudit.id,
-    qID.model_plan_id,
-    tAudit.actor_id,
-    actor.common_name AS actor_name,
-    tAudit.change_id,
-    tAudit.date,
-    tAudit.table_id,
-    audit.table_config.name AS table_name,
-    tAudit.primary_key,
-    tAudit.action,
-    tAudit.restricted,
-    tAudit.meta_data_type,
-    tAudit.meta_data,
-    tAudit.created_by,
-    tAudit.created_dts,
-    tAudit.modified_by,
-    tAudit.modified_dts,
-    qID.table_names,
-    qID.is_admin,
-    qID.excluded_fields
-
-
-FROM translated_audit AS tAudit
-LEFT JOIN audit.table_config ON tAudit.table_id = audit.table_config.id
-LEFT JOIN user_account AS actor ON tAudit.actor_id = actor.id
-JOIN KEYS_WITH_ACCESS_CHECK AS qID ON tAudit.model_plan_id = qID.model_plan_id
-WHERE
-    (
-        (tAudit.restricted = FALSE AND qID.has_restricted_access = FALSE) -- user does not have access to restricted audits, only show non-restricted
-        OR qID.has_restricted_access = TRUE
-    ) -- show all audits if the user has access to restricted audits
-    AND (
-        NOT (qID.should_filter)
-        OR  table_config.name = ANY(qID.table_names) -- filter by table names
-    )
-    AND (         
-        NOT (qID.should_exclude_fields)        
-        OR
-        (
-            -- This ensures that theres is at least one field that is not excluded, or there are no fields (like for an insert)
-            EXISTS (
-                SELECT 1
-                FROM translated_audit_field taf
-                WHERE
-                    taf.translated_audit_id = tAudit.id
-                    AND (
-                        taf.field_name IS NULL
-                        OR
-                        taf.field_name <> ALL(qID.excluded_fields)
-                    )
-            )
-        )
-    )
-ORDER BY tAudit.change_id DESC
-LIMIT 1
+    id,
+    model_plan_id,
+    actor_id,
+    actor_name,
+    change_id,
+    date,
+    table_id,
+    table_name,
+    primary_key,
+    action,
+    restricted,
+    meta_data_type,
+    meta_data,
+    created_by,
+    created_dts,
+    modified_by,
+    modified_dts,
+    table_names,
+    is_admin,
+    excluded_fields
+FROM RANKED_AUDIT
+WHERE rn = 1
+ORDER BY input_idx;
