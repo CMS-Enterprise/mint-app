3cd5723cbbf0358c034098fd16f39083771f9388
Patrick Segura <patrick.segura@oddball.io>
2025-05-29 09:46:11 -0500

    started added step one content

--- src/i18n/en-US/helpAndKnowledge/Articles/sharingExportingMto.ts
+++ src/i18n/en-US/helpAndKnowledge/Articles/sharingExportingMto.ts
@@ -1,7 +1,50 @@
 const sharingExportingMto = {
   title: 'Sharing and exporting your MTO',
   description:
-    'Sharing your Model-to-operations matrix (MTO) with colleagues or other interested parties is easy in MINT. You may share your MTO (or a section of it) via email, you may export it as a PDF or CSV, and may also copy a link to the read view of your MTO in MINT. When you share your MTO via email, MINT will automatically send an email to your desired recipients which includes a link to the read view of your MTO in MINT. CMS staff who do not already have MINT access will be able to find instructions to get access to MINT, and those who do already have MINT access will be able to sign in to see your MTO.'
+    'Sharing your Model-to-operations matrix (MTO) with colleagues or other interested parties is easy in MINT. You may share your MTO (or a section of it) via email, you may export it as a PDF or CSV, and may also copy a link to the read view of your MTO in MINT. When you share your MTO via email, MINT will automatically send an email to your desired recipients which includes a link to the read view of your MTO in MINT. CMS staff who do not already have MINT access will be able to find instructions to get access to MINT, and those who do already have MINT access will be able to sign in to see your MTO.',
+  fromCollabArea: 'From the model collaboration area',
+  step1: {
+    heading: 'Navigate to your model’s collaboration area',
+    altText:
+      'A screenshot of the MINT homepage showing the “My Model Plans” section as well as the ability to customize a user’s homepage',
+    text: 'From the MINT homepage, navigate to the model whose MTO you wish to share. You can find a list of models you have been assigned to in the “My Model Plans” section of the homepage. Because MINT offers customizable homepages, it’s possible that this section may not appear on your homepage. If you don’t see it, locate the area titled “Did you know you can customize this page?” in the top right of the page and click the “Edit homepage settings” link to add the “My Model Plans” section to your home page.',
+    caption:
+      'Image caption: A screenshot of the MINT homepage showing the “My Model Plans” section as well as the ability to customize a user’s homepage.'
+  },
+  step2: {
+    heading: 'Locate the MTO card',
+    altText:
+      'A screenshot of the model collaboration area for a model called Rural Health Enablement Model which shows the collaboration area cards for “Model Plan”, “Data exchange approach”, and “Model-to-operations matrix”',
+    text: 'On your model collaboration area page, find the card labeled “Model-to-operations matrix” on the second row of cards.',
+    caption:
+      'Image caption: A screenshot of the model collaboration area for a model called Rural Health Enablement Model which shows the collaboration area cards for “Model Plan”, “Data exchange approach”, and “Model-to-operations matrix”.'
+  },
+  step3: {
+    heading: 'Click “Share or export matrix”',
+    text: 'On the card’s actions footer, click the button labeled “Share or export matrix”. This will open a screen allowing you to select share and export options.'
+  },
+  step4: {
+    heading: 'Choose share or export and complete the content in the form',
+    altText: 'A screenshot of the form to share MTO or other model content',
+    text: `On the top tab bar of the pop-up, choose “Share” or “Export” depending on your needs.
+To share, complete the following fields in the form:`,
+    list: [
+      '<bold>Selected content:</bold> Choose the subset of content that you wish to share. For the MTO, you will have the option to choose the whole MTO, just the milestone view, or just the solutions and IT systems view.',
+      '<bold>Recipients:</bold> Search the EUA database for the individuals you would like to share this information with. When you click the share button, these individuals will receive an email with a link to MINT to view the content you choose to share with them.',
+      '<bold>(Optional) additional message:</bold> Add an additional message for your recipients. This will be included in the email you send.'
+    ],
+    text2: 'To export, complete the following fields in the form:',
+    list2: [
+      '<bold>Selected content:</bold> Choose the subset of content that you wish to share. For the MTO, you will have the option to choose the whole MTO, just the milestone view, or just the solutions and IT systems view.',
+      '<bold>Format:</bold> Choose CSV and/or PDF to export your chosen content.'
+    ],
+    caption:
+      'Image caption: A screenshot of the form to share MTO or other model content.'
+  },
+  step5: {
+    heading: 'Click “Share” or “Export”',
+    text: 'If you have chosen to share your content, click the button labeled “Share”. From the Share screen, you may also choose to click the button labeled “Copy link to Read View”. This will copy a link to your MTO content to your clipboard, allowing you to paste that link in other situations such as email, Slack, or another chat tool. If you have chosen to export, click the button labeled “Export”. If at any time you change your mind, you may click the “Cancel” button or the X icon in the top right corner to close the pop-up without sharing or exporting.'
+  }
 };
 
 export default sharingExportingMto;
