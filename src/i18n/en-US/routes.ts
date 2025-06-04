e3ff4976838333f93fca751b89da71f64d2956c6
Elle Li <68085997+yiranli624@users.noreply.github.com>
2025-06-04 10:48:28 -0600

    [MINT-3374] Change it-systems-and-solutions route (#1749)
    * [Change it-systems-and-solutions route]
    
    * [Change it-systems-and-solutions route in route]

--- src/i18n/en-US/routes.ts
+++ src/i18n/en-US/routes.ts
@@ -120,7 +120,7 @@ const routes = {
       'Read view - Operations, evaluation, and learning',
     '/read-view/payment': 'Read view - Payment',
     '/read-view/milestones': 'Read view - Milestones',
-    '/read-view/it-systems-and-solutions':
+    '/read-view/solutions-and-it-systems':
       'Read view - Solutions and IT systems',
     '/read-view/data-exchange-approach': 'Data exchange approach',
     '/read-view/it-solutions':
