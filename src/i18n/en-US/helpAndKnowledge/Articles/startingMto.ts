413fd7875d5653a4e5e2502c8727610087b7ed73
Elle Li <68085997+yiranli624@users.noreply.github.com>
2025-06-06 09:35:21 -0600

    [NOREF] Add missing help articles breadcrumbs (#1752)
    [Add missing help articles breadcrumbs]

--- src/i18n/en-US/helpAndKnowledge/Articles/startingMto.ts
+++ src/i18n/en-US/helpAndKnowledge/Articles/startingMto.ts
@@ -1,5 +1,6 @@
 const startingMto = {
   title: 'Starting an MTO for a new model',
+  breadcrumb: 'Starting an MTO for a new model',
   description:
     'New models all start with an empty Model-to-operations matrix (MTO). IT Leads newly assigned to a model can start creating a MTO by following the steps below.',
   step1: {
