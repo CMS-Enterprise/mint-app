3e98c6b07cc3e414b0c20cffe36b3a7a087348e3
Steven Wade <steven.wade@oddball.io>
2025-06-30 15:36:12 -0700

    chore: remove commented out code from shell script

--- scripts/format-graphql.sh
+++ scripts/format-graphql.sh
@@ -25,28 +25,3 @@ if [ -n "$changed_files_output" ]; then
   eval yarn prettier --write $graphql_files --log-level silent > /dev/null 2>&1
 
 fi
-
-
-
-# echo "$changed_files_output" | grep '.graphql'
-
-
-# output=$(eval yarn prettier --write $graphql_files --log-level silent)
-# # echo "$output"
-# # Print only the changed files
-# # Prettier returns each modified file path, so extract and print those
-# changed_files=$(echo "$output" | grep -E '\.graphql$')
-# # echo "$changed_files"
-# if [ -n "$changed_files" ]; then
-#   echo "Formatted the following GraphQL files:"
-#   echo "$changed_files"
-# fi
-
-# # Exit with success
-# exit 0
-
-# # if [ $? -ne 0 ]; then
-# #   echo "GraphQL formatting failed."
-# #   exit 1
-# # fi
-
