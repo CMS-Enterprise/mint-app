a62036532f59bf97cee642f07692ecbb7abcfbb6
Steven Wade <steven.wade@oddball.io>
2025-06-30 14:11:20 -0700

    feat: working format-graphql shell

--- scripts/format-graphql.sh
+++ scripts/format-graphql.sh
@@ -14,30 +14,39 @@ if [ -z "$graphql_files" ]; then
   exit 0
 fi
 
-# echo "Formatting GraphQL files:"
-# echo $graphql_files
 
-# Run prettier on these files
-# eval yarn prettier --write $graphql_files
+# Run prettier check on these files
 changed=$(eval yarn prettier --check $graphql_files 2>&1 )
-changed_files1=$(echo "$changed" | grep '^\[warn\] .*\.graphql$' | sed 's/^\[warn\] //')
-# changed=$(eval yarn prettier --check $graphql_files 2>&1 | grep 'Code style issues found in the following file')
 echo "$changed" | awk '/^\[warn\] .*\.graphql$/ { sub(/^\[warn\] /, ""); print }'
-output=$(eval yarn prettier --write $graphql_files --log-level silent)
-# echo "$output"
-# Print only the changed files
-# Prettier returns each modified file path, so extract and print those
-changed_files=$(echo "$output" | grep -E '\.graphql$')
-# echo "$changed_files"
-if [ -n "$changed_files" ]; then
-  echo "Formatted the following GraphQL files:"
-  echo "$changed_files"
+changed_files_output=$(echo "$changed" | awk '/^\[warn\] .*\.graphql$/ { sub(/^\[warn\] /, ""); print }' | grep '.graphql')
+if [ -n "$changed_files_output" ]; then
+  echo "$changed_files_output"
+  # output=$(eval yarn prettier --write $graphql_files --log-level silent)
+  eval yarn prettier --write $graphql_files --log-level silent
+
 fi
 
-# Exit with success
-exit 0
 
-# if [ $? -ne 0 ]; then
-#   echo "GraphQL formatting failed."
-#   exit 1
+
+# echo "$changed_files_output" | grep '.graphql'
+
+
+# output=$(eval yarn prettier --write $graphql_files --log-level silent)
+# # echo "$output"
+# # Print only the changed files
+# # Prettier returns each modified file path, so extract and print those
+# changed_files=$(echo "$output" | grep -E '\.graphql$')
+# # echo "$changed_files"
+# if [ -n "$changed_files" ]; then
+#   echo "Formatted the following GraphQL files:"
+#   echo "$changed_files"
 # fi
+
+# # Exit with success
+# exit 0
+
+# # if [ $? -ne 0 ]; then
+# #   echo "GraphQL formatting failed."
+# #   exit 1
+# # fi
+
