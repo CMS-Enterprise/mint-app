5390928d3e338925fb7690fd79d74b2acaa8ab0a
Patrick Segura <patrick.segura@oddball.io>
2025-07-08 10:54:49 -0500

    updated unit tests

--- src/features/HelpAndKnowledge/SolutionsHelp/_components/SolutionsHeader/index.test.tsx
+++ src/features/HelpAndKnowledge/SolutionsHelp/_components/SolutionsHeader/index.test.tsx
@@ -25,7 +25,7 @@ describe('Operation Solution Help Header', () => {
 
     // Page results info
     expect(
-      getByText('Showing 9 of 50 operational solutions')
+      getByText('Showing 9 of 54 operational solutions')
     ).toBeInTheDocument();
   });
 
