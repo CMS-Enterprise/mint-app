508ab093a48c5f386fd5898b50093600f0005df8
Elle Li <elle.li@oddball.io>
2025-07-10 14:34:26 -0600

    [WIP migrations]

--- migrations/V209__Update_CMS_Qualtrics_Solution_to_Custom_Solution.sql
+++ migrations/V209__Update_CMS_Qualtrics_Solution_to_Custom_Solution.sql
@@ -1,7 +1,10 @@
--- UPDATE mto_solution 
--- SET mto_common_solution_key = NULL,
---     name = 'CMS QUALTRICS',
---     type = 'IT_SYSTEM',
---     poc_name = 'MINT Team',
---     poc_email = 'MINTTeam@cms.hhs.gov'
--- WHERE mto_common_solution_key = 'CMS_QUALTRICS'
+UPDATE mto_solution 
+SET
+    mto_common_solution_key = NULL,
+    name = 'CMS QUALTRICS',
+    type = 'IT_SYSTEM',
+    poc_name = 'MINT Team',
+    poc_email = 'MINTTeam@cms.hhs.gov',
+    modified_by = '00000001-0001-0001-0001-000000000001', --System Account
+    modified_dts= CURRENT_TIMESTAMP
+WHERE mto_common_solution_key = 'CMS_QUALTRICS';
