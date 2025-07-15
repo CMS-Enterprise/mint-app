93677dbe92eaaa8f4074aff48a26bee2cd159168
Patrick Segura <patrick.segura@oddball.io>
2025-07-15 15:46:38 -0500

    added proper caching fields to mutation

--- src/gql/operations/ModelPlanSubscription/UnlockAllSections.ts
+++ src/gql/operations/ModelPlanSubscription/UnlockAllSections.ts
@@ -4,6 +4,14 @@ export default gql(/* GraphQL */ `
   mutation UnlockAllSections($modelPlanID: UUID!) {
     unlockAllLockableSections(modelPlanID: $modelPlanID) {
       modelPlanID
+      section
+      # lockedBy
+      lockedByUserAccount {
+        id
+        username
+        commonName
+      }
+      isAssessment
     }
   }
 `);
