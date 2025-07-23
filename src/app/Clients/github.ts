bc2bb8a085c325c045e80642f616aaa01bf24cbd
Patrick Segura <patrick.segura@oddball.io>
2025-07-23 12:48:14 -0500

    conditionally added github token

--- src/app/Clients/github.ts
+++ src/app/Clients/github.ts
@@ -2,13 +2,24 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
 const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
 
-const githubApolloClient = new ApolloClient({
-  link: new HttpLink({
-    uri: 'https://api.github.com/graphql',
-    headers: {
-      Authorization: `Bearer ${GITHUB_TOKEN}`
+/**
+ * If a GitHub token is provided, add it to the headers
+ */
+const githubHeaders = GITHUB_TOKEN
+  ? {
+      headers: {
+        Authorization: `Bearer ${GITHUB_TOKEN}`
+      }
     }
-  }),
+  : {};
+
+const githubLink = new HttpLink({
+  uri: 'https://api.github.com/graphql',
+  ...githubHeaders
+});
+
+const githubApolloClient = new ApolloClient({
+  link: githubLink,
   cache: new InMemoryCache()
 });
 
