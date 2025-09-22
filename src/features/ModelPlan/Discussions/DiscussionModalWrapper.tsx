051182a4c899a8fc643bd0d23137f4ae9dee07bb
Patrick Segura <patrick.segura@oddball.io>
2025-09-22 12:00:00 -0500

    fixed width on discussion and crtdl

--- src/features/ModelPlan/Discussions/DiscussionModalWrapper.tsx
+++ src/features/ModelPlan/Discussions/DiscussionModalWrapper.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Grid, GridContainer } from '@trussworks/react-uswds';
 
 import Sidepanel from 'components/Sidepanel';
 
@@ -26,9 +25,7 @@ const DiscussionModalWrapper = ({
       testid="discussion-modal"
       noScrollable
     >
-      <GridContainer className="padding-y-6">
-        <Grid desktop={{ col: 12 }}>{children}</Grid>
-      </GridContainer>
+      <div className="padding-y-6 padding-x-8 maxw-tablet">{children}</div>
     </Sidepanel>
   );
 };
