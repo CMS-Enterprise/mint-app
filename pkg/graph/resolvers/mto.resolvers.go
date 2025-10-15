81fa9c83862fb11a00503a65c233efb2bdfbe25b
Patrick Segura <patrick.segura@oddball.io>
2025-10-15 16:18:33 -0500

    fixed lint

--- pkg/graph/resolvers/mto.resolvers.go
+++ pkg/graph/resolvers/mto.resolvers.go
@@ -7,10 +7,11 @@ package resolvers
 import (
 	"context"
 
+	"github.com/google/uuid"
+
 	"github.com/cms-enterprise/mint-app/pkg/appcontext"
 	"github.com/cms-enterprise/mint-app/pkg/graph/generated"
 	"github.com/cms-enterprise/mint-app/pkg/models"
-	"github.com/google/uuid"
 )
 
 // Categories is the resolver for the categories field.
