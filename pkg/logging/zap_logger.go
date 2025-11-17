faa2e38841676a2350d0aa1638e7c6af06303f72
Steven Wade <steven.wade@oddball.io>
2025-11-17 15:28:07 -0700

    feat: some more base implementations of the error or warn logger

---
+++ pkg/logging/zap_logger.go
@@ -0,0 +1,103 @@
+package logging
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type ZapLogger struct {
+	logger *zap.Logger
+}
+
+func NewZapLogger(logger *zap.Logger) *ZapLogger {
+	return &ZapLogger{logger}
+}
+
+func (l *ZapLogger) Named(s string) *ZapLogger {
+	l.logger = l.logger.Named(s)
+	return l
+}
+
+func (l *ZapLogger) WithOptions(opts ...zap.Option) *ZapLogger {
+	l.logger = l.logger.WithOptions(opts...)
+	return l
+}
+
+func (l *ZapLogger) With(fields ...zap.Field) *ZapLogger {
+	l.logger = l.logger.With(fields...)
+	return l
+}
+
+func (l *ZapLogger) Level() zapcore.Level {
+	return l.logger.Level()
+}
+
+func (l *ZapLogger) Check(lvl zapcore.Level, msg string) *zapcore.CheckedEntry {
+	return l.logger.Check(lvl, msg)
+}
+
+func (l *ZapLogger) Log(lvl zapcore.Level, msg string, fields ...zap.Field) {
+	l.logger.Log(lvl, msg, fields...)
+}
+
+func (l *ZapLogger) Debug(msg string, fields ...zap.Field) {
+	l.logger.Debug(msg, fields...)
+}
+
+func (l *ZapLogger) Info(msg string, fields ...zap.Field) {
+	l.logger.Info(msg, fields...)
+}
+
+func (l *ZapLogger) Warn(msg string, fields ...zap.Field) {
+	l.logger.Warn(msg, fields...)
+}
+
+func (l *ZapLogger) Error(msg string, fields ...zap.Field) {
+	l.logger.Error(msg, fields...)
+}
+
+func (l *ZapLogger) DPanic(msg string, fields ...zap.Field) {
+	l.logger.DPanic(msg, fields...)
+}
+
+func (l *ZapLogger) Panic(msg string, fields ...zap.Field) {
+	l.logger.Panic(msg, fields...)
+}
+
+func (l *ZapLogger) Fatal(msg string, fields ...zap.Field) {
+	l.logger.Fatal(msg, fields...)
+}
+
+func (l *ZapLogger) Sync() error {
+	return l.logger.Sync()
+}
+
+func (l *ZapLogger) Core() zapcore.Core {
+	return l.logger.Core()
+}
+
+func (l *ZapLogger) Name() string {
+	return l.logger.Name()
+}
+func (l *ZapLogger) LoggerType() LoggerType {
+	return LoggerTypeZap
+}
+
+func (l *ZapLogger) Underlying() (any, LoggerType) {
+	return l.logger, l.LoggerType()
+}
+
+func (l *ZapLogger) Zap() *zap.Logger {
+	return l.logger
+}
+
+// ErrorOrWarn logs a message at Error level. It Satisfies the ErrorOrWarnLogger interface.
+func (l *ZapLogger) ErrorOrWarn(msg string, fields ...zap.Field) {
+	l.logger.Error(msg, fields...)
+}
+
+// Interface compliance checks
+var _ ILogger = (*ZapLogger)(nil)
+var _ ChainableLogger[*ZapLogger] = (*ZapLogger)(nil)
+var _ ErrorOrWarnLogger = (*ZapLogger)(nil)
+var _ ChainableErrorOrWarnLogger[*ZapLogger] = (*ZapLogger)(nil)
