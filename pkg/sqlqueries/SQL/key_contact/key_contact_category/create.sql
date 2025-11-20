b3d3c77443258d03d8ef030ae1ccbce8decfd333
Gary Zhao <gary.j.zhao@gmail.com>
2025-11-20 13:04:22 -0500

    add create, udpate, and delete sql to key contact subject

---
+++ pkg/sqlqueries/SQL/key_contact/key_contact_category/create.sql
@@ -0,0 +1,19 @@
+INSERT INTO key_contact_category (
+    id,
+    category,
+    created_by,
+    created_dts
+) VALUES (
+    :id,
+    :category,
+    :created_by,
+    :created_dts,
+    CURRENT_TIMESTAMP
+)
+RETURNING
+    id,
+    category,
+    created_by,
+    created_dts,
+    modified_by,
+    modified_dts;
