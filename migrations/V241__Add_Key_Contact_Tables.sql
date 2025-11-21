26765d19a6007230de9452648230b15d450eb36b
Elle Li <elle.li@oddball.io>
2025-11-21 12:12:16 -0700

    [Minor twix]

--- migrations/V241__Add_Key_Contact_Tables.sql
+++ migrations/V241__Add_Key_Contact_Tables.sql
@@ -25,7 +25,6 @@ CREATE TABLE IF NOT EXISTS  key_contact (
     subject_area ZERO_STRING NOT NULL,
     subject_category_id UUID NOT NULL REFERENCES key_contact_category(id) ON DELETE CASCADE,
 
-    is_team BOOLEAN NOT NULL DEFAULT FALSE,
     mailbox_title ZERO_STRING NULL,
     mailbox_address ZERO_STRING NULL,
 
@@ -54,12 +53,12 @@ CREATE TABLE IF NOT EXISTS  key_contact (
 COMMENT ON TABLE key_contact IS
 'Table for storing key contact information related to subject matter experts, including team and individual contacts.';
 
--- Ensure that only one user is set for each key_contact_category
+-- Ensure that same user is set only once for each key_contact_category
 CREATE UNIQUE INDEX IF NOT EXISTS uniq_user_id_per_category
 ON key_contact (subject_category_id, user_id)
 WHERE user_id IS NOT NULL;
 
--- Ensure that only one mailbox is set for each key_contact_category
+-- Ensure that same mailbox is set only once for each key_contact_category
 CREATE UNIQUE INDEX IF NOT EXISTS uniq_mailbox_address_per_category
 ON key_contact (subject_category_id, mailbox_address)
 WHERE mailbox_address IS NOT NULL;
