f48fc399b3f098e0adf7169707c0522abb342b2b
Gary Zhao <gary.j.zhao@gmail.com>
2025-11-24 15:26:27 -0500

    bye bye trailing commas

--- pkg/sqlqueries/SQL/key_contact/key_contact_category/create.sql
+++ pkg/sqlqueries/SQL/key_contact/key_contact_category/create.sql
@@ -1,11 +1,11 @@
 INSERT INTO key_contact_category (
     id,
     category,
-    created_by,
+    created_by
 ) VALUES (
     :id,
     :category,
-    :created_by,
+    :created_by
 )
 RETURNING
     id,
