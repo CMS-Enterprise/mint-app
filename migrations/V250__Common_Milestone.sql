f2a5ea751845ccdece736b025ddcc72408bee58e
Elle Li <elle.li@oddball.io>
2026-02-20 17:52:06 -0700

    [Add missed constraints]

--- migrations/V250__Common_Milestone.sql
+++ migrations/V250__Common_Milestone.sql
@@ -122,7 +122,24 @@ ALTER TABLE mto_milestone
 ADD CONSTRAINT unique_mto_common_milestone_per_model_plan UNIQUE (model_plan_id, mto_common_milestone_id);
 COMMENT ON CONSTRAINT unique_mto_common_milestone_per_model_plan ON mto_milestone IS 'Constraint to ensure that each common milestone can be linked to a model plan only once';
 
+ALTER TABLE mto_milestone
+DROP CONSTRAINT IF EXISTS check_name_or_common_milestone_null;
+
+ALTER TABLE mto_milestone
+ADD CONSTRAINT check_name_or_common_milestone_null CHECK (
+    (mto_common_milestone_id IS NULL OR name IS NULL)
+    AND NOT (mto_common_milestone_id IS NULL AND name IS NULL)
+);
+COMMENT ON CONSTRAINT check_name_or_common_milestone_null ON mto_milestone IS 
+'Ensures either mto_common_milestone_id or name is null, but not both: if a common milestone is referenced, name must be null; if name is specified, no common milestone may be referenced.';
+
+DROP INDEX IF EXISTS unique_name_per_model_plan_when_mto_common_milestone_is_null;
+CREATE UNIQUE INDEX unique_name_per_model_plan_when_mto_common_milestone_is_null
+ON mto_milestone (model_plan_id, name)
+WHERE mto_common_milestone_id IS NULL;
+COMMENT ON INDEX unique_name_per_model_plan_when_mto_common_milestone_is_null IS 'Unique index to enforce that milestone names are unique per model plan when no common milestone is associated';
+
 DROP INDEX IF EXISTS uniq_template_common_milestone;
-CREATE UNIQUE INDEX IF NOT EXISTS uniq_template_common_milestone
+CREATE UNIQUE INDEX uniq_template_common_milestone
 ON mto_template_milestone (template_id, mto_common_milestone_id);
 COMMENT ON INDEX uniq_template_common_milestone IS 'Constraint to ensure that each common milestone can be linked to a template only once';
