deb1635c2a5cffb8d567d681b47f72768d6db94d
Gary Zhao <gary.j.zhao@gmail.com>
2026-02-27 14:11:16 -0500

    add tasks translations

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -79,6 +79,7 @@ import prepareForClearance from './modelPlan/prepareForClearance';
 import replies from './modelPlan/replies';
 import solutions from './modelPlan/solutions';
 import modelPlanTaskList from './modelPlan/taskList';
+import tasks from './modelPlan/tasks';
 import tdls from './modelPlan/tdls';
 import { timeline, timelineMisc } from './modelPlan/timeline';
 import filterView from './readOnly/filterView';
@@ -207,7 +208,8 @@ const enUS = {
   tableAndPagination,
   termsAndConditions,
   externalLinkModal,
-  analytics
+  analytics,
+  tasks
 };
 
 export default enUS;
--- src/i18n/en-US/modelPlan/tasks.ts
+++ src/i18n/en-US/modelPlan/tasks.ts
@@ -2,20 +2,24 @@ import { PlanTaskKey, PlanTaskStatus } from 'gql/generated/graphql';
 
 const tasks = {
   heading: 'Current tasks',
-  modelPlanTasks: {
-    [PlanTaskKey.MODEL_PLAN]: {
-      [PlanTaskStatus.TO_DO]: {
-        heading: 'Start your Model Plan',
-        copy: 'The Model Plan will help components across CMS evaluate your model’s operational requirements and IT needs. It contains questions about payments, providers, general characteristics, and more. The Model Plan is flexible, so you may leave questions blank, add new information, and change information as you iterate on your model or learn of new dependencies.'
-      },
-      [PlanTaskStatus.IN_PROGRESS]: {
-        heading: 'Iterate on your Model Plan',
-        copy: 'The Model Plan will help components across CMS evaluate your model’s operational requirements and IT needs. It contains questions about payments, providers, general characteristics, and more. The Model Plan is flexible, so you may leave questions blank, add new information, and change information as you iterate on your model or learn of new dependencies.'
-      },
-      [PlanTaskStatus.COMPLETE]: {
-        heading: 'Iterate on your Model Plan',
-        copy: 'The Model Plan will help components across CMS evaluate your model’s operational requirements and IT needs. It contains questions about payments, providers, general characteristics, and more. The Model Plan is flexible, so you may leave questions blank, add new information, and change information as you iterate on your model or learn of new dependencies.'
-      }
+  [PlanTaskKey.MODEL_PLAN]: {
+    [PlanTaskStatus.TO_DO]: {
+      heading: 'Start your Model Plan',
+      copy: 'The Model Plan will help components across CMS evaluate your model’s operational requirements and IT needs. It contains questions about payments, providers, general characteristics, and more. The Model Plan is flexible, so you may leave questions blank, add new information, and change information as you iterate on your model or learn of new dependencies.',
+      primaryAction: 'Start',
+      secondaryAction: 'View sample Model Plan'
+    },
+    [PlanTaskStatus.IN_PROGRESS]: {
+      heading: 'Iterate on your Model Plan',
+      copy: 'The Model Plan will help components across CMS evaluate your model’s operational requirements and IT needs. It contains questions about payments, providers, general characteristics, and more. The Model Plan is flexible, so you may leave questions blank, add new information, and change information as you iterate on your model or learn of new dependencies.',
+      primaryAction: 'Continue',
+      secondaryAction: 'View sample Model Plan'
+    },
+    [PlanTaskStatus.COMPLETE]: {
+      heading: 'Iterate on your Model Plan',
+      copy: 'The Model Plan will help components across CMS evaluate your model’s operational requirements and IT needs. It contains questions about payments, providers, general characteristics, and more. The Model Plan is flexible, so you may leave questions blank, add new information, and change information as you iterate on your model or learn of new dependencies.',
+      primaryAction: 'Go to Model Plan',
+      secondaryAction: 'View sample Model Plan'
     }
   }
 };
