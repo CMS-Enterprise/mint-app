4921aa6a389d74c0991ca3adfb4496f3a47df634
Gary Zhao <gary.j.zhao@gmail.com>
2026-03-04 11:40:01 -0500

    updated Checkbox with functionality

--- src/features/ModelPlan/ModelToOperations/_components/MTOTableFilters/index.tsx
+++ src/features/ModelPlan/ModelToOperations/_components/MTOTableFilters/index.tsx
@@ -1,40 +1,40 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import CheckboxField from 'components/CheckboxField';
 
 const MTOTableFilters = () => {
   const { t } = useTranslation('modelToOperationsMisc');
+  const [filterNeededWithinThirtyDays, setFilterNeededWithinThirtyDays] =
+    useState(false);
+  const location = useLocation();
+  const params = useMemo(() => {
+    return new URLSearchParams(location.search);
+  }, [location.search]);
+  const navigate = useNavigate();
   return (
-    <div className="margin-y-3 display-flex ">
+    <div className="margin-y-3 display-flex" style={{ gap: '1rem' }}>
       <p className="margin-y-0 text-bold">
         {t('table.tableFilters.tableFilters')}
       </p>
       <CheckboxField
         noMargin
-        id="hide-added-solutions"
-        name="hide-added-solutions"
+        id="needed-within-thirty-days"
+        name="needed-within-thirty-days"
         label={t('table.tableFilters.neededWithinThirtyDays')}
-        value="true"
-        checked={false}
+        value="false"
+        checked={filterNeededWithinThirtyDays}
         onBlur={() => null}
-        // onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-        //   console.log(e);
-        // }}
-        // label={t('solutionLibrary.hideAdded', {
-        //   count: addedSolutions.length
-        // })}
-        // value="true"
-        // checked={hideAddedSolutions}
-        // onBlur={() => null}
-        // onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-        //   params.set(
-        //     'hide-added-solutions',
-        //     hideAddedSolutions ? 'false' : 'true'
-        //   );
-        //   params.set('page', '1');
-        //   navigate({ search: params.toString() }, { replace: true });
-        // }}
+        onChange={() => {
+          setFilterNeededWithinThirtyDays(!filterNeededWithinThirtyDays);
+          params.set(
+            'needed-within-thirty-days',
+            filterNeededWithinThirtyDays ? 'true' : 'false'
+          );
+          params.set('page', '1');
+          navigate({ search: params.toString() }, { replace: true });
+        }}
       />
     </div>
   );
