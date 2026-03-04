39fa6f9d3daa188cab69ca8e1a6cc2d8ea65bab0
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2026-03-04 10:50:46 -0600

    db migration

---
+++ migrations/V256__Add_Plan_Task.sql
@@ -0,0 +1,38 @@
+CREATE TYPE PLAN_TASK_KEY AS ENUM (
+    'MODEL_PLAN',
+    'MTO',
+    'DATA_EXCHANGE'
+);
+
+CREATE TYPE PLAN_TASK_STATUS AS ENUM (
+    'NOT_NEEDED',
+    'UPCOMING',
+    'TO_DO',
+    'IN_PROGRESS',
+    'COMPLETE'
+);
+
+CREATE TABLE plan_task (
+    id UUID PRIMARY KEY NOT NULL,
+    model_plan_id UUID NOT NULL,
+
+    key PLAN_TASK_KEY NOT NULL,
+    status PLAN_TASK_STATUS NOT NULL,
+
+    completed_by EUA_ID,
+    completed_dts TIMESTAMP WITH TIME ZONE,
+
+    created_by EUA_ID NOT NULL,
+    created_dts TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_by EUA_ID,
+    modified_dts TIMESTAMP WITH TIME ZONE
+);
+
+ALTER TABLE plan_task
+ADD CONSTRAINT fk_plan_task_plan FOREIGN KEY (model_plan_id)
+REFERENCES public.model_plan (id) MATCH SIMPLE
+ON UPDATE NO ACTION
+ON DELETE NO ACTION;
+
+ALTER TABLE plan_task
+ADD CONSTRAINT unique_plan_task_per_key_per_plan UNIQUE (model_plan_id, key);
