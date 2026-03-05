86eed3d5547e2519ed56036ad2c2be6e5dc0b3cb
Gary Zhao <gary.j.zhao@gmail.com>
2026-03-05 16:08:56 -0500

    add urls to translation files pkg/sqlqueries/SQL/translated_audit_field/collection_get_by_translated_audit_change_id.sql

--- src/i18n/en-US/modelPlan/tasks.ts
+++ src/i18n/en-US/modelPlan/tasks.ts
@@ -16,6 +16,8 @@ const tasks = {
   },
   [PlanTaskKey.MODEL_PLAN]: {
     copy: 'The Model Plan will help components across CMS evaluate your model’s operational requirements and IT needs. It contains questions about payments, providers, general characteristics, and more. The Model Plan is flexible, so you may leave questions blank, add new information, and change information as you iterate on your model or learn of new dependencies.',
+    primaryPath: '/models/{{modelID}}/collaboration-area/model-plan',
+    secondaryPath: '/help-and-knowledge/sample-model-plan',
     secondaryAction: 'View sample Model Plan',
     [PlanTaskStatus.TO_DO]: {
       heading: 'Start your Model Plan',
@@ -32,6 +34,9 @@ const tasks = {
   },
   [PlanTaskKey.DATA_EXCHANGE]: {
     copy: 'Work with your IT Lead to determine how your model will exchange data so that CMMI and BSG can help with new policy or technology opportunities. Some models may have more complex needs or could be avenues to explore broader HHS policy initiatives around data exchange, so detailed completion of the data exchange approach will also help the MINT Team understand how best to help you.',
+    primaryPath:
+      '/models/{{modelID}}/collaboration-area/data-exchange-approach/about-completing-data-exchange',
+    secondaryPath: '/help-and-knowledge/evaluating-data-exchange-approach',
     secondaryAction: 'View help article',
     [PlanTaskStatus.TO_DO]: {
       heading: 'Start your data exchange approach',
@@ -48,6 +53,8 @@ const tasks = {
   },
   [PlanTaskKey.MTO]: {
     copy: 'Work with your IT Lead to document model milestones or functions required for your model and choose the solutions and IT systems your model will utilize. Track your progress towards implementation, browse milestone and solution libraries, see points of contact for available solutions, and monitor deadlines. Keeping this information up-to-date will also help the MINT Team understand how best to help you.',
+    primaryPath: '/models/{{modelID}}/collaboration-area/model-to-operations',
+    secondaryPath: '/help-and-knowledge/creating-mto-matrix',
     secondaryAction: 'View help article',
     [PlanTaskStatus.TO_DO]: {
       heading: 'Start your model-to-operations matrix (MTO)',
