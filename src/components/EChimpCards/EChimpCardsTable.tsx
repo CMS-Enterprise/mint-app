8a36e36460296a867aa82f04e3dbd96763dd1afa
Gary Zhao <garyjzhao@users.noreply.github.com>
2026-04-01 17:16:13 -0400

    [MINT-3134] ECHIMP Post-Launch Cleanup (#2208)
    * ECHIMP post-launch cleanup first pass
    
    * remove echimpFFSURLEnabled feature flag as well
    
    * simplify code
    
    * remove comment

--- src/components/EChimpCards/EChimpCardsTable.tsx
+++ src/components/EChimpCards/EChimpCardsTable.tsx
@@ -9,7 +9,6 @@ import {
   useGetEchimpCrandTdlQuery
 } from 'gql/generated/graphql';
 import i18next from 'i18next';
-import { useFlags } from 'launchdarkly-react-client-sdk';
 
 import Alert from 'components/Alert';
 import CRAndTDLSidePanel from 'components/CRAndTDLSidePanel';
@@ -18,7 +17,7 @@ import Sidepanel from 'components/Sidepanel';
 import Spinner from 'components/Spinner';
 import GlobalClientFilter from 'components/TableFilter';
 import TablePageSize from 'components/TablePageSize';
-import { ECHIMP_URL_SSO, ECHIMP_URL_TEMP_SSO } from 'constants/echimp';
+import { ECHIMP_URL_SSO } from 'constants/echimp';
 import usePagination from 'hooks/usePagination';
 
 import EChimpCard from './EChimpCard';
@@ -104,8 +103,6 @@ const EChimpCardsTable = ({
 }) => {
   const { t: crtdlsT } = useTranslation('crtdlsMisc');
 
-  const flags = useFlags();
-
   const { modelID = '' } = useParams<{ modelID: string }>();
   const { data, loading } = useGetEchimpCrandTdlQuery({
     variables: {
@@ -169,14 +166,7 @@ const EChimpCardsTable = ({
             i18nKey="tableState.empty.copy"
             components={{
               el: (
-                <ExternalLink
-                  className="margin-right-0"
-                  href={
-                    flags.echimpIntegrationEnabled
-                      ? ECHIMP_URL_TEMP_SSO
-                      : ECHIMP_URL_SSO
-                  }
-                >
+                <ExternalLink className="margin-right-0" href={ECHIMP_URL_SSO}>
                   {' '}
                 </ExternalLink>
               )
