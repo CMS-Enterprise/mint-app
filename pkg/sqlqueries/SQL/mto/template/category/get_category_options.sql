dfed7a2a4b18080e68230925525c7c002cc34367
Sam Lawson <samlawson@Sam-Lawson-MacBook-Pro.local>
2026-04-13 09:35:23 -0700

    lint

--- pkg/sqlqueries/SQL/mto/template/category/get_category_options.sql
+++ pkg/sqlqueries/SQL/mto/template/category/get_category_options.sql
@@ -7,15 +7,17 @@ WITH parent_categories AS (
         tc.parent_id IS NULL
         AND tc.name <> ''
 )
+
 SELECT
     parent.name,
     COALESCE(
-        array_remove(array_agg(DISTINCT child.name ORDER BY child.name), NULL),
+        ARRAY_REMOVE(ARRAY_AGG(DISTINCT child.name ORDER BY child.name), NULL),
         CAST(ARRAY[] AS TEXT[])
     ) AS sub_categories
 FROM parent_categories parent
 LEFT JOIN mto_template_category child
-    ON child.parent_id = parent.id
-    AND child.name <> ''
+    ON
+        child.parent_id = parent.id
+        AND child.name <> ''
 GROUP BY parent.name
 ORDER BY parent.name;
