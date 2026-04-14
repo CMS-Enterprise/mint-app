537251ec221648a7fb8a9e3bab7a5cfc3a6738fb
Sam Lawson <samlawson@Sam-Lawson-MacBook-Pro.local>
2026-04-14 08:38:05 -0700

    self-contained test common milestone

--- pkg/graph/resolvers/mto_common_milestone_test.go
+++ pkg/graph/resolvers/mto_common_milestone_test.go
@@ -1,78 +1,114 @@
 package resolvers
 
 import (
-	"github.com/google/uuid"
+	"fmt"
 
+	"github.com/google/uuid"
+	"github.com/lib/pq"
+
+	"github.com/cms-enterprise/mint-app/pkg/models"
 	"github.com/cms-enterprise/mint-app/pkg/sqlutils"
 	"github.com/cms-enterprise/mint-app/pkg/storage"
 )
 
 func (suite *ResolverSuite) TestArchiveMTOCommonMilestone() {
-	commonMilestone := suite.getMTOCommonMilestoneByName("Manage Part C/D enrollment")
-	if suite.NotNil(commonMilestone) {
-		actorUserID := suite.testConfigs.Principal.UserAccount.ID
-		suite.Require().NoError(setTestMTOCommonMilestoneArchivedState(
+	actorUserID := suite.testConfigs.Principal.UserAccount.ID
+	commonMilestoneID := uuid.New()
+
+	suite.Require().NoError(insertTestMTOCommonMilestone(
+		suite.testConfigs.Store,
+		commonMilestoneID,
+		actorUserID,
+	))
+	suite.T().Cleanup(func() {
+		suite.NoError(deleteTestMTOCommonMilestone(
 			suite.testConfigs.Store,
-			commonMilestone.ID,
-			actorUserID,
-			false,
+			commonMilestoneID,
 		))
-		commonMilestone.IsArchived = false
-		suite.T().Cleanup(func() {
-			suite.NoError(setTestMTOCommonMilestoneArchivedState(
-				suite.testConfigs.Store,
-				commonMilestone.ID,
-				actorUserID,
-				false,
-			))
-		})
+	})
 
-		suite.False(commonMilestone.IsArchived)
+	archivedMilestone, err := ArchiveMTOCommonMilestone(
+		suite.testConfigs.Logger,
+		suite.testConfigs.Store,
+		commonMilestoneID,
+		actorUserID,
+	)
+	suite.NoError(err)
+	if suite.NotNil(archivedMilestone) {
+		suite.Equal(commonMilestoneID, archivedMilestone.ID)
+		suite.True(archivedMilestone.IsArchived)
+	}
 
-		archivedMilestone, err := ArchiveMTOCommonMilestone(
-			suite.testConfigs.Logger,
-			suite.testConfigs.Store,
-			commonMilestone.ID,
-			actorUserID,
-		)
-		suite.NoError(err)
-		if suite.NotNil(archivedMilestone) {
-			suite.Equal(commonMilestone.ID, archivedMilestone.ID)
-			suite.True(archivedMilestone.IsArchived)
-		}
-
-		reloaded, err := storage.MTOCommonMilestoneGetByIDLoader(
-			suite.testConfigs.Store,
-			suite.testConfigs.Logger,
-			[]uuid.UUID{commonMilestone.ID},
-		)
-		suite.NoError(err)
-		if suite.Len(reloaded, 1) {
-			suite.True(reloaded[0].IsArchived)
-		}
+	reloaded, err := storage.MTOCommonMilestoneGetByIDLoader(
+		suite.testConfigs.Store,
+		suite.testConfigs.Logger,
+		[]uuid.UUID{commonMilestoneID},
+	)
+	suite.NoError(err)
+	if suite.Len(reloaded, 1) {
+		suite.True(reloaded[0].IsArchived)
 	}
 }
 
-func setTestMTOCommonMilestoneArchivedState(
+func insertTestMTOCommonMilestone(
 	np sqlutils.NamedPreparer,
 	id uuid.UUID,
 	actorUserID uuid.UUID,
-	isArchived bool,
 ) error {
 	return sqlutils.ExecProcedure(
 		np,
 		`
-			UPDATE mto_common_milestone
-			SET
-				is_archived = :is_archived,
-				modified_by = :modified_by,
-				modified_dts = CURRENT_TIMESTAMP
-			WHERE id = :id
+			INSERT INTO mto_common_milestone (
+				id,
+				name,
+				description,
+				category_name,
+				sub_category_name,
+				facilitated_by_role,
+				section,
+				trigger_table,
+				trigger_col,
+				trigger_vals,
+				created_by
+			)
+			VALUES (
+				:id,
+				:name,
+				:description,
+				:category_name,
+				:sub_category_name,
+				:facilitated_by_role,
+				:section,
+				:trigger_table,
+				:trigger_col,
+				:trigger_vals,
+				:created_by
+			)
 		`,
 		map[string]any{
-			"id":          id,
-			"is_archived": isArchived,
-			"modified_by": actorUserID,
+			"id":                id,
+			"name":              fmt.Sprintf("Archive resolver test milestone %s", id.String()),
+			"description":       "Temporary common milestone used for resolver archive testing.",
+			"category_name":     "Operations",
+			"sub_category_name": "Archive tests",
+			"facilitated_by_role": models.EnumArray[models.MTOFacilitator]{
+				models.MTOFacilitatorITLead,
+			},
+			"section":       models.TLSBasics,
+			"trigger_table": "plan_basics",
+			"trigger_col":   pq.Array([]string{"status"}),
+			"trigger_vals":  pq.Array([]string{"PLAN_DRAFT"}),
+			"created_by":    actorUserID,
+		},
+	)
+}
+
+func deleteTestMTOCommonMilestone(np sqlutils.NamedPreparer, id uuid.UUID) error {
+	return sqlutils.ExecProcedure(
+		np,
+		`DELETE FROM mto_common_milestone WHERE id = :id`,
+		map[string]any{
+			"id": id,
 		},
 	)
 }
