3dfafe581ff5643963a3addafdcf4282420b62fe
Sam Lawson <samlawson@Sam-Lawson-MacBook-Pro.local>
2026-04-20 10:52:32 -0700

    init pass for mto common milestone emails

---
+++ pkg/email/mto_common_milestone_created.go
@@ -0,0 +1,24 @@
+package email
+
+import (
+	_ "embed"
+)
+
+const MTOCommonMilestoneCreatedTemplateName = "mto_common_milestone_created"
+
+//go:embed templates/mto_common_milestone_created_subject.html
+var MTOCommonMilestoneCreatedSubjectTemplate string
+
+//go:embed templates/mto_common_milestone_created_body.html
+var MTOCommonMilestoneCreatedBodyTemplate string
+
+type MTOCommonMilestoneCreatedSubjectContent struct{}
+
+type MTOCommonMilestoneCreatedBodyContent struct {
+	UserName       string
+	MilestoneTitle string
+	CategoryAndSub string
+	Roles          string // comma-separated
+	Solutions      string // comma-separated
+	Link           string
+}
---
+++ pkg/email/mto_common_milestone_removed.go
@@ -0,0 +1,23 @@
+package email
+
+import (
+	_ "embed"
+)
+
+const MTOCommonMilestoneRemovedTemplateName = "mto_common_milestone_removed"
+
+//go:embed templates/mto_common_milestone_removed_subject.html
+var MTOCommonMilestoneRemovedSubjectTemplate string
+
+//go:embed templates/mto_common_milestone_removed_body.html
+var MTOCommonMilestoneRemovedBodyTemplate string
+
+type MTOCommonMilestoneRemovedSubjectContent struct{}
+
+type MTOCommonMilestoneRemovedBodyContent struct {
+	UserName       string
+	MilestoneTitle string
+	CategoryAndSub string
+	Roles          string // comma-separated
+	Solutions      string // comma-separated
+}
