1aa316e65f0d84aa9c36facbc9ffa32e2f7f8dd0
Peyton232 <peyton.thibodeaux@oddball.io>
2026-05-13 09:48:23 -0500

    added models for waiver assessment survey

---
+++ pkg/models/common_waiver.go
@@ -0,0 +1,27 @@
+package models
+
+import "github.com/google/uuid"
+
+// CommonWaiver represents a waiver type in the CMMI waiver library
+type CommonWaiver struct {
+	baseStruct
+
+	Name                               string  `json:"name" db:"name"`
+	Description                        *string `json:"description" db:"description"`
+	ParticipationAgreementLanguageLink *string `json:"participationAgreementLanguageLink" db:"participation_agreement_language_link"`
+	CmmiWaiverPointOfContact           *string `json:"cmmiWaiverPointOfContact" db:"cmmi_waiver_point_of_contact"`
+	WaiverType                         *string `json:"waiverType" db:"waiver_type"`
+	WaiverFocus                        *string `json:"waiverFocus" db:"waiver_focus"`
+	WhatIsWaived                       *string `json:"whatIsWaived" db:"what_is_waived"`
+	HasStandardizationEffort           *bool   `json:"hasStandardizationEffort" db:"has_standardization_effort"`
+	HasClaimsDataOrRREGAnalysis        *string `json:"hasClaimsDataOrRREGAnalysis" db:"has_claims_data_or_rreg_analysis"`
+	IsUsedInActiveModels               *bool   `json:"isUsedInActiveModels" db:"is_used_in_active_models"`
+}
+
+// NewCommonWaiver returns a new CommonWaiver object
+func NewCommonWaiver(createdBy uuid.UUID, name string) *CommonWaiver {
+	return &CommonWaiver{
+		baseStruct: NewBaseStruct(createdBy),
+		Name:       name,
+	}
+}
