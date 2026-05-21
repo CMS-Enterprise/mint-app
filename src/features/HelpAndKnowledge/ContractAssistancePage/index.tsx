eb517290243694d5929dd74f833a4922b07f1bba
Gary Zhao <gary.j.zhao@gmail.com>
2026-05-21 14:24:17 -0400

    add breadcrumbs and abc sorted the breadcrumb

--- src/features/HelpAndKnowledge/ContractAssistancePage/index.tsx
+++ src/features/HelpAndKnowledge/ContractAssistancePage/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { GridContainer } from '@trussworks/react-uswds';
 
+import Breadcrumbs, { BreadcrumbItemOptions } from 'components/Breadcrumbs';
 import MainContent from 'components/MainContent';
 
 const ContractAssistancePage = () => {
   return (
     <MainContent>
       <GridContainer>
+        <Breadcrumbs
+          items={[BreadcrumbItemOptions.CONTRACT_ASSISTANCE]}
+          // customItem={hkcT('milestoneLibrary.hkcHeading')}
+        />
         <h1>Contract Assistance</h1>
       </GridContainer>
     </MainContent>
