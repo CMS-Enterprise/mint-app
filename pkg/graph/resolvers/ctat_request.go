8b6ed40e70b2e14bd961c62bec84ef2bc1f437cc
Sam Lawson <samlawson@Sam-Lawson-MacBook-Pro.local>
2026-05-26 12:23:50 -0700

    admin resolver

--- pkg/graph/resolvers/ctat_request.go
+++ pkg/graph/resolvers/ctat_request.go
@@ -6,10 +6,16 @@ import (
 	"github.com/google/uuid"
 
 	"github.com/cms-enterprise/mint-app/pkg/models"
+	"github.com/cms-enterprise/mint-app/pkg/storage"
 	"github.com/cms-enterprise/mint-app/pkg/storage/loaders"
 )
 
 // CTATRequestLiteGetByRequesterIDLOADER implements resolver logic to get lite CTAT requests by requester ID using a data loader.
 func CTATRequestLiteGetByRequesterIDLOADER(ctx context.Context, requesterID uuid.UUID) ([]*models.CTATRequestLite, error) {
 	return loaders.CTATRequest.ByRequesterID.Load(ctx, requesterID)
+}
+
+// CTATRequestLiteCollectionGetForAdmin implements resolver logic to get lite CTAT requests for the admin table view.
+func CTATRequestLiteCollectionGetForAdmin(ctx context.Context, store *storage.Store) ([]*models.CTATRequestLite, error) {
+	return storage.CTATRequestLiteCollectionGetForAdmin(store)
 }
