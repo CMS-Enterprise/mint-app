144a28407b17117e8fdafb57c87ef244f730e2cf
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2026-05-27 13:57:05 -0500

    yes/no field buttons

--- src/features/ModelPlan/AdditionalQuestionnaires/WaiverAssessmentSurvey/_components/WaiverInfoPanel/index.tsx
+++ src/features/ModelPlan/AdditionalQuestionnaires/WaiverAssessmentSurvey/_components/WaiverInfoPanel/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { GridContainer } from '@trussworks/react-uswds';
+import { Controller, useForm } from 'react-hook-form';
+import {
+  Fieldset,
+  Form,
+  FormGroup,
+  GridContainer,
+  Icon,
+  Label
+} from '@trussworks/react-uswds';
 
 import {
   DescriptionDefinition,
@@ -8,7 +16,10 @@ import {
 } from 'components/DescriptionGroup';
 import Divider from 'components/Divider';
 import ExternalLink from 'components/ExternalLink';
+import HelpText from 'components/HelpText';
 import Sidepanel from 'components/Sidepanel';
+
+import './index.scss';
 
 /**
  * TO DO:
@@ -31,12 +42,15 @@ type CommonWaiver = {
   isUsedInActiveModels: boolean;
 };
 
+type WaiverInfoFields = {
+  willUseWaiver: boolean;
+  notUsingReason: string;
+};
+
 type WaiverInfoPanelProps = {
   isOpen: boolean;
   closeModal: () => void;
-  waiverInfo: {
-    willUseWaiver: boolean;
-    notUsingReason: string;
+  waiverInfo: WaiverInfoFields & {
     commonWaiver: CommonWaiver;
   };
 };
@@ -46,7 +60,7 @@ const WaiverInfoPanel = ({
   closeModal,
   waiverInfo
 }: WaiverInfoPanelProps) => {
-  const { commonWaiver } = waiverInfo;
+  const { willUseWaiver, notUsingReason, commonWaiver } = waiverInfo;
 
   const {
     cmmiWaiverPointOfContact,
@@ -60,6 +74,13 @@ const WaiverInfoPanel = ({
     hasClaimsDataOrRREGAnalysis,
     isUsedInActiveModels
   } = commonWaiver;
+
+  const { handleSubmit, control } = useForm<WaiverInfoFields>({
+    defaultValues: {
+      willUseWaiver,
+      notUsingReason
+    }
+  });
 
   return (
     <Sidepanel
@@ -126,6 +147,67 @@ const WaiverInfoPanel = ({
               definition={isUsedInActiveModels ? 'Yes' : 'No'}
             />
           </DescriptionList>
+
+          <Divider className="margin-top-3 margin-bottom-4" />
+
+          <Form className="maxw-none" onSubmit={handleSubmit(() => {})}>
+            <FormGroup>
+              <Label
+                id="willUseWaiverLabel"
+                htmlFor="waiver-info-panel-will-use-waiver-yes"
+              >
+                Do you plan to use this waiver with your model?
+              </Label>
+              <HelpText id="willUseWaiverHelpText">
+                Your answer to this question may reveal additional questions to
+                be answered.
+              </HelpText>
+              <Controller
+                name="willUseWaiver"
+                control={control}
+                render={({ field }) => (
+                  <Fieldset
+                    className="mint-yes-no-button-group margin-top-2"
+                    aria-labelledby="willUseWaiverLabel"
+                    aria-describedby="willUseWaiverHelpText"
+                  >
+                    <div className="mint-yes-no-button mint-yes-no-button--yes">
+                      <input
+                        type="radio"
+                        id="waiver-info-panel-will-use-waiver-yes"
+                        data-testid="waiver-info-panel-will-use-waiver-yes"
+                        {...field}
+                        value="true"
+                      />
+                      <label
+                        className="usa-button"
+                        htmlFor="waiver-info-panel-will-use-waiver-yes"
+                      >
+                        <Icon.Check aria-hidden />
+                        Yes
+                      </label>
+                    </div>
+                    <div className="mint-yes-no-button mint-yes-no-button--no">
+                      <input
+                        type="radio"
+                        id="waiver-info-panel-will-use-waiver-no"
+                        data-testid="waiver-info-panel-will-use-waiver-no"
+                        {...field}
+                        value="false"
+                      />
+                      <label
+                        className="usa-button"
+                        htmlFor="waiver-info-panel-will-use-waiver-no"
+                      >
+                        <Icon.Close aria-hidden />
+                        No
+                      </label>
+                    </div>
+                  </Fieldset>
+                )}
+              />
+            </FormGroup>
+          </Form>
         </div>
       </GridContainer>
     </Sidepanel>
