cb450c45c8d1c4aba2c641c0d1cd27b83062958e
Gary Zhao <gary.j.zhao@gmail.com>
2026-06-25 14:08:56 -0400

    add hdie/show toggle for admin action

--- src/features/Home/components/AdminActions/index.test.tsx
+++ src/features/Home/components/AdminActions/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createMemoryRouter, RouterProvider } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import AdminActions from './index';
 
@@ -45,6 +45,36 @@ describe('AdminActions Component', () => {
       'href',
       '/help-and-knowledge/contract-assistance'
     );
+  });
+
+  it('toggles admin action content visibility', () => {
+    const router = createMemoryRouter(
+      [
+        {
+          path: '/',
+          element: <AdminActions />
+        }
+      ],
+      {
+        initialEntries: ['/']
+      }
+    );
+
+    const { getByTestId, getByText, queryByText } = render(
+      <RouterProvider router={router} />
+    );
+
+    expect(getByText('Manage common milestones')).toBeInTheDocument();
+
+    fireEvent.click(getByTestId('admin-actions-toggle'));
+
+    expect(queryByText('Manage common milestones')).not.toBeInTheDocument();
+    expect(getByText('Show admin actions')).toBeInTheDocument();
+
+    fireEvent.click(getByTestId('admin-actions-toggle'));
+
+    expect(getByText('Manage common milestones')).toBeInTheDocument();
+    expect(getByText('Hide admin actions')).toBeInTheDocument();
   });
 
   it('matches snapshot', () => {
