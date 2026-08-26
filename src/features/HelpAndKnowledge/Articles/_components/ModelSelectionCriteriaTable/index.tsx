0b69a028748a59e75e2e7f9748881faa911f509c
Elle Li <elle.li@oddball.io>
2026-08-26 11:49:13 -0600

    [Temperately move old text to six pager, until six pager is updated next]

--- src/features/HelpAndKnowledge/Articles/_components/ModelSelectionCriteriaTable/index.tsx
+++ src/features/HelpAndKnowledge/Articles/_components/ModelSelectionCriteriaTable/index.tsx
@@ -12,10 +12,12 @@ type ContentTypes = {
 };
 
 const ModelSectionCriteriaTable = () => {
-  const headers = tArray('twoPageMeeting:examplePapers.criteria.table.header');
+  const headers = tArray(
+    'sixPageMeeting:howSixPagerReviewed.modelSelectionCriteria.table.header'
+  );
 
   const content = tObject(
-    'twoPageMeeting:examplePapers.criteria.table.content'
+    'sixPageMeeting:howSixPagerReviewed.modelSelectionCriteria.table.content'
   ) as ContentTypes[];
 
   return (
