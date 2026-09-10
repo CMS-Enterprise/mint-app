eab3b6fd16b8bd09ff4caf73f29c2b76b1c67778
Gary Zhao <gary.j.zhao@gmail.com>
2026-09-10 10:37:59 -0400

    MINT-3843: Allow OA_PRESENTATION to be manually marked complete
    Add OA_PRESENTATION to manuallyMarkablePlanTaskKeys and give it a
    display name for task notification emails. Includes resolver coverage
    for mark-complete and revert-to-TO_DO.

--- pkg/models/plan_task.go
+++ pkg/models/plan_task.go
@@ -36,8 +36,9 @@ const (
 // markable), the seeding list in ModelPlanCreate (pkg/graph/resolvers/model_plan.go), and either a
 // new calculated-status function in plan_task_status_updates.go or reuse of PlanTaskMarkComplete.
 var manuallyMarkablePlanTaskKeys = map[PlanTaskKey]bool{
-	PlanTaskKeyTwoPager: true,
-	PlanTaskKeySixPager: true,
+	PlanTaskKeyTwoPager:       true,
+	PlanTaskKeySixPager:       true,
+	PlanTaskKeyOaPresentation: true,
 }
 
 // IsManuallyMarkable reports whether a PlanTaskKey's status is set directly by a user
@@ -68,8 +69,9 @@ var planTaskKeyDisplayNames = map[PlanTaskKey]string{
 	PlanTaskKeyModelPlan:    "Model Plan",
 	PlanTaskKeyDataExchange: "Data exchange approach",
 	PlanTaskKeyMto:          "Model-to-operations matrix (MTO)",
-	PlanTaskKeyTwoPager:     "2-pager review",
-	PlanTaskKeySixPager:     "6-pager review",
+	PlanTaskKeyTwoPager:       "2-pager review",
+	PlanTaskKeySixPager:       "6-pager review",
+	PlanTaskKeyOaPresentation: "Office of the Administrator (OA) presentation",
 }
 
 // DisplayName returns a short human-readable name for this task key.
